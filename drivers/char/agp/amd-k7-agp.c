b108855a9fecc9593a1eda080e2b9fa656a244bf
Dave Jones <davej@delerium.kernelslacker.org>
2005-02-22 11:23:42 -0500

    [AGPGART] Remove pointless tests for bridge vendor from amd-k7-agp driver.
    If we got far enough to run this code, we *must* have an AMD bridge.
    
    Signed-off-by: Dave Jones <davej@redhat.com>

--- drivers/char/agp/amd-k7-agp.c
+++ drivers/char/agp/amd-k7-agp.c
@@ -425,8 +425,7 @@ static int __devinit agp_amdk7_probe(struct pci_dev *pdev,
 	   erratum 20: strobe glitch with Nvidia NV10 GeForce cards.
 	   system controller may experience noise due to strong drive strengths
 	 */
-	if (agp_bridge->dev->vendor == PCI_VENDOR_ID_AMD &&
-		agp_bridge->dev->device == PCI_DEVICE_ID_AMD_FE_GATE_7006) {
+	if (agp_bridge->dev->device == PCI_DEVICE_ID_AMD_FE_GATE_7006) {
 		u8 cap_ptr=0;
 		struct pci_dev *gfxcard=NULL;
 		while (!cap_ptr) {
@@ -458,8 +457,7 @@ static int __devinit agp_amdk7_probe(struct pci_dev *pdev,
 	 * erratum 45: Timing problem prevents fast writes -- Disable fast write.
 	 * erratum 46: Setup violation on AGP SBA pins - Disable side band addressing.
 	 * With this lot disabled, we should prevent lockups. */
-	if (agp_bridge->dev->vendor == PCI_VENDOR_ID_AMD &&
-		agp_bridge->dev->device == PCI_DEVICE_ID_AMD_FE_GATE_700E) {
+	if (agp_bridge->dev->device == PCI_DEVICE_ID_AMD_FE_GATE_700E) {
 		u8 revision=0;
 		pci_read_config_byte(pdev, PCI_REVISION_ID, &revision);
 		if (revision == 0x10 || revision == 0x11) {
