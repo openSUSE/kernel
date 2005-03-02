038963f5be7f2a90f9b7a2b3aa2e139d0444813c
Jean Delvare <khali@linux-fr.org>
2005-03-01 19:52:31 -0800

    [PATCH] I2C: Kill i2c_client.id (3/5)
    (3/5) Stop using i2c_client.id in misc drivers.
    
    Affected drivers:
    * acorn/char/pcf8583
    * acorn/char/i2c
    * i2c/i2c-dev
    * macintosh/therm_windtunnel
    * sound/oss/dmasound/dac3550a
    * sound/ppc/keywest
    
    The Acorn pcf8583 driver would give the i2c_client id the same value as
    the i2c_driver id, and later test that client id (in i2c). I changed it
    to test the client's driver id instead. The result is the same and the
    client id is then useless and can be removed.
    
    All other drivers here would allocate the client id to some value and
    then never use it. They are unaffected by the change.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/i2c/i2c-dev.c
+++ drivers/i2c/i2c-dev.c
@@ -507,7 +507,6 @@ static struct i2c_driver i2cdev_driver = {
 
 static struct i2c_client i2cdev_client_template = {
 	.name		= "I2C /dev entry",
-	.id		= 1,
 	.addr		= -1,
 	.driver		= &i2cdev_driver,
 };
