50c65d55a6118e64289e12e522aac5f616c2b931
Greg Kroah-Hartman <greg@kroah.com>
2005-03-07 06:19:06 -0800

    [PATCH] USB: remove UB checks in the usb-storage driver.
    This allows either the ub or usb-storage driver to bind to the same device,
    allowing people to use both without rebuilding their kernels.  It can be
    a bit messy at times...
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>
    Acked-by: Matthew Dharm <mdharm-kernel@one-eyed-alien.net>
    Acked-by: Pete Zaitcev <zaitcev@redhat.com>

--- drivers/usb/storage/usb.c
+++ drivers/usb/storage/usb.c
@@ -144,9 +144,7 @@ static struct usb_device_id storage_usb_ids [] = {
 	{ USB_INTERFACE_INFO(USB_CLASS_MASS_STORAGE, US_SC_QIC, US_PR_BULK) },
 	{ USB_INTERFACE_INFO(USB_CLASS_MASS_STORAGE, US_SC_UFI, US_PR_BULK) },
 	{ USB_INTERFACE_INFO(USB_CLASS_MASS_STORAGE, US_SC_8070, US_PR_BULK) },
-#if !defined(CONFIG_BLK_DEV_UB) && !defined(CONFIG_BLK_DEV_UB_MODULE)
 	{ USB_INTERFACE_INFO(USB_CLASS_MASS_STORAGE, US_SC_SCSI, US_PR_BULK) },
-#endif
 
 	/* Terminating entry */
 	{ }
@@ -220,10 +218,8 @@ static struct us_unusual_dev us_unusual_dev_list[] = {
 	  .useTransport = US_PR_BULK},
 	{ .useProtocol = US_SC_8070,
 	  .useTransport = US_PR_BULK},
-#if !defined(CONFIG_BLK_DEV_UB) && !defined(CONFIG_BLK_DEV_UB_MODULE)
 	{ .useProtocol = US_SC_SCSI,
 	  .useTransport = US_PR_BULK},
-#endif
 
 	/* Terminating entry */
 	{ NULL }
