1ba955f6763e2756d00ca12297a0fcfa0c75ddf1
Patrick McHardy <kaber@trash.net>
2005-03-09 04:49:05 -0800

    [XFRM]: Fix ICMP tempsel
    The selector ports are initialized to fl_ip_sport/fl_ip_dport instead
    of xfrm_flowi_sport(fl)/xfrm_flowi_dport(fl). This is wrong for ICMP,
    type and code should be stored in sport and dport, in struct flowi both
    are contained in fl_ip_sport.
    
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/xfrm6_state.c
+++ net/ipv6/xfrm6_state.c
@@ -27,9 +27,9 @@ __xfrm6_init_tempsel(struct xfrm_state *x, struct flowi *fl,
 	 * to current session. */
 	ipv6_addr_copy((struct in6_addr *)&x->sel.daddr, &fl->fl6_dst);
 	ipv6_addr_copy((struct in6_addr *)&x->sel.saddr, &fl->fl6_src);
-	x->sel.dport = fl->fl_ip_dport;
+	x->sel.dport = xfrm_flowi_dport(fl);
 	x->sel.dport_mask = ~0;
-	x->sel.sport = fl->fl_ip_sport;
+	x->sel.sport = xfrm_flowi_sport(fl);
 	x->sel.sport_mask = ~0;
 	x->sel.prefixlen_d = 128;
 	x->sel.prefixlen_s = 128;
