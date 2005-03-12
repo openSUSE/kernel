8eaa6900ea5afc2b9b9ebb0c320baba527fc6ff0
Matthew Wilcox <willy@parisc-linux.org>
2005-03-11 17:17:01 -0800

    [PATCH] Update Thibaut Varene's email address
    Update my copyright information in a few files (email change)
    
    Signed-off-by: Thibaut VARENE <varenet@parisc-linux.org>
    Signed-off-by: Matthew Wilcox <willy@parisc-linux.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/parisc/kernel/pdc_chassis.c
+++ arch/parisc/kernel/pdc_chassis.c
@@ -2,7 +2,7 @@
  *    interfaces to log Chassis Codes via PDC (firmware)
  *
  *    Copyright (C) 2002 Laurent Canet <canetl@esiee.fr>
- *    Copyright (C) 2002-2004 Thibaut VARENE <varenet@esiee.fr>
+ *    Copyright (C) 2002-2004 Thibaut VARENE <varenet@parisc-linux.org>
  *
  *    This program is free software; you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
