35316f6a0207c2a0491d07494d81b1094a26a889
Ingo Molnar <mingo@elte.hu>
2009-07-03 08:29:36 -0500

    softirq: provide rt api variants
    add new, -rt specific IRQ API variants. Maps to the same as before
    on non-PREEMPT_RT.
    
     include/linux/bottom_half.h |    8 ++++++++
     1 file changed, 8 insertions(+)
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/bottom_half.h
+++ include/linux/bottom_half.h
@@ -1,9 +1,17 @@
 #ifndef _LINUX_BH_H
 #define _LINUX_BH_H
 
+#ifdef CONFIG_PREEMPT_RT
+# define local_bh_disable()		do { } while (0)
+# define __local_bh_disable(ip)		do { } while (0)
+# define _local_bh_enable()		do { } while (0)
+# define local_bh_enable()		do { } while (0)
+# define local_bh_enable_ip(ip)		do { } while (0)
+#else
 extern void local_bh_disable(void);
 extern void _local_bh_enable(void);
 extern void local_bh_enable(void);
 extern void local_bh_enable_ip(unsigned long ip);
+#endif
 
 #endif /* _LINUX_BH_H */
