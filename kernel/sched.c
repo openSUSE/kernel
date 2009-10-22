cddfcb89afe2840f8a2f002a2bb00f42e1526eaf
Dinakar Guniguntala <dino@in.ibm.com>
2009-10-22 18:07:58 +0530

    sched: Add a missing =
    Signed-off-by: Dinakar Guniguntala <dino@in.ibm.com>
    Cc: John Stultz <johnstul@us.ibm.com>
    Cc: Darren Hart <dvhltc@us.ibm.com>
    Cc: John Kacur <jkacur@redhat.com>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/sched.c
+++ kernel/sched.c
@@ -3844,7 +3844,7 @@ static void update_cpu_power(struct sched_domain *sd, int cpu)
 	struct sched_group *sdg = sd->groups;
 
 	power *= arch_scale_freq_power(sd, cpu);
-	power >> SCHED_LOAD_SHIFT;
+	power >>= SCHED_LOAD_SHIFT;
 
 	if ((sd->flags & SD_SHARE_CPUPOWER) && weight > 1) {
 		power *= arch_scale_smt_power(sd, cpu);
