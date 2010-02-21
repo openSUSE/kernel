f892b149b98479d18f78275a7cd36aaa8fb91c72
Wu Zhangjin <wuzhangjin@gmail.com>
2010-02-22 00:47:45 +0800

    RT: MIPS: Make the die_lock be raw
    Kernel is going to die, the lock must be raw to ensure nobody can
    preempt it at this urgent time.
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- arch/mips/kernel/traps.c
+++ arch/mips/kernel/traps.c
@@ -349,7 +349,7 @@ void show_registers(const struct pt_regs *regs)
 	printk("\n");
 }
 
-static DEFINE_SPINLOCK(die_lock);
+static DEFINE_RAW_SPINLOCK(die_lock);
 
 void __noreturn die(const char * str, const struct pt_regs * regs)
 {
@@ -359,7 +359,7 @@ void __noreturn die(const char * str, const struct pt_regs * regs)
 #endif /* CONFIG_MIPS_MT_SMTC */
 
 	console_verbose();
-	spin_lock_irq(&die_lock);
+	raw_spin_lock_irq(&die_lock);
 	bust_spinlocks(1);
 #ifdef CONFIG_MIPS_MT_SMTC
 	mips_mt_regdump(dvpret);
@@ -367,7 +367,7 @@ void __noreturn die(const char * str, const struct pt_regs * regs)
 	printk("%s[#%d]:\n", str, ++die_counter);
 	show_registers(regs);
 	add_taint(TAINT_DIE);
-	spin_unlock_irq(&die_lock);
+	raw_spin_unlock_irq(&die_lock);
 
 	if (in_interrupt())
 		panic("Fatal exception in interrupt");
