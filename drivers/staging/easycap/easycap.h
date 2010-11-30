20bb0f1a94209a0f4715c9b800ff409f9ca9f369
Michal Marek <mmarek@suse.cz>
2010-11-30 10:48:27 +0100

    - staging: easycap: fix build failure.
    suse-commit: 58057d2b22292790676ed214c344185b57420ffd

--- drivers/staging/easycap/easycap.h
+++ drivers/staging/easycap/easycap.h
@@ -88,6 +88,7 @@
 #include <linux/fs.h>
 #include <linux/delay.h>
 #include <linux/types.h>
+#include <linux/smp_lock.h>
 
 /*vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv*/
 #if defined(EASYCAP_IS_VIDEODEV_CLIENT)
