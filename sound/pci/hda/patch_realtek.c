08c6bef77b5a183c03fa95ff266f29b95aba44a7
Takashi Iwai <tiwai@suse.de>
2011-03-14 12:55:56 +0100

    - ALSA: hda - Add a generic fixup callback for Realtek codecs   (bnc#679016).
    suse-commit: 35da09942aa19e6dd32702400ade63fbc9878a29

--- sound/pci/hda/patch_realtek.c
+++ sound/pci/hda/patch_realtek.c
@@ -1682,6 +1682,8 @@ struct alc_fixup {
 	unsigned int sku;
 	const struct alc_pincfg *pins;
 	const struct hda_verb *verbs;
+	void (*func)(struct hda_codec *codec, const struct alc_fixup *fix,
+		     int pre_init);
 };
 
 static void alc_pick_fixup(struct hda_codec *codec,
@@ -1720,6 +1722,13 @@ static void alc_pick_fixup(struct hda_codec *codec,
 			    codec->chip_name, quirk->name);
 #endif
 		add_verb(codec->spec, fix->verbs);
+	}
+	if (fix->func) {
+#ifdef CONFIG_SND_DEBUG_VERBOSE
+		snd_printdd(KERN_INFO "hda_codec: %s: Apply fix-func for %s\n",
+			    codec->chip_name, quirk->name);
+#endif
+		fix->func(codec, fix, pre_init);
 	}
 }
 
