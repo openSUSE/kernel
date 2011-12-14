80722cd426e65401921780b8874329b347492abf
David Sterba <dsterba@suse.cz>
2011-12-14 17:28:21 +0100

    Btrfs: fix defragmentation regression (FATE#306586).
    suse-commit: d07ca4f70ba66429ac106f72606c2eebe0bae306

--- fs/btrfs/ioctl.c
+++ fs/btrfs/ioctl.c
@@ -1096,7 +1096,6 @@ int btrfs_defrag_file(struct inode *inode, struct file *file,
 
 		defrag_count += ret;
 		balance_dirty_pages_ratelimited_nr(inode->i_mapping, ret);
-		i += ret;
 
 		if (newer_than) {
 			if (newer_off == (u64)-1)
@@ -1116,7 +1115,10 @@ int btrfs_defrag_file(struct inode *inode, struct file *file,
 				break;
 			}
 		} else {
-			i++;
+			if (ret > 0)
+				i += ret;
+			else
+				i++;
 		}
 	}
 
