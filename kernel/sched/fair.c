6610ac243c2afd6097c69aebca253b034711a0fa
Mike Galbraith <mgalbraith@suse.de>
2017-09-01 04:24:04 +0200

    - Per IBM request, drop drop IBM specific patches from SLE15 (bnc#1012582) - Delete patches.suse/sched-disable-FAIR_SLEEPERS-for-S390.patch. - Delete patches.suse/sched-reinstate-FAIR_SLEEPERS-feature.patch. - Delete patches.suse/sched-reinstate-sched_compat_yield.patch.
    suse-commit: 3ee70fef830faa913fcfecdeb6c61941073f896d

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -86,14 +86,6 @@ static unsigned int sched_nr_latency = 3;
 unsigned int sysctl_sched_child_runs_first __read_mostly;
 
 /*
- * sys_sched_yield() compat mode
- *
- * This option switches the agressive yield implementation of the
- * old scheduler back on.
- */
-unsigned int sysctl_sched_compat_yield __read_mostly;
-
-/*
  * SCHED_OTHER wake-up granularity.
  *
  * This option delays the preemption effects of decoupled workloads
@@ -620,6 +612,7 @@ static struct sched_entity *__pick_next_entity(struct sched_entity *se)
 	return rb_entry(next, struct sched_entity, run_node);
 }
 
+#ifdef CONFIG_SCHED_DEBUG
 struct sched_entity *__pick_last_entity(struct cfs_rq *cfs_rq)
 {
 	struct rb_node *last = rb_last(&cfs_rq->tasks_timeline);
@@ -630,7 +623,6 @@ struct sched_entity *__pick_last_entity(struct cfs_rq *cfs_rq)
 	return rb_entry(last, struct sched_entity, run_node);
 }
 
-#ifdef CONFIG_SCHED_DEBUG
 /**************************************************************
  * Scheduling class statistics methods:
  */
@@ -3672,7 +3664,7 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 		vruntime += sched_vslice(cfs_rq, se);
 
 	/* sleeps up to a single latency don't count. */
-	if (!initial && sched_feat(FAIR_SLEEPERS)) {
+	if (!initial) {
 		unsigned long thresh = sysctl_sched_latency;
 
 		/*
@@ -6378,7 +6370,7 @@ static void yield_task_fair(struct rq *rq)
 {
 	struct task_struct *curr = rq->curr;
 	struct cfs_rq *cfs_rq = task_cfs_rq(curr);
-	struct sched_entity *se = &curr->se, *rightmost;
+	struct sched_entity *se = &curr->se;
 
 	/*
 	 * Are we the only task in the tree?
@@ -6403,27 +6395,6 @@ static void yield_task_fair(struct rq *rq)
 	}
 
 	set_skip_buddy(se);
-
-	if (likely(!sysctl_sched_compat_yield))
-		return;
-
-	/*
-	 * Find the rightmost entry in the rbtree:
-	 */
-	rightmost = __pick_last_entity(cfs_rq);
-
-	/*
-	 * Already in the rightmost position?
-	 */
-	if (unlikely(!rightmost || entity_before(rightmost, se)))
-		return;
-
-	/*
-	 * Minimally necessary key value to be last in the tree:
-	 * Upon rescheduling, sched_class::put_prev_task() will place
-	 * 'current' within the tree based on its new key value.
-	 */
-	se->vruntime = rightmost->vruntime + 1;
 }
 
 static bool yield_to_task_fair(struct rq *rq, struct task_struct *p, bool preempt)
