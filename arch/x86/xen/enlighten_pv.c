11e23d3de76fd366432485f7a2239107ff30df5c
Borislav Petkov <bp@suse.de>
2017-09-04 13:15:53 +0200

    xen/x86: Remove SME feature in PV guests (fate#322123).
    suse-commit: b2b2f82917ab96d24f8153b5d74477c9a5ce40bb

--- arch/x86/xen/enlighten_pv.c
+++ arch/x86/xen/enlighten_pv.c
@@ -292,6 +292,7 @@ static void __init xen_init_capabilities(void)
 	setup_clear_cpu_cap(X86_FEATURE_MTRR);
 	setup_clear_cpu_cap(X86_FEATURE_ACC);
 	setup_clear_cpu_cap(X86_FEATURE_X2APIC);
+	setup_clear_cpu_cap(X86_FEATURE_SME);
 
 	if (!xen_initial_domain())
 		setup_clear_cpu_cap(X86_FEATURE_ACPI);
