c5be46c7b80db04f2635410f9d511694f70ea5f7
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2017-09-05 14:31:40 +0200

    cxgb4: Add PTP Hardware Clock (PHC) support (bsc#1046540 bsc#1046648).
    suse-commit: e73825a2bb9e022f4180ac5b241e3bf6d2105e63

--- drivers/net/ethernet/chelsio/cxgb4/cxgb4_main.c
+++ drivers/net/ethernet/chelsio/cxgb4/cxgb4_main.c
@@ -5213,6 +5213,9 @@ static int init_one(struct pci_dev *pdev, const struct pci_device_id *ent)
 		mutex_unlock(&uld_mutex);
 	}
 
+	if (!is_t4(adapter->params.chip))
+		cxgb4_ptp_init(adapter);
+
 	print_adapter_info(adapter);
 	setup_fw_sge_queues(adapter);
 	return 0;
@@ -5323,6 +5326,9 @@ static void remove_one(struct pci_dev *pdev)
 				unregister_netdev(adapter->port[i]);
 
 		debugfs_remove_recursive(adapter->debugfs_root);
+
+		if (!is_t4(adapter->params.chip))
+			cxgb4_ptp_stop(adapter);
 
 		/* If we allocated filters, free up state associated with any
 		 * valid filters ...
