2e6025ed94c992a309d96c2fcf893ce79070cdd5
Takashi Iwai <tiwai@suse.de>
2017-09-07 12:12:13 +0200

    drm/i915/cfl: Add Coffee Lake PCI IDs for H Sku (FATE#322643 bsc#1055900).
    suse-commit: 56797ea018b9fe04cc3bce916c94a5c23c67265a

--- include/drm/i915_pciids.h
+++ include/drm/i915_pciids.h
@@ -342,4 +342,9 @@
 	INTEL_VGA_DEVICE(0x3E92, info), /* SRV GT2 */ \
 	INTEL_VGA_DEVICE(0x3E96, info)  /* SRV GT2 */
 
+/* CFL H */
+#define INTEL_CFL_H_IDS(info) \
+	INTEL_VGA_DEVICE(0x3E9B, info), /* Halo GT2 */ \
+	INTEL_VGA_DEVICE(0x3E94, info)  /* Halo GT2 */
+
 #endif /* _I915_PCIIDS_H */
