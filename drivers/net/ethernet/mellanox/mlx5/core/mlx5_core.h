4fd72a0834d07969407e545e629ce349a9a88922
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2017-09-11 11:02:26 +0200

    net/mlx5: Add mlxfw callbacks (bsc#1046303 FATE#322944).
    suse-commit: 73413cea3d3435df4ea258f4f86f323fd67afb93

--- drivers/net/ethernet/mellanox/mlx5/core/mlx5_core.h
+++ drivers/net/ethernet/mellanox/mlx5/core/mlx5_core.h
@@ -37,6 +37,7 @@
 #include <linux/kernel.h>
 #include <linux/sched.h>
 #include <linux/if_link.h>
+#include <linux/firmware.h>
 
 #define DRIVER_NAME "mlx5_core"
 #define DRIVER_VERSION "5.0-0"
@@ -157,6 +158,8 @@ int mlx5_set_mtppse(struct mlx5_core_dev *mdev, u8 pin, u8 arm, u8 mode);
 			    MLX5_CAP_GEN((mdev), pps_modify) &&		\
 			    MLX5_CAP_MCAM_FEATURE((mdev), mtpps_fs) &&	\
 			    MLX5_CAP_MCAM_FEATURE((mdev), mtpps_enh_out_per_adj))
+
+int mlx5_firmware_flash(struct mlx5_core_dev *dev, const struct firmware *fw);
 
 void mlx5e_init(void);
 void mlx5e_cleanup(void);
