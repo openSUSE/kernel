273d04e69101a259a58632a85e49b3a37afeadcc
Borislav Petkov <bp@suse.de>
2017-09-26 15:20:52 +0200

    EDAC, ghes: Remove symbol exports (bsc#1042420).
    suse-commit: 32b922b8b09946afd767d4dfe541e0ffebbf8e1f

--- drivers/edac/ghes_edac.c
+++ drivers/edac/ghes_edac.c
@@ -403,7 +403,6 @@ void ghes_edac_report_mem_error(struct ghes *ghes, int sev,
 	/* Report the error via EDAC API */
 	edac_raw_mc_handle_error(type, mci, e);
 }
-EXPORT_SYMBOL_GPL(ghes_edac_report_mem_error);
 
 int ghes_edac_register(struct ghes *ghes, struct device *dev)
 {
@@ -508,7 +507,6 @@ int ghes_edac_register(struct ghes *ghes, struct device *dev)
 	mutex_unlock(&ghes_edac_lock);
 	return 0;
 }
-EXPORT_SYMBOL_GPL(ghes_edac_register);
 
 void ghes_edac_unregister(struct ghes *ghes)
 {
@@ -524,4 +522,3 @@ void ghes_edac_unregister(struct ghes *ghes)
 		}
 	}
 }
-EXPORT_SYMBOL_GPL(ghes_edac_unregister);
