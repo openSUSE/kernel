a5a4b4797cca570556bab911927bf2c3b25733c9
Vlastimil Babka <vbabka@suse.cz>
2017-10-05 15:21:52 +0200

    mm/page_alloc.c: apply gfp_allowed_mask before the first allocation attempt (VM Functionality, git fixes).
    suse-commit: 9d48e99ec28f5d9fb1c6e388ff0c622c4dc98f85

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4028,10 +4028,11 @@ __alloc_pages_nodemask(gfp_t gfp_mask, unsigned int order,
 {
 	struct page *page;
 	unsigned int alloc_flags = ALLOC_WMARK_LOW;
-	gfp_t alloc_mask = gfp_mask; /* The gfp_t that was actually used for allocation */
+	gfp_t alloc_mask; /* The gfp_t that was actually used for allocation */
 	struct alloc_context ac = { };
 
 	gfp_mask &= gfp_allowed_mask;
+	alloc_mask = gfp_mask;
 	if (!prepare_alloc_pages(gfp_mask, order, zonelist, nodemask, &ac, &alloc_mask, &alloc_flags))
 		return NULL;
 
