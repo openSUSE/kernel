88b511b46b3e6148e10ecafeae57934afd074ebf
Jeff Mahoney <jeffm@suse.com>
2017-11-20 10:25:15 -0500

    ext4: in ext4_seek_{hole,data}, return -ENXIO for negative offsets (bsc#1068897).
    suse-commit: b65087cc52b8fb8042fafc902ece2719ef73c01e

--- fs/ext4/file.c
+++ fs/ext4/file.c
@@ -572,7 +572,7 @@ static loff_t ext4_seek_data(struct file *file, loff_t offset, loff_t maxsize)
 	inode_lock(inode);
 
 	isize = i_size_read(inode);
-	if (offset >= isize) {
+	if (offset < 0 || offset >= isize) {
 		inode_unlock(inode);
 		return -ENXIO;
 	}
@@ -635,7 +635,7 @@ static loff_t ext4_seek_hole(struct file *file, loff_t offset, loff_t maxsize)
 	inode_lock(inode);
 
 	isize = i_size_read(inode);
-	if (offset >= isize) {
+	if (offset < 0 || offset >= isize) {
 		inode_unlock(inode);
 		return -ENXIO;
 	}
