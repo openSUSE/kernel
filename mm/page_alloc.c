286b234eacab7a8e252ab64a2e7d99df5860f567
Vlastimil Babka <vbabka@suse.cz>
2017-11-24 13:13:36 +0100

    mm, page_alloc: fix potential false positive in __zone_watermark_ok (Git-fixes, bsc#1068978).
    suse-commit: 79749ae0c1ed77447592d33be48a907c121d5d84

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2982,9 +2982,6 @@ bool __zone_watermark_ok(struct zone *z, unsigned int order, unsigned long mark,
 		if (!area->nr_free)
 			continue;
 
-		if (alloc_harder)
-			return true;
-
 		for (mt = 0; mt < MIGRATE_PCPTYPES; mt++) {
 			if (!list_empty(&area->free_list[mt]))
 				return true;
@@ -2996,6 +2993,9 @@ bool __zone_watermark_ok(struct zone *z, unsigned int order, unsigned long mark,
 			return true;
 		}
 #endif
+		if (alloc_harder &&
+			!list_empty(&area->free_list[MIGRATE_HIGHATOMIC]))
+			return true;
 	}
 	return false;
 }
