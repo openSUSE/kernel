68fc126eb3704863b9f73f3bdd11aa3ba8bcea48
Jiri Slaby <jslaby@suse.cz>
2018-01-03 21:41:58 +0100

    x86/syscall: Clear unused extra registers on 32-bit compatible syscall entrance (bnc#1068032 CVE-2017-5715).
    suse-commit: ac29dcc2b18b095d06ffbf875759927a558b88ac

--- arch/x86/entry/calling.h
+++ arch/x86/entry/calling.h
@@ -156,6 +156,17 @@ For 32-bit we have the following conventions - kernel is built with
 	popq %rbx
 	.endm
 
+	.macro CLEAR_R8_TO_R15
+	xorq %r15, %r15
+	xorq %r14, %r14
+	xorq %r13, %r13
+	xorq %r12, %r12
+	xorq %r11, %r11
+	xorq %r10, %r10
+	xorq %r9, %r9
+	xorq %r8, %r8
+	.endm
+
 	.macro RESTORE_EXTRA_REGS offset=0
 	movq 0*8+\offset(%rsp), %r15
 	movq 1*8+\offset(%rsp), %r14
