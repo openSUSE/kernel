58944752bdd5ab862c9e8b29f84894998afb7bc9
Takashi Iwai <tiwai@suse.de>
2018-01-09 15:46:12 +0100

    Move Wireless driver patches into the sorted section
    Update patch tags accordingly, too.
    Also, correct the bad backport in
     patches.drivers/0001-iwlwifi-mvm-send-all-non-bufferable-frames-on-the-pr
    
    Refreshed patches:
     patches.drivers/0123-iwlwifi-mvm-map-cab_queue-to-real-one-earlier.patch
     patches.drivers/iwlwifi-mvm-quietly-accept-non-sta-assoc-response-fr
     patches.drivers/iwlwifi-mvm-quietly-accept-non-sta-disassoc-frames
     patches.drivers/iwlwifi-pcie-fix-TVQM-queue-ID-range-check
     patches.drivers/iwlwifi-pcie-fix-command-completion-name-debug
     patches.drivers/0001-iwlwifi-mvm-send-all-non-bufferable-frames-on-the-pr.patch
     patches.drivers/0039-iwlwifi-pcie-don-t-init-a-Tx-queue-with-an-SSN-size-.patch
     patches.drivers/0048-iwlwifi-pcie-support-short-Tx-queues-for-A000-device.patch
     patches.drivers/0144-iwlwifi-mvm-send-delba-upon-rx-ba-session-timeout.patch
     patches.drivers/iwlwifi-mvm-fix-a-NULL-pointer-dereference-of-error-
    
    suse-commit: 7a1ea610de9eed16bf10bfaf13ca43bff851a504

--- drivers/net/wireless/intel/iwlwifi/mvm/tx.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/tx.c
@@ -565,7 +565,7 @@ static int iwl_mvm_get_ctrl_vif_queue(struct iwl_mvm *mvm,
 	case NL80211_IFTYPE_ADHOC:
 		/*
 		 * Non-bufferable frames use the broadcast station, thus they
-		 * use the probe queue
+		 * use the probe queue.
 		 * Also take care of the case where we send a deauth to a
 		 * station that we don't have, or similarly an association
 		 * response (with non-success status) for a station we can't
@@ -575,8 +575,7 @@ static int iwl_mvm_get_ctrl_vif_queue(struct iwl_mvm *mvm,
 		 */
 		if (ieee80211_is_mgmt(fc) &&
 		    (!ieee80211_is_bufferable_mmpdu(fc) ||
-		    ieee80211_is_deauth(fc) || ieee80211_is_assoc_resp(fc) ||
-		    ieee80211_is_disassoc(fc)))
+		     ieee80211_is_deauth(fc) || ieee80211_is_disassoc(fc)))
 			return mvm->probe_queue;
 		if (info->hw_queue == info->control.vif->cab_queue)
 			return mvmvif->cab_queue;
