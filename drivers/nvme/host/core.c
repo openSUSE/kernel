c41f42520026ab73dc8f658d2705e50a97a97b60
Johannes Thumshirn <jthumshirn@suse.de>
2018-01-10 10:47:11 +0100

    nvme-mpath: fix last path removal during traffic (FATE#323952, FATE#322506).
    suse-commit: d221a0cc5b48771333075c7461c755c325500b62

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -2989,6 +2989,7 @@ static void nvme_ns_remove(struct nvme_ns *ns)
 	mutex_unlock(&ns->ctrl->namespaces_mutex);
 
 	synchronize_srcu(&head->srcu);
+	nvme_mpath_check_last_path(ns);
 	nvme_put_ns(ns);
 }
 
