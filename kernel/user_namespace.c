046c5cde668983599ee3f3d34dc7758e47723162
Jiri Slaby <jslaby@suse.cz>
2018-01-24 19:31:26 +0100

    - Documentation: document array_ptr (bsc#1068032 CVE-2017-5715). - asm/nospec, array_ptr: sanitize speculative array de-references   (bsc#1068032 CVE-2017-5715). - x86: implement array_ptr_mask() (bsc#1068032 CVE-2017-5715). - x86: introduce __uaccess_begin_nospec and ifence (bsc#1068032   CVE-2017-5715). - x86, __get_user: use __uaccess_begin_nospec (bsc#1068032   CVE-2017-5715). - x86, get_user: use pointer masking to limit speculation   (bsc#1068032 CVE-2017-5715). - x86: narrow out of bounds syscalls to sys_read under speculation   (bsc#1068032 CVE-2017-5715). - vfs, fdtable: prevent bounds-check bypass via speculative   execution (bsc#1068032 CVE-2017-5715). - kvm, x86: update spectre-v1 mitigation (bsc#1068032   CVE-2017-5715). - nl80211: sanitize array index in parse_txq_params (bsc#1068032   CVE-2017-5715). - Delete   patches.suse/0003-locking-barriers-introduce-new-observable-speculatio.patch. - Delete   patches.suse/0005-x86-bpf-jit-prevent-speculative-execution-when-JIT-i.patch. - Delete   patches.suse/0006-uvcvideo-prevent-speculative-execution.patch. - Delete   patches.suse/0007-carl9170-prevent-speculative-execution.patch. - Delete   patches.suse/0008-p54-prevent-speculative-execution.patch. - Delete   patches.suse/0009-qla2xxx-prevent-speculative-execution.patch. - Delete   patches.suse/0010-cw1200-prevent-speculative-execution.patch. - Delete   patches.suse/0011-Thermal-int340x-prevent-speculative-execution.patch. - Delete   patches.suse/0012-ipv4-prevent-speculative-execution.patch. - Delete   patches.suse/0013-ipv6-prevent-speculative-execution.patch. - Delete patches.suse/0014-fs-prevent-speculative-execution.patch. - Delete   patches.suse/0015-net-mpls-prevent-speculative-execution.patch. - Delete   patches.suse/0016-udf-prevent-speculative-execution.patch. - Delete   patches.suse/0017-userns-prevent-speculative-execution.patch.
    Replace intel's shit by the potential upstream solution for spectre_v1.
    
    suse-commit: 6fdb1dfd9395517b28292475255df68a60a79953

--- kernel/user_namespace.c
+++ kernel/user_namespace.c
@@ -543,10 +543,8 @@ static void *m_start(struct seq_file *seq, loff_t *ppos,
 	struct uid_gid_extent *extent = NULL;
 	loff_t pos = *ppos;
 
-	if (pos < map->nr_extents) {
-		osb();
+	if (pos < map->nr_extents)
 		extent = &map->extent[pos];
-	}
 
 	return extent;
 }
