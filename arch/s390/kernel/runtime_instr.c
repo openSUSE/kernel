8bdec969ce6b9e1d082d8f469a1fc48977d617a4
Alexander Graf <agraf@suse.de>
2018-02-07 16:09:15 +0100

    s390/runtime_instrumentation: clean up struct runtime_instr_cb (bsc#1077761).
    suse-commit: 51c2ad5f3be557843c41c0a89735e063c723c70a

--- arch/s390/kernel/runtime_instr.c
+++ arch/s390/kernel/runtime_instr.c
@@ -36,13 +36,13 @@ static void disable_runtime_instr(void)
 
 static void init_runtime_instr_cb(struct runtime_instr_cb *cb)
 {
-	cb->buf_limit = 0xfff;
-	cb->pstate = 1;
-	cb->pstate_set_buf = 1;
-	cb->pstate_sample = 1;
-	cb->pstate_collect = 1;
+	cb->rla = 0xfff;
+	cb->s = 1;
+	cb->k = 1;
+	cb->ps = 1;
+	cb->pc = 1;
 	cb->key = PAGE_DEFAULT_KEY;
-	cb->valid = 1;
+	cb->v = 1;
 }
 
 void exit_thread_runtime_instr(void)
