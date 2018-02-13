8d7b65d3cd28d16658b8c6ac5ec882358d767fee
Filipe Manana <fdmanana@suse.com>
2018-02-13 12:49:43 +0000

    Btrfs: fix missing inode i_size update after zero range operation (fate#323233).
    suse-commit: a150fc9a970a2a6a407063d80cb9e0efe838de93

--- fs/btrfs/file.c
+++ fs/btrfs/file.c
@@ -3004,9 +3004,12 @@ reserve_space:
 		unlock_extent_cached(&BTRFS_I(inode)->io_tree, lockstart,
 				     lockend, &cached_state, GFP_KERNEL);
 		/* btrfs_prealloc_file_range releases reserved space on error */
-		if (ret)
+		if (ret) {
 			space_reserved = false;
+			goto out;
+		}
 	}
+	ret = btrfs_fallocate_update_isize(inode, offset + len, mode);
  out:
 	if (ret && space_reserved)
 		btrfs_free_reserved_data_space(inode,
