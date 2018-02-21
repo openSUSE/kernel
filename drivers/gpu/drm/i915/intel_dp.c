74a88f25c798fc0f64b1cd27273b71fd25a1ba22
Takashi Iwai <tiwai@suse.de>
2018-02-21 12:09:27 +0100

    drm/i915: Rename global i915 to i915_modparams (FATE#322643 bsc#1055900).
    suse-commit: b0868dbe55bf6f38f6c2f45454de7589d9376a2a

--- drivers/gpu/drm/i915/intel_dp.c
+++ drivers/gpu/drm/i915/intel_dp.c
@@ -3855,7 +3855,7 @@ intel_dp_can_mst(struct intel_dp *intel_dp)
 {
 	u8 mstm_cap;
 
-	if (!i915.enable_dp_mst)
+	if (!i915_modparams.enable_dp_mst)
 		return false;
 
 	if (!intel_dp->can_mst)
@@ -3873,7 +3873,7 @@ intel_dp_can_mst(struct intel_dp *intel_dp)
 static void
 intel_dp_configure_mst(struct intel_dp *intel_dp)
 {
-	if (!i915.enable_dp_mst)
+	if (!i915_modparams.enable_dp_mst)
 		return;
 
 	if (!intel_dp->can_mst)
