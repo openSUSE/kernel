2bf1177a56177e8df9e10780a009837b429877f6
Luis R. Rodriguez <mcgrof@suse.com>
2018-03-16 11:07:24 -0700

    xfs: fix incorrect log_flushed on fsync (bsc#1085535).
    suse-commit: 55862a4e04f6be01b46c004cbfe1a303769b3539

--- fs/xfs/xfs_log.c
+++ fs/xfs/xfs_log.c
@@ -3342,8 +3342,6 @@ maybe_sleep:
 		 */
 		if (iclog->ic_state & XLOG_STATE_IOERROR)
 			return -EIO;
-		if (log_flushed)
-			*log_flushed = 1;
 	} else {
 
 no_sleep:
@@ -3447,8 +3445,6 @@ try_again:
 
 				xlog_wait(&iclog->ic_prev->ic_write_wait,
 							&log->l_icloglock);
-				if (log_flushed)
-					*log_flushed = 1;
 				already_slept = 1;
 				goto try_again;
 			}
@@ -3482,9 +3478,6 @@ try_again:
 			 */
 			if (iclog->ic_state & XLOG_STATE_IOERROR)
 				return -EIO;
-
-			if (log_flushed)
-				*log_flushed = 1;
 		} else {		/* just return */
 			spin_unlock(&log->l_icloglock);
 		}
