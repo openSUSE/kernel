e95f7cda881055b4bf957bad77e1b91cb30da292
Jiri Slaby <jslaby@suse.cz>
2019-02-19 07:34:39 +0100

    Revert "signal: Always notice exiting tasks" (strace threads-execve regression).
    suse-commit: 08d03457aa642c82543eca6014d7c1a89c053245

--- kernel/signal.c
+++ kernel/signal.c
@@ -2435,11 +2435,6 @@ relock:
 		goto relock;
 	}
 
-	/* Has this task already been marked for death? */
-	ksig->info.si_signo = signr = SIGKILL;
-	if (signal_group_exit(signal))
-		goto fatal;
-
 	for (;;) {
 		struct k_sigaction *ka;
 
@@ -2543,7 +2538,6 @@ relock:
 			continue;
 		}
 
-	fatal:
 		spin_unlock_irq(&sighand->siglock);
 
 		/*
