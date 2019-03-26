28be4a73ef98cd394a6627f2118b62bfa96d352b
Oliver Neukum <oneukum@suse.com>
2019-03-26 15:24:10 +0100

    USB: Add new USB LPM helpers (bsc#1129770).
    suse-commit: 3825dea52426c9644b2b699176fc54db74abe5bf

--- drivers/usb/core/usb.h
+++ drivers/usb/core/usb.h
@@ -83,6 +83,8 @@ extern int usb_runtime_suspend(struct device *dev);
 extern int usb_runtime_resume(struct device *dev);
 extern int usb_runtime_idle(struct device *dev);
 extern int usb_set_usb2_hardware_lpm(struct usb_device *udev, int enable);
+extern int usb_enable_usb2_hardware_lpm(struct usb_device *udev);
+extern int usb_disable_usb2_hardware_lpm(struct usb_device *udev);
 
 #else
 
@@ -97,7 +99,12 @@ static inline int usb_remote_wakeup(struct usb_device *udev)
 	return 0;
 }
 
-static inline int usb_set_usb2_hardware_lpm(struct usb_device *udev, int enable)
+static inline int usb_enable_usb2_hardware_lpm(struct usb_device *udev)
+{
+	return 0;
+}
+
+static inline int usb_disable_usb2_hardware_lpm(struct usb_device *udev)
 {
 	return 0;
 }
