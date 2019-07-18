6777308152c1f6172dba38a212c86d5fac5e5aab
Takashi Iwai <tiwai@suse.de>
2019-07-18 12:32:10 +0200

    Fix kABI breakage by mwifiex security fix (CVE-2019-3846,bsc#1136424).
    suse-commit: b967f30149adeb0f1c1a93e8629cb4b70da4f3c6

--- drivers/net/wireless/mwifiex/fw.h
+++ drivers/net/wireless/mwifiex/fw.h
@@ -985,10 +985,17 @@ struct mwifiex_ie_types_wmm_queue_status {
 struct ieee_types_vendor_header {
 	u8 element_id;
 	u8 len;
+#ifdef __GENKSYMS__
+	u8 oui[3];
+	u8 oui_type;
+	u8 oui_subtype;
+	u8 version;
+#else
 	struct {
 		u8 oui[3];
 		u8 oui_type;
 	} __packed oui;
+#endif
 } __packed;
 
 struct ieee_types_wmm_ac_parameters {
@@ -1008,8 +1015,10 @@ struct ieee_types_wmm_parameter {
 	 *   Version     [1]
 	 */
 	struct ieee_types_vendor_header vend_hdr;
+#ifndef __GENKSYMS__
 	u8 oui_subtype;
 	u8 version;
+#endif
 
 	u8 qos_info_bitmap;
 	u8 reserved;
@@ -1028,8 +1037,10 @@ struct ieee_types_wmm_info {
 	 *   Version     [1]
 	 */
 	struct ieee_types_vendor_header vend_hdr;
+#ifndef __GENKSYMS__
 	u8 oui_subtype;
 	u8 version;
+#endif
 
 	u8 qos_info_bitmap;
 } __packed;
