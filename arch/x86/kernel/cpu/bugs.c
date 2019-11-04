91a25fa23c909c3bb81ddc9d63a9b176a73b27a1
Borislav Petkov <bp@suse.de>
2019-11-04 20:11:53 +0100

    - x86/speculation/taa: Add sysfs reporting for TSX Async Abort   (bsc#1139073 CVE-2019-11135). - Refresh   patches.arch/0001-x86-Add-ITLB_MULTIHIT-bug-infrastructure.patch. - Refresh   patches.arch/0003-kvm-mmu-ITLB_MULTIHIT-mitigation.patch.
    suse-commit: 46fbbbcf6d98ed14d30f02f4b754e021ad554a94

--- arch/x86/kernel/cpu/bugs.c
+++ arch/x86/kernel/cpu/bugs.c
@@ -1420,6 +1420,27 @@ ssize_t cpu_show_mds(struct device *dev, struct device_attribute *attr, char *bu
 {
 	return mds_show_state(buf);
 }
+#endif
+
+static ssize_t tsx_async_abort_show_state(char *buf)
+{
+	if ((taa_mitigation == TAA_MITIGATION_TSX_DISABLED) ||
+	    (taa_mitigation == TAA_MITIGATION_OFF))
+		return sprintf(buf, "%s\n", taa_strings[taa_mitigation]);
+
+	if (boot_cpu_has(X86_FEATURE_HYPERVISOR)) {
+		return sprintf(buf, "%s; SMT Host state unknown\n",
+			       taa_strings[taa_mitigation]);
+	}
+
+	return sprintf(buf, "%s; SMT %s\n", taa_strings[taa_mitigation],
+		       sched_smt_active() ? "vulnerable" : "disabled");
+}
+
+ssize_t cpu_show_tsx_async_abort(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	return tsx_async_abort_show_state(buf);
+}
 
 ssize_t cpu_show_itlb_multihit(struct device *dev, struct device_attribute *attr, char *buf)
 {
@@ -1428,7 +1449,6 @@ ssize_t cpu_show_itlb_multihit(struct device *dev, struct device_attribute *attr
 	else
 		return sprintf(buf, "KVM: Vulnerable\n");
 }
-#endif
 
 void x86_spec_ctrl_set(u64 val)
 {
