f5f8edba93a579a1bce0612bfe51a74ebaf04dd8
Oliver Neukum <oneukum@suse.com>
2020-04-16 18:21:59 +0200

    Refresh patches.fixes/0001-media-stv06xx-add-missing-descriptor-sanity-checks.patch.
    Mismerge
    
    suse-commit: e87e781f5ad95e166009d78a584dd5f44bd0a20a

--- drivers/media/video/gspca/stv06xx/stv06xx.c
+++ drivers/media/video/gspca/stv06xx/stv06xx.c
@@ -305,6 +305,8 @@ static int stv06xx_isoc_init(struct gspca_dev *gspca_dev)
 	struct usb_host_interface *alt;
 	struct sd *sd = (struct sd *) gspca_dev;
 
+	intfc = gspca_dev->dev->actconfig->intf_cache[0];
+
 	if (intfc->num_altsetting < 2)
 		return -ENODEV;
 
