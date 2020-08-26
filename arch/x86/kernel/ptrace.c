0d3f60e04ec50a755c8af35b8bea8f65e85ab335
Borislav Petkov <bp@suse.de>
2020-08-26 18:05:05 +0200

    x86/ptrace: Fix 32-bit PTRACE_SETREGS vs fsbase and gsbase (jsc#SLE-14846).
    suse-commit: a2d9b1ce1b9bdb1077f8b25ac5198f6a9fc374d9

--- arch/x86/kernel/ptrace.c
+++ arch/x86/kernel/ptrace.c
@@ -188,13 +188,9 @@ static int set_segment_reg(struct task_struct *task,
 		return -EIO;
 
 	/*
-	 * For %cs and %ss we cannot permit a null selector.
-	 * We can permit a bogus selector as long as it has USER_RPL.
-	 * Null selectors are fine for other segment registers, but
-	 * we will never get back to user mode with invalid %cs or %ss
-	 * and will take the trap in iret instead.  Much code relies
-	 * on user_mode() to distinguish a user trap frame (which can
-	 * safely use invalid selectors) from a kernel trap frame.
+	 * Writes to FS and GS will change the stored selector.  Whether
+	 * this changes the segment base as well depends on whether
+	 * FSGSBASE is enabled.
 	 */
 	switch (offset) {
 	case offsetof(struct user_regs_struct, cs):
@@ -855,14 +851,39 @@ long arch_ptrace(struct task_struct *child, long request,
 static int putreg32(struct task_struct *child, unsigned regno, u32 value)
 {
 	struct pt_regs *regs = task_pt_regs(child);
+	int ret;
 
 	switch (regno) {
 
 	SEG32(cs);
 	SEG32(ds);
 	SEG32(es);
-	SEG32(fs);
-	SEG32(gs);
+
+	/*
+	 * A 32-bit ptracer on a 64-bit kernel expects that writing
+	 * FS or GS will also update the base.  This is needed for
+	 * operations like PTRACE_SETREGS to fully restore a saved
+	 * CPU state.
+	 */
+
+	case offsetof(struct user32, regs.fs):
+		ret = set_segment_reg(child,
+				      offsetof(struct user_regs_struct, fs),
+				      value);
+		if (ret == 0)
+			child->thread.fsbase =
+				x86_fsgsbase_read_task(child, value);
+		return ret;
+
+	case offsetof(struct user32, regs.gs):
+		ret = set_segment_reg(child,
+				      offsetof(struct user_regs_struct, gs),
+				      value);
+		if (ret == 0)
+			child->thread.gsbase =
+				x86_fsgsbase_read_task(child, value);
+		return ret;
+
 	SEG32(ss);
 
 	R32(ebx, bx);
