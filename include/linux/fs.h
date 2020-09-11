193e0fc70180e743f055563de9dc5446e1bdbde0
Hannes Reinecke <hare@suse.de>
2020-09-11 11:12:34 +0200

    fs: remove an unused block_device_operations forward (bsc#1175995,jsc#SLE-15608).
    suse-commit: 496401daa50477902ba925b624c9c32e0f225683

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1754,8 +1754,6 @@ struct dir_context {
 	loff_t pos;
 };
 
-struct block_device_operations;
-
 /* These macros are for out of kernel modules to test that
  * the kernel supports the unlocked_ioctl and compat_ioctl
  * fields in struct file_operations. */
