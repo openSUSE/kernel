74ce8ae04987621136984ca75bfaa840d63ba8e8
Coly Li <colyli@suse.de>
2020-09-19 21:12:42 +0800

    libnvdimm: Move nd_region_attribute_group to device_type (jsc#SLE-13338).
    suse-commit: cc93c6860706ffb190231542fda7dc9ab3ff071d

--- include/linux/libnvdimm.h
+++ include/linux/libnvdimm.h
@@ -69,7 +69,6 @@ enum {
 
 extern struct attribute_group nvdimm_bus_attribute_group;
 extern struct attribute_group nvdimm_attribute_group;
-extern struct attribute_group nd_region_attribute_group;
 extern struct attribute_group nd_mapping_attribute_group;
 
 struct nvdimm;
