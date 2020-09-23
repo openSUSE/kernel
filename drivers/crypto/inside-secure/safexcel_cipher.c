9dbbb6df326d5b63efc4aa59c43a8be8a41ce29c
Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
2020-09-23 16:49:43 +0200

    - crypto: des - split off DES library from generic DES cipher driver (jsc#SLE-16106). - Update configs - Refresh: patches.suse/crypto-blake2s-generic-c-library-implementation-and-selftest.patch - Refresh: patches.suse/crypto-chacha-move-existing-library-code-into-lib-crypto.patch - Refresh: patches.suse/crypto-chacha20poly1305-import-construction-and-selftest-from-zinc.patch - Refresh: patches.suse/crypto-lib-curve25519-re-add-selftests.patch - Refresh: patches.suse/crypto-lib-tidy-up-lib-crypto-kconfig-and-makefile.patch - Refresh: patches.suse/crypto-poly1305-move-core-routines-into-a-separate-library.patch - supported.conf: add libdes
    suse-commit: 81dc1372ec99c3dae1744cc10aecbb9b115c0106

--- drivers/crypto/inside-secure/safexcel_cipher.c
+++ drivers/crypto/inside-secure/safexcel_cipher.c
@@ -12,7 +12,7 @@
 #include <crypto/aead.h>
 #include <crypto/aes.h>
 #include <crypto/authenc.h>
-#include <crypto/des.h>
+#include <crypto/internal/des.h>
 #include <crypto/sha.h>
 #include <crypto/skcipher.h>
 #include <crypto/internal/aead.h>
