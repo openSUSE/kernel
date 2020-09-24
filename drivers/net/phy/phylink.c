9c0dfeebd9a57972648e38037338dc6c9e9dfa79
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2020-09-24 17:05:05 +0200

    net: phylink: allow in-band AN for USXGMII (bsc#1176447).
    suse-commit: 5b3fbc097e463c7ac7b20b48c5200e8f07876778

--- drivers/net/phy/phylink.c
+++ drivers/net/phy/phylink.c
@@ -298,6 +298,7 @@ static int phylink_parse_mode(struct phylink *pl, struct fwnode_handle *fwnode)
 			phylink_set(pl->supported, 2500baseX_Full);
 			break;
 
+		case PHY_INTERFACE_MODE_USXGMII:
 		case PHY_INTERFACE_MODE_10GKR:
 			phylink_set(pl->supported, 10baseT_Half);
 			phylink_set(pl->supported, 10baseT_Full);
