c951818fdfea6befc8f3083bab7374ce76cf2de4
Mian Yousaf Kaukab <yousaf.kaukab@suse.com>
2020-09-24 12:50:39 +0200

    gpio: max77620: Don't shadow error code of platform_get_irq() (jsc#SLE-15847).
    suse-commit: d1a487e2973fa90ebf91efb89f92ed65179d1406

--- drivers/gpio/gpio-max77620.c
+++ drivers/gpio/gpio-max77620.c
@@ -264,14 +264,16 @@ static int max77620_gpio_probe(struct platform_device *pdev)
 {
 	struct max77620_chip *chip =  dev_get_drvdata(pdev->dev.parent);
 	struct max77620_gpio *mgpio;
-	int gpio_irq;
+	unsigned int gpio_irq;
 	int ret;
 
-	gpio_irq = platform_get_irq(pdev, 0);
-	if (gpio_irq <= 0) {
+	ret = platform_get_irq(pdev, 0);
+	if (ret < 0) {
 		dev_err(&pdev->dev, "GPIO irq not available %d\n", gpio_irq);
-		return -ENODEV;
+		return ret;
 	}
+
+	gpio_irq = ret;
 
 	mgpio = devm_kzalloc(&pdev->dev, sizeof(*mgpio), GFP_KERNEL);
 	if (!mgpio)
