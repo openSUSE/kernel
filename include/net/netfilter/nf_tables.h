dd72a4f0baa530993a39cf601eb66bc6a8b8e0af
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2020-09-24 17:05:05 +0200

    netfilter: nf_tables: dynamically allocate hooks per net_device in flowtables (bsc#1176447).
    suse-commit: ab293fe33a433ceda413f90ab2fed1564aca90d4

--- include/net/netfilter/nf_tables.h
+++ include/net/netfilter/nf_tables.h
@@ -964,6 +964,12 @@ struct nft_stats {
 	struct u64_stats_sync	syncp;
 };
 
+struct nft_hook {
+	struct list_head	list;
+	struct nf_hook_ops	ops;
+	struct rcu_head		rcu;
+};
+
 /**
  *	struct nft_base_chain - nf_tables base chain
  *
@@ -1174,7 +1180,7 @@ struct nft_flowtable {
 					use:30;
 	u64				handle;
 	/* runtime data below here */
-	struct nf_hook_ops		*ops ____cacheline_aligned;
+	struct list_head		hook_list ____cacheline_aligned;
 	struct nf_flowtable		data;
 };
 
