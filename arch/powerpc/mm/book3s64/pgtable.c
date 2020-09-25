004d6b86d7851866f3b4f9a245eb333ac7b609e1
Michal Suchanek <msuchanek@suse.de>
2020-09-25 15:04:34 +0200

    powerpc/64s: make mmu_partition_table_set_entry TLB flush optional (jsc#SLE-13512).
    suse-commit: b0f192d90eeac48e5cb7a19eefda5246f29b153a

--- arch/powerpc/mm/book3s64/pgtable.c
+++ arch/powerpc/mm/book3s64/pgtable.c
@@ -210,12 +210,15 @@ void __init mmu_partition_table_init(void)
 }
 
 void mmu_partition_table_set_entry(unsigned int lpid, unsigned long dw0,
-				   unsigned long dw1)
+				   unsigned long dw1, bool flush)
 {
 	unsigned long old = be64_to_cpu(partition_tb[lpid].patb0);
 
 	partition_tb[lpid].patb0 = cpu_to_be64(dw0);
 	partition_tb[lpid].patb1 = cpu_to_be64(dw1);
+
+	if (!flush)
+		return;
 
 	/*
 	 * Global flush of TLBs and partition table caches for this lpid.
