f775a240ce09e1ac0952a15eea7c0c0ab54fbb03
Mian Yousaf Kaukab <yousaf.kaukab@suse.com>
2020-09-28 14:47:56 +0200

    clk: tegra: Export functions for EMC clock scaling (jsc#SLE-15847).
    suse-commit: 31f66386bac0b2364f742e42e0a98b8479dc07fb

--- include/linux/clk/tegra.h
+++ include/linux/clk/tegra.h
@@ -118,5 +118,8 @@ extern void tegra210_set_sata_pll_seq_sw(bool state);
 extern void tegra210_put_utmipll_in_iddq(void);
 extern void tegra210_put_utmipll_out_iddq(void);
 extern int tegra210_clk_handle_mbist_war(unsigned int id);
+extern void tegra210_clk_emc_dll_enable(bool flag);
+extern void tegra210_clk_emc_dll_update_setting(u32 emc_dll_src_value);
+extern void tegra210_clk_emc_update_setting(u32 emc_src_value);
 
 #endif /* __LINUX_CLK_TEGRA_H_ */
