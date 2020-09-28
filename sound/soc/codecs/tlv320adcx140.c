7044a3629884dd56db3880fc9947e81f6aa48b14
Takashi Iwai <tiwai@suse.de>
2020-09-28 14:17:52 +0200

    ASoC: tlv320adcx140: Configure PDM sampling edge (jsc#SLE-16518).
    suse-commit: 2a060a5fb4d154227b2bb31f401a03069cb155d0

--- sound/soc/codecs/tlv320adcx140.c
+++ sound/soc/codecs/tlv320adcx140.c
@@ -761,6 +761,10 @@ static int adcx140_codec_probe(struct snd_soc_component *component)
 	u32 bias_source;
 	u32 vref_source;
 	u8 bias_cfg;
+	int pdm_count;
+	u32 pdm_edges[ADCX140_NUM_PDM_EDGES];
+	u32 pdm_edge_val = 0;
+	int i;
 	int ret;
 
 	ret = device_property_read_u32(adcx140->dev, "ti,mic-bias-source",
@@ -786,6 +790,24 @@ static int adcx140_codec_probe(struct snd_soc_component *component)
 	}
 
 	bias_cfg = bias_source << ADCX140_MIC_BIAS_SHIFT | vref_source;
+
+	pdm_count = device_property_count_u32(adcx140->dev,
+					      "ti,pdm-edge-select");
+	if (pdm_count <= ADCX140_NUM_PDM_EDGES && pdm_count > 0) {
+		ret = device_property_read_u32_array(adcx140->dev,
+						     "ti,pdm-edge-select",
+						     pdm_edges, pdm_count);
+		if (ret)
+			return ret;
+
+		for (i = 0; i < pdm_count; i++)
+			pdm_edge_val |= pdm_edges[i] << (ADCX140_PDM_EDGE_SHIFT - i);
+
+		ret = regmap_write(adcx140->regmap, ADCX140_PDM_CFG,
+				   pdm_edge_val);
+		if (ret)
+			return ret;
+	}
 
 	ret = adcx140_reset(adcx140);
 	if (ret)
