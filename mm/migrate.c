73153f34278d55f39e6fa7fa3a107abdd31891fb
Vlastimil Babka <vbabka@suse.cz>
2020-09-30 17:14:47 +0200

    - mm: remove CONFIG_MIGRATE_VMA_HELPER (jsc#SLE-16387). - Update config files.
    suse-commit: 7a1f65510b66460685ca153553208fc0f2dcec75

--- mm/migrate.c
+++ mm/migrate.c
@@ -2115,7 +2115,7 @@ out_unlock:
 
 #endif /* CONFIG_NUMA */
 
-#if defined(CONFIG_MIGRATE_VMA_HELPER)
+#ifdef CONFIG_DEVICE_PRIVATE
 static int migrate_vma_collect_hole(unsigned long start,
 				    unsigned long end,
 				    struct mm_walk *walk)
@@ -2937,4 +2937,4 @@ void migrate_vma_finalize(struct migrate_vma *migrate)
 	}
 }
 EXPORT_SYMBOL(migrate_vma_finalize);
-#endif /* defined(MIGRATE_VMA_HELPER) */
+#endif /* CONFIG_DEVICE_PRIVATE */
