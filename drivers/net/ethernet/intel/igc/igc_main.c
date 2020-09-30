dd302ee7fcda9b2c14106c46969ff2615c8bd6b7
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2020-09-30 22:51:08 +0200

    igc: Complete to commit Add basic skeleton for PTP (jsc#SLE-13533).
    suse-commit: 1eb9df71e6316b51c2cbfbb61aca1b83d532938a

--- drivers/net/ethernet/intel/igc/igc_main.c
+++ drivers/net/ethernet/intel/igc/igc_main.c
@@ -4897,6 +4897,8 @@ static int __igc_shutdown(struct pci_dev *pdev, bool *enable_wake,
 	if (netif_running(netdev))
 		__igc_close(netdev, true);
 
+	igc_ptp_suspend(adapter);
+
 	igc_clear_interrupt_scheme(adapter);
 	rtnl_unlock();
 
