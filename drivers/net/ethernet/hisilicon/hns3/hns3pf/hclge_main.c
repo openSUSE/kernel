46e308f0f6469b0a2ddaa1f92c73ccf2826b48b3
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2020-10-01 23:51:05 +0200

    net: hns3: delete some reduandant code (jsc#SLE-14777).
    suse-commit: a5fbec6b3ad60a7e6aa54acadb9897b1b8f30d83

--- drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_main.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_main.c
@@ -7351,7 +7351,6 @@ int hclge_add_mc_addr_common(struct hclge_vport *vport,
 		return -EINVAL;
 	}
 	memset(&req, 0, sizeof(req));
-	hnae3_set_bit(req.entry_type, HCLGE_MAC_VLAN_BIT0_EN_B, 0);
 	hclge_prepare_mac_addr(&req, addr, true);
 	status = hclge_lookup_mac_vlan_tbl(vport, &req, desc, true);
 	if (status) {
@@ -7396,7 +7395,6 @@ int hclge_rm_mc_addr_common(struct hclge_vport *vport,
 	}
 
 	memset(&req, 0, sizeof(req));
-	hnae3_set_bit(req.entry_type, HCLGE_MAC_VLAN_BIT0_EN_B, 0);
 	hclge_prepare_mac_addr(&req, addr, true);
 	status = hclge_lookup_mac_vlan_tbl(vport, &req, desc, true);
 	if (!status) {
