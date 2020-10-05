5619a28b8b7fb0681177f1039ab36901f0d5515f
Gary Lin <glin@suse.com>
2020-10-05 13:45:50 +0800

    bpf: Remove set but not used variable 'first_key' (bsc#1177028).
    suse-commit: 12eb72d2ca361352c277f0e441cffde9aa7c10df

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -1346,7 +1346,6 @@ int generic_map_lookup_batch(struct bpf_map *map,
 	void *buf, *buf_prevkey, *prev_key, *key, *value;
 	int err, retry = MAP_LOOKUP_RETRIES;
 	u32 value_size, cp, max_count;
-	bool first_key = false;
 
 	if (attr->batch.elem_flags & ~BPF_F_LOCK)
 		return -EINVAL;
@@ -1375,7 +1374,6 @@ int generic_map_lookup_batch(struct bpf_map *map,
 	}
 
 	err = -EFAULT;
-	first_key = false;
 	prev_key = NULL;
 	if (ubatch && copy_from_user(buf_prevkey, ubatch, map->key_size))
 		goto free_buf;
