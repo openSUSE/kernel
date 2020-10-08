e04e994f1e9784fdf45f0eef4ec40e4ac1a0b337
Gary Lin <glin@suse.com>
2020-10-08 11:26:44 +0800

    libbpf: Poison kernel-only integer types (bsc#1177028).
    suse-commit: 28174e97cba2713253870464d0da7576628d2b4c

--- tools/lib/bpf/netlink.c
+++ tools/lib/bpf/netlink.c
@@ -14,6 +14,9 @@
 #include "libbpf.h"
 #include "nlattr.h"
 
+/* make sure libbpf doesn't use kernel-only integer typedefs */
+#pragma GCC poison u8 u16 u32 u64 s8 s16 s32 s64
+
 #ifndef SOL_NETLINK
 #define SOL_NETLINK 270
 #endif
