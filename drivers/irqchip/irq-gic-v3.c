ac85fecb83793ca6d8d5a36e503b3273e50886de
Matthias Brugger <mbrugger@suse.com>
2020-10-09 14:33:35 +0200

    irqchip/gic-v4.1: Advertise support v4.1 to KVM (jsc#SLE-14773).
    suse-commit: 3bc0458e0a705708aed4c3b8a7725290e673641e

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -1792,6 +1792,7 @@ static void __init gic_of_setup_kvm_info(struct device_node *node)
 		gic_v3_kvm_info.vcpu = r;
 
 	gic_v3_kvm_info.has_v4 = gic_data.rdists.has_vlpis;
+	gic_v3_kvm_info.has_v4_1 = gic_data.rdists.has_rvpeid;
 	gic_set_kvm_info(&gic_v3_kvm_info);
 }
 
@@ -2107,6 +2108,7 @@ static void __init gic_acpi_setup_kvm_info(void)
 	}
 
 	gic_v3_kvm_info.has_v4 = gic_data.rdists.has_vlpis;
+	gic_v3_kvm_info.has_v4_1 = gic_data.rdists.has_rvpeid;
 	gic_set_kvm_info(&gic_v3_kvm_info);
 }
 
