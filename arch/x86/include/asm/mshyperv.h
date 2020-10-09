787519310b4cf8b10c46bfacf38dd12c307ae169
Olaf Hering <ohering@suse.de>
2020-10-09 18:42:03 +0200

    x86/hyperv: Make hv_setup_sched_clock inline (jsc#sle-12891).
    suse-commit: eff3b16c9a156e745c57424400f94904e039206a

--- arch/x86/include/asm/mshyperv.h
+++ arch/x86/include/asm/mshyperv.h
@@ -8,6 +8,7 @@
 #include <asm/io.h>
 #include <asm/hyperv-tlfs.h>
 #include <asm/nospec-branch.h>
+#include <asm/paravirt.h>
 
 typedef int (*hyperv_fill_flush_list_func)(
 		struct hv_guest_mapping_flush_list *flush,
@@ -51,6 +52,17 @@ typedef int (*hyperv_fill_flush_list_func)(
 #define hv_set_clocksource_vdso(val) \
 	((val).archdata.vclock_mode = VCLOCK_HVCLOCK)
 #define hv_get_raw_timer() rdtsc_ordered()
+
+/*
+ * Reference to pv_ops must be inline so objtool
+ * detection of noinstr violations can work correctly.
+ */
+static __always_inline void hv_setup_sched_clock(void *sched_clock)
+{
+#ifdef CONFIG_PARAVIRT
+	pv_ops.time.sched_clock = sched_clock;
+#endif
+}
 
 void hyperv_callback_vector(void);
 void hyperv_reenlightenment_vector(void);
