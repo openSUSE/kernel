44316b29299d848ef01bbb40e2c6fc61d60b2135
Thomas Zimmermann <tzimmermann@suse.de>
2020-10-21 16:24:39 +0200

    mm: Add write-protect and clean utilities for address space ranges (bsc#1152489)
    suse-commit: fe1828a648f3b85f3d3d53c032c9f8feb87640e6

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2648,7 +2648,6 @@ typedef int (*pte_fn_t)(pte_t *pte, unsigned long addr, void *data);
 extern int apply_to_page_range(struct mm_struct *mm, unsigned long address,
 			       unsigned long size, pte_fn_t fn, void *data);
 
-
 #ifdef CONFIG_PAGE_POISONING
 extern bool page_poisoning_enabled(void);
 extern void kernel_poison_pages(struct page *page, int numpages, int enable);
@@ -2895,6 +2894,18 @@ static inline bool page_is_guard(struct page *page) { return false; }
 void __init setup_nr_node_ids(void);
 #else
 static inline void setup_nr_node_ids(void) {}
+#endif
+
+#ifdef CONFIG_MAPPING_DIRTY_HELPERS
+unsigned long clean_record_shared_mapping_range(struct address_space *mapping,
+						pgoff_t first_index, pgoff_t nr,
+						pgoff_t bitmap_pgoff,
+						unsigned long *bitmap,
+						pgoff_t *start,
+						pgoff_t *end);
+
+unsigned long wp_shared_mapping_range(struct address_space *mapping,
+				      pgoff_t first_index, pgoff_t nr);
 #endif
 
 #endif /* __KERNEL__ */
