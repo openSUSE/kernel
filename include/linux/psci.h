ec7278c4eeeba07b65f334b8e62a6ba52d9e84b7
Mian Yousaf Kaukab <yousaf.kaukab@suse.com>
2020-10-22 17:13:29 +0200

    PSCI: cpuidle: Refactor CPU suspend power_state parameter handling (jsc#SLE-12251).
    suse-commit: 8a18c027ce26cb918e5ddec2a627af7ca8beba12

--- include/linux/psci.h
+++ include/linux/psci.h
@@ -16,8 +16,8 @@
 
 bool psci_tos_resident_on(int cpu);
 
-int psci_cpu_init_idle(unsigned int cpu);
-int psci_cpu_suspend_enter(unsigned long index);
+int psci_cpu_suspend_enter(u32 state);
+bool psci_power_state_is_valid(u32 state);
 
 struct psci_operations {
 	u32 (*get_version)(void);
