3e45030c78fe0b3f0b5ec100eaff0c4f8bb52fc5
Borislav Petkov <bp@suse.de>
2020-10-23 12:17:18 +0200

    perf/x86/rapl: Move RAPL support to common x86 code (jsc#SLE-16869).
    suse-commit: 1ff01f01fe9864e528ad191077239f447d0f8bbc

--- arch/x86/events/rapl.c
+++ arch/x86/events/rapl.c
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Support Intel RAPL energy consumption counters
+ * Support Intel/AMD RAPL energy consumption counters
  * Copyright (C) 2013 Google, Inc., Stephane Eranian
  *
  * Intel RAPL interface is specified in the IA-32 Manual Vol3b
  * section 14.7.1 (September 2013)
+ *
+ * AMD RAPL interface for Fam17h is described in the public PPR:
+ * https://bugzilla.kernel.org/show_bug.cgi?id=206537
  *
  * RAPL provides more controls than just reporting energy consumption
  * however here we only expose the 3 energy consumption free running
@@ -58,8 +61,8 @@
 #include <linux/nospec.h>
 #include <asm/cpu_device_id.h>
 #include <asm/intel-family.h>
-#include "../perf_event.h"
-#include "../probe.h"
+#include "perf_event.h"
+#include "probe.h"
 
 MODULE_LICENSE("GPL");
 
@@ -634,7 +637,7 @@ static void cleanup_rapl_pmus(void)
 	kfree(rapl_pmus);
 }
 
-const struct attribute_group *rapl_attr_update[] = {
+static const struct attribute_group *rapl_attr_update[] = {
 	&rapl_events_cores_group,
 	&rapl_events_pkg_group,
 	&rapl_events_ram_group,
