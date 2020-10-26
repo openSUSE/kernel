ccaa3f13994c47268b96038d84d0d859c140074d
Joerg Roedel <jroedel@suse.de>
2020-10-26 15:30:33 +0100

    x86/sev-es: Handle MONITOR/MONITORX Events (jsc#SLE-14337).
    suse-commit: 700fd55e12016ff88e9da112f016991a77faf75e

--- arch/x86/kernel/sev-es.c
+++ arch/x86/kernel/sev-es.c
@@ -874,6 +874,16 @@ static enum es_result vc_handle_rdpmc(struct ghcb *ghcb, struct es_em_ctxt *ctxt
 	return ES_OK;
 }
 
+static enum es_result vc_handle_monitor(struct ghcb *ghcb,
+					struct es_em_ctxt *ctxt)
+{
+	/*
+	 * Treat it as a NOP and do not leak a physical address to the
+	 * hypervisor.
+	 */
+	return ES_OK;
+}
+
 static enum es_result vc_handle_exitcode(struct es_em_ctxt *ctxt,
 					 struct ghcb *ghcb,
 					 unsigned long exit_code)
@@ -909,6 +919,9 @@ static enum es_result vc_handle_exitcode(struct es_em_ctxt *ctxt,
 		break;
 	case SVM_EXIT_WBINVD:
 		result = vc_handle_wbinvd(ghcb, ctxt);
+		break;
+	case SVM_EXIT_MONITOR:
+		result = vc_handle_monitor(ghcb, ctxt);
 		break;
 	case SVM_EXIT_NPF:
 		result = vc_handle_mmio(ghcb, ctxt);
