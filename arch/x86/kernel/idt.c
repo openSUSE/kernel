e91fb808b85e1b19e970e1e30c6f4dd1f934be11
Joerg Roedel <jroedel@suse.de>
2020-10-26 15:30:33 +0100

    x86/idt: Make IDT init functions static inlines (jsc#SLE-14337).
    suse-commit: 3a7ed257c7a325e542321608a4e31bf514b1dabc

--- arch/x86/kernel/idt.c
+++ arch/x86/kernel/idt.c
@@ -9,13 +9,6 @@
 #include <asm/desc.h>
 #include <asm/hw_irq.h>
 
-struct idt_data {
-	unsigned int	vector;
-	unsigned int	segment;
-	struct idt_bits	bits;
-	const void	*addr;
-};
-
 #define DPL0		0x0
 #define DPL3		0x3
 
@@ -200,20 +193,6 @@ const struct desc_ptr debug_idt_descr = {
 };
 #endif
 
-static inline void idt_init_desc(gate_desc *gate, const struct idt_data *d)
-{
-	unsigned long addr = (unsigned long) d->addr;
-
-	gate->offset_low	= (u16) addr;
-	gate->segment		= (u16) d->segment;
-	gate->bits		= d->bits;
-	gate->offset_middle	= (u16) (addr >> 16);
-#ifdef CONFIG_X86_64
-	gate->offset_high	= (u32) (addr >> 32);
-	gate->reserved		= 0;
-#endif
-}
-
 static void
 idt_setup_from_table(gate_desc *idt, const struct idt_data *t, int size, bool sys)
 {
@@ -225,19 +204,6 @@ idt_setup_from_table(gate_desc *idt, const struct idt_data *t, int size, bool sy
 		if (sys)
 			set_bit(t->vector, system_vectors);
 	}
-}
-
-static void init_idt_data(struct idt_data *data, unsigned int n,
-			  const void *addr)
-{
-	BUG_ON(n > 0xFF);
-
-	memset(data, 0, sizeof(*data));
-	data->vector	= n;
-	data->addr	= addr;
-	data->segment	= __KERNEL_CS;
-	data->bits.type	= GATE_INTERRUPT;
-	data->bits.p	= 1;
 }
 
 static void set_intr_gate(unsigned int n, const void *addr)
