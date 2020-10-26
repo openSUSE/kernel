a7e33ffc3da7d86a4cedc493ce7155e3238a5f61
Joerg Roedel <jroedel@suse.de>
2020-10-26 15:30:33 +0100

    x86/fpu: Move xgetbv()/xsetbv() into a separate header (jsc#SLE-14337).
    suse-commit: 0838cb016a2a5bc81b0d1fb15bcf5056164d719f

--- arch/x86/include/asm/fpu/internal.h
+++ arch/x86/include/asm/fpu/internal.h
@@ -19,6 +19,7 @@
 #include <asm/user.h>
 #include <asm/fpu/api.h>
 #include <asm/fpu/xstate.h>
+#include <asm/fpu/xcr.h>
 #include <asm/cpufeature.h>
 #include <asm/trace/fpu.h>
 
@@ -624,33 +625,6 @@ static inline void switch_fpu_finish(struct fpu *new_fpu)
 	 * unless it's different from mm->pasid to reduce overhead.
 	 */
 	update_pasid();
-}
-
-/*
- * MXCSR and XCR definitions:
- */
-
-extern unsigned int mxcsr_feature_mask;
-
-#define XCR_XFEATURE_ENABLED_MASK	0x00000000
-
-static inline u64 xgetbv(u32 index)
-{
-	u32 eax, edx;
-
-	asm volatile(".byte 0x0f,0x01,0xd0" /* xgetbv */
-		     : "=a" (eax), "=d" (edx)
-		     : "c" (index));
-	return eax + ((u64)edx << 32);
-}
-
-static inline void xsetbv(u32 index, u64 value)
-{
-	u32 eax = value;
-	u32 edx = value >> 32;
-
-	asm volatile(".byte 0x0f,0x01,0xd1" /* xsetbv */
-		     : : "a" (eax), "d" (edx), "c" (index));
 }
 
 #endif /* _ASM_X86_FPU_INTERNAL_H */
