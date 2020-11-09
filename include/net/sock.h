99e5ec2970f37e0504554278754848b539c5703f
Gary Lin <glin@suse.com>
2020-11-09 15:14:56 +0800

    net: add sock_set_reuseaddr (bsc#1177028).
    Refresh patches.suse/net-add-sock_bindtoindex.patch and
    patches.suse/net-Make-locking-in-sock_bindtoindex-optional.patch
    
    suse-commit: b8352a27090a2e261b2ba1f7c52269893e93f803

--- include/net/sock.h
+++ include/net/sock.h
@@ -2678,5 +2678,6 @@ static inline bool sk_dev_equal_l3scope(struct sock *sk, int dif)
 void sock_def_readable(struct sock *sk);
 
 int sock_bindtoindex(struct sock *sk, int ifindex, bool lock_sk);
+void sock_set_reuseaddr(struct sock *sk);
 
 #endif	/* _SOCK_H */
