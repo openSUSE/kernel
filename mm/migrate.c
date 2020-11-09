085a8ba5fee0d4d4e09f8ded3fcd7a2847443b20
Vlastimil Babka <vbabka@suse.cz>
2020-11-09 20:12:12 +0100

    mm: pagewalk: add 'depth' parameter to pte_hole (jsc#SLE-16387).
    suse-commit: 422f5fcc027635e1fa77807c6bcee9df8be3d3a7

--- mm/migrate.c
+++ mm/migrate.c
@@ -2146,6 +2146,7 @@ out_unlock:
 #ifdef CONFIG_DEVICE_PRIVATE
 static int migrate_vma_collect_hole(unsigned long start,
 				    unsigned long end,
+				    __always_unused int depth,
 				    struct mm_walk *walk)
 {
 	struct migrate_vma *migrate = walk->private;
@@ -2190,7 +2191,7 @@ static int migrate_vma_collect_pmd(pmd_t *pmdp,
 
 again:
 	if (pmd_none(*pmdp))
-		return migrate_vma_collect_hole(start, end, walk);
+		return migrate_vma_collect_hole(start, end, -1, walk);
 
 	if (pmd_trans_huge(*pmdp)) {
 		struct page *page;
@@ -2223,7 +2224,7 @@ again:
 				return migrate_vma_collect_skip(start, end,
 								walk);
 			if (pmd_none(*pmdp))
-				return migrate_vma_collect_hole(start, end,
+				return migrate_vma_collect_hole(start, end, -1,
 								walk);
 		}
 	}
