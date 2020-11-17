a07a841b4a3fd67b0bdca3f38edef1085c9a8101
Gary Lin <glin@suse.com>
2020-11-17 14:50:03 +0800

    bpf: Allow %pB in bpf_seq_printf() and bpf_trace_printk() (bsc#1177028).
    suse-commit: 3a83cb2cacc044504686f9a2dd4be92be1fa5b5c

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -327,7 +327,7 @@ static const struct bpf_func_proto *bpf_get_probe_write_proto(void)
 
 /*
  * Only limited trace_printk() conversion specifiers allowed:
- * %d %i %u %x %ld %li %lu %lx %lld %lli %llu %llx %p %pks %pus %s
+ * %d %i %u %x %ld %li %lu %lx %lld %lli %llu %llx %p %pB %pks %pus %s
  */
 BPF_CALL_5(bpf_trace_printk, char *, fmt, u32, fmt_size, u64, arg1,
 	   u64, arg2, u64, arg3)
@@ -369,6 +369,11 @@ BPF_CALL_5(bpf_trace_printk, char *, fmt, u32, fmt_size, u64, arg1,
 				fmt_ptype = fmt[i + 1];
 				i += 2;
 				goto fmt_str;
+			}
+
+			if (fmt[i + 1] == 'B') {
+				i++;
+				goto fmt_next;
 			}
 
 			/* disallow any further format extensions */
@@ -601,7 +606,8 @@ BPF_CALL_5(bpf_seq_printf, struct seq_file *, m, char *, fmt, u32, fmt_size,
 		if (fmt[i] == 'p') {
 			if (fmt[i + 1] == 0 ||
 			    fmt[i + 1] == 'K' ||
-			    fmt[i + 1] == 'x') {
+			    fmt[i + 1] == 'x' ||
+			    fmt[i + 1] == 'B') {
 				/* just kernel pointers */
 				params[fmt_cnt] = args[fmt_cnt];
 				fmt_cnt++;
