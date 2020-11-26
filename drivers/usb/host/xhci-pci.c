998cac1052de267a23f1233ed3d07bc31b1ff66a
Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
2020-11-26 13:23:57 +0100

    - usb: xhci: Enable LPM for VIA LABS VL805 (jsc#SLE-17068). - Refresh:  patches.suse/usb-xhci-define-IDs-for-various-ASMedia-host-control.patch
    suse-commit: 550134c0928e0502f1c22bf86a2cba7e2343d066

--- drivers/usb/host/xhci-pci.c
+++ drivers/usb/host/xhci-pci.c
@@ -250,6 +250,9 @@ static void xhci_pci_quirks(struct device *dev, struct xhci_hcd *xhci)
 			pdev->device == 0x3432)
 		xhci->quirks |= XHCI_BROKEN_STREAMS;
 
+	if (pdev->vendor == PCI_VENDOR_ID_VIA && pdev->device == 0x3483)
+		xhci->quirks |= XHCI_LPM_SUPPORT;
+
 	if (pdev->vendor == PCI_VENDOR_ID_ASMEDIA &&
 		pdev->device == PCI_DEVICE_ID_ASMEDIA_1042_XHCI)
 		xhci->quirks |= XHCI_BROKEN_STREAMS;
