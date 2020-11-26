2e0a1a03c82e15895ead05d12e72bf4501cf7929
Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
2020-11-26 13:23:57 +0100

    firmware: raspberrypi: Keep count of all consumers (jsc#SLE-16543).
    suse-commit: 030acb8d4e44b05c12139465aa1b5d75265c2b0d

--- include/soc/bcm2835/raspberrypi-firmware.h
+++ include/soc/bcm2835/raspberrypi-firmware.h
@@ -140,6 +140,7 @@ int rpi_firmware_property(struct rpi_firmware *fw,
 			  u32 tag, void *data, size_t len);
 int rpi_firmware_property_list(struct rpi_firmware *fw,
 			       void *data, size_t tag_size);
+void rpi_firmware_put(struct rpi_firmware *fw);
 struct rpi_firmware *rpi_firmware_get(struct device_node *firmware_node);
 struct rpi_firmware *devm_rpi_firmware_get(struct device *dev,
 					   struct device_node *firmware_node);
@@ -156,6 +157,7 @@ static inline int rpi_firmware_property_list(struct rpi_firmware *fw,
 	return -ENOSYS;
 }
 
+static inline void rpi_firmware_put(struct rpi_firmware *fw) { }
 static inline struct rpi_firmware *rpi_firmware_get(struct device_node *firmware_node)
 {
 	return NULL;
