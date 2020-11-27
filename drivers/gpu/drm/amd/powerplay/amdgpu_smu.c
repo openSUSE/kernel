7c662b22eb6ee49983ca071b6191547ef53ad159
Patrik Jakobsson <pjakobsson@suse.de>
2020-11-27 12:47:31 +0100

    drm/amd/powerplay: drop dead vce powergate code (jsc#SLE-12680, jsc#SLE-12880, jsc#SLE-12882, jsc#SLE-12883, jsc#SLE-13496, jsc#SLE-15322).
    suse-commit: 0c3491202122a8ab919a201cef49a7cb4d8f3b03

--- drivers/gpu/drm/amd/powerplay/amdgpu_smu.c
+++ drivers/gpu/drm/amd/powerplay/amdgpu_smu.c
@@ -462,9 +462,6 @@ int smu_dpm_set_power_gate(struct smu_context *smu, uint32_t block_type,
 			dev_err(smu->adev->dev, "Failed to power %s UVD!\n",
 				gate ? "gate" : "ungate");
 		break;
-	case AMD_IP_BLOCK_TYPE_VCE:
-		ret = smu_dpm_set_vce_enable(smu, !gate);
-		break;
 	case AMD_IP_BLOCK_TYPE_GFX:
 		ret = smu_gfx_off_control(smu, gate);
 		if (ret)
@@ -484,7 +481,8 @@ int smu_dpm_set_power_gate(struct smu_context *smu, uint32_t block_type,
 				gate ? "gate" : "ungate");
 		break;
 	default:
-		break;
+		dev_err(smu->adev->dev, "Unsupported block type!\n");
+		return -EINVAL;
 	}
 
 	return ret;
