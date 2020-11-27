75ffc0973082ba8e1af81899d059b3ca35816671
Lee, Chun-Yi <jlee@suse.com>
2020-11-27 22:04:52 +0800

    efi/libstub/x86: Permit cmdline data to be allocated above 4 GB (jsc#SLE-16407).
    suse-commit: bf86828e562837f070463030ed145706b6c0b073

--- drivers/firmware/efi/libstub/arm-stub.c
+++ drivers/firmware/efi/libstub/arm-stub.c
@@ -160,7 +160,7 @@ unsigned long efi_entry(void *handle, efi_system_table_t *sys_table_arg,
 	 * protocol. We are going to copy the command line into the
 	 * device tree, so this can be allocated anywhere.
 	 */
-	cmdline_ptr = efi_convert_cmdline(image, &cmdline_size);
+	cmdline_ptr = efi_convert_cmdline(image, &cmdline_size, ULONG_MAX);
 	if (!cmdline_ptr) {
 		pr_efi_err("getting command line via LOADED_IMAGE_PROTOCOL\n");
 		goto fail;
