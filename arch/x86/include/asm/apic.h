47ad79605c81d658b356e1dc12f9459eec39649d
Jiri Slaby <jslaby@suse.cz>
2020-12-02 11:01:03 +0100

    x86/msi: Move compose message callback where it belongs (jsc#SLE-16823).
    suse-commit: f1acfc2383975ff427e74f507fc0f693c3b14fb8

--- arch/x86/include/asm/apic.h
+++ arch/x86/include/asm/apic.h
@@ -550,6 +550,14 @@ static inline void exiting_ack_irq(void)
 	irq_exit();
 }
 
+struct msi_msg;
+
+#ifdef CONFIG_PCI_MSI
+void x86_vector_msi_compose_msg(struct irq_data *data, struct msi_msg *msg);
+#else
+# define x86_vector_msi_compose_msg NULL
+#endif
+
 extern void ioapic_zap_locks(void);
 
 #endif /* _ASM_X86_APIC_H */
