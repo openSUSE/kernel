574fdd4e68ca70ab1f6ccc893012c30d290f3cf7
Takashi Iwai <tiwai@suse.de>
2020-12-16 14:20:21 +0100

    spi: imx: fix reference leak in two imx operations (git-fixes).
    suse-commit: 76202c83771e044d7775e586f56e5e232554f431

--- drivers/spi/spi-imx.c
+++ drivers/spi/spi-imx.c
@@ -1536,6 +1536,7 @@ spi_imx_prepare_message(struct spi_master *master, struct spi_message *msg)
 
 	ret = pm_runtime_get_sync(spi_imx->dev);
 	if (ret < 0) {
+		pm_runtime_put_noidle(spi_imx->dev);
 		dev_err(spi_imx->dev, "failed to enable clock\n");
 		return ret;
 	}
@@ -1745,6 +1746,7 @@ static int spi_imx_remove(struct platform_device *pdev)
 
 	ret = pm_runtime_get_sync(spi_imx->dev);
 	if (ret < 0) {
+		pm_runtime_put_noidle(spi_imx->dev);
 		dev_err(spi_imx->dev, "failed to enable clock\n");
 		return ret;
 	}
