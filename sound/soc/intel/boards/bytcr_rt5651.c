80cd03fe09f3df58cdf1ad52cc5fb47631019bcd
Takashi Iwai <tiwai@suse.de>
2020-12-16 10:55:53 +0100

    ASoC: Intel: byt/cht: set pm ops dynamically (jsc#SLE-16518).
    suse-commit: 5bab29350bb888681c89e2ee9ba5ea466653eb6f

--- sound/soc/intel/boards/bytcr_rt5651.c
+++ sound/soc/intel/boards/bytcr_rt5651.c
@@ -1101,6 +1101,10 @@ static int snd_byt_rt5651_mc_probe(struct platform_device *pdev)
 		byt_rt5651_card.driver_name = DRIVER_NAME;
 	}
 
+	/* set pm ops */
+	if (sof_parent)
+		pdev->dev.driver->pm = &snd_soc_pm_ops;
+
 	ret_val = devm_snd_soc_register_card(&pdev->dev, &byt_rt5651_card);
 
 	if (ret_val) {
@@ -1115,9 +1119,6 @@ static int snd_byt_rt5651_mc_probe(struct platform_device *pdev)
 static struct platform_driver snd_byt_rt5651_mc_driver = {
 	.driver = {
 		.name = "bytcr_rt5651",
-#if IS_ENABLED(CONFIG_SND_SOC_SOF_BAYTRAIL)
-		.pm = &snd_soc_pm_ops,
-#endif
 	},
 	.probe = snd_byt_rt5651_mc_probe,
 };
