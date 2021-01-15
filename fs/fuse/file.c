d4918de4ddd640b81df9f99d2d8026746899ed9a
Luis Henriques <lhenriques@suse.de>
2021-01-15 10:26:45 +0000

    fuse: fix fuse_send_readpages() in the syncronous read case (jsc#SLE-13782).
    suse-commit: a5ee2f44aa2d88a305d17023607d60aebde87623

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -883,6 +883,7 @@ static void fuse_send_readpages(struct fuse_io_args *ia, struct file *file)
 	struct fuse_args_pages *ap = &ia->ap;
 	loff_t pos = page_offset(ap->pages[0]);
 	size_t count = ap->num_pages << PAGE_SHIFT;
+	ssize_t res;
 	int err;
 
 	ap->args.out_pages = true;
@@ -897,7 +898,8 @@ static void fuse_send_readpages(struct fuse_io_args *ia, struct file *file)
 		if (!err)
 			return;
 	} else {
-		err = fuse_simple_request(fc, &ap->args);
+		res = fuse_simple_request(fc, &ap->args);
+		err = res < 0 ? res : 0;
 	}
 	fuse_readpages_end(fc, &ap->args, err);
 }
