8d5e7703b7b6d731ba50e09819124ca90d3e088f
Michal Suchanek <msuchanek@suse.de>
2021-02-02 15:29:33 +0100

    powerpc/numa: remove timed_topology_update() (bsc#1181674 ltc#189159).
    suse-commit: 040ca348cb7fdcd65c8c35597ac3d0e60b0d2b80

--- arch/powerpc/mm/numa.c
+++ arch/powerpc/mm/numa.c
@@ -1149,14 +1149,6 @@ u64 memory_hotplug_max(void)
 static int topology_inited;
 
 /*
- * Change polling interval for associativity changes.
- */
-int timed_topology_update(int nsecs)
-{
-	return 0;
-}
-
-/*
  * Retrieve the new associativity information for a virtual processor's
  * home node.
  */
@@ -1182,7 +1174,6 @@ static long vphn_get_associativity(unsigned long cpu,
 		break;
 	case H_SUCCESS:
 		dbg("VPHN hcall succeeded. Reset polling...\n");
-		timed_topology_update(0);
 		break;
 	}
 
