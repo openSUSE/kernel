ba241f6ac5c1253f9309698cda163a4e7ebaa8a3
Thomas Zimmermann <tzimmermann@suse.de>
2021-02-17 09:56:02 +0100

    drm/meson: Unbind all connectors on module removal (bsc#1152472)
    suse-commit: b5e929bbb4173254a1944d44969ac56e8615c292

--- drivers/gpu/drm/meson/meson_drv.c
+++ drivers/gpu/drm/meson/meson_drv.c
@@ -349,9 +349,11 @@ static void meson_drv_unbind(struct device *dev)
 	}
 
 	drm_dev_unregister(drm);
-	drm_irq_uninstall(drm);
 	drm_kms_helper_poll_fini(drm);
 	drm_mode_config_cleanup(drm);
+	drm_atomic_helper_shutdown(drm);
+	component_unbind_all(dev, drm);
+	drm_irq_uninstall(drm);
 	drm_dev_put(drm);
 
 }
