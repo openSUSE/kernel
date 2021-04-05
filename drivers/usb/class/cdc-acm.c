e19d558668b80891f858885abd615891193fe5d9
Takashi Iwai <tiwai@suse.de>
2021-04-05 11:36:51 +0200

    USB: cdc-acm: fix double free on probe failure (git-fixes).
    suse-commit: d5ae2814e9ad9d742cb41a5e3507cca33ba0c65f

--- drivers/usb/class/cdc-acm.c
+++ drivers/usb/class/cdc-acm.c
@@ -1518,7 +1518,6 @@ alloc_fail6:
 				&dev_attr_wCountryCodes);
 		device_remove_file(&acm->control->dev,
 				&dev_attr_iCountryCodeRelDate);
-		kfree(acm->country_codes);
 	}
 	device_remove_file(&acm->control->dev, &dev_attr_bmCapabilities);
 alloc_fail5:
