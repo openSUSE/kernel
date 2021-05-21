d4cb7424a2afeb740fd05840619e26de413aed45
Daniel Wagner <dwagner@suse.de>
2021-05-21 14:12:47 +0200

    lpfc: Decouple port_template and vport_template (bsc#185032).
    suse-commit: 77503a84889ad8802ab47086dd1b08addf8d9a22

--- drivers/scsi/lpfc/lpfc.h
+++ drivers/scsi/lpfc/lpfc.h
@@ -1253,11 +1253,6 @@ struct lpfc_hba {
 #define LPFC_POLL_SLOWPATH	1	/* called from slowpath */
 
 	char os_host_name[MAXHOSTNAMELEN];
-
-	/* SCSI host template information - for physical port */
-	struct scsi_host_template port_template;
-	/* SCSI host template information - for all vports */
-	struct scsi_host_template vport_template;
 	atomic_t dbg_log_idx;
 	atomic_t dbg_log_cnt;
 	atomic_t dbg_log_dmping;
