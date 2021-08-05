e2e4a422fadd6b656e29802a5c41d432213a18e3
NeilBrown <neilb@suse.de>
2021-08-05 13:57:27 +1000

    NFSv4.1: Don't rebind to the same source port when (bnc#1186264 bnc#1189021)
    suse-commit: 4b89a4012683f512baa62581e3190b236fc87d92

--- fs/nfs/client.c
+++ fs/nfs/client.c
@@ -516,6 +516,8 @@ int nfs_create_rpc_client(struct nfs_client *clp,
 		args.flags |= RPC_CLNT_CREATE_NONPRIVPORT;
 	if (test_bit(NFS_CS_INFINITE_SLOTS, &clp->cl_flags))
 		args.flags |= RPC_CLNT_CREATE_INFINITE_SLOTS;
+	if (test_bit(NFS_CS_REUSEPORT, &clp->cl_flags))
+		args.flags |= RPC_CLNT_CREATE_REUSEPORT;
 
 	if (!IS_ERR(clp->cl_rpcclient))
 		return 0;
@@ -663,6 +665,7 @@ static int nfs_init_server(struct nfs_server *server,
 		.timeparms = &timeparms,
 		.cred = server->cred,
 		.nconnect = data->nfs_server.nconnect,
+		.init_flags = (1UL << NFS_CS_REUSEPORT),
 	};
 	struct nfs_client *clp;
 	int error;
