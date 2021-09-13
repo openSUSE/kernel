b66eb33ab865910f7d132a899cc7a7f4a1206eb2
Oliver Neukum <oneukum@suse.com>
2021-09-13 14:01:29 +0200

    usb: dwc2: Add missing cleanups when usb_add_gadget_udc() fails (git-fixes).
    suse-commit: bc5a06226a229c4d306b4dfe588c10ed31c260cd

--- drivers/usb/dwc2/platform.c
+++ drivers/usb/dwc2/platform.c
@@ -516,11 +516,15 @@ static int dwc2_driver_probe(struct platform_device *dev)
 		if (retval) {
 			hsotg->gadget.udc = NULL;
 			dwc2_hsotg_remove(hsotg);
-			goto error;
+			goto error_debugfs;
 		}
 	}
 #endif /* CONFIG_USB_DWC2_PERIPHERAL || CONFIG_USB_DWC2_DUAL_ROLE */
 	return 0;
+error_debugfs:
+	dwc2_debugfs_exit(hsotg);
+	if (hsotg->hcd_enabled)
+		dwc2_hcd_remove(hsotg);
 
 error:
 	if (hsotg->dr_mode != USB_DR_MODE_PERIPHERAL)
