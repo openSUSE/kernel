d34b152f3d583f8b3c5e0a1f6c439da05146b82e
Takashi Iwai <tiwai@suse.de>
2021-09-16 07:33:19 +0200

    ASoC: rt5682: Implement remove callback (git-fixes).
    suse-commit: 0038f077e8aa94e691a18317ea36bee5290a1a47

--- sound/soc/codecs/rt5682-i2c.c
+++ sound/soc/codecs/rt5682-i2c.c
@@ -275,6 +275,16 @@ static void rt5682_i2c_shutdown(struct i2c_client *client)
 	rt5682_reset(rt5682);
 }
 
+static int rt5682_i2c_remove(struct i2c_client *client)
+{
+	struct rt5682_priv *rt5682 = i2c_get_clientdata(client);
+
+	rt5682_i2c_shutdown(client);
+	regulator_bulk_disable(ARRAY_SIZE(rt5682->supplies), rt5682->supplies);
+
+	return 0;
+}
+
 static const struct of_device_id rt5682_of_match[] = {
 	{.compatible = "realtek,rt5682i"},
 	{},
@@ -300,6 +310,7 @@ static struct i2c_driver rt5682_i2c_driver = {
 		.acpi_match_table = rt5682_acpi_match,
 	},
 	.probe = rt5682_i2c_probe,
+	.remove = rt5682_i2c_remove,
 	.shutdown = rt5682_i2c_shutdown,
 	.id_table = rt5682_i2c_id,
 };
