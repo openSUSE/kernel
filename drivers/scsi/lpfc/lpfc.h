44fdfbe45a2603bce76f79f32099c9a9f2b65fdd
Daniel Wagner <dwagner@suse.de>
2021-09-20 14:46:44 +0200

    scsi: lpfc: Add cmf_info sysfs entry (bsc#1190576).
    suse-commit: 8ba8b7b8181df0c98805c891a4614bd773e32af9

--- drivers/scsi/lpfc/lpfc.h
+++ drivers/scsi/lpfc/lpfc.h
@@ -1595,6 +1595,7 @@ struct lpfc_hba {
 };
 
 #define LPFC_MAX_RXMONITOR_ENTRY	800
+#define LPFC_MAX_RXMONITOR_DUMP		32
 struct rxtable_entry {
 	uint64_t total_bytes;   /* Total no of read bytes requested */
 	uint64_t rcv_bytes;     /* Total no of read bytes completed */
