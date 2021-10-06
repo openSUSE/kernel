cbb134c864d3dbdb4cc40931a1b51f8dfa36a311
Takashi Iwai <tiwai@suse.de>
2021-10-06 14:07:57 +0200

    dmaengine: idxd: remove bus shutdown (jsc#SLE-18899).
    suse-commit: f31b4c72e19e90bb6e82324e282fe83a49fd9a29

--- drivers/dma/idxd/sysfs.c
+++ drivers/dma/idxd/sysfs.c
@@ -124,17 +124,11 @@ static void idxd_config_bus_remove(struct device *dev)
 	}
 }
 
-static void idxd_config_bus_shutdown(struct device *dev)
-{
-	dev_dbg(dev, "%s called\n", __func__);
-}
-
 struct bus_type dsa_bus_type = {
 	.name = "dsa",
 	.match = idxd_config_bus_match,
 	.probe = idxd_config_bus_probe,
 	.remove = idxd_config_bus_remove,
-	.shutdown = idxd_config_bus_shutdown,
 };
 
 static struct idxd_device_driver dsa_drv = {
