d07b3985ace8e6bc4f32f2108734629b1e8d52a7
Daniel Wagner <dwagner@suse.de>
2021-10-14 10:57:56 +0200

    scsi: qla2xxx: Changes to support kdump kernel for NVMe BFS (bsc#1190941).
    suse-commit: d05a9cf8ca21037214ce66104cb0c1037dddc8e5

--- drivers/scsi/qla2xxx/qla_def.h
+++ drivers/scsi/qla2xxx/qla_def.h
@@ -4023,7 +4023,6 @@ struct qla_hw_data {
 				/* Enabled in Driver */
 		uint32_t	scm_enabled:1;
 		uint32_t	edif_enabled:1;
-		uint32_t	max_req_queue_warned:1;
 		uint32_t	plogi_template_valid:1;
 		uint32_t	port_isolated:1;
 	} flags;
