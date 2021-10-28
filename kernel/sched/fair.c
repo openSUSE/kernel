ba0846a84418e82bd8363a18595966af27821768
Mel Gorman <mgorman@suse.de>
2021-10-28 14:33:06 +0100

    sched/fair: Adjust the allowed NUMA imbalance when SD_NUMA spans multiple LLCS (bsc#1192120).
    suse-commit: c881665b9893b66f8d54666c56f3813d291bce21

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1565,7 +1565,7 @@ struct task_numa_env {
 static unsigned long cpu_load(struct rq *rq);
 static unsigned long cpu_runnable(struct rq *rq);
 static unsigned long cpu_util(int cpu);
-static inline long adjust_numa_imbalance(int imbalance,
+static inline long adjust_numa_imbalance(int imbalance, int dst_cpu,
 					int dst_running, int dst_weight);
 
 static inline enum
@@ -1946,8 +1946,8 @@ static void task_numa_find_cpu(struct task_numa_env *env,
 		src_running = env->src_stats.nr_running - 1;
 		dst_running = env->dst_stats.nr_running + 1;
 		imbalance = max(0, dst_running - src_running);
-		imbalance = adjust_numa_imbalance(imbalance, dst_running,
-							env->dst_stats.weight);
+		imbalance = adjust_numa_imbalance(imbalance, env->dst_cpu,
+					dst_running, env->dst_stats.weight);
 
 		/* Use idle CPU if there is no imbalance */
 		if (!imbalance) {
@@ -9120,9 +9120,13 @@ static bool update_pick_idlest(struct sched_group *idlest,
  * This is an approximation as the number of running tasks may not be
  * related to the number of busy CPUs due to sched_setaffinity.
  */
-static inline bool allow_numa_imbalance(int dst_running, int dst_weight)
+static inline bool
+allow_numa_imbalance(int dst_cpu, int dst_running, int dst_weight)
 {
-	return (dst_running < (dst_weight >> 2));
+	/* Allowed NUMA imbalance */
+	dst_weight >>= per_cpu(sd_numaimb_shift, dst_cpu);
+
+	return dst_running < dst_weight;
 }
 
 /*
@@ -9242,8 +9246,9 @@ find_idlest_group(struct sched_domain *sd, struct task_struct *p, int this_cpu)
 
 	case group_has_spare:
 		if (sd->flags & SD_NUMA) {
+			int idlest_cpu = cpumask_first(sched_group_span(idlest));
+
 #ifdef CONFIG_NUMA_BALANCING
-			int idlest_cpu;
 			/*
 			 * If there is spare capacity at NUMA, try to select
 			 * the preferred node
@@ -9251,7 +9256,6 @@ find_idlest_group(struct sched_domain *sd, struct task_struct *p, int this_cpu)
 			if (cpu_to_node(this_cpu) == p->numa_preferred_nid)
 				return NULL;
 
-			idlest_cpu = cpumask_first(sched_group_span(idlest));
 			if (cpu_to_node(idlest_cpu) == p->numa_preferred_nid)
 				return idlest;
 #endif
@@ -9261,8 +9265,10 @@ find_idlest_group(struct sched_domain *sd, struct task_struct *p, int this_cpu)
 			 * a real need of migration, periodic load balance will
 			 * take care of it.
 			 */
-			if (allow_numa_imbalance(local_sgs.sum_nr_running, sd->span_weight))
+			if (allow_numa_imbalance(idlest_cpu,
+			    local_sgs.sum_nr_running, sd->span_weight)) {
 				return NULL;
+			}
 		}
 
 		/*
@@ -9352,10 +9358,10 @@ next_group:
 
 #define NUMA_IMBALANCE_MIN 2
 
-static inline long adjust_numa_imbalance(int imbalance,
+static inline long adjust_numa_imbalance(int imbalance, int dst_cpu,
 				int dst_running, int dst_weight)
 {
-	if (!allow_numa_imbalance(dst_running, dst_weight))
+	if (!allow_numa_imbalance(dst_cpu, dst_running, dst_weight))
 		return imbalance;
 
 	/*
@@ -9467,6 +9473,7 @@ static inline void calculate_imbalance(struct lb_env *env, struct sd_lb_stats *s
 		/* Consider allowing a small imbalance between NUMA groups */
 		if (env->sd->flags & SD_NUMA) {
 			env->imbalance = adjust_numa_imbalance(env->imbalance,
+				env->src_cpu,
 				busiest->sum_nr_running, busiest->group_weight);
 		}
 
