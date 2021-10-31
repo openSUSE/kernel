446435a8c839efbaa88443e8bfe83dd8fd1c0759
Michal Kubecek <mkubecek@suse.cz>
2021-11-01 00:42:15 +0100

    sctp: check asoc peer.asconf_capable before processing asconf (bsc#1190351).
    suse-commit: b351be4153cefb7b12910839eb77e25a84c509f7

--- net/sctp/sm_statefuns.c
+++ net/sctp/sm_statefuns.c
@@ -3735,7 +3735,8 @@ enum sctp_disposition sctp_sf_do_asconf(struct net *net,
 	 * is received unauthenticated it MUST be silently discarded as
 	 * described in [I-D.ietf-tsvwg-sctp-auth].
 	 */
-	if (!net->sctp.addip_noauth && !chunk->auth)
+	if (!asoc->peer.asconf_capable ||
+	    (!net->sctp.addip_noauth && !chunk->auth))
 		return sctp_sf_discard_chunk(net, ep, asoc, type, arg,
 					     commands);
 
@@ -3877,7 +3878,8 @@ enum sctp_disposition sctp_sf_do_asconf_ack(struct net *net,
 	 * is received unauthenticated it MUST be silently discarded as
 	 * described in [I-D.ietf-tsvwg-sctp-auth].
 	 */
-	if (!net->sctp.addip_noauth && !asconf_ack->auth)
+	if (!asoc->peer.asconf_capable ||
+	    (!net->sctp.addip_noauth && !asconf_ack->auth))
 		return sctp_sf_discard_chunk(net, ep, asoc, type, arg,
 					     commands);
 
