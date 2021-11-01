cc2054994f194f7f499e97d0724e8a5410628dec
Joerg Roedel <jroedel@suse.de>
2021-11-01 12:15:20 +0100

    iommu/arm-smmu: Fix race condition during iommu_group creation (bsc#1192202).
    suse-commit: 0eb5d6d1fa7b9995d28bea4d24637e79dc435ca5

--- drivers/iommu/arm/arm-smmu/arm-smmu.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu.c
@@ -1477,6 +1477,7 @@ static struct iommu_group *arm_smmu_device_group(struct device *dev)
 	struct iommu_group *group = NULL;
 	int i, idx;
 
+	mutex_lock(&smmu->stream_map_mutex);
 	for_each_cfg_sme(cfg, fwspec, i, idx) {
 		if (group && smmu->s2crs[idx].group &&
 		    group != smmu->s2crs[idx].group)
@@ -1485,8 +1486,10 @@ static struct iommu_group *arm_smmu_device_group(struct device *dev)
 		group = smmu->s2crs[idx].group;
 	}
 
-	if (group)
+	if (group) {
+		mutex_unlock(&smmu->stream_map_mutex);
 		return iommu_group_ref_get(group);
+	}
 
 	if (dev_is_pci(dev))
 		group = pci_device_group(dev);
@@ -1500,6 +1503,7 @@ static struct iommu_group *arm_smmu_device_group(struct device *dev)
 		for_each_cfg_sme(cfg, fwspec, i, idx)
 			smmu->s2crs[idx].group = group;
 
+	mutex_unlock(&smmu->stream_map_mutex);
 	return group;
 }
 
