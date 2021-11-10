aa42b6939260ba1672d96b6377021a4705808ace
Oliver Neukum <oneukum@suse.com>
2021-11-10 13:37:21 +0100

    r8152: disable U2P3 for RTL8153B (git-fixes).
    suse-commit: d6c58f704b554636f2fc68440e34b8b280e4a75b

--- drivers/net/usb/r8152.c
+++ drivers/net/usb/r8152.c
@@ -3049,7 +3049,6 @@ static void rtl8153b_runtime_enable(struct r8152 *tp, bool enable)
 		r8153b_ups_en(tp, false);
 		r8153_queue_wake(tp, false);
 		rtl_runtime_suspend_enable(tp, false);
-		r8153_u2p3en(tp, true);
 		r8153b_u1u2en(tp, true);
 	}
 }
@@ -3606,7 +3605,6 @@ static void r8153b_hw_phy_cfg(struct r8152 *tp)
 
 	r8153b_aldps_en(tp, true);
 	r8153b_enable_fc(tp);
-	r8153_u2p3en(tp, true);
 
 	set_bit(PHY_RESET, &tp->flags);
 }
@@ -3972,7 +3970,6 @@ static void rtl8153b_up(struct r8152 *tp)
 	ocp_write_dword(tp, MCU_TYPE_USB, USB_RX_BUF_TH, RX_THR_B);
 
 	r8153b_aldps_en(tp, true);
-	r8153_u2p3en(tp, true);
 	r8153b_u1u2en(tp, true);
 }
 
