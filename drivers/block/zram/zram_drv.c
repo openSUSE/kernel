c39cc5ff6a4847fe07e4bade20fece0225d84efb
Luis Chamberlain <mcgrof@suse.com>
2021-11-16 14:47:52 -0800

    patches.suse/zram-replace-fsync_bdev-with-sync_blockdev.patch: (bsc#1170269).
    suse-commit: 75a41c204f7aa00214cfd425b9ac9d6f35c83f8b

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1785,7 +1785,7 @@ static ssize_t reset_store(struct device *dev,
 	mutex_unlock(&bdev->bd_mutex);
 
 	/* Make sure all the pending I/O are finished */
-	fsync_bdev(bdev);
+	sync_blockdev(bdev);
 	zram_reset_device(zram);
 	revalidate_disk(zram->disk);
 	bdput(bdev);
@@ -1999,7 +1999,7 @@ static int zram_remove(struct zram *zram)
 		;
 	} else {
 		/* Make sure all the pending I/O are finished */
-		fsync_bdev(bdev);
+		sync_blockdev(bdev);
 		zram_reset_device(zram);
 		bdput(bdev);
 	}
