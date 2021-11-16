1d45dcc7d9a0a410d52bb27fff75e2b5be747f09
Takashi Iwai <tiwai@suse.de>
2021-11-16 14:49:04 +0100

    ALSA: hda: Free card instance properly at probe errors (git-fixes).
    suse-commit: 64070649182b029d2c52a2ca7bee9ba58195a09f

--- sound/pci/hda/hda_intel.c
+++ sound/pci/hda/hda_intel.c
@@ -2395,7 +2395,8 @@ static int azx_probe_continue(struct azx *chip)
 
 out_free:
 	if (err < 0) {
-		azx_free(chip);
+		pci_set_drvdata(pci, NULL);
+		snd_card_free(chip->card);
 		return err;
 	}
 
