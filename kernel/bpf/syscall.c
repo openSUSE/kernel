d4d9eeab1f441eb90e86c4282867342d789d7a3a
Shung-Hsi Yu <shung-hsi.yu@suse.com>
2021-11-17 20:53:47 +0800

    - bpf: Add kconfig knob for disabling unpriv bpf   by default (jsc#SLE-22573) - Update config files: Add   CONFIG_BPF_UNPRIV_DEFAULT_OFF is not set
    suse-commit: ee96d3995b5ee2a581b9dc0e80f3da62c5026314

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -41,7 +41,8 @@ static DEFINE_SPINLOCK(prog_idr_lock);
 static DEFINE_IDR(map_idr);
 static DEFINE_SPINLOCK(map_idr_lock);
 
-int sysctl_unprivileged_bpf_disabled __read_mostly;
+int sysctl_unprivileged_bpf_disabled __read_mostly =
+	IS_BUILTIN(CONFIG_BPF_UNPRIV_DEFAULT_OFF) ? 2 : 0;
 
 static const struct bpf_map_ops * const bpf_map_types[] = {
 #define BPF_PROG_TYPE(_id, _name, prog_ctx_type, kern_ctx_type)
