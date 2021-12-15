873e19b12fdfc424d0f6b6e2420345ffa93244ce
Juergen Gross <jgross@suse.com>
2021-12-15 15:55:13 +0100

    xen/pvh: add missing prototype to header (git-fixes).
    suse-commit: 3762eb94ff770fa93d9c3a2d0178f0ff3d7b8666

--- arch/x86/include/asm/xen/hypervisor.h
+++ arch/x86/include/asm/xen/hypervisor.h
@@ -66,6 +66,7 @@ extern void xen_set_iopl_mask(unsigned mask);
 
 #ifdef CONFIG_PVH
 void __init xen_pvh_init(struct boot_params *boot_params);
+void __init mem_map_via_hcall(struct boot_params *boot_params_p);
 #endif
 
 #endif /* _ASM_X86_XEN_HYPERVISOR_H */
