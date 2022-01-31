0b99d309f1d0a36346c3f529955a52da1b87a38c
Nicolai Stange <nstange@suse.de>
2022-01-31 16:28:57 +0100

    crypto: testmgr - disallow plain ghash in FIPS mode (jsc#SLE-21132,bsc#1194777).
    suse-commit: a8532d3db015b88d4d8a649e6c778313895e8c74

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -4960,7 +4960,6 @@ static const struct alg_test_desc alg_test_descs[] = {
 	}, {
 		.alg = "ghash",
 		.test = alg_test_hash,
-		.fips_allowed = 1,
 		.suite = {
 			.hash = __VECS(ghash_tv_template)
 		}
