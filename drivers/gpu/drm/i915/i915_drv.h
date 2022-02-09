899039ebc4a971d5a055f736cc02255813199059
Takashi Iwai <tiwai@suse.de>
2022-02-09 16:30:48 +0100

    drm/i915: Nuke not needed members of dram_info (bsc#1195211).
    suse-commit: 39709c52bb199abd7d081fa04246ebf8948500b1

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -1142,11 +1142,8 @@ struct drm_i915_private {
 	} wm;
 
 	struct dram_info {
-		bool valid;
 		bool is_16gb_dimm;
 		u8 num_channels;
-		u8 ranks;
-		u32 bandwidth_kbps;
 		bool symmetric_memory;
 		enum intel_dram_type {
 			INTEL_DRAM_UNKNOWN,
