332d4f3ab7a0fd9accde0c36f5e6927a411eda97
Mel Gorman <mgorman@suse.de>
2022-03-23 11:00:16 +0000

    net: dev: Remove preempt_disable() and get_cpu() in netif_rx_internal() (SLE Realtime Extension).
    suse-commit: 1527fc855b92d776ef48588afd65cb98a102ad58

--- net/core/dev.c
+++ net/core/dev.c
@@ -4957,7 +4957,6 @@ static int netif_rx_internal(struct sk_buff *skb)
 		struct rps_dev_flow voidflow, *rflow = &voidflow;
 		int cpu;
 
-		preempt_disable();
 		rcu_read_lock();
 
 		cpu = get_rps_cpu(skb->dev, skb, &rflow);
@@ -4967,14 +4966,12 @@ static int netif_rx_internal(struct sk_buff *skb)
 		ret = enqueue_to_backlog(skb, cpu, &rflow->last_qtail);
 
 		rcu_read_unlock();
-		preempt_enable();
 	} else
 #endif
 	{
 		unsigned int qtail;
 
-		ret = enqueue_to_backlog(skb, get_cpu(), &qtail);
-		put_cpu();
+		ret = enqueue_to_backlog(skb, smp_processor_id(), &qtail);
 	}
 	return ret;
 }
