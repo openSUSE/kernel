7edfbff46e155dc0c35eb66508f2c1edd91ff5d3
Michal Hocko <mhocko@suse.com>
2022-04-01 14:35:07 +0200

    mm/page_alloc.c: do not warn allocation failure on zone DMA if no managed pages (bsc#1197501).
    suse-commit: c0f79a1c6c51ab9d874de434097c32c0bb741c28

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3838,7 +3838,9 @@ void warn_alloc(gfp_t gfp_mask, nodemask_t *nodemask, const char *fmt, ...)
 	static DEFINE_RATELIMIT_STATE(nopage_rs, DEFAULT_RATELIMIT_INTERVAL,
 				      DEFAULT_RATELIMIT_BURST);
 
-	if ((gfp_mask & __GFP_NOWARN) || !__ratelimit(&nopage_rs))
+	if ((gfp_mask & __GFP_NOWARN) ||
+	     !__ratelimit(&nopage_rs) ||
+	     ((gfp_mask & __GFP_DMA) && !has_managed_dma()))
 		return;
 
 	va_start(args, fmt);
