ecf6c5df87b3b6c93b60c1eceeb1cf28e7395b85
Lee Duncan <lduncan@suse.com>
2022-04-19 11:18:18 -0700

    SCSI: iscsi: fix iscsi_cls_conn changes (bsc#1197685).
    suse-commit: fada9f025facaa1d66a37dc504dd1a05591b10a2

--- include/scsi/scsi_transport_iscsi.h
+++ include/scsi/scsi_transport_iscsi.h
@@ -213,13 +213,15 @@ struct iscsi_cls_conn {
 	struct mutex ep_mutex;
 	struct iscsi_endpoint *ep;
 
-	/* Used when accessing flags and queueing work. */
-	spinlock_t lock;
 	unsigned long flags;
 	struct work_struct cleanup_work;
 
 	struct device dev;		/* sysfs transport/container device */
 	enum iscsi_connection_state state;
+#ifndef __GENKSYMS__
+	/* Used when accessing flags and queueing work. */
+	spinlock_t lock;
+#endif
 };
 
 #define iscsi_dev_to_conn(_dev) \
