c32ffd9cbaa691642ee14ff62355c0b20f3db0af
NeilBrown <neilb@suse.de>
2022-04-28 13:59:53 +1000

    SUNRPC: Handle ENOMEM in call_transmit_status() (git-fixes).
    suse-commit: 81dfb9e8381c42336ee927ca07d90816f02a0ac4

--- net/sunrpc/clnt.c
+++ net/sunrpc/clnt.c
@@ -2205,6 +2205,7 @@ call_transmit_status(struct rpc_task *task)
 		 * socket just returned a connection error,
 		 * then hold onto the transport lock.
 		 */
+	case -ENOMEM:
 	case -ENOBUFS:
 		rpc_delay(task, HZ>>2);
 		fallthrough;
@@ -2288,6 +2289,7 @@ call_bc_transmit_status(struct rpc_task *task)
 	case -ENOTCONN:
 	case -EPIPE:
 		break;
+	case -ENOMEM:
 	case -ENOBUFS:
 		rpc_delay(task, HZ>>2);
 		fallthrough;
