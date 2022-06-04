0f925260e2da6b66c12555108c29960389ef4bd8
Takashi Iwai <tiwai@suse.de>
2022-06-04 09:44:43 +0200

    driver core: fix deadlock in __device_attach (git-fixes).
    suse-commit: e63964302988ee28877e17dea23b8be09ab5f141

--- drivers/base/dd.c
+++ drivers/base/dd.c
@@ -947,6 +947,7 @@ out_unlock:
 static int __device_attach(struct device *dev, bool allow_async)
 {
 	int ret = 0;
+	bool async = false;
 
 	device_lock(dev);
 	if (dev->p->dead) {
@@ -985,7 +986,7 @@ static int __device_attach(struct device *dev, bool allow_async)
 			 */
 			dev_dbg(dev, "scheduling asynchronous probe\n");
 			get_device(dev);
-			async_schedule_dev(__device_attach_async_helper, dev);
+			async = true;
 		} else {
 			pm_request_idle(dev);
 		}
@@ -995,6 +996,8 @@ static int __device_attach(struct device *dev, bool allow_async)
 	}
 out_unlock:
 	device_unlock(dev);
+	if (async)
+		async_schedule_dev(__device_attach_async_helper, dev);
 	return ret;
 }
 
