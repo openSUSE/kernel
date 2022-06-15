77a80a579d8ddbf7d0fe50b4a873edbcf8d58ed7
Takashi Iwai <tiwai@suse.de>
2022-06-15 10:54:07 +0200

    usb: dwc2: gadget: don't reset gadget's driver->bus (git-fixes).
    suse-commit: 08d277fc98c4b041e9dc23bea19db1783424505a

--- drivers/usb/dwc2/gadget.c
+++ drivers/usb/dwc2/gadget.c
@@ -4520,7 +4520,6 @@ static int dwc2_hsotg_udc_start(struct usb_gadget *gadget,
 
 	WARN_ON(hsotg->driver);
 
-	driver->driver.bus = NULL;
 	hsotg->driver = driver;
 	hsotg->gadget.dev.of_node = hsotg->dev->of_node;
 	hsotg->gadget.speed = USB_SPEED_UNKNOWN;
