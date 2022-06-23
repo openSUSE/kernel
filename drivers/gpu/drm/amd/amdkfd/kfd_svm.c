b7babd1aaeba1f60bfb8466abf614ba59dbc26ab
Takashi Iwai <tiwai@suse.de>
2022-06-23 07:39:11 +0200

    drm/amdkfd: Use mmget_not_zero in MMU notifier (git-fixes).
    suse-commit: e7d29e50494bcbb61c0867c80c7f9fd311dcaac1

--- drivers/gpu/drm/amd/amdkfd/kfd_svm.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_svm.c
@@ -2164,6 +2164,8 @@ svm_range_cpu_invalidate_pagetables(struct mmu_interval_notifier *mni,
 
 	if (range->event == MMU_NOTIFY_RELEASE)
 		return true;
+	if (!mmget_not_zero(mni->mm))
+		return true;
 
 	start = mni->interval_tree.start;
 	last = mni->interval_tree.last;
@@ -2190,6 +2192,7 @@ svm_range_cpu_invalidate_pagetables(struct mmu_interval_notifier *mni,
 	}
 
 	svm_range_unlock(prange);
+	mmput(mni->mm);
 
 	return true;
 }
