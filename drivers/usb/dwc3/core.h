fa11ac1591ab2ca589784fdc6dda62555f8fcb71
Oliver Neukum <oneukum@suse.com>
2022-06-23 13:31:09 +0200

    usb: dwc3: gadget: Wait for ep0 xfers to complete during dequeue (git-fixes).
    suse-commit: ab00b5f6a60560b4208b1d2bc8f16e5846960c84

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -723,6 +723,7 @@ struct dwc3_ep {
 #define DWC3_EP_FORCE_RESTART_STREAM	BIT(9)
 #define DWC3_EP_FIRST_STREAM_PRIMED	BIT(10)
 #define DWC3_EP_PENDING_CLEAR_STALL	BIT(11)
+#define DWC3_EP_DELAY_STOP		BIT(13)
 
 	/* This last one is specific to EP0 */
 #define DWC3_EP0_DIR_IN		BIT(31)
