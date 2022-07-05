87efcb60c05f7a684d5d27650dab331cdbf314a2
Daniel Wagner <dwagner@suse.de>
2022-07-05 15:17:48 +0200

    blk-mq: drop workarounds for cpu hotplug queue management (bsc#1185762)
    This patches never made it to mainline. Instead a simpler solution was
    added upstream 14dc7a18abbe ("block: Fix handling of offline queues in
    blk_mq_alloc_request_hctx().
    
    suse-commit: a4e127658d79ec6b0df224ee072f634e0cd42cd9

--- block/blk-mq-virtio.c
+++ block/blk-mq-virtio.c
@@ -7,9 +7,6 @@
 #include <linux/blk-mq-virtio.h>
 #include <linux/virtio_config.h>
 #include <linux/module.h>
-#ifndef __GENKSYMS__
-#include <linux/msi.h>
-#endif
 #include "blk-mq.h"
 
 /**
@@ -41,7 +38,6 @@ int blk_mq_virtio_map_queues(struct blk_mq_queue_map *qmap,
 		for_each_cpu(cpu, mask)
 			qmap->mq_map[cpu] = qmap->queue_offset + queue;
 	}
-	qmap->use_managed_irq = device_has_managed_msi_irq(&vdev->dev);
 
 	return 0;
 fallback:
