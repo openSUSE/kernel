da9205196d853434ba04581fb6e2bcb9b5f4b249
Lee Duncan <lduncan@suse.com>
2022-07-16 09:07:41 -0700

    scsi: core: sd: Add silence_suspend flag to suppress some PM messages (git-fixes).
    suse-commit: 0d76c7d290418e617f3377d2a4ad102a643756df

--- include/scsi/scsi_device.h
+++ include/scsi/scsi_device.h
@@ -208,6 +208,7 @@ struct scsi_device {
 	unsigned unmap_limit_for_ws:1;	/* Use the UNMAP limit for WRITE SAME */
 	unsigned rpm_autosuspend:1;	/* Enable runtime autosuspend at device
 					 * creation time */
+	unsigned silence_suspend:1;	/* Do not print runtime PM related messages */
 
 	bool offline_already;		/* Device offline message logged */
 
