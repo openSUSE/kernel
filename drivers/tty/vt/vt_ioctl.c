1e014baf5aa413160306f1419808831583f442b2
Jiri Slaby <jslaby@suse.cz>
2022-07-21 12:44:09 +0200

    vt: vt_ioctl: fix race in VT_RESIZEX (bsc#1200910 CVE-2020-36558).
    suse-commit: 67d5da3eb464e28f168fc997944ef82e45162bcf

--- drivers/tty/vt/vt_ioctl.c
+++ drivers/tty/vt/vt_ioctl.c
@@ -1177,15 +1177,20 @@ int vt_ioctl(struct tty_struct *tty,
 		}
 		    
 		for (i = 0; i < MAX_NR_CONSOLES; i++) {
+			struct vc_data *vcp;
+
 			if (!vc_cons[i].d)
 				continue;
 			console_lock();
-			if (vlin)
-				vc_cons[i].d->vc_scan_lines = vlin;
-			if (clin)
-				vc_cons[i].d->vc_font.height = clin;
-			vc_cons[i].d->vc_resize_user = 1;
-			vc_resize(vc_cons[i].d, cc, ll);
+			vcp = vc_cons[i].d;
+			if (vcp) {
+				if (vlin)
+					vcp->vc_scan_lines = vlin;
+				if (clin)
+					vcp->vc_font.height = clin;
+				vcp->vc_resize_user = 1;
+				vc_resize(vcp, cc, ll);
+			}
 			console_unlock();
 		}
 		break;
