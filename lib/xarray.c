5e6dfbf92b2d7e7f0c5cd296e33d655d9ee8fc97
Takashi Iwai <tiwai@suse.de>
2022-07-29 13:36:32 +0200

    XArray: Update the LRU list in xas_split() (git-fixes).
    suse-commit: 6cdc8e9038eaec35eb6abc07f1a6f2346d77f827

--- lib/xarray.c
+++ lib/xarray.c
@@ -1079,6 +1079,7 @@ void xas_split(struct xa_state *xas, void *entry, unsigned int order)
 					xa_mk_node(child));
 			if (xa_is_value(curr))
 				values--;
+			xas_update(xas, child);
 		} else {
 			unsigned int canon = offset - xas->xa_sibs;
 
@@ -1093,6 +1094,7 @@ void xas_split(struct xa_state *xas, void *entry, unsigned int order)
 	} while (offset-- > xas->xa_offset);
 
 	node->nr_values += values;
+	xas_update(xas, node);
 }
 EXPORT_SYMBOL_GPL(xas_split);
 #endif
