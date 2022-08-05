88c9b19ea0f1086d6f4b0adbdeee79825afaaae3
Takashi Iwai <tiwai@suse.de>
2022-08-05 13:15:17 +0200

    iio: core: Fix IIO_ALIGN and rename as it was not sufficiently large (git-fixes).
    suse-commit: 13d38b288d9ab04f0ddec3a2c02fa5eaa06cc1e7

--- include/linux/iio/iio.h
+++ include/linux/iio/iio.h
@@ -9,6 +9,7 @@
 
 #include <linux/device.h>
 #include <linux/cdev.h>
+#include <linux/slab.h>
 #include <linux/iio/types.h>
 #include <linux/of.h>
 /* IIO TODO LIST */
@@ -674,19 +675,24 @@ static inline void *iio_device_get_drvdata(struct iio_dev *indio_dev)
 	return dev_get_drvdata(&indio_dev->dev);
 }
 
-/* Can we make this smaller? */
-#define IIO_ALIGN L1_CACHE_BYTES
+/*
+ * Used to ensure the iio_priv() structure is aligned to allow that structure
+ * to in turn include IIO_DMA_MINALIGN'd elements such as buffers which
+ * must not share  cachelines with the rest of the structure, thus making
+ * them safe for use with non-coherent DMA.
+ */
+#define IIO_DMA_MINALIGN ARCH_KMALLOC_MINALIGN
 struct iio_dev *iio_device_alloc(int sizeof_priv);
 
 static inline void *iio_priv(const struct iio_dev *indio_dev)
 {
-	return (char *)indio_dev + ALIGN(sizeof(struct iio_dev), IIO_ALIGN);
+	return (char *)indio_dev + ALIGN(sizeof(struct iio_dev), IIO_DMA_MINALIGN);
 }
 
 static inline struct iio_dev *iio_priv_to_dev(void *priv)
 {
 	return (struct iio_dev *)((char *)priv -
-				  ALIGN(sizeof(struct iio_dev), IIO_ALIGN));
+				  ALIGN(sizeof(struct iio_dev), IIO_DMA_MINALIGN));
 }
 
 void iio_device_free(struct iio_dev *indio_dev);
