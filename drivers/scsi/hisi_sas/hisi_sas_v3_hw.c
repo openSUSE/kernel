7fb57530415cbe63c82c016cd3a150f86775e62a
Lee Duncan <lduncan@suse.com>
2022-08-18 08:22:17 -0700

    scsi: hisi_sas: Keep controller active between ISR of phyup and the event being processed (bsc#1202471).
    suse-commit: cb111f7ffc692ea347887eeb6d681a1598c3e6ae

--- drivers/scsi/hisi_sas/hisi_sas_v3_hw.c
+++ drivers/scsi/hisi_sas/hisi_sas_v3_hw.c
@@ -1559,7 +1559,11 @@ static irqreturn_t phy_up_v3_hw(int phy_no, struct hisi_hba *hisi_hba)
 
 	phy->port_id = port_id;
 	phy->phy_attached = 1;
-	hisi_sas_notify_phy_event(phy, HISI_PHYE_PHY_UP);
+
+	/* Call pm_runtime_put_sync() with pairs in hisi_sas_phyup_pm_work() */
+	pm_runtime_get_noresume(dev);
+	hisi_sas_notify_phy_event(phy, HISI_PHYE_PHY_UP_PM);
+
 	res = IRQ_HANDLED;
 end:
 	if (phy->reset_completion)
