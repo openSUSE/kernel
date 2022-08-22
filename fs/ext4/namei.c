3cefc0ad9e39f9819ac616e386329586e2271cbd
Jan Kara <jack@suse.cz>
2022-08-22 13:12:22 +0200

    ext4: check if directory block is within i_size (bsc#1198577 CVE-2022-1184).
    suse-commit: d289dcd54645ff44223982ba3099e5ac817884c2

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -109,6 +109,13 @@ static struct buffer_head *__ext4_read_dirblock(struct inode *inode,
 	struct ext4_dir_entry *dirent;
 	int is_dx_block = 0;
 
+	if (block >= inode->i_size) {
+		ext4_error_inode(inode, func, line, block,
+		       "Attempting to read directory block (%u) that is past i_size (%llu)",
+		       block, inode->i_size);
+		return ERR_PTR(-EFSCORRUPTED);
+	}
+
 	bh = ext4_bread(NULL, inode, block, 0);
 	if (IS_ERR(bh)) {
 		__ext4_warning(inode->i_sb, func, line,
