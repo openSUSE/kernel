6a284a2c5637caed9114d3940756e7c98bd10a8c
Michal Suchanek <msuchanek@suse.de>
2022-08-23 16:27:59 +0200

    KVM: PPC: Book3s HV: Remove unused function kvmppc_bad_interrupt (bsc#1194869).
    suse-commit: fad23feae78189269f6f4cf0a901e4b34a0709fe

--- arch/powerpc/kvm/book3s_hv_builtin.c
+++ arch/powerpc/kvm/book3s_hv_builtin.c
@@ -555,24 +555,6 @@ int kvmppc_rm_h_eoi(struct kvm_vcpu *vcpu, unsigned long xirr)
 }
 #endif /* CONFIG_KVM_XICS */
 
-void kvmppc_bad_interrupt(struct pt_regs *regs)
-{
-	/*
-	 * 100 could happen at any time, 200 can happen due to invalid real
-	 * address access for example (or any time due to a hardware problem).
-	 */
-	if (TRAP(regs) == 0x100) {
-		get_paca()->in_nmi++;
-		system_reset_exception(regs);
-		get_paca()->in_nmi--;
-	} else if (TRAP(regs) == 0x200) {
-		machine_check_exception(regs);
-	} else {
-		die("Bad interrupt in KVM entry/exit code", regs, SIGABRT);
-	}
-	panic("Bad KVM trap");
-}
-
 static void kvmppc_end_cede(struct kvm_vcpu *vcpu)
 {
 	vcpu->arch.ceded = 0;
