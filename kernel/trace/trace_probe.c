6f561560237b39d665fabf17c981a73bbbc45cf3
Petr Pavlu <petr.pavlu@suse.com>
2022-08-26 13:44:25 +0200

    tracing/probes: Have kprobes and uprobes use $COMM too (git-fixes).
    suse-commit: 26bf0d1b5c430d8c8f8f5c37dae7e09cc0e50ee2

--- kernel/trace/trace_probe.c
+++ kernel/trace/trace_probe.c
@@ -290,7 +290,7 @@ static int parse_probe_vars(char *arg, const struct fetch_type *t,
 			}
 		} else
 			goto inval_var;
-	} else if (strcmp(arg, "comm") == 0) {
+	} else if (strcmp(arg, "comm") == 0 || strcmp(arg, "COMM") == 0) {
 		code->op = FETCH_OP_COMM;
 #ifdef CONFIG_HAVE_FUNCTION_ARG_ACCESS_API
 	} else if (((flags & TPARG_FL_MASK) ==
@@ -543,7 +543,7 @@ static int traceprobe_parse_probe_arg_body(char *arg, ssize_t *size,
 	}
 
 	/* Since $comm can not be dereferred, we can find $comm by strcmp */
-	if (strcmp(arg, "$comm") == 0) {
+	if (strcmp(arg, "$comm") == 0 || strcmp(arg, "$COMM") == 0) {
 		/* The type of $comm must be "string", and not an array. */
 		if (parg->count || (t && strcmp(t, "string")))
 			return -EINVAL;
