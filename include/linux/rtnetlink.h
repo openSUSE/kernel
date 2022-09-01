46b676299b9ea9cb460d5177acc9c195e9a13d06
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2022-09-01 14:26:34 +0200

    net: core: netlink: add helper refcount dec and lock function (bsc#1196973 CVE-2021-39713).
    suse-commit: a6d6c6f73f1a8828bcfe16299dc5dc86529a178a

--- include/linux/rtnetlink.h
+++ include/linux/rtnetlink.h
@@ -748,6 +748,8 @@ extern void rtnl_lock(void);
 extern void rtnl_unlock(void);
 extern int rtnl_trylock(void);
 extern int rtnl_is_locked(void);
+extern bool atomic_dec_and_rtnl_lock(atomic_t *r);
+
 #ifdef CONFIG_PROVE_LOCKING
 extern int lockdep_rtnl_is_held(void);
 #endif /* #ifdef CONFIG_PROVE_LOCKING */
