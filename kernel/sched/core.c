8deddd292c34ddd6a9d4e272a858bcb87c3d7a27
Mel Gorman <mgorman@suse.de>
2022-09-05 15:46:06 +0100

    sched/numa: Fix NUMA topology for systems with CPU-less nodes (jsc#PED-827).
    suse-commit: 333ac73d544a9f0d16aa2dfec68521645457de43

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8892,6 +8892,7 @@ int sched_cpu_activate(unsigned int cpu)
 	set_cpu_active(cpu, true);
 
 	if (sched_smp_initialized) {
+		sched_update_numa(cpu, true);
 		sched_domains_numa_masks_set(cpu);
 		cpuset_cpu_active();
 	}
@@ -8970,10 +8971,12 @@ int sched_cpu_deactivate(unsigned int cpu)
 	if (!sched_smp_initialized)
 		return 0;
 
+	sched_update_numa(cpu, false);
 	ret = cpuset_cpu_inactive(cpu);
 	if (ret) {
 		balance_push_set(cpu, false);
 		set_cpu_active(cpu, true);
+		sched_update_numa(cpu, true);
 		return ret;
 	}
 	sched_domains_numa_masks_clear(cpu);
@@ -9076,7 +9079,7 @@ int sched_cpu_dying(unsigned int cpu)
 
 void __init sched_init_smp(void)
 {
-	sched_init_numa();
+	sched_init_numa(NUMA_NO_NODE);
 
 	/*
 	 * There's no userspace yet to cause hotplug operations; hence all the
