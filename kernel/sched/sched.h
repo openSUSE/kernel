f6931a9fed16b6fec0fb7db3ba5f4c30b599e411
Mel Gorman <mgorman@suse.de>
2022-09-05 15:46:06 +0100

    sched/fair: Decay task PELT values during wakeup migration (bnc#1202494 (Scheduler functional and performance backports)).
    suse-commit: b1e7191089dceb8b8802df4a29838bd4cd3ca018

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -654,6 +654,10 @@ struct cfs_rq {
 	int			runtime_enabled;
 	s64			runtime_remaining;
 
+	u64			throttled_pelt_idle;
+#ifndef CONFIG_64BIT
+	u64                     throttled_pelt_idle_copy;
+#endif
 	u64			throttled_clock;
 	u64			throttled_clock_pelt;
 	u64			throttled_clock_pelt_time;
@@ -1026,6 +1030,12 @@ struct rq {
 	u64			clock_task ____cacheline_aligned;
 	u64			clock_pelt;
 	unsigned long		lost_idle_time;
+	u64			clock_pelt_idle;
+	u64			clock_idle;
+#ifndef CONFIG_64BIT
+	u64			clock_pelt_idle_copy;
+	u64			clock_idle_copy;
+#endif
 
 	atomic_t		nr_iowait;
 
