c7415fc8fea2b477679e888a7beab0c566ecbc3d
Oliver Neukum <oneukum@suse.com>
2022-09-07 16:55:58 +0200

    USB: serial: ch341: fix lost character on LCR updates (git-fixes).
    suse-commit: bf1a3202de2c4e4257ca200692f87eef4f84078f

--- drivers/usb/serial/ch341.c
+++ drivers/usb/serial/ch341.c
@@ -100,7 +100,10 @@ struct ch341_private {
 	u8 mcr;
 	u8 msr;
 	u8 lcr;
+
 	unsigned long quirks;
+	u8 version;
+
 };
 
 static void ch341_set_termios(struct tty_struct *tty,
@@ -182,6 +185,9 @@ static int ch341_set_baudrate_lcr(struct usb_device *dev,
 	 */
 	a |= BIT(7);
 
+	if (priv->version < 0x30)
+		return 0;
+
 	r = ch341_control_out(dev, CH341_REQ_WRITE_REG,
 		       CH341_REG_DIVISOR << 8 | CH341_REG_PRESCALER,
 	       	       a);
@@ -240,7 +246,9 @@ static int ch341_configure(struct usb_device *dev, struct ch341_private *priv)
 	r = ch341_control_in(dev, CH341_REQ_READ_VERSION, 0, 0, buffer, size);
 	if (r < 0)
 		goto out;
-	dev_dbg(&dev->dev, "Chip version: 0x%02x\n", buffer[0]);
+
+	priv->version = buffer[0];
+	dev_dbg(&dev->dev, "Chip version: 0x%02x\n", priv->version);
 
 	r = ch341_control_out(dev, CH341_REQ_SERIAL_INIT, 0, 0);
 	if (r < 0)
