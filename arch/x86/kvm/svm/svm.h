b2c4390ef2369dce81fecd9cdda3203e7b03e9b6
Joerg Roedel <jroedel@suse.de>
2022-09-19 16:51:49 +0200

    KVM: SVM: Create a separate mapping for the SEV-ES save area (jsc#SLE-19924, jsc#SLE-24814).
    suse-commit: 8d86c9f96079fdcbd0a391e0f6392888f11ea946

--- arch/x86/kvm/svm/svm.h
+++ arch/x86/kvm/svm/svm.h
@@ -129,7 +129,7 @@ struct svm_nested_state {
 
 struct vcpu_sev_es_state {
 	/* SEV-ES support */
-	struct vmcb_save_area *vmsa;
+	struct sev_es_save_area *vmsa;
 	struct ghcb *ghcb;
 	struct kvm_host_map ghcb_map;
 	bool received_first_sipi;
