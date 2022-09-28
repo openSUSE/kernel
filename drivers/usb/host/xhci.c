476060a26cd07c694f1aa3f4b501d9361c2f5dab
Oliver Neukum <oneukum@suse.com>
2022-09-28 11:37:13 +0200

    xhci: dbc: Don't call dbc_tty_init() on every dbc tty probe (jsc#PED-531).
    suse-commit: c0f405191934e4d439d2fb45edcca48e41c96287

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -5516,6 +5516,7 @@ static int __init xhci_hcd_init(void)
 		return -ENODEV;
 
 	xhci_debugfs_create_root();
+	xhci_dbc_init();
 
 	return 0;
 }
@@ -5527,6 +5528,7 @@ static int __init xhci_hcd_init(void)
 static void __exit xhci_hcd_fini(void)
 {
 	xhci_debugfs_remove_root();
+	xhci_dbc_exit();
 }
 
 module_init(xhci_hcd_init);
