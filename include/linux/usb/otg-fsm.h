ba494c50c63477c54fa0a846e971603fdde14fb5
Oliver Neukum <oneukum@suse.com>
2022-10-04 11:01:01 +0200

    struct otg_fsm: hide new boolean member in gap (git-fixes).
    suse-commit: f6f0e1f53ef03b249a99c4049904030f98966b31

--- include/linux/usb/otg-fsm.h
+++ include/linux/usb/otg-fsm.h
@@ -196,8 +196,10 @@ struct otg_fsm {
 	struct mutex lock;
 	u8 *host_req_flag;
 	struct delayed_work hnp_polling_work;
-	bool hnp_work_inited;
 	bool state_changed;
+#ifndef __GENKSYMS__
+	bool hnp_work_inited;
+#endif
 };
 
 struct otg_fsm_ops {
