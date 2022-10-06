fc051bbdbbe791979fa8a8b7d5c918eb55ecf527
Lee, Chun-Yi <jlee@suse.com>
2022-10-06 14:57:32 +0800

    ACPI: APEI: rename ghes_init() with an "acpi_" prefix (jsc#PED-1408).
    Refresh patches.suse/ACPI-VIOT-Fix-ACS-setup.patch.
    
    suse-commit: a0d889dc9feaeb3c52ab704f673fc552055f42a6

--- drivers/acpi/bus.c
+++ drivers/acpi/bus.c
@@ -1354,7 +1354,7 @@ static int __init acpi_init(void)
 	acpi_iort_init();
 	acpi_viot_early_init();
 	acpi_hest_init();
-	ghes_init();
+	acpi_ghes_init();
 	acpi_scan_init();
 	acpi_ec_init();
 	acpi_debugfs_init();
