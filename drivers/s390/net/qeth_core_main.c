9e4aec143373823ff2f9f7e77a06e1b24f1d8af1
Petr Tesarik <ptesarik@suse.cz>
2022-10-14 14:38:21 +0200

    s390/qeth: update kerneldoc for qeth_add_hw_header() (jsc#PED-448 LTC#198619).
    suse-commit: 3154da57ab3d1eb829f18c52f5adb15a81aade06

--- drivers/s390/net/qeth_core_main.c
+++ drivers/s390/net/qeth_core_main.c
@@ -3942,12 +3942,14 @@ EXPORT_SYMBOL_GPL(qeth_count_elements);
 
 /**
  * qeth_add_hw_header() - add a HW header to an skb.
+ * @queue: TX queue that the skb will be placed on.
  * @skb: skb that the HW header should be added to.
  * @hdr: double pointer to a qeth_hdr. When returning with >= 0,
  *	 it contains a valid pointer to a qeth_hdr.
  * @hdr_len: length of the HW header.
  * @proto_len: length of protocol headers that need to be in same page as the
  *	       HW header.
+ * @elements: returns the required number of buffer elements for this skb.
  *
  * Returns the pushed length. If the header can't be pushed on
  * (eg. because it would cross a page boundary), it is allocated from
