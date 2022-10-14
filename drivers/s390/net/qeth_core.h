3f561030e2c79431f2bfff7b0a6b43c46694ea20
Petr Tesarik <ptesarik@suse.cz>
2022-10-14 14:38:21 +0200

    s390/qeth: add __printf format attribute to qeth_dbf_longtext (jsc#PED-448 LTC#198619).
    suse-commit: 32fb7ee8f8c1d58377be50137c22bfb8fce4f4d5

--- drivers/s390/net/qeth_core.h
+++ drivers/s390/net/qeth_core.h
@@ -1121,6 +1121,7 @@ int qeth_do_send_packet(struct qeth_card *card, struct qeth_qdio_out_q *queue,
 int qeth_do_ioctl(struct net_device *dev, struct ifreq *rq, int cmd);
 int qeth_siocdevprivate(struct net_device *dev, struct ifreq *rq,
 			void __user *data, int cmd);
+__printf(3, 4)
 void qeth_dbf_longtext(debug_info_t *id, int level, char *text, ...);
 int qeth_configure_cq(struct qeth_card *, enum qeth_cq);
 int qeth_hw_trap(struct qeth_card *, enum qeth_diags_trap_action);
