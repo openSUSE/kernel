a3638ddd03c25f99b139b5d1693d35b1fe726f7f
Mel Gorman <mgorman@suse.de>
2022-10-27 13:09:08 +0100

    - mm/pagealloc: sysctl: change watermark_scale_factor max limit   to 30% (bnc#1189998 (PREEMPT_RT prerequisite backports)). - Refresh   patches.suse/sysctl-move-some-boundary-constants-from-sysctl.c-to.patch.
    This was previously in the SLE15-SP4-RT branch to avoid a change in
    behaviour of a sysctl after GA.
    
    suse-commit: dce66e641a38362d493a5c4254f16a0a2f89edb4

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -3049,7 +3049,7 @@ static struct ctl_table vm_table[] = {
 		.mode		= 0644,
 		.proc_handler	= watermark_scale_factor_sysctl_handler,
 		.extra1		= SYSCTL_ONE,
-		.extra2		= SYSCTL_ONE_THOUSAND,
+		.extra2		= SYSCTL_THREE_THOUSAND,
 	},
 	{
 		.procname	= "percpu_pagelist_high_fraction",
