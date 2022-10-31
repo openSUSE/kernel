8b99b667b2b9c68cb3fc3a5e2401401849dbbeb0
Nicolas Morey-Chaisemartin <nmoreychaisemartin@suse.com>
2022-10-31 17:35:19 +0100

    RDMA/rxe: Fix over copying in get_srq_wqe (git-fixes)
    suse-commit: 8d71bad8016407f8d755d575743a42bfa6b26b4f

--- drivers/infiniband/sw/rxe/rxe_resp.c
+++ drivers/infiniband/sw/rxe/rxe_resp.c
@@ -319,6 +319,7 @@ static enum resp_states get_srq_wqe(struct rxe_qp *qp)
 	struct rxe_queue *q = srq->rq.queue;
 	struct rxe_recv_wqe *wqe;
 	struct ib_event ev;
+	size_t size;
 
 	if (srq->error)
 		return RESPST_ERR_RNR;
@@ -331,8 +332,13 @@ static enum resp_states get_srq_wqe(struct rxe_qp *qp)
 		return RESPST_ERR_RNR;
 	}
 
-	/* note kernel and user space recv wqes have same size */
-	memcpy(&qp->resp.srq_wqe, wqe, sizeof(qp->resp.srq_wqe));
+	/* don't trust user space data */
+	if (unlikely(wqe->dma.num_sge > srq->rq.max_sge)) {
+		pr_warn("%s: invalid num_sge in SRQ entry\n", __func__);
+		return RESPST_ERR_MALFORMED_WQE;
+	}
+	size = sizeof(wqe) + wqe->dma.num_sge*sizeof(struct rxe_sge);
+	memcpy(&qp->resp.srq_wqe, wqe, size);
 
 	qp->resp.wqe = &qp->resp.srq_wqe.wqe;
 	advance_consumer(q);
