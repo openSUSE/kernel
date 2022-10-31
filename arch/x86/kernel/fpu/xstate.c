36a1f4a91791f7281c449e7ab5106040f00bb6d7
Jiri Slaby <jslaby@suse.cz>
2022-10-31 10:27:35 +0100

    x86/fpu: Configure init_fpstate attributes orderly (jsc#PED-1816).
    suse-commit: 1fb0701f2960a70187ebb6d0f3ca3a5e0a808fb0

--- arch/x86/kernel/fpu/xstate.c
+++ arch/x86/kernel/fpu/xstate.c
@@ -422,7 +422,7 @@ static void __init setup_init_fpu_buf(void)
 	setup_xstate_features();
 	print_xstate_features();
 
-	xstate_init_xcomp_bv(&init_fpstate.regs.xsave, fpu_kernel_cfg.max_features);
+	xstate_init_xcomp_bv(&init_fpstate.regs.xsave, init_fpstate.xfeatures);
 
 	/*
 	 * Init all the features state with header.xfeatures being 0x0
@@ -948,6 +948,10 @@ void __init fpu__init_system_xstate(unsigned int legacy_size)
 	 */
 	update_regset_xstate_info(fpu_user_cfg.max_size,
 				  fpu_user_cfg.max_features);
+
+	/* Bring init_fpstate size and features up to date */
+	init_fpstate.size		= fpu_kernel_cfg.max_size;
+	init_fpstate.xfeatures		= fpu_kernel_cfg.max_features;
 
 	setup_init_fpu_buf();
 	setup_xstate_comp_offsets();
