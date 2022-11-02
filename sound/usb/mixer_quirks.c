128b72d0042bf397e857a1e9e67f60ce90d8f326
Takashi Iwai <tiwai@suse.de>
2022-11-02 12:37:53 +0100

    ALSA: usb-audio: Fix regression with Dell Dock jack detection (bsc#1204719).
    suse-commit: 286383cfa14a93136c51ce0c64d5d86ec9ed7b17

--- sound/usb/mixer_quirks.c
+++ sound/usb/mixer_quirks.c
@@ -2042,7 +2042,7 @@ err:
 }
 
 static const struct snd_kcontrol_new realtek_connector_ctl_ro = {
-	.iface = SNDRV_CTL_ELEM_IFACE_CARD,
+	.iface = SNDRV_CTL_ELEM_IFACE_MIXER,
 	.name = "", /* will be filled later manually */
 	.access = SNDRV_CTL_ELEM_ACCESS_READ,
 	.info = snd_ctl_boolean_mono_info,
