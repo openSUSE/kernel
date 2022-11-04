a1cd9e1ad8ba9a04d8580888148f7833c9852473
Michal Suchanek <msuchanek@suse.de>
2022-11-04 19:44:36 +0100

    mm/mmap: drop ARCH_HAS_VM_GET_PAGE_PROT (jsc#PED-1981).
    suse-commit: 564b5039d1529ac1f980a073d549c61ec022d80c

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -430,9 +430,6 @@ extern unsigned int kobjsize(const void *objp);
  * mapping from the currently active vm_flags protection bits (the
  * low four bits) to a page protection mask..
  */
-#ifndef CONFIG_ARCH_HAS_VM_GET_PAGE_PROT
-extern pgprot_t protection_map[16];
-#endif
 
 /**
  * enum fault_flag - Fault flag definitions.
