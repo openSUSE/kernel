2f0aa1850d6458fa70d7fdf31cf293c2550bde57
Hannes Reinecke <hare@suse.de>
2022-11-07 14:35:15 +0100

    dm: move setting md->type into dm_setup_md_queue (jsc#PED-1183).
    suse-commit: a6af6a49a5621486c41835ee34943b8209eeb00b

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -2064,9 +2064,9 @@ EXPORT_SYMBOL_GPL(dm_get_queue_limits);
  */
 int dm_setup_md_queue(struct mapped_device *md, struct dm_table *t)
 {
-	int r;
+	enum dm_queue_mode type = dm_table_get_type(t);
 	struct queue_limits limits;
-	enum dm_queue_mode type = dm_get_md_type(md);
+	int r;
 
 	switch (type) {
 	case DM_TYPE_REQUEST_BASED:
@@ -2093,6 +2093,7 @@ int dm_setup_md_queue(struct mapped_device *md, struct dm_table *t)
 	r = dm_table_set_restrictions(t, md->queue, &limits);
 	if (r)
 		return r;
+	md->type = type;
 
 	blk_register_queue(md->disk);
 
