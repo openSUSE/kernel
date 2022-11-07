6c16a1d792e2d74ddc5d053c80425625e8f20e5c
Hannes Reinecke <hare@suse.de>
2022-11-07 14:45:52 +0100

    - nvme: remove the GENHD_FL_UP check in nvme_ns_remove   (jsc#PED-1183). - Refresh   patches.suse/nvme-fix-RCU-hole-that-allowed-for-endless-looping-i.patch. - Refresh   patches.suse/nvme-only-call-synchronize_srcu-when-clearing-curren.patch.
    suse-commit: 5f9ae94c5f5b148e9af49d49ccbd2becbf800243

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -4064,14 +4064,12 @@ static void nvme_ns_remove(struct nvme_ns *ns)
 	/* guarantee not available in head->list */
 	synchronize_rcu();
 
-	if (ns->disk->flags & GENHD_FL_UP) {
-		if (!nvme_ns_head_multipath(ns->head))
-			nvme_cdev_del(&ns->cdev, &ns->cdev_device);
-		del_gendisk(ns->disk);
-		blk_cleanup_queue(ns->queue);
-		if (blk_get_integrity(ns->disk))
-			blk_integrity_unregister(ns->disk);
-	}
+	if (!nvme_ns_head_multipath(ns->head))
+		nvme_cdev_del(&ns->cdev, &ns->cdev_device);
+	del_gendisk(ns->disk);
+	blk_cleanup_queue(ns->queue);
+	if (blk_get_integrity(ns->disk))
+		blk_integrity_unregister(ns->disk);
 
 	down_write(&ns->ctrl->namespaces_rwsem);
 	list_del_init(&ns->list);
