3cc0b98fd71a4f097a5c857bb7eb3323bbf2190b
Chester Lin <clin@suse.com>
2022-11-09 01:12:01 +0800

    crypto: hisilicon/qm - fix deadlock for remove driver (jsc#PED-1109).
    suse-commit: a842b4ed4a83b12757f88c4f25af7710e0859694

--- drivers/crypto/hisilicon/qm.c
+++ drivers/crypto/hisilicon/qm.c
@@ -3278,6 +3278,7 @@ void hisi_qm_uninit(struct hisi_qm *qm)
 		dma_free_coherent(dev, qm->qdma.size,
 				  qm->qdma.va, qm->qdma.dma);
 	}
+	up_write(&qm->qps_lock);
 
 	qm_irq_unregister(qm);
 	hisi_qm_pci_uninit(qm);
@@ -3285,8 +3286,6 @@ void hisi_qm_uninit(struct hisi_qm *qm)
 		uacce_remove(qm->uacce);
 		qm->uacce = NULL;
 	}
-
-	up_write(&qm->qps_lock);
 }
 EXPORT_SYMBOL_GPL(hisi_qm_uninit);
 
