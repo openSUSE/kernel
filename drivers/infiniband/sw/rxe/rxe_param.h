a30c2a4940fba635ede15de4531204d0ae608e8e
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2022-11-10 15:29:08 +0100

    RDMA/rxe: Remove type 2A memory window capability (jsc#PED-1111).
    suse-commit: 4ba5ee9e81d6bce4b4b29faac27bc5a71e6a07ce

--- drivers/infiniband/sw/rxe/rxe_param.h
+++ drivers/infiniband/sw/rxe/rxe_param.h
@@ -52,7 +52,6 @@ enum rxe_device_param {
 					| IB_DEVICE_MEM_MGT_EXTENSIONS
 					| IB_DEVICE_ALLOW_USER_UNREG
 					| IB_DEVICE_MEM_WINDOW
-					| IB_DEVICE_MEM_WINDOW_TYPE_2A
 					| IB_DEVICE_MEM_WINDOW_TYPE_2B,
 	RXE_MAX_SGE			= 32,
 	RXE_MAX_WQE_SIZE		= sizeof(struct rxe_send_wqe) +
