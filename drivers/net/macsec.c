13b2d3fa4f44d2b7e4b97fa75c69014dc05ac97a
Takashi Iwai <tiwai@suse.de>
2022-11-11 08:30:20 +0100

    macsec: clear encryption keys from the stack after setting up offload (git-fixes).
    suse-commit: 657b138355560636663d72fdfb96cc1b2cf52ec1

--- drivers/net/macsec.c
+++ drivers/net/macsec.c
@@ -1825,6 +1825,7 @@ static int macsec_add_rxsa(struct sk_buff *skb, struct genl_info *info)
 		       secy->key_len);
 
 		err = macsec_offload(ops->mdo_add_rxsa, &ctx);
+		memzero_explicit(ctx.sa.key, secy->key_len);
 		if (err)
 			goto cleanup;
 	}
@@ -2067,6 +2068,7 @@ static int macsec_add_txsa(struct sk_buff *skb, struct genl_info *info)
 		       secy->key_len);
 
 		err = macsec_offload(ops->mdo_add_txsa, &ctx);
+		memzero_explicit(ctx.sa.key, secy->key_len);
 		if (err)
 			goto cleanup;
 	}
