35872209e38c7988c633cbf33c42aaf946984be4
Shung-Hsi Yu <shung-hsi.yu@suse.com>
2022-11-15 13:43:13 +0800

    bpf: Introduce BPF support for kernel module function calls (jsc#PED-1368).
    - Refresh patches.suse/bpf-Stop-caching-subprog-index-in-the-bpf_pseudo_fun.patch
    - Refresh patches.suse/bpf-Introduce-composable-reg-ret-and-arg-types.patch
    
    suse-commit: 2e96d7404fbf3d8ecf65a93b71d1fa4543c48f06

--- include/linux/bpf_verifier.h
+++ include/linux/bpf_verifier.h
@@ -541,6 +541,8 @@ int bpf_check_attach_target(struct bpf_verifier_log *log,
 			    const struct bpf_prog *tgt_prog,
 			    u32 btf_id,
 			    struct bpf_attach_target_info *tgt_info);
+void bpf_free_kfunc_btf_tab(struct bpf_kfunc_btf_tab *tab);
+
 
 #define BPF_BASE_TYPE_MASK	GENMASK(BPF_BASE_TYPE_BITS - 1, 0)
 
