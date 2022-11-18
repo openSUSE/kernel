77fefd8b4c3ed065e793b05241e40290d52961c1
Hannes Reinecke <hare@suse.de>
2022-11-18 10:09:43 +0100

    iomap: remove the iomap arguments to ->page_{prepare,done} (jsc#PED-1183).
    suse-commit: 5858d5d1ac15b5348f4f91b182b95451ce4e7032

--- fs/iomap/buffered-io.c
+++ fs/iomap/buffered-io.c
@@ -614,7 +614,7 @@ iomap_write_begin(struct inode *inode, loff_t pos, unsigned len, unsigned flags,
 		return -EINTR;
 
 	if (page_ops && page_ops->page_prepare) {
-		status = page_ops->page_prepare(inode, pos, len, iomap);
+		status = page_ops->page_prepare(inode, pos, len);
 		if (status)
 			return status;
 	}
@@ -647,7 +647,7 @@ out_unlock:
 
 out_no_page:
 	if (page_ops && page_ops->page_done)
-		page_ops->page_done(inode, pos, 0, NULL, iomap);
+		page_ops->page_done(inode, pos, 0, NULL);
 	return status;
 }
 
@@ -723,7 +723,7 @@ static size_t iomap_write_end(struct inode *inode, loff_t pos, size_t len,
 	if (old_size < pos)
 		pagecache_isize_extended(inode, old_size, pos);
 	if (page_ops && page_ops->page_done)
-		page_ops->page_done(inode, pos, ret, page, iomap);
+		page_ops->page_done(inode, pos, ret, page);
 	put_page(page);
 
 	if (ret < len)
