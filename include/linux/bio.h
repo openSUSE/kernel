5c3c36602522c4b78c2d8a2283bc85f182839d7e
Hannes Reinecke <hare@suse.de>
2022-11-18 13:45:40 +0100

    - block: avoid extra iter advance with async iocb (jsc#PED-1183). - Refresh   patches.suse/oracleasm-reinstate-bio_map_user_iov-declaration-in-.patch.
    suse-commit: 5f7e9993960c3c1c9c91cffd446df36f22fddf4e

--- include/linux/bio.h
+++ include/linux/bio.h
@@ -418,6 +418,7 @@ int bio_add_zone_append_page(struct bio *bio, struct page *page,
 void __bio_add_page(struct bio *bio, struct page *page,
 		unsigned int len, unsigned int off);
 int bio_iov_iter_get_pages(struct bio *bio, struct iov_iter *iter);
+void bio_iov_bvec_set(struct bio *bio, struct iov_iter *iter);
 void __bio_release_pages(struct bio *bio, bool mark_dirty);
 extern int bio_map_user_iov(struct request *, struct iov_iter *, gfp_t);
 extern void bio_set_pages_dirty(struct bio *bio);
