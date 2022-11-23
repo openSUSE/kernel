d24fb81a370938788f75d3e3842355c623ffa7f0
Vlastimil Babka <vbabka@suse.cz>
2022-11-23 19:10:59 +0100

    - mm/migrate: enable returning precise migrate_pages() success   count (jsc#PED-1247). - Refresh   patches.suse/mm-refactor-check_and_migrate_movable_pages.patch.
    suse-commit: e58ace641048c28c1ef34f3d0451306c12102e63

--- include/linux/migrate.h
+++ include/linux/migrate.h
@@ -44,7 +44,8 @@ extern int migrate_page(struct address_space *mapping,
 			struct page *newpage, struct page *page,
 			enum migrate_mode mode);
 extern int migrate_pages(struct list_head *l, new_page_t new, free_page_t free,
-		unsigned long private, enum migrate_mode mode, int reason);
+		unsigned long private, enum migrate_mode mode, int reason,
+		unsigned int *ret_succeeded);
 extern struct page *alloc_migration_target(struct page *page, unsigned long private);
 extern int isolate_movable_page(struct page *page, isolate_mode_t mode);
 
@@ -61,7 +62,7 @@ void migration_entry_wait_on_locked(swp_entry_t entry, pte_t *ptep,
 static inline void putback_movable_pages(struct list_head *l) {}
 static inline int migrate_pages(struct list_head *l, new_page_t new,
 		free_page_t free, unsigned long private, enum migrate_mode mode,
-		int reason)
+		int reason, unsigned int *ret_succeeded)
 	{ return -ENOSYS; }
 static inline struct page *alloc_migration_target(struct page *page,
 		unsigned long private)
