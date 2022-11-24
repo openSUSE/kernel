5a4b0c18c4e0ec4b86686b746581d8cfc8dd5d05
Vlastimil Babka <vbabka@suse.cz>
2022-11-24 19:55:09 +0100

    mm/migration: use helper macro min in do_pages_stat (jsc#PED-1247).
    suse-commit: 3c548177f71b9797d21242bf66087ebde1448918

--- mm/migrate.c
+++ mm/migrate.c
@@ -1860,16 +1860,12 @@ static int do_pages_stat(struct mm_struct *mm, unsigned long nr_pages,
 			 const void __user * __user *pages,
 			 int __user *status)
 {
-#define DO_PAGES_STAT_CHUNK_NR 16
+#define DO_PAGES_STAT_CHUNK_NR 16UL
 	const void __user *chunk_pages[DO_PAGES_STAT_CHUNK_NR];
 	int chunk_status[DO_PAGES_STAT_CHUNK_NR];
 
 	while (nr_pages) {
-		unsigned long chunk_nr;
-
-		chunk_nr = nr_pages;
-		if (chunk_nr > DO_PAGES_STAT_CHUNK_NR)
-			chunk_nr = DO_PAGES_STAT_CHUNK_NR;
+		unsigned long chunk_nr = min(nr_pages, DO_PAGES_STAT_CHUNK_NR);
 
 		if (copy_from_user(chunk_pages, pages, chunk_nr * sizeof(*chunk_pages)))
 			break;
