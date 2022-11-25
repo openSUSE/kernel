c10681dc7a18b6fe4515ba48451f7ea0a0ceb870
Nicolas Morey-Chaisemartin <nmoreychaisemartin@suse.com>
2022-11-25 10:52:15 +0100

    RDMA/hns: Replace tab with space in the right-side comments (git-fixes)
    suse-commit: 4c89a776e6b7ed0a2c10402e54806d4411468a09

--- drivers/infiniband/hw/hns/hns_roce_hw_v2.h
+++ drivers/infiniband/hw/hns/hns_roce_hw_v2.h
@@ -1441,7 +1441,7 @@ struct hns_roce_v2_priv {
 struct hns_roce_dip {
 	u8 dgid[GID_LEN_V2];
 	u32 dip_idx;
-	struct list_head node;	/* all dips are on a list */
+	struct list_head node; /* all dips are on a list */
 };
 
 /* only for RNR timeout issue of HIP08 */
