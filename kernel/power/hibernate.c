f9d378fde421f331e9f1f18f3b2cbdaf9fcd0a1a
Hannes Reinecke <hare@suse.de>
2022-11-26 13:51:49 +0100

    - block: remove genhd.h (jsc#PED-1183). - Refresh   patches.suse/0006-nvdimm-blk-Delete-the-block-aperture-window-driver.patch. - Refresh   patches.suse/0007-PM-hibernate-encrypt-hidden-area.patch. - Refresh patches.suse/target-add-rbd-backend.patch.
    suse-commit: dd0982ae4edd5e7142e6ed1676269d54ac51d7df

--- kernel/power/hibernate.c
+++ kernel/power/hibernate.c
@@ -28,7 +28,6 @@
 #include <linux/gfp.h>
 #include <linux/syscore_ops.h>
 #include <linux/ctype.h>
-#include <linux/genhd.h>
 #include <linux/ktime.h>
 #include <linux/efi.h>
 #include <linux/security.h>
