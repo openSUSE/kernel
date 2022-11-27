53d3b345b98f14f06404558fb4251e20bcf054d7
Patrik Jakobsson <pjakobsson@suse.de>
2022-11-27 09:45:11 +0100

    - drm/vmwgfx: Simplify devcaps code (jsc#PED-1166 jsc#PED-1168   jsc#PED-1170 jsc#PED-1218 jsc#PED-1220 jsc#PED-1222 jsc#PED-1223   jsc#PED-1225). - Refresh   patches.suse/drm-vmwgfx-Remove-explicit-transparent-hugepages-sup.patch.
    suse-commit: 8ae992b03c21952d8d7b49e4b14dfca34882ff98

--- drivers/gpu/drm/vmwgfx/vmwgfx_drv.h
+++ drivers/gpu/drm/vmwgfx/vmwgfx_drv.h
@@ -510,7 +510,6 @@ struct vmw_private {
 	bool has_gmr;
 	bool has_mob;
 	spinlock_t hw_lock;
-	spinlock_t cap_lock;
 	bool assume_16bpp;
 
 	enum vmw_sm_type sm_type;
@@ -626,6 +625,8 @@ struct vmw_private {
 
 	/* Validation memory reservation */
 	struct vmw_validation_mem vvm;
+
+	uint32 *devcaps;
 };
 
 static inline struct vmw_surface *vmw_res_to_srf(struct vmw_resource *res)
