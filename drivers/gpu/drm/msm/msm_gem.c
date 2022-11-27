69a0bb6bd4cfce63303e9f7f1bc6647d7b975f42
Patrik Jakobsson <pjakobsson@suse.de>
2022-11-27 10:55:50 +0100

    - drm/msm: Drop submit bo_list (jsc#PED-1166 jsc#PED-1168   jsc#PED-1170 jsc#PED-1218 jsc#PED-1220 jsc#PED-1222 jsc#PED-1223   jsc#PED-1225). - Refresh   patches.suse/drm-msm-fix-potential-NULL-dereference-in-cleanup.patch.
    suse-commit: 5e29a13e2dc29c301d80d866bb6ca167a9355711

--- drivers/gpu/drm/msm/msm_gem.c
+++ drivers/gpu/drm/msm/msm_gem.c
@@ -1149,7 +1149,6 @@ static int msm_gem_new_impl(struct drm_device *dev,
 	msm_obj->flags = flags;
 	msm_obj->madv = MSM_MADV_WILLNEED;
 
-	INIT_LIST_HEAD(&msm_obj->submit_entry);
 	INIT_LIST_HEAD(&msm_obj->node);
 	INIT_LIST_HEAD(&msm_obj->vmas);
 
