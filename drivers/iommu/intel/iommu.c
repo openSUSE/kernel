5652e6986f3bb41e7cb96a2e1dd00162591eebe2
Joerg Roedel <jroedel@suse.de>
2022-11-28 19:11:06 +0100

    - iommu/vt-d: Remove finding domain in dmar_insert_one_dev_info()   (bsc#1205701). - Refresh   patches.suse/iommu-vt-d-Acquiring-lock-in-domain-ID-allocation-helpers.
    suse-commit: bab5fd615ae921db7fe453808aff6027baa12182

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -2561,7 +2561,6 @@ static struct dmar_domain *dmar_insert_one_dev_info(struct intel_iommu *iommu,
 						    struct device *dev,
 						    struct dmar_domain *domain)
 {
-	struct dmar_domain *found = NULL;
 	struct device_domain_info *info;
 	unsigned long flags;
 	int ret;
@@ -2612,26 +2611,6 @@ static struct dmar_domain *dmar_insert_one_dev_info(struct intel_iommu *iommu,
 	}
 
 	spin_lock_irqsave(&device_domain_lock, flags);
-	if (dev)
-		found = find_domain(dev);
-
-	if (!found) {
-		struct device_domain_info *info2;
-		info2 = dmar_search_domain_by_dev_info(info->segment, info->bus,
-						       info->devfn);
-		if (info2) {
-			found      = info2->domain;
-			info2->dev = dev;
-		}
-	}
-
-	if (found) {
-		spin_unlock_irqrestore(&device_domain_lock, flags);
-		free_devinfo_mem(info);
-		/* Caller must free the original domain */
-		return found;
-	}
-
 	ret = domain_attach_iommu(domain, iommu);
 
 	if (ret) {
