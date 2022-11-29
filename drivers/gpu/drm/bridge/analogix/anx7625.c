e5cace22fbdf8b779ecef2b1c0c88d1ba9baf46a
Patrik Jakobsson <pjakobsson@suse.de>
2022-11-29 15:00:08 +0100

    drm/bridge: anx7625: Propagate errors from sp_tx_edid_read() (jsc#PED-1166 jsc#PED-1168 jsc#PED-1170 jsc#PED-1218 jsc#PED-1220 jsc#PED-1222 jsc#PED-1223 jsc#PED-1225)
    suse-commit: 39bddb9ce936cf64a097da56d039ca40bae4223a

--- drivers/gpu/drm/bridge/analogix/anx7625.c
+++ drivers/gpu/drm/bridge/analogix/anx7625.c
@@ -846,8 +846,11 @@ static int sp_tx_edid_read(struct anx7625_data *ctx,
 				if (g_edid_break == 1)
 					break;
 
-				segments_edid_read(ctx, count / 2,
-						   pblock_buf, offset);
+				ret = segments_edid_read(ctx, count / 2,
+							 pblock_buf, offset);
+				if (ret < 0)
+					return ret;
+
 				memcpy(&pedid_blocks_buf[edid_pos],
 				       pblock_buf,
 				       MAX_DPCD_BUFFER_SIZE);
@@ -864,8 +867,11 @@ static int sp_tx_edid_read(struct anx7625_data *ctx,
 				if (g_edid_break == 1)
 					break;
 
-				segments_edid_read(ctx, count / 2,
-						   pblock_buf, offset);
+				ret = segments_edid_read(ctx, count / 2,
+							 pblock_buf, offset);
+				if (ret < 0)
+					return ret;
+
 				memcpy(&pedid_blocks_buf[edid_pos],
 				       pblock_buf,
 				       MAX_DPCD_BUFFER_SIZE);
