4bf62726bf20d8278e6df2c34a4d7442ea86767f
Patrik Jakobsson <pjakobsson@suse.de>
2022-11-30 09:47:54 +0100

    - drm/i915: Release i915_gem_context from a worker (jsc#PED-1166   jsc#PED-1168 jsc#PED-1170 jsc#PED-1218 jsc#PED-1220 jsc#PED-1222   jsc#PED-1223 jsc#PED-1225). - Refresh   patches.suse/0001-drm-i915-Keep-gem-ctx-vm-alive-until-the-final-put.patch.
    suse-commit: 2b46bea2a4b25859d9219a8ff0efb22492dd8906

--- drivers/gpu/drm/i915/gem/i915_gem_context.c
+++ drivers/gpu/drm/i915/gem/i915_gem_context.c
@@ -990,9 +990,10 @@ free_engines:
 	return err;
 }
 
-void i915_gem_context_release(struct kref *ref)
+static void i915_gem_context_release_work(struct work_struct *work)
 {
-	struct i915_gem_context *ctx = container_of(ref, typeof(*ctx), ref);
+	struct i915_gem_context *ctx = container_of(work, typeof(*ctx),
+						    release_work);
 	struct i915_address_space *vm;
 
 	trace_i915_context_free(ctx);
@@ -1012,6 +1013,13 @@ void i915_gem_context_release(struct kref *ref)
 	mutex_destroy(&ctx->mutex);
 
 	kfree_rcu(ctx, rcu);
+}
+
+void i915_gem_context_release(struct kref *ref)
+{
+	struct i915_gem_context *ctx = container_of(ref, typeof(*ctx), ref);
+
+	queue_work(ctx->i915->wq, &ctx->release_work);
 }
 
 static inline struct i915_gem_engines *
@@ -1319,6 +1327,7 @@ i915_gem_create_context(struct drm_i915_private *i915,
 	ctx->sched = pc->sched;
 	mutex_init(&ctx->mutex);
 	INIT_LIST_HEAD(&ctx->link);
+	INIT_WORK(&ctx->release_work, i915_gem_context_release_work);
 
 	spin_lock_init(&ctx->stale.lock);
 	INIT_LIST_HEAD(&ctx->stale.engines);
