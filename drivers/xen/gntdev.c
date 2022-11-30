6165e219259b4334a5f994ec827f25744c751703
Juergen Gross <jgross@suse.com>
2022-11-30 08:41:58 +0100

    xen/gntdev: Ignore failure to unmap INVALID_GRANT_HANDLE (git-fixes).
    suse-commit: 310f73b21e3f7fd70ea620240d398eecc2007b94

--- drivers/xen/gntdev.c
+++ drivers/xen/gntdev.c
@@ -413,13 +413,15 @@ static void __unmap_grant_pages_done(int result,
 	unsigned int offset = data->unmap_ops - map->unmap_ops;
 
 	for (i = 0; i < data->count; i++) {
-		WARN_ON(map->unmap_ops[offset+i].status);
+		WARN_ON(map->unmap_ops[offset + i].status != GNTST_okay &&
+			map->unmap_ops[offset + i].handle != -1);
 		pr_debug("unmap handle=%d st=%d\n",
 			map->unmap_ops[offset+i].handle,
 			map->unmap_ops[offset+i].status);
 		map->unmap_ops[offset+i].handle = -1;
 		if (use_ptemod) {
-			WARN_ON(map->kunmap_ops[offset+i].status);
+			WARN_ON(map->kunmap_ops[offset + i].status != GNTST_okay &&
+				map->kunmap_ops[offset + i].handle != -1);
 			pr_debug("kunmap handle=%u st=%d\n",
 				 map->kunmap_ops[offset+i].handle,
 				 map->kunmap_ops[offset+i].status);
