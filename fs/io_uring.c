1842ade9de6e8f6cba726a795d54116615afa800
Gabriel Krisman Bertazi <krisman@suse.de>
2022-12-08 20:59:39 -0300

    io_uring: consistent typing for issue_flags (bsc#1205205).
    suse-commit: e0d1ea77ffbc31d4c72bfd52bea41e1055f48b9a

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -3784,7 +3784,7 @@ static int io_mkdirat_prep(struct io_kiocb *req,
 	return 0;
 }
 
-static int io_mkdirat(struct io_kiocb *req, int issue_flags)
+static int io_mkdirat(struct io_kiocb *req, unsigned int issue_flags)
 {
 	struct io_mkdir *mkd = &req->mkdir;
 	int ret;
@@ -3833,7 +3833,7 @@ static int io_symlinkat_prep(struct io_kiocb *req,
 	return 0;
 }
 
-static int io_symlinkat(struct io_kiocb *req, int issue_flags)
+static int io_symlinkat(struct io_kiocb *req, unsigned int issue_flags)
 {
 	struct io_symlink *sl = &req->symlink;
 	int ret;
@@ -3883,7 +3883,7 @@ static int io_linkat_prep(struct io_kiocb *req,
 	return 0;
 }
 
-static int io_linkat(struct io_kiocb *req, int issue_flags)
+static int io_linkat(struct io_kiocb *req, unsigned int issue_flags)
 {
 	struct io_hardlink *lnk = &req->hardlink;
 	int ret;
