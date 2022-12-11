0b1a71195546d249a1a4af42927a55688e531fc5
Nicolai Stange <nstange@suse.de>
2022-12-11 13:35:27 +0100

    random: remove outdated INT_MAX >> 6 check in urandom_read() (bsc#1204911).
    suse-commit: 485f33081ef367f448b63bdc587e6d1a8cdfee74

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -1836,9 +1836,8 @@ static ssize_t
 urandom_read_nowarn(struct file *file, char __user *buf, size_t nbytes,
 		    loff_t *ppos)
 {
-	int ret;
+	ssize_t ret;
 
-	nbytes = min_t(size_t, nbytes, INT_MAX >> (ENTROPY_SHIFT + 3));
 	ret = extract_crng_user(buf, nbytes);
 	trace_urandom_read(8 * nbytes, 0, ENTROPY_BITS(&input_pool));
 	return ret;
