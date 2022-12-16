4a77daa757aae533b38767519fbc26239d94d5c9
Joerg Roedel <jroedel@suse.de>
2022-12-16 16:04:00 +0100

    s390/sclp: detect the AISI facility (bsc#1205701).
    suse-commit: 24889d4aa296d04d5cdbe2478f1d4f0bb84cff89

--- arch/s390/include/asm/sclp.h
+++ arch/s390/include/asm/sclp.h
@@ -89,6 +89,7 @@ struct sclp_info {
 	unsigned char has_zpci_lsi : 1;
 	unsigned char has_aisii : 1;
 	unsigned char has_aeni : 1;
+	unsigned char has_aisi : 1;
 	unsigned int ibc;
 	unsigned int mtid;
 	unsigned int mtid_cp;
