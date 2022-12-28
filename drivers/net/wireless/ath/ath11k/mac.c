f35845f54876a5d524921485c9fb69f6e2d5882a
Takashi Iwai <tiwai@suse.de>
2022-12-28 16:22:58 +0100

    ath11k: Fix 'unused-but-set-parameter' error (bsc#1206451).
    suse-commit: 205bb78727919a8ab351a266e58aea9ed51f3d16

--- drivers/net/wireless/ath/ath11k/mac.c
+++ drivers/net/wireless/ath/ath11k/mac.c
@@ -5987,7 +5987,6 @@ static void ath11k_mac_op_configure_filter(struct ieee80211_hw *hw,
 
 	mutex_lock(&ar->conf_mutex);
 
-	changed_flags &= SUPPORTED_FILTERS;
 	*total_flags &= SUPPORTED_FILTERS;
 	ar->filter_flags = *total_flags;
 
