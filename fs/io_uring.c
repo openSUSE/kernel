1edeb952d51cd5189f9ddec7635d49f66209b60c
Gabriel Krisman Bertazi <krisman@suse.de>
2023-01-02 11:16:34 -0300

    Update patches.suse/io_uring-place-fixed-tables-under-memcg-limits.patch (bsc#1205205).
    Originally backported from -stable, it was now missing a hunk.
    
    suse-commit: ae15ad7c3025acc0d32cea2fe7649e268dc07f75

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -8955,7 +8955,8 @@ fail:
 
 static bool io_alloc_file_tables(struct io_file_table *table, unsigned nr_files)
 {
-	table->files = kvcalloc(nr_files, sizeof(table->files[0]), GFP_KERNEL);
+	table->files = kvcalloc(nr_files, sizeof(table->files[0]),
+				GFP_KERNEL_ACCOUNT);
 	return !!table->files;
 }
 
