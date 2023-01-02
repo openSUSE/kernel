a615e1aadd10096cac6348c8661eb10fd535fe21
Takashi Iwai <tiwai@suse.de>
2023-01-02 10:00:46 +0100

    ACPI: resource: Add Asus ExpertBook B2502 to Asus quirks (git-fixes).
    suse-commit: ba863273972feb71bcb857efda764ea1bc3c37a7

--- drivers/acpi/resource.c
+++ drivers/acpi/resource.c
@@ -439,6 +439,13 @@ static const struct dmi_system_id lenovo_laptop[] = {
 			DMI_MATCH(DMI_BOARD_NAME, "S5602ZA"),
 		},
 	},
+	{
+		.ident = "Asus ExpertBook B2502",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
+			DMI_MATCH(DMI_BOARD_NAME, "B2502CBA"),
+		},
+	},
 	{ }
 };
 
