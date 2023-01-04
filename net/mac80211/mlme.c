70b0ac7abc0b6a83112488f3dd18ef2db9d539db
Takashi Iwai <tiwai@suse.de>
2023-01-04 11:00:31 +0100

    mac80211: always have ieee80211_sta_restart() (git-fixes).
    suse-commit: 8446aaf26150aabd402b61fa93520c221b694af4

--- net/mac80211/mlme.c
+++ net/mac80211/mlme.c
@@ -4835,6 +4835,7 @@ void ieee80211_mgd_quiesce(struct ieee80211_sub_if_data *sdata)
 
 	sdata_unlock(sdata);
 }
+#endif
 
 void ieee80211_sta_restart(struct ieee80211_sub_if_data *sdata)
 {
@@ -4870,7 +4871,6 @@ void ieee80211_sta_restart(struct ieee80211_sub_if_data *sdata)
 
 	sdata_unlock(sdata);
 }
-#endif
 
 /* interface setup */
 void ieee80211_sta_setup_sdata(struct ieee80211_sub_if_data *sdata)
