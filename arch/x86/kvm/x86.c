0f57591bae6bec5efb2f6b6387cd1153c9a68d38
Vasant Karasulli <vkarasulli@suse.de>
2023-01-05 19:18:42 +0100

    KVM: x86: forcibly leave nested mode on vCPU reset (bsc#1204652 CVE-2022-3344).
    suse-commit: 6648e317e3bade7983e492dd05b75176366c2114

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -10880,8 +10880,18 @@ void kvm_vcpu_reset(struct kvm_vcpu *vcpu, bool init_event)
 {
 	unsigned long old_cr0 = kvm_read_cr0(vcpu);
 
+	/*
+	 * SVM doesn't unconditionally VM-Exit on INIT and SHUTDOWN, thus it's
+	 * possible to INIT the vCPU while L2 is active.  Force the vCPU back
+	 * into L1 as EFER.SVME is cleared on INIT (along with all other EFER
+	 * bits), i.e. virtualization is disabled.
+	 */
+	if (is_guest_mode(vcpu))
+		kvm_leave_nested(vcpu);
+
 	kvm_lapic_reset(vcpu, init_event);
 
+	WARN_ON_ONCE(is_guest_mode(vcpu) || is_smm(vcpu));
 	vcpu->arch.hflags = 0;
 
 	vcpu->arch.smi_pending = 0;
