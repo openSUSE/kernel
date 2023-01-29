ef9bd608b5c88c836bd967185c5240357d60d1a5
Coly Li <colyli@suse.de>
2023-01-29 15:35:09 +0800

    block/bfq_wf2q: correct weight to ioprio (git-fixes).
    suse-commit: 7f22324b90abe68ff9f6e0c12f03207430e204b9

--- block/bfq-wf2q.c
+++ block/bfq-wf2q.c
@@ -519,7 +519,7 @@ unsigned short bfq_ioprio_to_weight(int ioprio)
 static unsigned short bfq_weight_to_ioprio(int weight)
 {
 	return max_t(int, 0,
-		     IOPRIO_BE_NR * BFQ_WEIGHT_CONVERSION_COEFF - weight);
+		     IOPRIO_BE_NR - weight / BFQ_WEIGHT_CONVERSION_COEFF);
 }
 
 static void bfq_get_entity(struct bfq_entity *entity)
