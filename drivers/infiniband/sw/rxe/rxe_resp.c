75732c285e6173af5d6378e1972b856447125ad7
Nicolas Morey <nmorey@suse.com>
2023-02-07 09:57:17 +0100

    RDMA/rxe: Fix oops with zero length reads (git-fixes)
    suse-commit: c904c2af1a040b9f7578c1fb1b56c2fa4cdc6e1b

--- drivers/infiniband/sw/rxe/rxe_resp.c
+++ drivers/infiniband/sw/rxe/rxe_resp.c
@@ -435,7 +435,7 @@ static enum resp_states check_rkey(struct rxe_qp *qp,
 		return RESPST_EXECUTE;
 	}
 
-	/* A zero-byte op is not required to set an addr or rkey. */
+	/* A zero-byte op is not required to set an addr or rkey. See C9-88 */
 	if ((pkt->mask & RXE_READ_OR_WRITE_MASK) &&
 	    (pkt->mask & RXE_RETH_MASK) &&
 	    reth_len(pkt) == 0) {
@@ -807,7 +807,8 @@ static enum resp_states read_reply(struct rxe_qp *qp,
 	skb = prepare_ack_packet(qp, &ack_pkt, opcode, payload,
 				 res->cur_psn, AETH_ACK_UNLIMITED);
 	if (!skb) {
-		rxe_put(mr);
+		if (mr)
+			rxe_put(mr);
 		return RESPST_ERR_RNR;
 	}
 
