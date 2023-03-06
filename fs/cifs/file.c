68d59235194df996f0b96d8d4e16634a4b8479e3
Paulo Alcantara <palcantara@suse.de>
2023-03-06 13:38:58 -0300

    cifs: Fix use-after-free in rdata->read_into_pages() (git-fixes).
    suse-commit: 107b2e5197989c13e1edf7d557e8d031a1cf79f9

--- fs/cifs/file.c
+++ fs/cifs/file.c
@@ -3871,7 +3871,7 @@ uncached_fill_pages(struct TCP_Server_Info *server,
 		rdata->got_bytes += result;
 	}
 
-	return rdata->got_bytes > 0 && result != -ECONNABORTED ?
+	return result != -ECONNABORTED && rdata->got_bytes > 0 ?
 						rdata->got_bytes : result;
 }
 
@@ -4645,7 +4645,7 @@ readpages_fill_pages(struct TCP_Server_Info *server,
 		rdata->got_bytes += result;
 	}
 
-	return rdata->got_bytes > 0 && result != -ECONNABORTED ?
+	return result != -ECONNABORTED && rdata->got_bytes > 0 ?
 						rdata->got_bytes : result;
 }
 
