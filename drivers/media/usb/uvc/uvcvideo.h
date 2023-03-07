ce2e5b4e2e4018310c1afb4b6bfa0139dabc3623
Oliver Neukum <oneukum@suse.com>
2023-03-07 14:14:20 +0100

    struct uvc_device move flush_status  new member to end (git-fixes).
    suse-commit: 8ba3f50b1536740e6fcc470f555053660a745709

--- drivers/media/usb/uvc/uvcvideo.h
+++ drivers/media/usb/uvc/uvcvideo.h
@@ -698,7 +698,6 @@ struct uvc_device {
 	struct usb_host_endpoint *int_ep;
 	struct urb *int_urb;
 	u8 *status;
-	bool flush_status;
 	struct input_dev *input;
 	char input_phys[64];
 
@@ -711,6 +710,9 @@ struct uvc_device {
 	} async_ctrl;
 
 	struct uvc_entity *gpio_unit;
+#ifndef __GENKSYMS__
+	bool flush_status;
+#endif
 };
 
 enum uvc_handle_state {
