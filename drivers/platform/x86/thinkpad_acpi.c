53bc03eec12a399e95530805d52fb9fc31597a8a
Takashi Iwai <tiwai@suse.de>
2023-04-03 12:35:31 +0200

    platform/x86: thinkpad_acpi: Use backlight helper (bsc#1210050).
    suse-commit: 19259d6eba79b908dc823e4e07c55389e229b805

--- drivers/platform/x86/thinkpad_acpi.c
+++ drivers/platform/x86/thinkpad_acpi.c
@@ -6791,10 +6791,7 @@ static int brightness_set(unsigned int value)
 
 static int brightness_update_status(struct backlight_device *bd)
 {
-	unsigned int level =
-		(bd->props.fb_blank == FB_BLANK_UNBLANK &&
-		 bd->props.power == FB_BLANK_UNBLANK) ?
-				bd->props.brightness : 0;
+	int level = backlight_get_brightness(bd);
 
 	dbg_printk(TPACPI_DBG_BRGHT,
 			"backlight: attempt to set level to %d\n",
