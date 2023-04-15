2ce1023d9e67dfed142685cea1c2aecaf41a5647
Lee Duncan <lduncan@suse.com>
2023-04-14 18:34:06 -0700

    scsi: sd: Fix wrong zone_write_granularity value during revalidate (git-fixes).
    suse-commit: 68953dc0c03adb1317d5f4fd09f3b248436d51a5

--- drivers/scsi/sd_zbc.c
+++ drivers/scsi/sd_zbc.c
@@ -826,14 +826,6 @@ int sd_zbc_read_zones(struct scsi_disk *sdkp, unsigned char *buf)
 	blk_queue_max_active_zones(q, 0);
 	nr_zones = round_up(sdkp->capacity, zone_blocks) >> ilog2(zone_blocks);
 
-	/*
-	 * Per ZBC and ZAC specifications, writes in sequential write required
-	 * zones of host-managed devices must be aligned to the device physical
-	 * block size.
-	 */
-	if (blk_queue_zoned_model(q) == BLK_ZONED_HM)
-		blk_queue_zone_write_granularity(q, sdkp->physical_block_size);
-
 	sdkp->rev_nr_zones = nr_zones;
 	sdkp->rev_zone_blocks = zone_blocks;
 
