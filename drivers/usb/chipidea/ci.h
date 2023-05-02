3567bca0a743df4472dcb4a550bdf7e46d33ef64
Oliver Neukum <oneukum@suse.com>
2023-05-02 14:07:14 +0200

    struct ci_hdrc: hide new member at end (git-fixes).
    suse-commit: d06f402735483f73a512bc15d6d7cec4a41726e7

--- drivers/usb/chipidea/ci.h
+++ drivers/usb/chipidea/ci.h
@@ -256,7 +256,9 @@ struct ci_hdrc {
 	bool				in_lpm;
 	bool				wakeup_int;
 	enum ci_revision		rev;
+#ifndef __GENKSYMS__
 	struct mutex                    mutex;
+#endif
 };
 
 static inline struct ci_role_driver *ci_role(struct ci_hdrc *ci)
