724653c08f579492494c76d582ff111b27c7563f
Daniel Wagner <dwagner@suse.de>
2023-05-03 14:01:06 +0200

    nvme: rename nvme_validate_or_alloc_ns to nvme_scan_ns (git-fixes).
    suse-commit: 3d2ad6bae6ab6c0cea577306b0cd11eeb83ff362

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -4154,7 +4154,7 @@ out:
 		nvme_ns_remove(ns);
 }
 
-static void nvme_validate_or_alloc_ns(struct nvme_ctrl *ctrl, unsigned nsid)
+static void nvme_scan_ns(struct nvme_ctrl *ctrl, unsigned nsid)
 {
 	struct nvme_ns_ids ids = { };
 	struct nvme_ns *ns;
@@ -4247,7 +4247,7 @@ static int nvme_scan_ns_list(struct nvme_ctrl *ctrl)
 
 			if (!nsid)	/* end of the list? */
 				goto out;
-			nvme_validate_or_alloc_ns(ctrl, nsid);
+			nvme_scan_ns(ctrl, nsid);
 			while (++prev < nsid)
 				nvme_ns_remove_by_nsid(ctrl, prev);
 		}
@@ -4270,7 +4270,7 @@ static void nvme_scan_ns_sequential(struct nvme_ctrl *ctrl)
 	kfree(id);
 
 	for (i = 1; i <= nn; i++)
-		nvme_validate_or_alloc_ns(ctrl, i);
+		nvme_scan_ns(ctrl, i);
 
 	nvme_remove_invalid_namespaces(ctrl, nn);
 }
