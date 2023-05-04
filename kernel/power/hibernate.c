961c5b9a5847bf542bf46afc17eac7d8327f6d91
Takashi Iwai <tiwai@suse.de>
2023-05-04 10:37:45 +0200

    - PM: hibernate: Turn snapshot_test into global variable   (git-fixes). - Refresh   patches.suse/0007-PM-hibernate-encrypt-hidden-area.patch.
    suse-commit: df2c2927591105eccb712cce02dadba0a6dfdb95

--- kernel/power/hibernate.c
+++ kernel/power/hibernate.c
@@ -66,6 +66,7 @@ enum {
 static int hibernation_mode = HIBERNATION_SHUTDOWN;
 
 bool freezer_test_done;
+bool snapshot_test;
 
 static const struct platform_hibernation_ops *hibernation_ops;
 
@@ -729,7 +730,6 @@ static int load_image_and_restore(void)
  */
 int hibernate(void)
 {
-	bool snapshot_test = false;
 	int error;
 	void *secret_key;
 
@@ -772,6 +772,9 @@ int hibernate(void)
 	error = freeze_processes();
 	if (error)
 		goto Exit;
+
+	/* protected by system_transition_mutex */
+	snapshot_test = false;
 
 	lock_device_hotplug();
 	/* Allocate memory management structures */
@@ -968,6 +971,8 @@ static int software_resume(void)
 	 * here to avoid lockdep complaining.
 	 */
 	mutex_lock_nested(&system_transition_mutex, SINGLE_DEPTH_NESTING);
+
+	snapshot_test = false;
 
 	if (swsusp_resume_device)
 		goto Check_image;
