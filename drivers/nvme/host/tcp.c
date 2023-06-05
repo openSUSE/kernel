411713cea3200465eaf5b6b019bd82085ec01ab4
Daniel Wagner <dwagner@suse.de>
2023-06-05 14:56:35 +0200

    nvme: fix regression when disconnect a recovering ctrl (git-fixes).
    Refresh:
      - patches.suse/nvme-rdma-fix-possible-hang-caused-during-ctrl-delet.patch
      - patches.suse/nvme-tcp-fix-possible-hang-caused-during-ctrl-deleti.patch
    
    suse-commit: ec99fcf5a8fefc4560a902ed5fc5bccb69663d81

--- drivers/nvme/host/tcp.c
+++ drivers/nvme/host/tcp.c
@@ -2185,9 +2185,6 @@ static void nvme_tcp_error_recovery_work(struct work_struct *work)
 
 static void nvme_tcp_teardown_ctrl(struct nvme_ctrl *ctrl, bool shutdown)
 {
-	flush_work(&to_tcp_ctrl(ctrl)->err_work);
-	cancel_delayed_work_sync(&to_tcp_ctrl(ctrl)->connect_work);
-
 	nvme_tcp_teardown_io_queues(ctrl, shutdown);
 	nvme_stop_admin_queue(ctrl);
 	if (shutdown)
@@ -2225,6 +2222,12 @@ static void nvme_reset_ctrl_work(struct work_struct *work)
 out_fail:
 	++ctrl->nr_reconnects;
 	nvme_tcp_reconnect_or_remove(ctrl);
+}
+
+static void nvme_tcp_stop_ctrl(struct nvme_ctrl *ctrl)
+{
+	flush_work(&to_tcp_ctrl(ctrl)->err_work);
+	cancel_delayed_work_sync(&to_tcp_ctrl(ctrl)->connect_work);
 }
 
 static void nvme_tcp_free_ctrl(struct nvme_ctrl *nctrl)
@@ -2550,6 +2553,7 @@ static const struct nvme_ctrl_ops nvme_tcp_ctrl_ops = {
 	.submit_async_event	= nvme_tcp_submit_async_event,
 	.delete_ctrl		= nvme_tcp_delete_ctrl,
 	.get_address		= nvmf_get_address,
+	.stop_ctrl		= nvme_tcp_stop_ctrl,
 };
 
 static bool
