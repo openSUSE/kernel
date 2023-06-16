ff3f885112f22bbbc13bd13481763cb923899512
Nikolay Borisov <nik.borisov@suse.com>
2023-06-16 17:59:00 +0300

    x86/init: Initialize signal frame size late (bsc#1212448).
    suse-commit: 95c2ee8f3833e80c1eabc26b37f570dc0e82e6ce

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -65,7 +65,6 @@
 #include <asm/cpu_device_id.h>
 #include <asm/uv/uv.h>
 #include <asm/set_memory.h>
-#include <asm/sigframe.h>
 #include <asm/sev.h>
 
 #include "cpu.h"
@@ -1433,8 +1432,6 @@ static void __init early_identify_cpu(struct cpuinfo_x86 *c)
 	sld_setup(c);
 
 	fpu__init_system(c);
-
-	init_sigframe_size();
 
 #ifdef CONFIG_X86_32
 	/*
