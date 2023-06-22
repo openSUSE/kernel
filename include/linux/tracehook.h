141497f01b5c1452ca51a26b8c2bd85866c26f5a
Gabriel Krisman Bertazi <krisman@suse.de>
2023-06-22 00:08:31 -0400

    task_work: Introduce task_work_pending (git-fixes).
    suse-commit: 4e278e8be3087e02dfe5b7d26881856b3a5bae6a

--- include/linux/tracehook.h
+++ include/linux/tracehook.h
@@ -185,7 +185,7 @@ static inline void tracehook_notify_resume(struct pt_regs *regs)
 	 * hlist_add_head(task->task_works);
 	 */
 	smp_mb__after_atomic();
-	if (unlikely(current->task_works))
+	if (unlikely(task_work_pending(current)))
 		task_work_run();
 
 #ifdef CONFIG_KEYS_REQUEST_CACHE
@@ -208,7 +208,7 @@ static inline void tracehook_notify_signal(void)
 {
 	clear_thread_flag(TIF_NOTIFY_SIGNAL);
 	smp_mb__after_atomic();
-	if (current->task_works)
+	if (task_work_pending(current))
 		task_work_run();
 }
 
