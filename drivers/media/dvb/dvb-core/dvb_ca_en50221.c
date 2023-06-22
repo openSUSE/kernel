9a8b0dda1c1e80976d9ffdb16bcfa7ef7544d0ab
Takashi Iwai <tiwai@suse.de>
2023-06-22 12:25:04 +0200

    media: dvb-core: Fix use-after-free due to race condition at dvb_ca_en50221 (CVE-2022-45919 bsc#1205803).
    suse-commit: 395821d0c27cf3aa1e03228b0eaa3652a1837656

--- drivers/media/dvb/dvb-core/dvb_ca_en50221.c
+++ drivers/media/dvb/dvb-core/dvb_ca_en50221.c
@@ -156,6 +156,12 @@ struct dvb_ca_private {
 
 	/* Slot to start looking for data to read from in the next user-space read operation */
 	int next_read_slot;
+
+	/* A mutex used when a device is disconnected */
+	struct mutex remove_mutex;
+
+	/* Whether the device is disconnected */
+	int exit;
 };
 
 static void dvb_ca_en50221_thread_wakeup(struct dvb_ca_private *ca);
@@ -1519,12 +1525,22 @@ static int dvb_ca_en50221_io_open(struct inode *inode, struct file *file)
 
 	dprintk("%s\n", __func__);
 
-	if (!try_module_get(ca->pub->owner))
+	mutex_lock(&ca->remove_mutex);
+
+	if (ca->exit) {
+		mutex_unlock(&ca->remove_mutex);
+		return -ENODEV;
+	}
+
+	if (!try_module_get(ca->pub->owner)) {
+		mutex_unlock(&ca->remove_mutex);
 		return -EIO;
+	}
 
 	err = dvb_generic_open(inode, file);
 	if (err < 0) {
 		module_put(ca->pub->owner);
+		mutex_unlock(&ca->remove_mutex);
 		return err;
 	}
 
@@ -1543,6 +1559,7 @@ static int dvb_ca_en50221_io_open(struct inode *inode, struct file *file)
 	dvb_ca_en50221_thread_update_delay(ca);
 	dvb_ca_en50221_thread_wakeup(ca);
 
+	mutex_unlock(&ca->remove_mutex);
 	return 0;
 }
 
@@ -1563,6 +1580,8 @@ static int dvb_ca_en50221_io_release(struct inode *inode, struct file *file)
 
 	dprintk("%s\n", __func__);
 
+	mutex_lock(&ca->remove_mutex);
+
 	/* mark the CA device as closed */
 	ca->open = 0;
 	dvb_ca_en50221_thread_update_delay(ca);
@@ -1570,6 +1589,13 @@ static int dvb_ca_en50221_io_release(struct inode *inode, struct file *file)
 	err = dvb_generic_release(inode, file);
 
 	module_put(ca->pub->owner);
+
+	if (dvbdev->users == 1 && ca->exit == 1) {
+		mutex_unlock(&ca->remove_mutex);
+		wake_up(&dvbdev->wait_queue);
+	} else {
+		mutex_unlock(&ca->remove_mutex);
+	}
 
 	return err;
 }
@@ -1691,6 +1717,8 @@ int dvb_ca_en50221_init(struct dvb_adapter *dvb_adapter,
 		mutex_init(&ca->slot_info[i].slot_lock);
 	}
 
+	mutex_init(&ca->remove_mutex);
+
 	if (signal_pending(current)) {
 		ret = -EINTR;
 		goto error;
@@ -1734,6 +1762,14 @@ void dvb_ca_en50221_release(struct dvb_ca_en50221 *pubca)
 	int i;
 
 	dprintk("%s\n", __func__);
+
+	mutex_lock(&ca->remove_mutex);
+	ca->exit = 1;
+	mutex_unlock(&ca->remove_mutex);
+
+	if (ca->dvbdev->users < 1)
+		wait_event(ca->dvbdev->wait_queue,
+				ca->dvbdev->users == 1);
 
 	/* shutdown the thread if there was one */
 	kthread_stop(ca->thread);
