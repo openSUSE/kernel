666fd57a1f20a2ebbbb22a2869cca5ff48b09b9f
Nikolay Borisov <nik.borisov@suse.com>
2023-07-24 15:15:55 +0300

    x86/fpu: Move FPU initialization into arch_cpu_finalize_init() (bsc#1206418, CVE-2022-40982).
    suse-commit: d9e45bd1f75d9d53bd8a22b6d029cb6dd2497acb

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -1339,8 +1339,6 @@ static void __init early_identify_cpu(struct cpuinfo_x86 *c)
 
 	cpu_set_core_cap_bits(c);
 
-	fpu__init_system();
-
 #ifdef CONFIG_X86_32
 	/*
 	 * Regardless of whether PCID is enumerated, the SDM says
@@ -2060,8 +2058,6 @@ void cpu_init(void)
 	clear_all_debug_regs();
 	dbg_restore_debug_regs();
 
-	fpu__init_cpu();
-
 	if (is_uv_system())
 		uv_cpu_init();
 
@@ -2198,6 +2194,13 @@ void __init arch_cpu_finalize_init(void)
 		init_utsname()->machine[1] =
 			'0' + (boot_cpu_data.x86 > 6 ? 6 : boot_cpu_data.x86);
 	}
+
+	/*
+	 * Must be before alternatives because it might set or clear
+	 * feature bits.
+	 */
+	fpu__init_system();
+	fpu__init_cpu();
 
 	alternative_instructions();
 
