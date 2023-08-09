5ad3767d424ad49a32ad1682b46937b7c0701847
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2023-08-09 10:37:42 +0200

    timers: Rename del_timer() to timer_delete() (bsc#1213970).
    suse-commit: 3f18ac2027106c37b2667f0938d2c7b0abf74949

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -1204,7 +1204,7 @@ void add_timer_on(struct timer_list *timer, int cpu)
 EXPORT_SYMBOL_GPL(add_timer_on);
 
 /**
- * del_timer - Deactivate a timer.
+ * timer_delete - Deactivate a timer
  * @timer:	The timer to be deactivated
  *
  * The function only deactivates a pending timer, but contrary to
@@ -1217,7 +1217,7 @@ EXPORT_SYMBOL_GPL(add_timer_on);
  * * %0 - The timer was not pending
  * * %1 - The timer was pending and deactivated
  */
-int del_timer(struct timer_list *timer)
+int timer_delete(struct timer_list *timer)
 {
 	struct timer_base *base;
 	unsigned long flags;
@@ -1232,6 +1232,20 @@ int del_timer(struct timer_list *timer)
 	}
 
 	return ret;
+}
+EXPORT_SYMBOL(timer_delete);
+
+/**
+ * del_timer - Delete a pending timer
+ * @timer:     The timer to be deleted
+ *
+ * See timer_delete() for detailed explanation.
+ *
+ * Do not use in new code. Use timer_delete() instead.
+ */
+int del_timer(struct timer_list *timer)
+{
+	return timer_delete(timer);
 }
 EXPORT_SYMBOL(del_timer);
 
