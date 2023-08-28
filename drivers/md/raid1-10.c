869dc5ad865a41e3e068cb513917d3cb93bd50e3
NeilBrown <neilb@suse.de>
2023-08-28 15:55:52 +1000

    Refresh patches.suse/md-display-timeout-error.patch.
    Enable this patch which is still needed for SP6
    
    suse-commit: dd44ffdca1efa07e67e810e827d5cc17521fddaa

--- drivers/md/raid1-10.c
+++ drivers/md/raid1-10.c
@@ -110,16 +110,19 @@ static void md_bio_reset_resync_pages(struct bio *bio, struct resync_pages *rp,
 	} while (idx++ < RESYNC_PAGES && size > 0);
 }
 
-
 static inline void raid1_submit_write(struct bio *bio)
 {
 	struct md_rdev *rdev = (void *)bio->bi_bdev;
 
 	bio->bi_next = NULL;
 	bio_set_dev(bio, rdev->bdev);
-	if (test_bit(Faulty, &rdev->flags))
-		bio_io_error(bio);
-	else if (unlikely(bio_op(bio) ==  REQ_OP_DISCARD &&
+	if (test_bit(Faulty, &rdev->flags)) {
+		if (test_bit(Timeout, &rdev->flags))
+			bio->bi_status = BLK_STS_TIMEOUT;
+		else
+			bio->bi_status = BLK_STS_IOERR;
+		 bio_endio(bio);
+	} else if (unlikely(bio_op(bio) ==  REQ_OP_DISCARD &&
 			  !bdev_max_discard_sectors(bio->bi_bdev)))
 		/* Just ignore it */
 		bio_endio(bio);
