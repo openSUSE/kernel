2a461e55e7277c623a95d0db8316e166ac1bed4f
Luís Henriques <lhenriques@suse.de>
2023-09-08 17:20:44 +0100

    ceph: add some fscrypt guardrails (jsc#SES-1880).
    suse-commit: 4d92bc25052a3a566122cd61857589b5dbcadf12

--- fs/ceph/dir.c
+++ fs/ceph/dir.c
@@ -1145,6 +1145,10 @@ static int ceph_link(struct dentry *old_dentry, struct inode *dir,
 	if (ceph_snap(dir) != CEPH_NOSNAP)
 		return -EROFS;
 
+	err = fscrypt_prepare_link(old_dentry, dir, dentry);
+	if (err)
+		return err;
+
 	dout("link in dir %p old_dentry %p dentry %p\n", dir,
 	     old_dentry, dentry);
 	req = ceph_mdsc_create_request(mdsc, CEPH_MDS_OP_LINK, USE_AUTH_MDS);
@@ -1383,6 +1387,11 @@ static int ceph_rename(struct user_namespace *mnt_userns, struct inode *old_dir,
 		return -EXDEV;
 
 	err = ceph_wait_on_conflict_unlink(new_dentry);
+	if (err)
+		return err;
+
+	err = fscrypt_prepare_rename(old_dir, old_dentry, new_dir, new_dentry,
+				     flags);
 	if (err)
 		return err;
 
