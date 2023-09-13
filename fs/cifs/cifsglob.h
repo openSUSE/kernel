4c4dc30c486403a81b9fd1f7940420932bf6d268
Enzo Matsumiya <ematsumiya@suse.de>
2023-09-13 11:37:53 -0300

    cifs: fix refresh of cached referrals (bsc#1213476).
    suse-commit: fcfdfe62bfd6a7ff4cfceabdd875fd72a1072d94

--- fs/cifs/cifsglob.h
+++ fs/cifs/cifsglob.h
@@ -102,6 +102,8 @@
 
 #define CIFS_MAX_WORKSTATION_LEN  (__NEW_UTS_LEN + 1)  /* reasonable max for client */
 
+#define CIFS_DFS_ROOT_SES(ses) ((ses)->dfs_root_ses ?: (ses))
+
 /*
  * CIFS vfs client Status information (based on what we know.)
  */
@@ -1036,6 +1038,7 @@ struct cifs_ses {
 	 */
 	unsigned long chans_need_reconnect;
 	/* ========= end: protected by chan_lock ======== */
+	struct cifs_ses *dfs_root_ses;
 };
 
 static inline bool
