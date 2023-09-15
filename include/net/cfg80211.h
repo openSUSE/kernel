34b55227a766641dbbfa6605e77d9f03bdd1ab7b
Takashi Iwai <tiwai@suse.de>
2023-09-15 17:08:18 +0200

    wifi: cfg80211: Retrieve PSD information from RNR AP information (jsc#PED-6081 jsc#PED-6130).
    suse-commit: 831735aec17c3a5d6c761c12c51390513a551c5a

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -2468,6 +2468,7 @@ struct cfg80211_scan_info {
  * @short_ssid_valid: @short_ssid is valid and can be used
  * @psc_no_listen: when set, and the channel is a PSC channel, no need to wait
  *       20 TUs before starting to send probe requests.
+ * @psd_20: The AP's 20 MHz PSD value.
  */
 struct cfg80211_scan_6ghz_params {
 	u32 short_ssid;
@@ -2476,6 +2477,7 @@ struct cfg80211_scan_6ghz_params {
 	bool unsolicited_probe;
 	bool short_ssid_valid;
 	bool psc_no_listen;
+	s8 psd_20;
 };
 
 /**
