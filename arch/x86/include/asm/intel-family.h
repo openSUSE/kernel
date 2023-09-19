817294cedf6e0647f38e553120af4fd957b13497
Nikolay Borisov <nik.borisov@suse.com>
2023-09-19 09:29:39 +0300

    x86/cpu: Fix Crestmont uarch (jsc#PED-6469).
    suse-commit: e882a7f1943c2be56298591513647e32369d313c

--- arch/x86/include/asm/intel-family.h
+++ arch/x86/include/asm/intel-family.h
@@ -159,9 +159,8 @@
 #define INTEL_FAM6_ATOM_TREMONT		0x96 /* Elkhart Lake */
 #define INTEL_FAM6_ATOM_TREMONT_L	0x9C /* Jasper Lake */
 
-#define INTEL_FAM6_SIERRAFOREST_X	0xAF
-
-#define INTEL_FAM6_GRANDRIDGE		0xB6
+#define INTEL_FAM6_ATOM_CRESTMONT_X	0xAF /* Sierra Forest */
+#define INTEL_FAM6_ATOM_CRESTMONT	0xB6 /* Grand Ridge */
 
 #define INTEL_FAM6_SIERRAFOREST_X	0xAF
 
