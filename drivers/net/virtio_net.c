50734151ce404a4c0f57ec7904697b471c054cc0
Juergen Gross <jgross@suse.com>
2023-09-21 07:32:05 +0200

    virtio-net: set queues after driver_ok (git-fixes).
    suse-commit: a8caba5ded7504e1ad75be737bcafc8c38fd230c

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -3333,8 +3333,6 @@ static int virtnet_probe(struct virtio_device *vdev)
 		}
 	}
 
-	_virtnet_set_queues(vi, vi->curr_queue_pairs);
-
 	/* serialize netdev register + virtio_device_ready() with ndo_open() */
 	rtnl_lock();
 
@@ -3346,6 +3344,8 @@ static int virtnet_probe(struct virtio_device *vdev)
 	}
 
 	virtio_device_ready(vdev);
+
+	_virtnet_set_queues(vi, vi->curr_queue_pairs);
 
 	rtnl_unlock();
 
