b878b86506348e7dbd446e158137d84b775f1856
Thomas Zimmermann <tzimmermann@suse.com>
2023-09-27 13:39:49 +0200

    drm/ast: Add BMC virtual connector (bsc#1152472)
    Backporting changes:
    
    	* rename ast_device to ast_private
    
    suse-commit: 0eaf20cbfa9bb3d000075a6022cc68a32a709adb

--- drivers/gpu/drm/ast/ast_drv.h
+++ drivers/gpu/drm/ast/ast_drv.h
@@ -192,6 +192,10 @@ struct ast_private {
 			struct drm_encoder encoder;
 			struct drm_connector connector;
 		} astdp;
+		struct {
+			struct drm_encoder encoder;
+			struct drm_connector connector;
+		} bmc;
 	} output;
 
 	bool support_wide_screen;
