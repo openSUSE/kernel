5cbfb51970084b813afbae3b684b4bab01b629be
Frederic Weisbecker <fweisbecker@suse.de>
2023-09-28 14:27:29 +0200

    sched/rt: pick_next_rt_entity(): check list_entry (bsc#1208600 CVE-2023-1077)
    suse-commit: 6b1e84674681db0a1fd65dd913be3de6d35137f3

--- kernel/sched_rt.c
+++ kernel/sched_rt.c
@@ -1170,6 +1170,12 @@ static struct sched_rt_entity *pick_next_rt_entity(struct rq *rq,
 	BUG_ON(idx >= MAX_RT_PRIO);
 
 	queue = array->queue + idx;
+
+#ifdef CONFIG_SCHED_DEBUG
+	if (WARN_ON_ONCE(list_empty(queue)))
+		return NULL;
+#endif
+
 	next = list_entry(queue->next, struct sched_rt_entity, run_list);
 
 	return next;
@@ -1191,7 +1197,8 @@ static struct task_struct *_pick_next_task_rt(struct rq *rq)
 
 	do {
 		rt_se = pick_next_rt_entity(rq, rt_rq);
-		BUG_ON(!rt_se);
+		if (unlikely(!rt_se))
+			return NULL;
 		rt_rq = group_rt_rq(rt_se);
 	} while (rt_rq);
 
