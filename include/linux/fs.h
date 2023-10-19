02afb60c3c39337d1348d4d010734cffcab8f50f
Hannes Reinecke <hare@suse.de>
2023-10-19 13:18:06 +0200

    splice, net: Use sendmsg(MSG_SPLICE_PAGES) rather than ->sendpage() (bsc#1216396).
    suse-commit: 8619893a31b0ac33955d2be90be64ff46fcfd786

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2802,8 +2802,6 @@ ssize_t copy_splice_read(struct file *in, loff_t *ppos,
 			 size_t len, unsigned int flags);
 extern ssize_t iter_file_splice_write(struct pipe_inode_info *,
 		struct file *, loff_t *, size_t, unsigned int);
-extern ssize_t generic_splice_sendpage(struct pipe_inode_info *pipe,
-		struct file *out, loff_t *, size_t len, unsigned int flags);
 extern long do_splice_direct(struct file *in, loff_t *ppos, struct file *out,
 		loff_t *opos, size_t len, unsigned int flags);
 
