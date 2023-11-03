4db674979167531033c95beac3fe0bae5e93481e
Takashi Iwai <tiwai@suse.de>
2023-11-03 09:19:50 +0100

    ALSA: hda: cs35l41: Undo runtime PM changes at driver exit time (git-fixes).
    suse-commit: 7715c0c6a878c2f9e5c747c5d06f550cfeb19238

--- sound/pci/hda/cs35l41_hda.c
+++ sound/pci/hda/cs35l41_hda.c
@@ -1538,6 +1538,7 @@ int cs35l41_hda_probe(struct device *dev, const char *device_name, int id, int i
 	return 0;
 
 err_pm:
+	pm_runtime_dont_use_autosuspend(cs35l41->dev);
 	pm_runtime_disable(cs35l41->dev);
 	pm_runtime_put_noidle(cs35l41->dev);
 
@@ -1555,6 +1556,7 @@ void cs35l41_hda_remove(struct device *dev)
 	struct cs35l41_hda *cs35l41 = dev_get_drvdata(dev);
 
 	pm_runtime_get_sync(cs35l41->dev);
+	pm_runtime_dont_use_autosuspend(cs35l41->dev);
 	pm_runtime_disable(cs35l41->dev);
 
 	if (cs35l41->halo_initialized)
