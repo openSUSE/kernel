b8ce4a68f139616f1d2f932e61126de6e58e4b5c
Takashi Iwai <tiwai@suse.de>
2023-11-09 09:43:57 +0100

    r8152: Check for unplug in r8153b_ups_en() / r8153c_ups_en() (git-fixes).
    suse-commit: b994636e950932f90aeb3ab107a51e14f6425f82

--- drivers/net/usb/r8152.c
+++ drivers/net/usb/r8152.c
@@ -3650,6 +3650,8 @@ static void r8153b_ups_en(struct r8152 *tp, bool enable)
 			int i;
 
 			for (i = 0; i < 500; i++) {
+				if (test_bit(RTL8152_UNPLUG, &tp->flags))
+					return;
 				if (ocp_read_word(tp, MCU_TYPE_PLA, PLA_BOOT_CTRL) &
 				    AUTOLOAD_DONE)
 					break;
@@ -3690,6 +3692,8 @@ static void r8153c_ups_en(struct r8152 *tp, bool enable)
 			int i;
 
 			for (i = 0; i < 500; i++) {
+				if (test_bit(RTL8152_UNPLUG, &tp->flags))
+					return;
 				if (ocp_read_word(tp, MCU_TYPE_PLA, PLA_BOOT_CTRL) &
 				    AUTOLOAD_DONE)
 					break;
