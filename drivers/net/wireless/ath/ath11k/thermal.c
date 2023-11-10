5e05822052f784bf15531fa034e9045d36750f67
Takashi Iwai <tiwai@suse.de>
2023-11-10 10:36:21 +0100

    wifi: ath11k: thermal: don't try to register multiple times (bsc#1207948).
    suse-commit: 632c7016ed002b304a954bccd9773f7d06f549ac

--- drivers/net/wireless/ath/ath11k/thermal.c
+++ drivers/net/wireless/ath/ath11k/thermal.c
@@ -162,6 +162,9 @@ int ath11k_thermal_register(struct ath11k_base *ab)
 	struct ath11k_pdev *pdev;
 	int i, ret;
 
+	if (test_bit(ATH11K_FLAG_REGISTERED, &ab->dev_flags))
+		return 0;
+
 	for (i = 0; i < ab->num_radios; i++) {
 		pdev = &ab->pdevs[i];
 		ar = pdev->ar;
