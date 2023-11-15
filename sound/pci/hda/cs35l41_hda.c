b0b45d5b71cb67475bbaea57a2c32b091abd6c63
Takashi Iwai <tiwai@suse.de>
2023-11-15 14:58:30 +0100

    ASoC: cs35l41: Fix broken shared boost activation (bsc#1213745).
    suse-commit: 418dee2f0b33f4fcce70a21475813ad454ef9a34

--- sound/pci/hda/cs35l41_hda.c
+++ sound/pci/hda/cs35l41_hda.c
@@ -598,7 +598,7 @@ static void cs35l41_hda_play_done(struct device *dev)
 
 	dev_dbg(dev, "Play (Complete)\n");
 
-	cs35l41_global_enable(dev, reg, cs35l41->hw_cfg.bst_type, 1, NULL,
+	cs35l41_global_enable(dev, reg, cs35l41->hw_cfg.bst_type, 1,
 			      cs35l41->firmware_running);
 	cs35l41_mute(dev, false);
 }
@@ -611,7 +611,7 @@ static void cs35l41_hda_pause_start(struct device *dev)
 	dev_dbg(dev, "Pause (Start)\n");
 
 	cs35l41_mute(dev, true);
-	cs35l41_global_enable(dev, reg, cs35l41->hw_cfg.bst_type, 0, NULL,
+	cs35l41_global_enable(dev, reg, cs35l41->hw_cfg.bst_type, 0,
 			      cs35l41->firmware_running);
 }
 
