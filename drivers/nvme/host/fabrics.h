e533503f689ded760cd7251cde5acc31b2d5c219
Daniel Wagner <dwagner@suse.de>
2023-11-15 09:55:37 +0100

    nvme-fabrics: parse options 'keyring' and 'tls_key' (jsc#PED-6252 jsc#PED-5728 jsc#PED-5062 jsc#PED-3535).
    Refresh:
     - patches.suse/nvme-tcp-add-recovery_delay-to-sysfs.patch
    
    suse-commit: ab26c1d7b10603e0d738148a94a8605c7c0b58f9

--- drivers/nvme/host/fabrics.h
+++ drivers/nvme/host/fabrics.h
@@ -71,7 +71,9 @@ enum {
 	NVMF_OPT_DHCHAP_SECRET	= 1 << 23,
 	NVMF_OPT_DHCHAP_CTRL_SECRET = 1 << 24,
 	NVMF_OPT_TLS		= 1 << 25,
-	NVMF_OPT_RECOVERY_DELAY = 1 << 26,
+	NVMF_OPT_KEYRING	= 1 << 26,
+	NVMF_OPT_TLS_KEY	= 1 << 27,
+	NVMF_OPT_RECOVERY_DELAY = 1 << 28,
 };
 
 /**
@@ -105,6 +107,8 @@ enum {
  * @dhchap_secret: DH-HMAC-CHAP secret
  * @dhchap_ctrl_secret: DH-HMAC-CHAP controller secret for bi-directional
  *              authentication
+ * @keyring:    Keyring to use for key lookups
+ * @tls_key:    TLS key for encrypted connections (TCP)
  * @tls:        Start TLS encrypted connections (TCP)
  * @disable_sqflow: disable controller sq flow control
  * @hdr_digest: generate/verify header digest (TCP)
@@ -133,6 +137,8 @@ struct nvmf_ctrl_options {
 	struct nvmf_host	*host;
 	char			*dhchap_secret;
 	char			*dhchap_ctrl_secret;
+	struct key		*keyring;
+	struct key		*tls_key;
 	bool			tls;
 	bool			disable_sqflow;
 	bool			hdr_digest;
