ea25c9941b4e61435685e098fe6559592f49edc9
Mel Gorman <mgorman@suse.de>
2023-11-16 12:17:57 +0000

    mm/page_alloc: correct start page when guard page debug is enabled (bsc#1212886 (MM functional and performance backports)).
    suse-commit: 4159e7f60a3bec7ecfbb2564ca82ef66c0769ad1

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -6525,6 +6525,7 @@ static void break_down_buddy_pages(struct zone *zone, struct page *page,
 			next_page = page;
 			current_buddy = page + size;
 		}
+		page = next_page;
 
 		if (set_page_guard(zone, current_buddy, high, migratetype))
 			continue;
@@ -6532,7 +6533,6 @@ static void break_down_buddy_pages(struct zone *zone, struct page *page,
 		if (current_buddy != target) {
 			add_to_free_list(current_buddy, zone, high, migratetype);
 			set_buddy_order(current_buddy, high);
-			page = next_page;
 		}
 	}
 }
