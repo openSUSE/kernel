51b384da8f1b9cc163c7f88c0959133702aa189d
Juergen Gross <jgross@suse.com>
2023-11-20 10:43:58 +0100

    KVM: VMX: Rename vmx_get_max_tdp_level() to vmx_get_max_ept_level() (jsc#PED-7322).
    suse-commit: 9fc841c19aa4d9b07150d3e3d23a3ed36c61f063

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -3375,7 +3375,7 @@ void vmx_set_cr0(struct kvm_vcpu *vcpu, unsigned long cr0)
 	vmx->emulation_required = vmx_emulation_required(vcpu);
 }
 
-static int vmx_get_max_tdp_level(void)
+static int vmx_get_max_ept_level(void)
 {
 	if (cpu_has_vmx_ept_5levels())
 		return 5;
@@ -8554,7 +8554,7 @@ static __init int hardware_setup(void)
 	 */
 	vmx_setup_me_spte_mask();
 
-	kvm_configure_mmu(enable_ept, 0, vmx_get_max_tdp_level(),
+	kvm_configure_mmu(enable_ept, 0, vmx_get_max_ept_level(),
 			  ept_caps_to_lpage_level(vmx_capability.ept));
 
 	/*
