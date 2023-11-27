750951c5d6afa0d006385102ec432138c3447fd6
Jan Kara <jack@suse.cz>
2023-11-27 14:30:07 +0100

    sbitmap: fix up kABI for sbitmap_queue_wake_up() (bsc#1217095 bsc#1217196).
    suse-commit: 47ffbc4b8c4062e43293faf6a5e78deebe36973b

--- lib/sbitmap.c
+++ lib/sbitmap.c
@@ -686,10 +686,16 @@ static bool __sbq_wake_up(struct sbitmap_queue *sbq, int *nr)
 	return *nr;
 }
 
-void sbitmap_queue_wake_up(struct sbitmap_queue *sbq, int nr)
+void sbitmap_queue_wake_up_batch(struct sbitmap_queue *sbq, int nr)
 {
 	while (__sbq_wake_up(sbq, &nr))
 		;
+}
+EXPORT_SYMBOL_GPL(sbitmap_queue_wake_up_batch);
+
+void sbitmap_queue_wake_up(struct sbitmap_queue *sbq)
+{
+	sbitmap_queue_wake_up_batch(sbq, 1);
 }
 EXPORT_SYMBOL_GPL(sbitmap_queue_wake_up);
 
@@ -728,7 +734,7 @@ void sbitmap_queue_clear_batch(struct sbitmap_queue *sbq, int offset,
 		atomic_long_andnot(mask, (atomic_long_t *) addr);
 
 	smp_mb__after_atomic();
-	sbitmap_queue_wake_up(sbq, nr_tags);
+	sbitmap_queue_wake_up_batch(sbq, nr_tags);
 	sbitmap_update_cpu_hint(&sbq->sb, raw_smp_processor_id(),
 					tags[nr_tags - 1] - offset);
 }
@@ -756,7 +762,7 @@ void sbitmap_queue_clear(struct sbitmap_queue *sbq, unsigned int nr,
 	 * waiter. See the comment on waitqueue_active().
 	 */
 	smp_mb__after_atomic();
-	sbitmap_queue_wake_up(sbq, 1);
+	sbitmap_queue_wake_up_batch(sbq, 1);
 	sbitmap_update_cpu_hint(&sbq->sb, cpu, nr);
 }
 EXPORT_SYMBOL_GPL(sbitmap_queue_clear);
