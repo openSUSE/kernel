6f4a56cde972f4a03d00063146a8280a1366c2b8
Juergen Gross <jgross@suse.com>
2023-12-10 07:35:19 +0100

    KVM: x86: Add IBPB_BRTYPE support (jsc#PED-7322).
    suse-commit: 3d5428113aa6dd9da860f0003523517236ef9f43

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -762,8 +762,8 @@ void kvm_set_cpu_caps(void)
 		F(PERFMON_V2)
 	);
 
-	if (cpu_feature_enabled(X86_FEATURE_SRSO_NO))
-		kvm_cpu_cap_set(X86_FEATURE_SRSO_NO);
+	kvm_cpu_cap_check_and_set(X86_FEATURE_IBPB_BRTYPE);
+	kvm_cpu_cap_check_and_set(X86_FEATURE_SRSO_NO);
 
 	/*
 	 * Synthesize "LFENCE is serializing" into the AMD-defined entry in
