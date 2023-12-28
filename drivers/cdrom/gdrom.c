622670388395c659178649441b36c450227cefd9
Lee Duncan <lduncan@suse.com>
2023-12-28 10:56:15 -0800

    block: remove the unused mode argument to ->release (bsc#1216436).
    suse-commit: cf9697e000a85f199ed010410501ae3d5d6eda49

--- drivers/cdrom/gdrom.c
+++ drivers/cdrom/gdrom.c
@@ -486,10 +486,10 @@ static int gdrom_bdops_open(struct gendisk *disk, fmode_t mode)
 	return ret;
 }
 
-static void gdrom_bdops_release(struct gendisk *disk, fmode_t mode)
+static void gdrom_bdops_release(struct gendisk *disk)
 {
 	mutex_lock(&gdrom_mutex);
-	cdrom_release(gd.cd_info, mode);
+	cdrom_release(gd.cd_info, NULL);
 	mutex_unlock(&gdrom_mutex);
 }
 
