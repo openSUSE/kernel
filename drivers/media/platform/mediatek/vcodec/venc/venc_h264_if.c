efa3f70830402573ae7936412efb1d5723a44167
Oliver Neukum <oneukum@suse.com>
2024-01-10 14:00:04 +0100

    - media: mediatek: vcodec: Removing struct 'mtk_vcodec_ctx/dev'   for shared interface (git-fixes). - Refresh   patches.suse/media-mediatek-vcodec-Removing-useless-debug-log.patch.
    suse-commit: 595721ba9cdef8c8f7a7b02e14902ff0eb1ce8a3

--- drivers/media/platform/mediatek/vcodec/venc/venc_h264_if.c
+++ drivers/media/platform/mediatek/vcodec/venc/venc_h264_if.c
@@ -599,7 +599,7 @@ static int h264_enc_init(struct mtk_vcodec_ctx *ctx)
 	inst->ctx = ctx;
 	inst->vpu_inst.ctx = ctx;
 	inst->vpu_inst.id = is_ext ? SCP_IPI_VENC_H264 : IPI_VENC_H264;
-	inst->hw_base = mtk_vcodec_get_reg_addr(inst->ctx, VENC_SYS);
+	inst->hw_base = mtk_vcodec_get_reg_addr(inst->ctx->dev->reg_base, VENC_SYS);
 
 	ret = vpu_enc_init(&inst->vpu_inst);
 
