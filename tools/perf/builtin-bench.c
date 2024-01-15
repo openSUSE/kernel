d87d066df2037e131a64ebe7c262d1a010b1e7fd
Tony Jones <tonyj@suse.de>
2024-01-14 23:55:52 -0800

    perf bench uprobe: Add benchmark to test uprobe overhead (perf-v6.7 (jsc#PED-6012 jsc#PED-6121)).
    suse-commit: 1f8707dd71e5beea5c95e17686f0b03cfc98e76b

--- tools/perf/builtin-bench.c
+++ tools/perf/builtin-bench.c
@@ -105,6 +105,11 @@ static struct bench breakpoint_benchmarks[] = {
 	{ NULL,	NULL, NULL },
 };
 
+static struct bench uprobe_benchmarks[] = {
+	{ "baseline",	"Baseline libc usleep(1000) call",	bench_uprobe_baseline,	},
+	{ NULL,	NULL, NULL },
+};
+
 struct collection {
 	const char	*name;
 	const char	*summary;
@@ -124,6 +129,7 @@ static struct collection collections[] = {
 #endif
 	{ "internals",	"Perf-internals benchmarks",			internals_benchmarks	},
 	{ "breakpoint",	"Breakpoint benchmarks",			breakpoint_benchmarks	},
+	{ "uprobe",	"uprobe benchmarks",				uprobe_benchmarks	},
 	{ "all",	"All benchmarks",				NULL			},
 	{ NULL,		NULL,						NULL			}
 };
