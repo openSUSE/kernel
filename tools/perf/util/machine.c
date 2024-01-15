6d66a808a9305c2c3ca461320bfd03592f355738
Tony Jones <tonyj@suse.de>
2024-01-14 22:58:56 -0800

    perf machine: Fix leak of kernel dso (perf-v6.7 (jsc#PED-6012 jsc#PED-6121)).
    suse-commit: dcc8817f9f610306bfc2ac8dbdef95713f6a3b73

--- tools/perf/util/machine.c
+++ tools/perf/util/machine.c
@@ -1867,7 +1867,7 @@ static int machine__process_kernel_mmap_event(struct machine *machine,
 				continue;
 
 
-			kernel = dso;
+			kernel = dso__get(dso);
 			break;
 		}
 
@@ -1912,6 +1912,7 @@ static int machine__process_kernel_mmap_event(struct machine *machine,
 			 */
 			dso__load(kernel, machine__kernel_map(machine));
 		}
+		dso__put(kernel);
 	} else if (perf_event__is_extra_kernel_mmap(machine, xm)) {
 		return machine__process_extra_kernel_map(machine, xm);
 	}
