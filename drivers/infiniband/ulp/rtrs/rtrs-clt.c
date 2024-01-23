ddb149dd373fdae1ffb92fc4ea069100f850a910
Nicolas Morey <nmorey@suse.com>
2024-01-23 11:01:06 +0100

    RDMA/rtrs-clt: Fix the max_send_wr setting (git-fixes)
    suse-commit: 63679fd49961f5c0b8476f7816ffd334d6530293

--- drivers/infiniband/ulp/rtrs/rtrs-clt.c
+++ drivers/infiniband/ulp/rtrs/rtrs-clt.c
@@ -1674,7 +1674,7 @@ static int create_con_cq_qp(struct rtrs_clt_con *con)
 		sess->s.dev_ref++;
 		max_send_wr = min_t(int, wr_limit,
 			      /* QD * (REQ + RSP + FR REGS or INVS) + drain */
-			      sess->queue_depth * 3 + 1);
+			      sess->queue_depth * 4 + 1);
 		max_recv_wr = min_t(int, wr_limit,
 			      sess->queue_depth * 3 + 1);
 		max_send_sge = 2;
