c3cfd90658c2e800fe9f50278d81bb75d25e1e1d
Takashi Iwai <tiwai@suse.de>
2024-01-27 09:34:04 +0100

    spi: spi-cadence: Reverse the order of interleaved write and read operations (git-fixes).
    suse-commit: 4963b941867499eebf76a6169cc48f3e78777b1f

--- drivers/spi/spi-cadence.c
+++ drivers/spi/spi-cadence.c
@@ -316,6 +316,15 @@ static void cdns_spi_process_fifo(struct cdns_spi *xspi, int ntx, int nrx)
 	xspi->rx_bytes -= nrx;
 
 	while (ntx || nrx) {
+		if (nrx) {
+			u8 data = cdns_spi_read(xspi, CDNS_SPI_RXD);
+
+			if (xspi->rxbuf)
+				*xspi->rxbuf++ = data;
+
+			nrx--;
+		}
+
 		if (ntx) {
 			if (xspi->txbuf)
 				cdns_spi_write(xspi, CDNS_SPI_TXD, *xspi->txbuf++);
@@ -325,14 +334,6 @@ static void cdns_spi_process_fifo(struct cdns_spi *xspi, int ntx, int nrx)
 			ntx--;
 		}
 
-		if (nrx) {
-			u8 data = cdns_spi_read(xspi, CDNS_SPI_RXD);
-
-			if (xspi->rxbuf)
-				*xspi->rxbuf++ = data;
-
-			nrx--;
-		}
 	}
 }
 
