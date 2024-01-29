f7c6612963c68a9724f0d3f1e989a052127935af
Petr Pavlu <petr.pavlu@suse.com>
2024-01-29 18:23:17 +0100

    bpf: Limit the number of kprobes when attaching program to multiple kprobes (git-fixes).
    suse-commit: ecd4878cba858a8fd60405ec315ddc17b468a432

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -37,6 +37,8 @@
 #define bpf_event_rcu_dereference(p)					\
 	rcu_dereference_protected(p, lockdep_is_held(&bpf_event_mutex))
 
+#define MAX_KPROBE_MULTI_CNT (1U << 20)
+
 #ifdef CONFIG_MODULES
 struct bpf_trace_module {
 	struct module *module;
@@ -2517,6 +2519,8 @@ int bpf_kprobe_multi_link_attach(const union bpf_attr *attr, struct bpf_prog *pr
 	cnt = attr->link_create.kprobe_multi.cnt;
 	if (!cnt)
 		return -EINVAL;
+	if (cnt > MAX_KPROBE_MULTI_CNT)
+		return -E2BIG;
 
 	size = cnt * sizeof(*addrs);
 	addrs = kvmalloc_array(cnt, sizeof(*addrs), GFP_KERNEL);
