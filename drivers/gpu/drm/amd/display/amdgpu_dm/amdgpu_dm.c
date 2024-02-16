4dc1e3804f9cdbf37b6932fdc67f6e6a2a326531
Patrik Jakobsson <pjakobsson@suse.de>
2024-02-16 14:21:49 +0100

    drm/amd: Propagate failures in dc_set_power_state() (jsc#PED-3527 jsc#PED-5475 jsc#PED-6068 jsc#PED-6070 jsc#PED-6116 jsc#PED-6120 jsc#PED-5065 jsc#PED-5477 jsc#PED-5511 jsc#PED-6041 jsc#PED-6069 jsc#PED-6071)
    suse-commit: 0393b45975da31388ba2194f98390ae8a3f1cb81

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -2674,9 +2674,7 @@ static int dm_suspend(void *handle)
 
 	hpd_rx_irq_work_suspend(dm);
 
-	dc_set_power_state(dm->dc, DC_ACPI_CM_POWER_STATE_D3);
-
-	return 0;
+	return dc_set_power_state(dm->dc, DC_ACPI_CM_POWER_STATE_D3);
 }
 
 struct amdgpu_dm_connector *
@@ -2870,7 +2868,10 @@ static int dm_resume(void *handle)
 		if (r)
 			DRM_ERROR("DMUB interface failed to initialize: status=%d\n", r);
 
-		dc_set_power_state(dm->dc, DC_ACPI_CM_POWER_STATE_D0);
+		r = dc_set_power_state(dm->dc, DC_ACPI_CM_POWER_STATE_D0);
+		if (r)
+			return r;
+
 		dc_resume(dm->dc);
 
 		amdgpu_dm_irq_resume_early(adev);
@@ -2919,7 +2920,9 @@ static int dm_resume(void *handle)
 	}
 
 	/* power on hardware */
-	dc_set_power_state(dm->dc, DC_ACPI_CM_POWER_STATE_D0);
+	r = dc_set_power_state(dm->dc, DC_ACPI_CM_POWER_STATE_D0);
+	if (r)
+		return r;
 
 	/* program HPD filter */
 	dc_resume(dm->dc);
