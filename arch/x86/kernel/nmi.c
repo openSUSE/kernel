827d255c05b43d58bb05aef7e4dd0cbc5cf01fe5
Nikolay Borisov <nik.borisov@suse.com>
2024-02-16 13:42:20 +0200

    x86/bugs: Use ALTERNATIVE() instead of mds_user_clear static key (git-fixes).
    Also add the removed mds_user_clear symbol to kABI severities as it is
    exposed just for KVM module and is generally a core kernel component so
    removing it is low risk.
    
    suse-commit: 6976e6f46f159be6e0494415d203f91fef335fe8

--- arch/x86/kernel/nmi.c
+++ arch/x86/kernel/nmi.c
@@ -571,9 +571,6 @@ nmi_restart:
 		write_cr2(this_cpu_read(nmi_cr2));
 	if (this_cpu_dec_return(nmi_state))
 		goto nmi_restart;
-
-	if (user_mode(regs))
-		mds_user_clear_cpu_buffers();
 }
 NOKPROBE_SYMBOL(do_nmi);
 
