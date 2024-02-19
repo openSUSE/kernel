2114728e2fa70ca1be9c267dbf5e9b16e5717ac1
Torsten Duwe <duwe@suse.de>
2024-02-19 14:48:15 +0100

    dmaengine: idxd: Rename drv_enable/disable_wq to idxd_drv_enable/disable_wq, and export (jsc#PED-7793).
    suse-commit: b1af12de4f185f2d5fefb1350698315d031aa605

--- drivers/dma/idxd/device.c
+++ drivers/dma/idxd/device.c
@@ -1359,7 +1359,7 @@ err_irq:
 	return rc;
 }
 
-int drv_enable_wq(struct idxd_wq *wq)
+int idxd_drv_enable_wq(struct idxd_wq *wq)
 {
 	struct idxd_device *idxd = wq->idxd;
 	struct device *dev = &idxd->pdev->dev;
@@ -1491,8 +1491,9 @@ err_map_portal:
 err:
 	return rc;
 }
+EXPORT_SYMBOL_NS_GPL(idxd_drv_enable_wq, IDXD);
 
-void drv_disable_wq(struct idxd_wq *wq)
+void idxd_drv_disable_wq(struct idxd_wq *wq)
 {
 	struct idxd_device *idxd = wq->idxd;
 	struct device *dev = &idxd->pdev->dev;
@@ -1512,6 +1513,7 @@ void drv_disable_wq(struct idxd_wq *wq)
 	wq->type = IDXD_WQT_NONE;
 	wq->client_count = 0;
 }
+EXPORT_SYMBOL_NS_GPL(idxd_drv_disable_wq, IDXD);
 
 int idxd_device_drv_probe(struct idxd_dev *idxd_dev)
 {
