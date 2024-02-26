85139480d9db4de122675a743b8d25fd2af82639
Denis Kirjanov <denis.kirjanov@suse.com>
2024-02-26 11:53:35 +0300

    net: fec: fix the unhandled context fault from smmu (git-fixes).
    suse-commit: 2b9f00cd9641c6be4dbd8dc91ea30efdf42abad3

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -1987,6 +1987,7 @@ static void fec_enet_adjust_link(struct net_device *ndev)
 
 		/* if any of the above changed restart the FEC */
 		if (status_change) {
+			netif_stop_queue(ndev);
 			napi_disable(&fep->napi);
 			netif_tx_lock_bh(ndev);
 			fec_restart(ndev);
@@ -1996,6 +1997,7 @@ static void fec_enet_adjust_link(struct net_device *ndev)
 		}
 	} else {
 		if (fep->link) {
+			netif_stop_queue(ndev);
 			napi_disable(&fep->napi);
 			netif_tx_lock_bh(ndev);
 			fec_stop(ndev);
