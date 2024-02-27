86c207b7c6f567078ac74cb82ec482a3cd04f4da
Jiri Wiesner <jwiesner@suse.de>
2024-02-27 21:28:10 +0100

    - tcp: enforce receive buffer memory limits by allowing the tcp   window to shrink (bsc#1220419). - Refresh   patches.suse/tcp-Set-pingpong-threshold-via-sysctl.patch.
    suse-commit: e635b781014423d1c0203b45fe2331c921ccfca8

--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -1481,6 +1481,15 @@ static struct ctl_table ipv4_net_table[] = {
 		.extra2		= &tcp_syn_linear_timeouts_max,
 	},
 	{
+		.procname	= "tcp_shrink_window",
+		.data		= &init_net.ipv4.sysctl_tcp_shrink_window,
+		.maxlen		= sizeof(u8),
+		.mode		= 0644,
+		.proc_handler	= proc_dou8vec_minmax,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_ONE,
+	},
+	{
 		.procname	= "tcp_pingpong_thresh",
 		.data		= &init_net.ipv4.sysctl_tcp_pingpong_thresh,
 		.maxlen		= sizeof(u8),
