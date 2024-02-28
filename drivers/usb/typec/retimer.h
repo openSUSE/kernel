c06888f6425da6521143d2b419bdc6f7d4b1e4a4
Oliver Neukum <oneukum@suse.com>
2024-02-28 13:20:26 +0100

    paddings: add paddings to TypeC stuff (bsc#1220369).
    suse-commit: 8b36dec4f7b101f547284f5a78b85f0c6f21e728

--- drivers/usb/typec/retimer.h
+++ drivers/usb/typec/retimer.h
@@ -8,6 +8,7 @@
 struct typec_retimer {
 	struct device dev;
 	typec_retimer_set_fn_t set;
+	void *suse_kabi_padding;
 };
 
 #define to_typec_retimer(_dev_) container_of(_dev_, struct typec_retimer, dev)
