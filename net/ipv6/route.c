60e9f320149c2c9f04984ebd52d2b435096ce07e
Michal Kubecek <mkubecek@suse.cz>
2024-03-05 00:14:22 +0100

    kabi: restore return type of dst_ops::gc() callback (CVE-2023-52340 bsc#1219295).
    suse-commit: 077e12dc23d1b29d7b1974460c1b0db1d0153bc7

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -88,7 +88,7 @@ static struct dst_entry *ip6_negative_advice(struct dst_entry *);
 static void		ip6_dst_destroy(struct dst_entry *);
 static void		ip6_dst_ifdown(struct dst_entry *,
 				       struct net_device *dev, int how);
-static void		 ip6_dst_gc(struct dst_ops *ops);
+static int		 ip6_dst_gc(struct dst_ops *ops);
 
 static int		ip6_pkt_discard(struct sk_buff *skb);
 static int		ip6_pkt_discard_out(struct net *net, struct sock *sk, struct sk_buff *skb);
@@ -3182,7 +3182,7 @@ out:
 	return dst;
 }
 
-static void ip6_dst_gc(struct dst_ops *ops)
+static int ip6_dst_gc(struct dst_ops *ops)
 {
 	struct net *net = container_of(ops, struct net, ipv6.ip6_dst_ops);
 	int rt_min_interval = net->ipv6.sysctl.ip6_rt_gc_min_interval;
@@ -3202,6 +3202,7 @@ static void ip6_dst_gc(struct dst_ops *ops)
 		net->ipv6.ip6_rt_gc_expire = rt_gc_timeout>>1;
 out:
 	net->ipv6.ip6_rt_gc_expire -= net->ipv6.ip6_rt_gc_expire>>rt_elasticity;
+	return 0;
 }
 
 static int ip6_nh_lookup_table(struct net *net, struct fib6_config *cfg,
