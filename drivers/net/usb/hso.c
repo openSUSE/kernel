a58522b949232b7f2dd67d81ceedcba89ff9cb5f
Oliver Neukum <oneukum@suse.com>
2024-03-07 14:26:07 +0100

    usb: hso: check for return value in hso_serial_common_create() (bsc#1220416 CVE-2021-46904).
    suse-commit: 0b84dbfaa9571e1ad51922e78ebebf228486b437

--- drivers/net/usb/hso.c
+++ drivers/net/usb/hso.c
@@ -2310,11 +2310,13 @@ static int hso_serial_common_create(struct hso_serial *serial, int num_urbs,
 
 	minor = get_free_serial_index();
 	if (minor < 0)
-		goto exit;
+		goto exit2;
 
 	/* register our minor number */
 	serial->parent->dev = tty_register_device(tty_drv, minor,
 					&serial->parent->interface->dev);
+	if (IS_ERR(serial->parent->dev))
+		goto exit2;
 	dev = serial->parent->dev;
 	dev_set_drvdata(dev, serial->parent);
 	i = device_create_file(dev, &dev_attr_hsotype);
@@ -2371,6 +2373,7 @@ static int hso_serial_common_create(struct hso_serial *serial, int num_urbs,
 	return 0;
 exit:
 	hso_serial_tty_unregister(serial);
+exit2:
 	hso_serial_common_free(serial);
 	return -1;
 }
