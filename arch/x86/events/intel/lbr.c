8b49f9f5dbddab0c5b67362045e2bcc38d2b8b03
Tony Jones <tonyj@suse.de>
2024-03-18 12:55:50 -0700

    perf/x86/lbr: Filter vsyscall addresses (bsc#1220703, CVE-2023-52476).
    suse-commit: c46d003032c885bd4396b65675704a68342b090e

--- arch/x86/events/intel/lbr.c
+++ arch/x86/events/intel/lbr.c
@@ -5,6 +5,7 @@
 #include <asm/perf_event.h>
 #include <asm/msr.h>
 #include <asm/insn.h>
+#include <linux/mm.h>
 
 #include "../perf_event.h"
 
@@ -873,9 +874,9 @@ static int branch_type(unsigned long from, unsigned long to, int abort)
 		 * The LBR logs any address in the IP, even if the IP just
 		 * faulted. This means userspace can control the from address.
 		 * Ensure we don't blindy read any address by validating it is
-		 * a known text address.
+		 * a known text address and not a vsyscall address.
 		 */
-		if (kernel_text_address(from)) {
+		if (kernel_text_address(from) && !in_gate_area_no_mm(from)) {
 			addr = (void *)from;
 			/*
 			 * Assume we can get the maximum possible size
