7c1758dc45a00a5ea8e9823adc0aaeb4f88acfc3
Takashi Iwai <tiwai@suse.de>
2024-03-19 11:00:53 +0100

    drm/amd/display: update extended blank for dcn314 onwards (git-fixes).
    suse-commit: 36ba0999808e8f8f684170c849acc716f87afc02

--- drivers/gpu/drm/amd/display/dc/dc.h
+++ drivers/gpu/drm/amd/display/dc/dc.h
@@ -1544,8 +1544,6 @@ struct dc_sink_init_data {
 	bool converter_disable_audio;
 };
 
-bool dc_extended_blank_supported(struct dc *dc);
-
 struct dc_sink *dc_sink_create(const struct dc_sink_init_data *init_params);
 
 /* Newer interfaces  */
