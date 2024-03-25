683edc27806513b4e723334141645899b5b2d9e8
Coly Li <colyli@suse.de>
2024-03-26 00:59:43 +0800

    md: preserve KABI in struct md_personality (jsc#PED-7542).
    suse-commit: ee8ae3ad8bc1d74cd4dae62d644c7498941fe155

--- drivers/md/md.h
+++ drivers/md/md.h
@@ -633,7 +633,6 @@ struct md_personality
 	int (*start_reshape) (struct mddev *mddev);
 	void (*finish_reshape) (struct mddev *mddev);
 	void (*update_reshape_pos) (struct mddev *mddev);
-	void (*prepare_suspend) (struct mddev *mddev);
 	/* quiesce suspends or resumes internal processing.
 	 * 1 - stop new actions and wait for action io to complete
 	 * 0 - return to normal behaviour
@@ -651,6 +650,9 @@ struct md_personality
 	void *(*takeover) (struct mddev *mddev);
 	/* Changes the consistency policy of an active array. */
 	int (*change_consistency_policy)(struct mddev *mddev, const char *buf);
+#ifndef __GENKSYMS__
+	void (*prepare_suspend) (struct mddev *mddev);
+#endif
 };
 
 struct md_sysfs_entry {
