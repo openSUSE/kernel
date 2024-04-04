cda8a8d375c31134f377ccb88e43087f29af679b
Takashi Iwai <tiwai@suse.de>
2024-04-04 08:31:55 +0200

    drm/amd/display: Override min required DCFCLK in dml1_validate (stable-fixes).
    suse-commit: 1a57f949e7fd068a5f06b6da5d7970ec4b4450aa

--- drivers/gpu/drm/amd/display/dc/dcn32/dcn32_resource_helpers.c
+++ drivers/gpu/drm/amd/display/dc/dcn32/dcn32_resource_helpers.c
@@ -778,3 +778,9 @@ bool dcn32_subvp_vblank_admissable(struct dc *dc, struct dc_state *context, int 
 
 	return result;
 }
+
+void dcn32_override_min_req_dcfclk(struct dc *dc, struct dc_state *context)
+{
+	if (dcn32_subvp_in_use(dc, context) && context->bw_ctx.bw.dcn.clk.dcfclk_khz <= MIN_SUBVP_DCFCLK_KHZ)
+		context->bw_ctx.bw.dcn.clk.dcfclk_khz = MIN_SUBVP_DCFCLK_KHZ;
+}
