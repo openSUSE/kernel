b8e711c8e4414ce25afdbb4a6322cc42d26b0492
Nikolay Borisov <nik.borisov@suse.com>
2024-04-05 11:42:31 +0300

    x86/bhi: Mitigate KVM by default (bsc#1217339 CVE-2024-2201).
    suse-commit: e8a52ff164379dd618b341aa602a2e0d8ddc7548

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -442,6 +442,7 @@
 #define X86_FEATURE_CLEAR_BHB_LOOP	(22*32+ 0) /* "" Clear branch history at syscall entry using SW loop */
 #define X86_FEATURE_BHI_CTRL		(22*32+ 1) /* "" BHI_DIS_S HW control available */
 #define X86_FEATURE_CLEAR_BHB_HW	(22*32+ 2) /* "" BHI_DIS_S HW control enabled */
+#define X86_FEATURE_CLEAR_BHB_LOOP_ON_VMEXIT (22*32+ 3) /* "" Clear branch history at vmexit using SW loop */
 
 /*
  * BUG word(s)
