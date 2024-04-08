d0cd07b7b157cce05e3cf27b055840456a960317
Mel Gorman <mgorman@suse.de>
2024-04-08 12:12:33 +0100

    sched/fair: Check root_domain::overload value before update (bsc#1222173).
    suse-commit: 01d478b9be9e36b5c7bfa786cda0d4482df7b34a

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10654,7 +10654,8 @@ next_group:
 
 	if (!env->sd->parent) {
 		/* update overload indicator if we are at root domain */
-		WRITE_ONCE(env->dst_rq->rd->overload, sg_status & SG_OVERLOAD);
+		if (READ_ONCE(env->dst_rq->rd->overload) != (sg_status & SG_OVERLOAD))
+			WRITE_ONCE(env->dst_rq->rd->overload, sg_status & SG_OVERLOAD);
 
 		/* Update over-utilization (tipping point, U >= 0) indicator */
 		set_rd_overutilized_status(env->dst_rq->rd,
