8b79a522802c0237bf904020e0452edc71f0a94f
Michal Koutný <mkoutny@suse.com>
2024-04-09 11:58:59 +0200

    mm: memcg: don't periodically flush stats when memcg is disabled (bsc#1222525).
    suse-commit: 5b28bba0b32f8fdd0878f75e513e728d18c26c8c

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -5457,7 +5457,7 @@ static int mem_cgroup_css_online(struct cgroup_subsys_state *css)
 	if (alloc_shrinker_info(memcg))
 		goto offline_kmem;
 
-	if (unlikely(mem_cgroup_is_root(memcg)))
+	if (unlikely(mem_cgroup_is_root(memcg)) && !mem_cgroup_disabled())
 		queue_delayed_work(system_unbound_wq, &stats_flush_dwork,
 				   2UL*HZ);
 	lru_gen_online_memcg(memcg);
