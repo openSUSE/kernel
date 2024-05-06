e81dd3e33a6320f652f225e7048005bb96886ae7
Thomas Zimmermann <tzimmermann@suse.com>
2024-05-06 12:36:29 +0200

    nouveau: lock the client object tree. (bsc#1223834 CVE-2024-27062)
    suse-commit: e8284988d8c0e9c0e07170d9efed472bf1d9d08e

--- drivers/gpu/drm/nouveau/include/nvkm/core/client.h
+++ drivers/gpu/drm/nouveau/include/nvkm/core/client.h
@@ -12,6 +12,7 @@ struct nvkm_client {
 
 	struct nvkm_client_notify *notify[32];
 	struct rb_root objroot;
+	spinlock_t obj_lock;
 
 	void *data;
 	int (*ntfy)(const void *, u32, const void *, u32);
