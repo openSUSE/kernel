16c6d27516a2f6ccf9420a02886fc497d41f95c1
Denis Kirjanov <denis.kirjanov@suse.com>
2024-05-09 12:11:54 +0300

    netfilter: nf_tables: mark newset as dead on transaction abort (git-fixes).
    suse-commit: deeefa08f2643c2edadb1393f07acdc7e430c73e

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -7290,6 +7290,7 @@ static int __nf_tables_abort(struct net *net, bool autoload)
 				nft_trans_destroy(trans);
 				break;
 			}
+			nft_trans_set(trans)->dead = 1;
 			list_del_rcu(&nft_trans_set(trans)->list);
 			break;
 		case NFT_MSG_DELSET:
