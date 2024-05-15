3047368fbfbdae17f0abbbe57ca7e6275479ca3d
David Sterba <dsterba@suse.com>
2024-05-15 15:37:11 +0200

    iomap: Fix inline extent handling in iomap_readpage (git-fixes)
    suse-commit: 61ce07471284bba61b1747e942b26788663e19d6

--- fs/iomap/buffered-io.c
+++ fs/iomap/buffered-io.c
@@ -251,9 +251,13 @@ static loff_t iomap_readpage_iter(struct iomap_iter *iter,
 	unsigned poff, plen;
 	sector_t sector;
 
-	if (iomap->type == IOMAP_INLINE)
-		return min(iomap_read_inline_data(iter->inode, page, iomap),
-						  length);
+	if (iomap->type == IOMAP_INLINE) {
+		loff_t ret = iomap_read_inline_data(iter->inode, page, iomap);
+
+		if (ret < 0)
+			return ret;
+		return 0;
+	}
 
 	/* zero post-eof blocks as the page may be mapped */
 	iop = iomap_page_create(iter->inode, page);
@@ -367,6 +371,8 @@ static loff_t iomap_readahead_iter(struct iomap_iter *iter,
 			ctx->cur_page_in_bio = false;
 		}
 		ret = iomap_readpage_iter(iter, ctx, done);
+		if (ret <= 0)
+			return ret;
 	}
 
 	return done;
