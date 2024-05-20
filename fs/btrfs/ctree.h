3b79ac56102dfb8ef24274f157698dfe7a610e21
Qu Wenruo <wqu@suse.com>
2024-05-21 08:41:12 +0930

    btrfs: do not start relocation until in progress drops are  done (bsc#1222251).
    suse-commit: 0f3d5ec59cc699ee1f8a5d6ba079ca872b149c3b

--- fs/btrfs/ctree.h
+++ fs/btrfs/ctree.h
@@ -569,6 +569,9 @@ enum {
 	 * so we don't need to offload checksums to workqueues.
 	 */
 	BTRFS_FS_CSUM_IMPL_FAST,
+
+	/* Indicate we have half completed snapshot deletions pending. */
+	BTRFS_FS_UNFINISHED_DROPS,
 };
 
 struct btrfs_fs_info {
@@ -995,7 +998,14 @@ enum {
 	BTRFS_ROOT_DEAD_TREE,
 	/* Qgroup flushing is in progress */
 	BTRFS_ROOT_QGROUP_FLUSHING,
+	/* This root has a drop operation that was started previously. */
+	BTRFS_ROOT_UNFINISHED_DROP,
 };
+
+static inline void btrfs_wake_unfinished_drop(struct btrfs_fs_info *fs_info)
+{
+	clear_and_wake_up_bit(BTRFS_FS_UNFINISHED_DROPS, &fs_info->flags);
+}
 
 /*
  * Record swapped tree blocks of a subvolume tree for delayed subtree trace
