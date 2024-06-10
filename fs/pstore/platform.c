82dcaddce000fee8ee66f55afba05edf580807e8
Gabriel Krisman Bertazi <krisman@suse.de>
2024-06-10 14:46:28 -0400

    - pstore/platform: Add check for kstrdup (bsc#1225050   CVE-2023-52869). - Refresh   patches.suse/pstore_disable_efi_backend_by_default.patch.
    While refreshing of pstore_disable_efi_backend_by_default.patch, also
    fix the non-conformant Patch-mainline tag.
    
    suse-commit: 6db9ce68e92fd6abdf9d40eec04befaca6ca068c

--- fs/pstore/platform.c
+++ fs/pstore/platform.c
@@ -561,6 +561,8 @@ out:
  */
 int pstore_register(struct pstore_info *psi)
 {
+	char *new_backend;
+
 	if (!backend && !strcmp(psi->name, "efi")) {
 		pr_info("Efi pstore disabled, enforce via pstore.backend=efi");
 		pr_info("On a broken BIOS, this can severely harm your system");
@@ -587,11 +589,16 @@ int pstore_register(struct pstore_info *psi)
 		return -EINVAL;
 	}
 
+	new_backend = kstrdup(psi->name, GFP_KERNEL);
+	if (!new_backend)
+		return -ENOMEM;
+
 	mutex_lock(&psinfo_lock);
 	if (psinfo) {
 		pr_warn("backend '%s' already loaded: ignoring '%s'\n",
 			psinfo->name, psi->name);
 		mutex_unlock(&psinfo_lock);
+		kfree(new_backend);
 		return -EBUSY;
 	}
 
@@ -624,7 +631,7 @@ int pstore_register(struct pstore_info *psi)
 	 * Update the module parameter backend, so it is visible
 	 * through /sys/module/pstore/parameters/backend
 	 */
-	backend = kstrdup(psi->name, GFP_KERNEL);
+	backend = new_backend;
 
 	pr_info("Registered %s as persistent store backend\n", psi->name);
 
