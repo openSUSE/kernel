98e642513eaea8bce4261e9b0a71756df4d6f36f
Takashi Iwai <tiwai@suse.de>
2024-06-14 09:58:29 +0200

    - nouveau: add an ioctl to report vram usage (stable-fixes). - Refresh   patches.suse/drm-nouveau-use-tile_mode-and-pte_kind-for-VM_BIND-b.patch.
    suse-commit: 7ea88ac85c5dffb3efce297f6537d6d7ecc52411

--- include/uapi/drm/nouveau_drm.h
+++ include/uapi/drm/nouveau_drm.h
@@ -62,6 +62,13 @@ extern "C" {
 #define NOUVEAU_GETPARAM_VRAM_BAR_SIZE 18
 
 /*
+ * NOUVEAU_GETPARAM_VRAM_USED
+ *
+ * Get remaining VRAM size.
+ */
+#define NOUVEAU_GETPARAM_VRAM_USED 19
+
+/*
  * NOUVEAU_GETPARAM_HAS_VMA_TILEMODE
  *
  * Query whether tile mode and PTE kind are accepted with VM allocs or not.
