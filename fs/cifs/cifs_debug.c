542563e2cc9d1afb9fc52fbfda3e582f838dd300
Enzo Matsumiya <ematsumiya@suse.de>
2024-06-14 19:20:40 -0300

    smb: client: fix use-after-free bug in cifs_debug_data_proc_show() (bsc#1225487, CVE-2023-52752).
    suse-commit: 4f9d290c15cd857976bb429eb5eeed0ae3653f80

--- fs/cifs/cifs_debug.c
+++ fs/cifs/cifs_debug.c
@@ -154,6 +154,8 @@ static int cifs_debug_data_proc_show(struct seq_file *m, void *v)
 		list_for_each(tmp2, &server->smb_ses_list) {
 			ses = list_entry(tmp2, struct cifs_ses,
 					 smb_ses_list);
+			if (ses->status == CifsExiting)
+				continue;
 			if ((ses->serverDomain == NULL) ||
 				(ses->serverOS == NULL) ||
 				(ses->serverNOS == NULL)) {
