557741e38e4f8df2b9d56dac713c03b018d49840
Davide Benini <davide.benini@suse.com>
2024-06-25 17:12:35 +0200

    mptcp: clear 'kern' flag from fallback sockets (CVE-2021-47593 bsc#1226551).
    suse-commit: 2659f40f23e82debb7ad6859c953e29c3cbc1191

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -2768,7 +2768,7 @@ static struct sock *mptcp_accept(struct sock *sk, int flags, int *err,
 		 */
 		if (WARN_ON_ONCE(!new_mptcp_sock)) {
 			tcp_sk(newsk)->is_mptcp = 0;
-			return newsk;
+			goto out;
 		}
 
 		/* acquire the 2nd reference for the owning socket */
@@ -2780,6 +2780,8 @@ static struct sock *mptcp_accept(struct sock *sk, int flags, int *err,
 				MPTCP_MIB_MPCAPABLEPASSIVEFALLBACK);
 	}
 
+out:
+	newsk->sk_kern_sock = kern;
 	return newsk;
 }
 
