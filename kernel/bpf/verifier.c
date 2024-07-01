96367206ed73509d0f478831792fc6e242268d9e
Shung-Hsi Yu <shung-hsi.yu@suse.com>
2024-07-01 20:57:46 +0800

    bpf: move explored_state() closer to the beginning of verifier.c (bsc#1225903).
    suse-commit: a8dee76ce955e29caa8f34d9c04a4158fb1fd028

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -1411,6 +1411,19 @@ static int copy_verifier_state(struct bpf_verifier_state *dst_state,
 	return 0;
 }
 
+static u32 state_htab_size(struct bpf_verifier_env *env)
+{
+	return env->prog->len;
+}
+
+static struct bpf_verifier_state_list **explored_state(struct bpf_verifier_env *env, int idx)
+{
+	struct bpf_verifier_state *cur = env->cur_state;
+	struct bpf_func_state *state = cur->frame[cur->curframe];
+
+	return &env->explored_states[(idx ^ state->callsite) % state_htab_size(env)];
+}
+
 static void update_branch_counts(struct bpf_verifier_env *env, struct bpf_verifier_state *st)
 {
 	while (st) {
@@ -11525,21 +11538,6 @@ enum {
 	FALLTHROUGH = 1,
 	BRANCH = 2,
 };
-
-static u32 state_htab_size(struct bpf_verifier_env *env)
-{
-	return env->prog->len;
-}
-
-static struct bpf_verifier_state_list **explored_state(
-					struct bpf_verifier_env *env,
-					int idx)
-{
-	struct bpf_verifier_state *cur = env->cur_state;
-	struct bpf_func_state *state = cur->frame[cur->curframe];
-
-	return &env->explored_states[(idx ^ state->callsite) % state_htab_size(env)];
-}
 
 static void mark_prune_point(struct bpf_verifier_env *env, int idx)
 {
