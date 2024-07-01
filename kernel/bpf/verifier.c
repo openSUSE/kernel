24506dea56ed59b329cfa0d95cfabae2f55f1b6a
Shung-Hsi Yu <shung-hsi.yu@suse.com>
2024-07-01 19:34:52 +0800

    bpf: take into account liveness when propagating precision (bsc#1225903).
    - Refresh patches.suse/bpf-fix-precision-propagation-verbose-logging.patch
    
    suse-commit: 49cb3acb78fd6dffed60576ea479c26b3e338fbf

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -12282,7 +12282,8 @@ static int propagate_precision(struct bpf_verifier_env *env,
 		state_reg = state->regs;
 		for (i = 0; i < BPF_REG_FP; i++, state_reg++) {
 			if (state_reg->type != SCALAR_VALUE ||
-			    !state_reg->precise)
+			    !state_reg->precise ||
+			    !(state_reg->live & REG_LIVE_READ))
 				continue;
 			if (env->log.level & BPF_LOG_LEVEL2)
 				verbose(env, "frame %d: propagating r%d\n", fr, i);
@@ -12296,7 +12297,8 @@ static int propagate_precision(struct bpf_verifier_env *env,
 				continue;
 			state_reg = &state->stack[i].spilled_ptr;
 			if (state_reg->type != SCALAR_VALUE ||
-			    !state_reg->precise)
+			    !state_reg->precise ||
+			    !(state_reg->live & REG_LIVE_READ))
 				continue;
 			if (env->log.level & BPF_LOG_LEVEL2)
 				verbose(env, "frame %d: propagating fp%d\n",
