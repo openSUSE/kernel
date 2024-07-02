4ba19d56f2681681af5e447eef103f2f7aa39fae
hmzhao <heming.zhao@suse.com>
2024-07-02 12:44:11 +0800

    gfs2: Fix potential glock use-after-free on unmount (bsc#1226775 CVE-2024-38570).
    suse-commit: f3adbcaa2de4dd311032739a67f7abc7e554caf0

--- fs/gfs2/incore.h
+++ fs/gfs2/incore.h
@@ -843,6 +843,7 @@ struct gfs2_sbd {
 	struct gfs2_holder sd_freeze_gh;
 	atomic_t sd_freeze_state;
 	struct mutex sd_freeze_mutex;
+	struct list_head sd_dead_glocks;
 
 	char sd_fsname[GFS2_FSNAME_LEN + 3 * sizeof(int) + 2];
 	char sd_table_name[GFS2_FSNAME_LEN];
