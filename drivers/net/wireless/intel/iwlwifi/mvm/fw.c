740087344fc49b5c82825de8a8c562dcd3816814
Takashi Iwai <tiwai@suse.de>
2024-07-11 16:28:02 +0200

    wifi: iwlwifi: read WTAS table from UEFI (bsc#1227149).
    suse-commit: 1c3b75704816a9546de090bd9a3cf9c961f95f37

--- drivers/net/wireless/intel/iwlwifi/mvm/fw.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/fw.c
@@ -1098,10 +1098,6 @@ static int iwl_mvm_ppag_init(struct iwl_mvm *mvm)
 	return iwl_mvm_ppag_send_cmd(mvm);
 }
 
-#ifdef CONFIG_ACPI
-
-
-
 static bool iwl_mvm_add_to_tas_block_list(__le32 *list, __le32 *le_size, unsigned int mcc)
 {
 	int i;
@@ -1139,7 +1135,7 @@ static void iwl_mvm_tas_init(struct iwl_mvm *mvm)
 		return;
 	}
 
-	ret = iwl_acpi_get_tas(&mvm->fwrt, &data);
+	ret = iwl_bios_get_tas_table(&mvm->fwrt, &data);
 	if (ret < 0) {
 		IWL_DEBUG_RADIO(mvm,
 				"TAS table invalid or unavailable. (%d)\n",
@@ -1194,6 +1190,8 @@ static void iwl_mvm_tas_init(struct iwl_mvm *mvm)
 	if (ret < 0)
 		IWL_DEBUG_RADIO(mvm, "failed to send TAS_CONFIG (%d)\n", ret);
 }
+
+#ifdef CONFIG_ACPI
 
 static u8 iwl_mvm_eval_dsm_rfi(struct iwl_mvm *mvm)
 {
@@ -1334,10 +1332,6 @@ static void iwl_mvm_lari_cfg(struct iwl_mvm *mvm)
 }
 
 #else /* CONFIG_ACPI */
-
-static void iwl_mvm_tas_init(struct iwl_mvm *mvm)
-{
-}
 
 static void iwl_mvm_lari_cfg(struct iwl_mvm *mvm)
 {
