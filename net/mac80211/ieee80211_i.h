a942febbc99a338375a2dcccd41f8e90478b7f02
Takashi Iwai <tiwai@suse.de>
2024-07-11 16:00:08 +0200

    - wifi: mac80211: process and save negotiated TID to Link mapping   request (bsc#1227149). - Refresh patches.kabi/wireless-kabi-workaround.patch.
    suse-commit: 32a5092bc1000eaf2d238ba0df36412500b9cd55

--- net/mac80211/ieee80211_i.h
+++ net/mac80211/ieee80211_i.h
@@ -2605,4 +2605,7 @@ ieee80211_eht_cap_ie_to_sta_eht_cap(struct ieee80211_sub_if_data *sdata,
 				    const struct ieee80211_eht_cap_elem *eht_cap_ie_elem,
 				    u8 eht_cap_len,
 				    struct link_sta_info *link_sta);
+void ieee80211_process_neg_ttlm_req(struct ieee80211_sub_if_data *sdata,
+				    struct ieee80211_mgmt *mgmt, size_t len);
+
 #endif /* IEEE80211_I_H */
