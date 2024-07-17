30b7336f4c36ca09f05694006178953baa662dad
NeilBrown <neilb@suse.de>
2024-07-17 16:59:30 +1000

    NFSD: handle errors better in write_ports_addfd() (git-fixes).
    suse-commit: ad1ae3c33141279e59ce15bf13ac15d87b387102

--- fs/nfsd/nfsctl.c
+++ fs/nfsd/nfsctl.c
@@ -738,7 +738,7 @@ static ssize_t __write_ports_addfd(char *buf, struct net *net, const struct cred
 		return err;
 
 	err = svc_addsock5(nn->nfsd_serv, net, fd, buf, SIMPLE_TRANSACTION_LIMIT, cred);
-	if (err < 0) {
+	if (err < 0 && list_empty(&nn->nfsd_serv->sv_permsocks)) {
 		nfsd_destroy(net);
 		return err;
 	}
