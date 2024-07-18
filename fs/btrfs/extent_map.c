e2dbd24676df6cbd9da32f0c169aed4351400aeb
Filipe Manana <fdmanana@suse.com>
2024-07-18 17:57:13 +0100

    btrfs: pass NOWAIT for set/clear extent bits as another bit (bsc#1223731 CVE-2024-26944).
    suse-commit: 33253dfb0c29f398adc583a2f255314cd547dd7b

--- fs/btrfs/extent_map.c
+++ fs/btrfs/extent_map.c
@@ -372,8 +372,8 @@ static void extent_map_device_set_bits(struct extent_map *em, unsigned bits)
 		struct btrfs_device *device = stripe->dev;
 
 		set_extent_bit(&device->alloc_state, stripe->physical,
-			       stripe->physical + stripe_size - 1, bits, NULL,
-			       GFP_NOWAIT);
+			       stripe->physical + stripe_size - 1,
+			       bits | EXTENT_NOWAIT, NULL, GFP_NOWAIT);
 	}
 }
 
@@ -388,7 +388,8 @@ static void extent_map_device_clear_bits(struct extent_map *em, unsigned bits)
 		struct btrfs_device *device = stripe->dev;
 
 		__clear_extent_bit(&device->alloc_state, stripe->physical,
-				   stripe->physical + stripe_size - 1, bits,
+				   stripe->physical + stripe_size - 1,
+				   bits | EXTENT_NOWAIT,
 				   NULL, GFP_NOWAIT, NULL);
 	}
 }
