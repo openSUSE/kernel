78913a845057db7bcdc16c7af6450e58452b907a
Joerg Roedel <jroedel@suse.de>
2024-07-26 15:37:20 +0200

    x86/CPU/AMD: Track SNP host status with cc_platform_*() (jsc#PED-5122).
    suse-commit: b3568b28c76af4e83ee0bb5d8c2c1ad1cb4805bb

--- drivers/crypto/ccp/sev-dev.c
+++ drivers/crypto/ccp/sev-dev.c
@@ -1076,7 +1076,7 @@ static int __sev_snp_init_locked(int *error)
 	void *arg = &data;
 	int cmd, rc = 0;
 
-	if (!cpu_feature_enabled(X86_FEATURE_SEV_SNP))
+	if (!cc_platform_has(CC_ATTR_HOST_SEV_SNP))
 		return -ENODEV;
 
 	sev = psp->sev_data;
