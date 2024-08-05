ca21c871a30b7373f7e92e94219f502522394119
Michal Suchanek <msuchanek@suse.de>
2024-08-05 20:57:57 +0200

    KVM: PPC: Book3S HV Nested: L2 LPCR should inherit L1 LPES setting (bsc#1194869).
    suse-commit: 90c3be7381f8051af9b01084b4a1d581bb5696b9

--- arch/powerpc/kvm/book3s_hv_nested.c
+++ arch/powerpc/kvm/book3s_hv_nested.c
@@ -151,8 +151,7 @@ static void sanitise_hv_regs(struct kvm_vcpu *vcpu, struct hv_guest_state *hr)
 	/*
 	 * Don't let L1 change LPCR bits for the L2 except these:
 	 */
-	mask = LPCR_DPFD | LPCR_ILE | LPCR_TC | LPCR_AIL | LPCR_LD |
-		LPCR_LPES | LPCR_MER;
+	mask = LPCR_DPFD | LPCR_ILE | LPCR_TC | LPCR_AIL | LPCR_LD | LPCR_MER;
 
 	/*
 	 * Additional filtering is required depending on hardware
