6434e3ad8ca023477a86edb11181d9ad556ee2a1
Jiri Slaby <jslaby@suse.cz>
2024-08-09 10:58:37 +0200

    genirq/ipi: Fix NULL pointer deref in irq_data_get_affinity_mask() (git-fixes).
    suse-commit: 68f00837756f890d7589f4b85830f6e479bd89c3

--- kernel/irq/ipi.c
+++ kernel/irq/ipi.c
@@ -186,15 +186,19 @@ EXPORT_SYMBOL_GPL(ipi_get_hwirq);
 static int ipi_send_verify(struct irq_chip *chip, struct irq_data *data,
 			   const struct cpumask *dest, unsigned int cpu)
 {
-	struct cpumask *ipimask = irq_data_get_affinity_mask(data);
+	struct cpumask *ipimask;
 
-	if (!chip || !ipimask)
+	if (!chip || !data)
 		return -EINVAL;
 
 	if (!chip->ipi_send_single && !chip->ipi_send_mask)
 		return -EINVAL;
 
 	if (cpu >= nr_cpu_ids)
+		return -EINVAL;
+
+	ipimask = irq_data_get_affinity_mask(data);
+	if (!ipimask)
 		return -EINVAL;
 
 	if (dest) {
