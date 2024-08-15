e6299dd2c40665d2a7ccbd2c7c7c992c5b9df1da
Kernel Build Daemon <kbuild@suse.de>
2024-08-15 09:49:42 +0200

    Delete patches.kabi/* workarounds
    suse-commit: 72e8d5ff3a63d4cb094a0a1e2c43b28461cb3f5d

--- arch/s390/kvm/kvm-s390.c
+++ arch/s390/kvm/kvm-s390.c
@@ -132,6 +132,7 @@ const struct _kvm_stats_desc kvm_vcpu_stats_desc[] = {
 	STATS_DESC_COUNTER(VCPU, instruction_io_other),
 	STATS_DESC_COUNTER(VCPU, instruction_lpsw),
 	STATS_DESC_COUNTER(VCPU, instruction_lpswe),
+	STATS_DESC_COUNTER(VCPU, instruction_lpswey),
 	STATS_DESC_COUNTER(VCPU, instruction_pfmf),
 	STATS_DESC_COUNTER(VCPU, instruction_ptff),
 	STATS_DESC_COUNTER(VCPU, instruction_sck),
@@ -179,8 +180,7 @@ const struct _kvm_stats_desc kvm_vcpu_stats_desc[] = {
 	STATS_DESC_COUNTER(VCPU, instruction_diagnose_308),
 	STATS_DESC_COUNTER(VCPU, instruction_diagnose_500),
 	STATS_DESC_COUNTER(VCPU, instruction_diagnose_other),
-	STATS_DESC_COUNTER(VCPU, pfault_sync),
-	STATS_DESC_COUNTER(VCPU, instruction_lpswey)
+	STATS_DESC_COUNTER(VCPU, pfault_sync)
 };
 
 const struct kvm_stats_header kvm_vcpu_stats_header = {
