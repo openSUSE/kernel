dd13fcdff7eaad2ef1143b1d835e0835bdedac40
Takashi Iwai <tiwai@suse.de>
2024-08-30 17:24:24 +0200

    ASoC: Name iov_iter argument as iterator instead of buffer (jsc#PED-9993 jsc#PED-10202).
    suse-commit: 64ce242e52127147a23e80788793c9b41c3eee96

--- sound/soc/soc-component.c
+++ sound/soc/soc-component.c
@@ -1055,7 +1055,7 @@ int snd_soc_pcm_component_sync_stop(struct snd_pcm_substream *substream)
 
 int snd_soc_pcm_component_copy(struct snd_pcm_substream *substream,
 			       int channel, unsigned long pos,
-			       struct iov_iter *buf, unsigned long bytes)
+			       struct iov_iter *iter, unsigned long bytes)
 {
 	struct snd_soc_pcm_runtime *rtd = asoc_substream_to_rtd(substream);
 	struct snd_soc_component *component;
@@ -1066,7 +1066,7 @@ int snd_soc_pcm_component_copy(struct snd_pcm_substream *substream,
 		if (component->driver->copy)
 			return soc_component_ret(component,
 				component->driver->copy(component, substream,
-					channel, pos, buf, bytes));
+					channel, pos, iter, bytes));
 
 	return -EINVAL;
 }
