1bb2be3361a89ed4d64baa6047bcb8e8bffff91c
Denis Kirjanov <denis.kirjanov@suse.com>
2024-09-02 16:18:57 +0300

    Bluetooth: L2CAP: Fix deadlock (git-fixes).
    suse-commit: 13aba13814214f6b30dd6094eca9820f208b9351

--- net/bluetooth/l2cap_sock.c
+++ net/bluetooth/l2cap_sock.c
@@ -1490,18 +1490,10 @@ static int l2cap_sock_recv_cb(struct l2cap_chan *chan, struct sk_buff *skb)
 	int err;
 
 
-	/* To avoid race with sock_release, a chan lock needs to be added here
-	 * to synchronize the sock.
-	 */
-	l2cap_chan_hold(chan);
-	l2cap_chan_lock(chan);
 	sk = chan->data;
 
-	if (!sk) {
-		l2cap_chan_unlock(chan);
-		l2cap_chan_put(chan);
+	if (!sk)
 		return -ENXIO;
-	}
 
 	lock_sock(sk);
 	if (l2cap_pi(sk)->rx_busy_skb) {
@@ -1538,8 +1530,6 @@ static int l2cap_sock_recv_cb(struct l2cap_chan *chan, struct sk_buff *skb)
 
 done:
 	release_sock(sk);
-	l2cap_chan_unlock(chan);
-	l2cap_chan_put(chan);
 
 	return err;
 }
