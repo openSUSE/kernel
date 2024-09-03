bd3436691d220579e9d36831d15cb3700ce3b1e3
Takashi Iwai <tiwai@suse.de>
2024-09-03 15:44:27 +0200

    ASoC: SOF: make .remove callback return void (jsc#PED-9993 jsc#PED-10202).
    suse-commit: 405335247c9ec6450917323a57ad525aed24906a

--- sound/soc/sof/intel/hda.c
+++ sound/soc/sof/intel/hda.c
@@ -1327,7 +1327,7 @@ hdac_bus_unmap:
 	return ret;
 }
 
-int hda_dsp_remove(struct snd_sof_dev *sdev)
+void hda_dsp_remove(struct snd_sof_dev *sdev)
 {
 	struct sof_intel_hda_dev *hda = sdev->pdata->hw_pdata;
 	const struct sof_intel_dsp_desc *chip = hda->desc;
@@ -1389,8 +1389,6 @@ int hda_dsp_remove_late(struct snd_sof_dev *sdev)
 	iounmap(sof_to_bus(sdev)->remap_addr);
 	sof_hda_bus_exit(sdev);
 	hda_codec_i915_exit(sdev);
-
-	return 0;
 }
 
 int hda_power_down_dsp(struct snd_sof_dev *sdev)
