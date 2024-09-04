f6aa08a3bd6264681bc549ba03b211bb3b482184
Davide Benini <davide.benini@suse.com>
2024-09-04 17:50:35 +0200

    wifi: cfg80211: restrict NL80211_ATTR_TXQ_QUANTUM values (CVE-2024-42114 bsc#1228564).
    Refresh patches.kabi/netlink-nla_policy-kabi-workaround.patch.
    
    suse-commit: 9abf38c7168fa7e67fd640703f32d0019656b40e

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -233,6 +233,10 @@ err:
 }
 
 /* policy for the attributes */
+static const struct netlink_range_validation q_range = {
+	.max = INT_MAX,
+};
+
 static const struct nla_policy nl80211_policy[NUM_NL80211_ATTR] = {
 	[NL80211_ATTR_WIPHY] = { .type = NLA_U32 },
 	[NL80211_ATTR_WIPHY_NAME] = { .type = NLA_NUL_STRING,
@@ -461,7 +465,7 @@ static const struct nla_policy nl80211_policy[NUM_NL80211_ATTR] = {
 
 	[NL80211_ATTR_TXQ_LIMIT] = { .type = NLA_U32 },
 	[NL80211_ATTR_TXQ_MEMORY_LIMIT] = { .type = NLA_U32 },
-	[NL80211_ATTR_TXQ_QUANTUM] = { .type = NLA_U32 },
+	[NL80211_ATTR_TXQ_QUANTUM] = NLA_POLICY_FULL_RANGE(NLA_U32, &q_range),
 	[NL80211_ATTR_HE_CAPABILITY] = { .type = NLA_BINARY,
 					 .len = NL80211_HE_MAX_CAPABILITY_LEN },
 };
