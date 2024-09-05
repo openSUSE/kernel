7cef75cf12ae0b264c92f5820f94761a57b29aaf
Takashi Iwai <tiwai@suse.de>
2024-09-05 18:06:26 +0200

    ASoC: cs-amp-lib: Fix NULL pointer crash if efi.get_variable is NULL (jsc#PED-9993 jsc#PED-10202).
    suse-commit: d154adffed0a09dbfa7bf1920d1c0d8d7fc83283

--- sound/soc/codecs/cs-amp-lib.c
+++ sound/soc/codecs/cs-amp-lib.c
@@ -103,7 +103,7 @@ static efi_status_t cs_amp_get_efi_variable(efi_char16_t *name,
 {
 	u32 attr;
 
-	if (IS_ENABLED(CONFIG_EFI))
+	if (efi_rt_services_supported(EFI_RT_SUPPORTED_GET_VARIABLE))
 		return efi.get_variable(name, guid, &attr, size, buf);
 
 	return EFI_NOT_FOUND;
