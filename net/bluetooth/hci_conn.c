2e7adb569d2025b687685b47af27fde9da0017e4
Takashi Iwai <tiwai@suse.de>
2024-09-11 10:02:04 +0200

    - Bluetooth: ISO: handle bound CIS cleanup via hci_conn   (jsc#PED-10412). - Refresh   patches.suse/Bluetooth-ISO-Use-defer-setup-to-separate-PA-sync-an.patch.
    suse-commit: 920025f94e90236964cc68ad6cbcf4b3b5013895

--- net/bluetooth/hci_conn.c
+++ net/bluetooth/hci_conn.c
@@ -1959,6 +1959,8 @@ struct hci_conn *hci_bind_cis(struct hci_dev *hdev, bdaddr_t *dst,
 		return ERR_PTR(-EINVAL);
 	}
 
+	hci_conn_hold(cis);
+
 	cis->iso_qos = *qos;
 	cis->state = BT_BOUND;
 
@@ -2315,6 +2317,9 @@ struct hci_conn *hci_connect_cis(struct hci_dev *hdev, bdaddr_t *dst,
 		hci_conn_drop(cis);
 		return ERR_PTR(-ENOLINK);
 	}
+
+	/* Link takes the refcount */
+	hci_conn_drop(cis);
 
 	cis->state = BT_CONNECT;
 
