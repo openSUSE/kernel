22c250b7ceffcac2e7feaf3acd596ecf31eb75f5
Takashi Iwai <tiwai@suse.de>
2024-09-11 10:35:25 +0200

    Bluetooth: Populate hci_set_hw_info for Intel and Realtek (jsc#PED-10412).
    suse-commit: 52963761ccc782a24519ec89ffbb3e7d3dcf1976

--- drivers/bluetooth/btintel.c
+++ drivers/bluetooth/btintel.c
@@ -2905,6 +2905,11 @@ static int btintel_setup_combined(struct hci_dev *hdev)
 			err = -EINVAL;
 		}
 
+		hci_set_hw_info(hdev,
+				"INTEL platform=%u variant=%u revision=%u",
+				ver.hw_platform, ver.hw_variant,
+				ver.hw_revision);
+
 		goto exit_error;
 	}
 
@@ -3011,6 +3016,10 @@ static int btintel_setup_combined(struct hci_dev *hdev)
 		err = -EINVAL;
 		break;
 	}
+
+	hci_set_hw_info(hdev, "INTEL platform=%u variant=%u",
+			INTEL_HW_PLATFORM(ver_tlv.cnvi_bt),
+			INTEL_HW_VARIANT(ver_tlv.cnvi_bt));
 
 exit_error:
 	kfree_skb(skb);
