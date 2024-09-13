c0e19efd95e0ef1b27aaa9c350d9dffcd8e9f52c
Oscar Salvador <osalvador@suse.de>
2024-09-13 09:49:12 +0200

    Revert "mm/sparsemem: fix race in accessing memory_section->usage"
    This reverts commit 0d596d0a794b02722dfba713ac7f7bfc7058a563.
    
    suse-commit: 12b6dd4aa4731a680fdb02dffd62dc6e1b6b8f49

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -1483,8 +1483,7 @@ extern struct mem_section mem_section[NR_SECTION_ROOTS][SECTIONS_PER_ROOT];
 
 static inline unsigned long *section_to_usemap(struct mem_section *ms)
 {
-	return (unsigned long *)((unsigned long)ms->usage->pageblock_flags +
-	       sizeof(struct rcu_head));
+	return ms->usage->pageblock_flags;
 }
 
 static inline struct mem_section *__nr_to_section(unsigned long nr)
@@ -1592,7 +1591,7 @@ static inline int pfn_section_valid(struct mem_section *ms, unsigned long pfn)
 {
 	int idx = subsection_map_index(pfn);
 
-	return test_bit(idx, READ_ONCE(ms->usage)->subsection_map);
+	return test_bit(idx, ms->usage->subsection_map);
 }
 #else
 static inline int pfn_section_valid(struct mem_section *ms, unsigned long pfn)
@@ -1616,7 +1615,6 @@ static inline int pfn_section_valid(struct mem_section *ms, unsigned long pfn)
 static inline int pfn_valid(unsigned long pfn)
 {
 	struct mem_section *ms;
-	int ret;
 
 	/*
 	 * Ensure the upper PAGE_SHIFT bits are clear in the
@@ -1630,19 +1628,13 @@ static inline int pfn_valid(unsigned long pfn)
 	if (pfn_to_section_nr(pfn) >= NR_MEM_SECTIONS)
 		return 0;
 	ms = __nr_to_section(pfn_to_section_nr(pfn));
-	rcu_read_lock();
-	if (!valid_section(ms)) {
-		rcu_read_unlock();
+	if (!valid_section(ms))
 		return 0;
-	}
 	/*
 	 * Traditionally early sections always returned pfn_valid() for
 	 * the entire section-sized span.
 	 */
-	ret = early_section(ms) || pfn_section_valid(ms, pfn);
-	rcu_read_unlock();
-
-	return ret;
+	return early_section(ms) || pfn_section_valid(ms, pfn);
 }
 #endif
 
