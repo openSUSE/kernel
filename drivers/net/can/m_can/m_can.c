1a23011251289c2da756dae8b4a8368d20027e94
Takashi Iwai <tiwai@suse.de>
2024-09-17 08:37:47 +0200

    can: m_can: m_can_close(): stop clocks after device has been shut down (git-fixes).
    suse-commit: aab2a12f55b4099f2836729836a8dbd9f557d861

--- drivers/net/can/m_can/m_can.c
+++ drivers/net/can/m_can/m_can.c
@@ -1585,7 +1585,6 @@ static int m_can_close(struct net_device *dev)
 	netif_stop_queue(dev);
 
 	m_can_stop(dev);
-	m_can_clk_stop(cdev);
 	free_irq(dev->irq, dev);
 
 	if (cdev->is_peripheral) {
@@ -1599,6 +1598,7 @@ static int m_can_close(struct net_device *dev)
 
 	close_candev(dev);
 
+	m_can_clk_stop(cdev);
 	phy_power_off(cdev->transceiver);
 
 	return 0;
