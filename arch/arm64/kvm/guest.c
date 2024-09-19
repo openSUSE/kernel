f1461e493c0b38528090f5e322f11156cd22320d
Juergen Gross <jgross@suse.com>
2024-09-19 10:26:04 +0200

    KVM: arm64: Release pfn, i.e. put page, if copying MTE tags hits ZONE_DEVICE (git-fixes).
    suse-commit: 64bccd6780408432b48f6f796b3e39464f2bf0a4

--- arch/arm64/kvm/guest.c
+++ arch/arm64/kvm/guest.c
@@ -1071,6 +1071,7 @@ int kvm_vm_ioctl_mte_copy_tags(struct kvm *kvm,
 		page = pfn_to_online_page(pfn);
 		if (!page) {
 			/* Reject ZONE_DEVICE memory */
+			kvm_release_pfn_clean(pfn);
 			ret = -EFAULT;
 			goto out;
 		}
