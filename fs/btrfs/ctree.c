f457a8af8f8b0a51880b7a3341b0b41bdb2d5881
Filipe Manana <fdmanana@suse.com>
2024-09-23 14:18:31 +0100

    btrfs: simplify setting the full backref flag at update_ref_for_cow() (bsc#1230794 CVE-2024-46752).
    suse-commit: 1d5fa94cfd07f08d7ef29d8c20af0dd0e6c5217b

--- fs/btrfs/ctree.c
+++ fs/btrfs/ctree.c
@@ -266,7 +266,6 @@ static noinline int update_ref_for_cow(struct btrfs_trans_handle *trans,
 	u64 refs;
 	u64 owner;
 	u64 flags;
-	u64 new_flags = 0;
 	int ret;
 
 	/*
@@ -327,7 +326,10 @@ static noinline int update_ref_for_cow(struct btrfs_trans_handle *trans,
 				if (ret)
 					return ret;
 			}
-			new_flags |= BTRFS_BLOCK_FLAG_FULL_BACKREF;
+			ret = btrfs_set_disk_extent_flags(trans, buf,
+						  BTRFS_BLOCK_FLAG_FULL_BACKREF);
+			if (ret)
+				return ret;
 		} else {
 
 			if (root->root_key.objectid ==
@@ -335,11 +337,6 @@ static noinline int update_ref_for_cow(struct btrfs_trans_handle *trans,
 				ret = btrfs_inc_ref(trans, root, cow, 1);
 			else
 				ret = btrfs_inc_ref(trans, root, cow, 0);
-			if (ret)
-				return ret;
-		}
-		if (new_flags != 0) {
-			ret = btrfs_set_disk_extent_flags(trans, buf, new_flags);
 			if (ret)
 				return ret;
 		}
