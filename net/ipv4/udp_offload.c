d32e1ec8c3742cb3ca29a518e882e182fa79fd96
Juergen Gross <jgross@suse.com>
2024-09-24 15:15:23 +0200

    udp: fix receiving fraglist GSO packets (git-fixes).
    suse-commit: fa1c6cd44abba0205488febe289d43e3c39259f4

--- net/ipv4/udp_offload.c
+++ net/ipv4/udp_offload.c
@@ -281,7 +281,8 @@ struct sk_buff *__udp_gso_segment(struct sk_buff *gso_skb,
 		return ERR_PTR(-EINVAL);
 
 	if (unlikely(skb_checksum_start(gso_skb) !=
-		     skb_transport_header(gso_skb)))
+		     skb_transport_header(gso_skb) &&
+		     !(skb_shinfo(gso_skb)->gso_type & SKB_GSO_FRAGLIST)))
 		return ERR_PTR(-EINVAL);
 
 	skb_pull(gso_skb, sizeof(*uh));
