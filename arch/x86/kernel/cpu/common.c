3a65d037874d8cf03867ec86f4c81c3d472e63c4
Nikolay Borisov <nik.borisov@suse.com>
2024-10-01 10:21:50 +0300

    x86/fred: Move FRED RSP initialization into a separate function (git-fixes).
    suse-commit: 202a0976feee55e8ed9362e53d04a9c424b6f93e

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -2351,10 +2351,12 @@ void cpu_init_exception_handling(void)
 	/* GHCB needs to be setup to handle #VC. */
 	setup_ghcb();
 
-	if (cpu_feature_enabled(X86_FEATURE_FRED))
+	if (cpu_feature_enabled(X86_FEATURE_FRED)) {
 		cpu_init_fred_exceptions();
-	else
+		cpu_init_fred_rsps();
+	} else {
 		load_current_idt();
+	}
 }
 
 /*
