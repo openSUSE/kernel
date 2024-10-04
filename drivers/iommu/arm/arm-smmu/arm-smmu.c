8f4d936c4a88cd8ad5b0d65838c5d4b4adc4ba62
Joerg Roedel <jroedel@suse.de>
2024-10-04 16:29:20 +0200

    - iommu: Mark dev_iommu_priv_set() with a lockdep (jsc#PED-10968). - Refresh   patches.suse/0002-iommu-vt-d-Fix-NULL-domain-on-device-release.patch. - Refresh   patches.suse/iommu-vt-d-Use-rbtree-to-track-iommu-probed-devices.patch.
    suse-commit: 5d6f1d90059fd3c9f30b8b38e2ef3e2b290c96d3

--- drivers/iommu/arm/arm-smmu/arm-smmu.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu.c
@@ -1465,7 +1465,6 @@ static void arm_smmu_release_device(struct device *dev)
 
 	arm_smmu_rpm_put(cfg->smmu);
 
-	dev_iommu_priv_set(dev, NULL);
 	kfree(cfg);
 }
 
--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -4324,7 +4324,6 @@ static struct iommu_device *intel_iommu_probe_device(struct device *dev)
 		ret = intel_pasid_alloc_table(dev);
 		if (ret) {
 			dev_err(dev, "PASID table allocation failed\n");
-			dev_iommu_priv_set(dev, NULL);
 			kfree(info);
 			goto clear_rbtree;
 		}
@@ -4357,7 +4356,6 @@ static void intel_iommu_release_device(struct device *dev)
 
 	intel_pasid_free_table(dev);
 	intel_iommu_debugfs_remove_dev(info);
-	dev_iommu_priv_set(dev, NULL);
 	kfree(info);
 	set_dma_ops(dev, NULL);
 }
