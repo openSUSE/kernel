3e0c413477105dee3a42bd5798547afffca61db1
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2024-10-04 13:46:20 +0200

    net: mana: Improve mana_set_channels() in low mem conditions (bsc#1230289).
    suse-commit: 057bf3f493e68ab0fb1f5630f2c8e14c8d17815e

--- include/net/mana/mana.h
+++ include/net/mana/mana.h
@@ -461,7 +461,7 @@ u32 mana_run_xdp(struct net_device *ndev, struct mana_rxq *rxq,
 struct bpf_prog *mana_xdp_get(struct mana_port_context *apc);
 void mana_chn_setxdp(struct mana_port_context *apc, struct bpf_prog *prog);
 int mana_bpf(struct net_device *ndev, struct netdev_bpf *bpf);
-int mana_pre_alloc_rxbufs(struct mana_port_context *apc, int mtu);
+int mana_pre_alloc_rxbufs(struct mana_port_context *apc, int mtu, int num_queues);
 void mana_pre_dealloc_rxbufs(struct mana_port_context *apc);
 
 extern const struct ethtool_ops mana_ethtool_ops;
