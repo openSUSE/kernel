50952154ff6e88dc79e3959789c40156af465dd4
Juergen Gross <jgross@suse.com>
2024-10-08 15:02:18 +0200

    vsock/virtio: fix packet delivery to tap device (git-fixes).
    suse-commit: 50a25ba5d0d17757891881bbf1e96fdce8cfdada

--- net/vmw_vsock/virtio_transport.c
+++ net/vmw_vsock/virtio_transport.c
@@ -116,7 +116,6 @@ virtio_transport_send_pkt_work(struct work_struct *work)
 		list_del_init(&pkt->list);
 		spin_unlock_bh(&vsock->send_pkt_list_lock);
 
-		virtio_transport_deliver_tap_pkt(pkt);
 
 		reply = pkt->reply;
 
@@ -137,6 +136,8 @@ virtio_transport_send_pkt_work(struct work_struct *work)
 			spin_unlock_bh(&vsock->send_pkt_list_lock);
 			break;
 		}
+
+		virtio_transport_deliver_tap_pkt(pkt);
 
 		if (reply) {
 			struct virtqueue *rx_vq = vsock->vqs[VSOCK_VQ_RX];
