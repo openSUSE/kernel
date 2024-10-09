194445b3338cf05a8c9f14f603c2ac3c2f2473c5
Patrik Jakobsson <pjakobsson@suse.de>
2024-10-09 14:56:11 +0200

    - drm/amdgpu: correct mca ipid die/socket/addr decode (drm-backport-placeholder) - Update patches.suse/drm-amd-display-Fix-potential-null-pointer-dereferen.patch - Delete patches.suse/0001-drm-amd-display-Add-disable-timeout-option.patch
    suse-commit: 983ef9bd4fdf9b6d122ac70fbc0c211bb09fb7ad

--- drivers/gpu/drm/amd/display/dc/dc_dmub_srv.c
+++ drivers/gpu/drm/amd/display/dc/dc_dmub_srv.c
@@ -245,12 +245,7 @@ bool dc_dmub_srv_cmd_run_list(struct dc_dmub_srv *dc_dmub_srv, unsigned int coun
 
 	// Wait for DMUB to process command
 	if (wait_type != DM_DMUB_WAIT_TYPE_NO_WAIT) {
-		if (dc_dmub_srv->ctx->dc->debug.disable_timeout) {
-			do {
-				status = dmub_srv_wait_for_idle(dmub, 100000);
-			} while (status != DMUB_STATUS_OK);
-		} else
-			status = dmub_srv_wait_for_idle(dmub, 100000);
+		status = dmub_srv_wait_for_idle(dmub, 100000);
 
 		if (status != DMUB_STATUS_OK) {
 			DC_LOG_DEBUG("No reply for DMUB command: status=%d\n", status);
@@ -1162,16 +1157,10 @@ bool dc_dmub_srv_is_hw_pwr_up(struct dc_dmub_srv *dc_dmub_srv, bool wait)
 	dc_ctx = dc_dmub_srv->ctx;
 
 	if (wait) {
-		if (dc_dmub_srv->ctx->dc->debug.disable_timeout) {
-			do {
-				status = dmub_srv_wait_for_hw_pwr_up(dc_dmub_srv->dmub, 500000);
-			} while (status != DMUB_STATUS_OK);
-		} else {
-			status = dmub_srv_wait_for_hw_pwr_up(dc_dmub_srv->dmub, 500000);
-			if (status != DMUB_STATUS_OK) {
-				DC_ERROR("Error querying DMUB hw power up status: error=%d\n", status);
-				return false;
-			}
+		status = dmub_srv_wait_for_hw_pwr_up(dc_dmub_srv->dmub, 500000);
+		if (status != DMUB_STATUS_OK) {
+			DC_ERROR("Error querying DMUB hw power up status: error=%d\n", status);
+			return false;
 		}
 	} else
 		return dmub_srv_is_hw_pwr_up(dc_dmub_srv->dmub);
@@ -1209,7 +1198,7 @@ static void dc_dmub_srv_exit_low_power_state(const struct dc *dc)
 	const uint32_t max_num_polls = 10000;
 	uint32_t allow_state = 0;
 	uint32_t commit_state = 0;
-	int i;
+	uint32_t i;
 
 	if (dc->debug.dmcub_emulation)
 		return;
@@ -1245,9 +1234,6 @@ static void dc_dmub_srv_exit_low_power_state(const struct dc *dc)
 						break;
 
 					udelay(1);
-
-					if (dc->debug.disable_timeout)
-						i--;
 				}
 				ASSERT(i < max_num_polls);
 
@@ -1270,9 +1256,6 @@ static void dc_dmub_srv_exit_low_power_state(const struct dc *dc)
 					break;
 
 				udelay(1);
-
-				if (dc->debug.disable_timeout)
-					i--;
 			}
 			ASSERT(i < max_num_polls);
 		}
