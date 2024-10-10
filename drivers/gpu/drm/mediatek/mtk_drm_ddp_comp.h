e3b3e06fc43020c89bda68d9d7ae24fabc893dfc
Patrik Jakobsson <pjakobsson@suse.de>
2024-10-10 11:52:02 +0200

    - drm/mediatek: Power on/off devices with function pointers (drm-backport-placeholder). - Delete patches.suse/drm-mediatek-Add-DRM_MODE_ROTATE_0-to-rotation-prope.patch.
    suse-commit: 404790fd9e618c71af5b06ffad19d2c9bf42d2de

--- drivers/gpu/drm/mediatek/mtk_drm_ddp_comp.h
+++ drivers/gpu/drm/mediatek/mtk_drm_ddp_comp.h
@@ -7,7 +7,7 @@
 #define MTK_DRM_DDP_COMP_H
 
 #include <linux/io.h>
-#include <drm/drm_modes.h>
+#include <linux/pm_runtime.h>
 #include <linux/soc/mediatek/mtk-cmdq.h>
 #include <linux/soc/mediatek/mtk-mmsys.h>
 #include <linux/soc/mediatek/mtk-mutex.h>
@@ -47,6 +47,8 @@ enum mtk_ddp_comp_type {
 struct mtk_ddp_comp;
 struct cmdq_pkt;
 struct mtk_ddp_comp_funcs {
+	int (*power_on)(struct device *dev);
+	void (*power_off)(struct device *dev);
 	int (*clk_enable)(struct device *dev);
 	void (*clk_disable)(struct device *dev);
 	void (*config)(struct device *dev, unsigned int w,
@@ -92,6 +94,23 @@ struct mtk_ddp_comp {
 	int encoder_index;
 	const struct mtk_ddp_comp_funcs *funcs;
 };
+
+static inline int mtk_ddp_comp_power_on(struct mtk_ddp_comp *comp)
+{
+	if (comp->funcs && comp->funcs->power_on)
+		return comp->funcs->power_on(comp->dev);
+	else
+		return pm_runtime_resume_and_get(comp->dev);
+	return 0;
+}
+
+static inline void mtk_ddp_comp_power_off(struct mtk_ddp_comp *comp)
+{
+	if (comp->funcs && comp->funcs->power_off)
+		comp->funcs->power_off(comp->dev);
+	else
+		pm_runtime_put(comp->dev);
+}
 
 static inline int mtk_ddp_comp_clk_enable(struct mtk_ddp_comp *comp)
 {
@@ -161,11 +180,7 @@ unsigned int mtk_ddp_comp_supported_rotations(struct mtk_ddp_comp *comp)
 	if (comp->funcs && comp->funcs->supported_rotations)
 		return comp->funcs->supported_rotations(comp->dev);
 
-	/*
-	 * In order to pass IGT tests, DRM_MODE_ROTATE_0 is required when
-	 * rotation is not supported.
-	 */
-	return DRM_MODE_ROTATE_0;
+	return 0;
 }
 
 static inline unsigned int mtk_ddp_comp_layer_nr(struct mtk_ddp_comp *comp)
