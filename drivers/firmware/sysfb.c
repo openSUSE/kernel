c3534edbc5a9a01095c0a77bf9c18ccbfc88b755
Patrik Jakobsson <pjakobsson@suse.de>
2024-10-11 11:25:19 +0200

    - firmware/sysfb: Clear screen_info state after consuming it (drm-backport-placeholder). - Refresh patches.suse/Add-parameter-to-disable-simple-framebuffer-devices.patch. - Refresh patches.suse/drivers-firmware-skip-simpledrm-if-nvidia-drm.modese.patch.
    suse-commit: a4ba9bb3ee2769a4ec9da71bde888807bde30161

--- drivers/firmware/sysfb.c
+++ drivers/firmware/sysfb.c
@@ -93,7 +93,7 @@ early_param("nosimplefb", simpledrm_disable_nosimplefb);
 
 static __init int sysfb_init(void)
 {
-	struct screen_info *si = &screen_info;
+	const struct screen_info *si = &screen_info;
 	struct simplefb_platform_data mode;
 	const char *name;
 	bool compatible;
@@ -140,6 +140,18 @@ static __init int sysfb_init(void)
 	ret = platform_device_add(pd);
 	if (ret)
 		goto err;
+
+	/*
+	 * The firmware framebuffer is now maintained by the created
+	 * device. Disable screen_info after we've consumed it. Prevents
+	 * invalid access during kexec reboots.
+	 *
+	 * TODO: Vgacon still relies on the global screen_info. Make
+	 *       vgacon work with the platform device, so we can clear
+	 *       the screen_info unconditionally.
+	 */
+	if (strcmp(name, "platform-framebuffer"))
+		screen_info.orig_video_isVGA = 0;
 
 	goto unlock_mutex;
 err:
