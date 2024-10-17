c8d093e56ddd91f2cbf8b1e8a9ea2ac480c79cc8
Chun-Yi Lee <jlee@suse.com>
2024-10-17 15:31:50 +0800

    thermal/debugfs: Pass cooling device state to thermal_debug_cdev_add() (jsc#PED-11164).
    suse-commit: e2b6ac4987483570b15946dd5eb228f54098b79d

--- drivers/thermal/thermal_debugfs.h
+++ drivers/thermal/thermal_debugfs.h
@@ -2,12 +2,12 @@
 
 #ifdef CONFIG_THERMAL_DEBUGFS
 void thermal_debug_init(void);
-void thermal_debug_cdev_add(struct thermal_cooling_device *cdev);
+void thermal_debug_cdev_add(struct thermal_cooling_device *cdev, int state);
 void thermal_debug_cdev_remove(struct thermal_cooling_device *cdev);
 void thermal_debug_cdev_state_update(const struct thermal_cooling_device *cdev, int state);
 #else
 static inline void thermal_debug_init(void) {}
-static inline void thermal_debug_cdev_add(struct thermal_cooling_device *cdev) {}
+static inline void thermal_debug_cdev_add(struct thermal_cooling_device *cdev, int state) {}
 static inline void thermal_debug_cdev_remove(struct thermal_cooling_device *cdev) {}
 static inline void thermal_debug_cdev_state_update(const struct thermal_cooling_device *cdev,
 						   int state) {}
