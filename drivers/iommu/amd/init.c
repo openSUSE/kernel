810f39c69eeb87f9e81f3f0bffd39bfc4f27d7db
Joerg Roedel <jroedel@suse.de>
2024-10-17 11:00:38 +0200

    iommu/amd: Enable Guest Translation before registering devices (jsc#PED-10968).
    suse-commit: 56ced35ed34edf8e6f5d70cabe996fdcd952e389

--- drivers/iommu/amd/init.c
+++ drivers/iommu/amd/init.c
@@ -2782,6 +2782,7 @@ static void early_enable_iommu(struct amd_iommu *iommu)
 	iommu_enable_command_buffer(iommu);
 	iommu_enable_event_buffer(iommu);
 	iommu_set_exclusion_range(iommu);
+	iommu_enable_gt(iommu);
 	iommu_enable_ga(iommu);
 	iommu_enable_xt(iommu);
 	iommu_enable_irtcachedis(iommu);
@@ -2838,6 +2839,7 @@ static void early_enable_iommus(void)
 			iommu_disable_irtcachedis(iommu);
 			iommu_enable_command_buffer(iommu);
 			iommu_enable_event_buffer(iommu);
+			iommu_enable_gt(iommu);
 			iommu_enable_ga(iommu);
 			iommu_enable_xt(iommu);
 			iommu_enable_irtcachedis(iommu);
@@ -2851,10 +2853,8 @@ static void enable_iommus_v2(void)
 {
 	struct amd_iommu *iommu;
 
-	for_each_iommu(iommu) {
+	for_each_iommu(iommu)
 		iommu_enable_ppr_log(iommu);
-		iommu_enable_gt(iommu);
-	}
 }
 
 static void enable_iommus_vapic(void)
