181740b2e5a587e3bcbbd8d47d181c10247d921b
Patrik Jakobsson <pjakobsson@suse.de>
2024-10-23 12:40:55 +0200

    drm/i915: Pass size to oprom_get_vbt (jsc#PED-9898 jsc#PED-10191 jsc#PED-10197 jsc#PED-10226 jsc#PED-10237 jsc#PED-10340 jsc#PED-10852 jsc#PED-11022)
    suse-commit: a513d51796558a0c13e2cf2aaf93a0fb7bc1c2c7

--- drivers/gpu/drm/i915/display/intel_bios.c
+++ drivers/gpu/drm/i915/display/intel_bios.c
@@ -3047,7 +3047,8 @@ err_not_found:
 	return NULL;
 }
 
-static struct vbt_header *oprom_get_vbt(struct drm_i915_private *i915)
+static struct vbt_header *oprom_get_vbt(struct drm_i915_private *i915,
+					size_t *sizep)
 {
 	struct pci_dev *pdev = to_pci_dev(i915->drm.dev);
 	void __iomem *p = NULL, *oprom;
@@ -3095,6 +3096,9 @@ static struct vbt_header *oprom_get_vbt(struct drm_i915_private *i915)
 		goto err_free_vbt;
 
 	pci_unmap_rom(pdev, oprom);
+
+	if (sizep)
+		*sizep = vbt_size;
 
 	drm_dbg_kms(&i915->drm, "Found valid VBT in PCI ROM\n");
 
@@ -3145,7 +3149,7 @@ void intel_bios_init(struct drm_i915_private *i915)
 	}
 
 	if (!vbt) {
-		oprom_vbt = oprom_get_vbt(i915);
+		oprom_vbt = oprom_get_vbt(i915, NULL);
 		vbt = oprom_vbt;
 	}
 
