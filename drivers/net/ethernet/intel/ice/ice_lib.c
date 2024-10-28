9cab84b2fab90870ee18cd550107734968b57ef6
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2024-10-28 14:17:25 +0100

    - ice: map XDP queues to vectors in ice_vsi_map_rings_to_vectors()   (git-fixes). - Refresh   patches.suse/ice-move-netif_queue_set_napi-to-rtnl-protected-sect.patch.
    suse-commit: 7b44c3c30707255b077acf7e2dfcd283b38427d2

--- drivers/net/ethernet/intel/ice/ice_lib.c
+++ drivers/net/ethernet/intel/ice/ice_lib.c
@@ -2415,10 +2415,6 @@ ice_vsi_cfg_def(struct ice_vsi *vsi, struct ice_vsi_cfg_params *params)
 		if (ret)
 			goto unroll_vector_base;
 
-		ice_vsi_map_rings_to_vectors(vsi);
-
-		vsi->stat_offsets_loaded = false;
-
 		if (ice_is_xdp_ena_vsi(vsi)) {
 			ret = ice_vsi_determine_xdp_res(vsi);
 			if (ret)
@@ -2428,6 +2424,10 @@ ice_vsi_cfg_def(struct ice_vsi *vsi, struct ice_vsi_cfg_params *params)
 			if (ret)
 				goto unroll_vector_base;
 		}
+
+		ice_vsi_map_rings_to_vectors(vsi);
+
+		vsi->stat_offsets_loaded = false;
 
 		/* ICE_VSI_CTRL does not need RSS so skip RSS processing */
 		if (vsi->type != ICE_VSI_CTRL)
