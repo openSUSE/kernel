9cb1ed9d2cb14269ef2a1dcfa59256b1914b7334
Patrik Jakobsson <pjakobsson@suse.de>
2024-10-31 22:04:17 +0100

    drm/xe/oa/uapi: Add OA data formats (jsc#PED-9898 jsc#PED-10191 jsc#PED-10197 jsc#PED-10226 jsc#PED-10237 jsc#PED-10340 jsc#PED-10852 jsc#PED-11022)
    suse-commit: 86582cc70203cde9b9eae17c38c037ff5253d1a9

--- drivers/gpu/drm/xe/xe_device.c
+++ drivers/gpu/drm/xe/xe_device.c
@@ -657,9 +657,13 @@ int xe_device_probe(struct xe_device *xe)
 
 	xe_heci_gsc_init(xe);
 
-	err = xe_display_init(xe);
+	err = xe_oa_init(xe);
 	if (err)
 		goto err_fini_gt;
+
+	err = xe_display_init(xe);
+	if (err)
+		goto err_fini_oa;
 
 	err = drm_dev_register(&xe->drm, 0);
 	if (err)
@@ -675,6 +679,9 @@ int xe_device_probe(struct xe_device *xe)
 
 err_fini_display:
 	xe_display_driver_remove(xe);
+
+err_fini_oa:
+	xe_oa_fini(xe);
 
 err_fini_gt:
 	for_each_gt(gt, xe, id) {
@@ -707,6 +714,8 @@ void xe_device_remove(struct xe_device *xe)
 	xe_device_remove_display(xe);
 
 	xe_display_fini(xe);
+
+	xe_oa_fini(xe);
 
 	xe_heci_gsc_fini(xe);
 
