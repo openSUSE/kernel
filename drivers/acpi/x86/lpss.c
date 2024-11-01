6fefee219b1cdabcd6ebb0bf52b3b4c53f0b516a
Chun-Yi Lee <jlee@suse.com>
2024-11-01 15:58:58 +0800

    ACPI: x86: Move LPSS to x86 folder (jsc#PED-11164).
    Refresh
    patches.suse/ACPI-LPSS-Switch-to-new-Intel-CPU-model-defines.patch.
    
    suse-commit: 2e7f938ec89cd7c2ca151082594c45ab27f4f99b

--- drivers/acpi/x86/lpss.c
+++ drivers/acpi/x86/lpss.c
@@ -25,7 +25,7 @@
 #include <linux/suspend.h>
 #include <linux/delay.h>
 
-#include "internal.h"
+#include "../internal.h"
 
 #ifdef CONFIG_X86_INTEL_LPSS
 
@@ -338,8 +338,8 @@ static const struct lpss_device_desc bsw_spi_dev_desc = {
 };
 
 static const struct x86_cpu_id lpss_cpu_ids[] = {
-	X86_MATCH_VFM(INTEL_ATOM_SILVERMONT,	NULL),
-	X86_MATCH_VFM(INTEL_ATOM_AIRMONT,	NULL),
+	X86_MATCH_VFM(INTEL_ATOM_SILVERMONT,    NULL),
+	X86_MATCH_VFM(INTEL_ATOM_AIRMONT,       NULL),
 	{}
 };
 
