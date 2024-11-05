36789ffeaa869e58c7f80b30680c3b0043416b9e
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2024-11-05 16:46:59 +0100

    net: dsa: mt7530: fix improper frames on all 25MHz and 40MHz XTAL MT7530 (bsc#1232178).
    suse-commit: e2d1db62d1838cf482183274e5d45080773d36d7

--- drivers/net/dsa/mt7530.c
+++ drivers/net/dsa/mt7530.c
@@ -2435,8 +2435,6 @@ mt7530_setup(struct dsa_switch *ds)
 		     SYS_CTRL_PHY_RST | SYS_CTRL_SW_RST |
 		     SYS_CTRL_REG_RST);
 
-	mt7530_pll_setup(priv);
-
 	/* Lower Tx driving for TRGMII path */
 	for (i = 0; i < NUM_TRGMII_CTRL; i++)
 		mt7530_write(priv, MT7530_TRGMII_TD_ODT(i),
@@ -2451,6 +2449,9 @@ mt7530_setup(struct dsa_switch *ds)
 	val &= ~MHWTRAP_P6_DIS & ~MHWTRAP_PHY_ACCESS;
 	val |= MHWTRAP_MANUAL;
 	mt7530_write(priv, MT7530_MHWTRAP, val);
+
+	if ((val & HWTRAP_XTAL_MASK) == HWTRAP_XTAL_40MHZ)
+		mt7530_pll_setup(priv);
 
 	mt753x_trap_frames(priv);
 
