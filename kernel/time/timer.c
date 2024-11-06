84e8be30c0e081f826802e2fcf7043fc8d3b6bff
Mel Gorman <mgorman@suse.de>
2024-11-06 04:35:39 +0000

    timers: Remove historical extra jiffie for timeout in msleep() (bsc#1232534 (PREEMPT_RT prerequisite backports)).
    suse-commit: f18d9b1de78bbe905f0d39fb5ee2b1657cac4d89

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -2723,7 +2723,7 @@ void __init init_timers(void)
  */
 void msleep(unsigned int msecs)
 {
-	unsigned long timeout = msecs_to_jiffies(msecs) + 1;
+	unsigned long timeout = msecs_to_jiffies(msecs);
 
 	while (timeout)
 		timeout = schedule_timeout_uninterruptible(timeout);
@@ -2737,7 +2737,7 @@ EXPORT_SYMBOL(msleep);
  */
 unsigned long msleep_interruptible(unsigned int msecs)
 {
-	unsigned long timeout = msecs_to_jiffies(msecs) + 1;
+	unsigned long timeout = msecs_to_jiffies(msecs);
 
 	while (timeout && !signal_pending(current))
 		timeout = schedule_timeout_interruptible(timeout);
