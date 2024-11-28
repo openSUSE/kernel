609d86bbddc2b6339c93f5b208d70dfa9e98ff59
Patrik Jakobsson <pjakobsson@suse.de>
2024-11-28 18:25:43 +0100

    drm/nouveau: Do not set struct drm_driver.lastclose (jsc#PED-9898 jsc#PED-10191 jsc#PED-10197 jsc#PED-10226 jsc#PED-10237 jsc#PED-10340 jsc#PED-10852 jsc#PED-11022)
    suse-commit: fa9338f7f2793dc5f9c0993a219f319068f20bb5

--- drivers/gpu/drm/nouveau/nouveau_drm.c
+++ drivers/gpu/drm/nouveau/nouveau_drm.c
@@ -1337,7 +1337,6 @@ driver_stub = {
 			   DRIVER_RENDER,
 	.open = nouveau_drm_open,
 	.postclose = nouveau_drm_postclose,
-	.lastclose = nouveau_vga_lastclose,
 
 #if defined(CONFIG_DEBUG_FS)
 	.debugfs_init = nouveau_drm_debugfs_init,
