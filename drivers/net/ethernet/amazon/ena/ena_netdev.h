602950308aae95dfd90452d900ace3c448bec307
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2024-12-03 11:11:03 +0100

    - net: ena: introduce ndo_xdp_xmit() function for XDP_REDIRECT   (bsc#1198778). - Refresh   patches.suse/0001-net-ena-optimize-data-access-in-fast-path-code.patch.
    suse-commit: 6b3c7663eb254d038f4021cc2cc5e86b2c245c44

--- drivers/net/ethernet/amazon/ena/ena_netdev.h
+++ drivers/net/ethernet/amazon/ena/ena_netdev.h
@@ -264,6 +264,7 @@ struct ena_ring {
 	struct ena_com_io_sq *ena_com_io_sq;
 	struct bpf_prog *xdp_bpf_prog;
 	struct xdp_rxq_info xdp_rxq;
+	spinlock_t xdp_tx_lock;	/* synchronize XDP TX/Redirect traffic */
 	/* Used for rx queues only to point to the xdp tx ring, to
 	 * which traffic should be redirected from this rx ring.
 	 */
