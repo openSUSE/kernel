73bdeb30e3ac471cf6328fae35a8d70befe2a257
Jan Kara <jack@suse.cz>
2024-12-09 18:27:26 +0100

    udf: fix uninit-value use in udf_get_fileshortad (bsc#1234243 bsc#1233038 CVE-2024-50143).
    suse-commit: 74fc0bfa85bd09fce3f23def53f969aaafce9c22

--- fs/udf/inode.c
+++ fs/udf/inode.c
@@ -2193,12 +2193,16 @@ int8_t udf_current_aext(struct inode *inode, struct extent_position *epos,
 		alen = udf_file_entry_alloc_offset(inode) +
 							iinfo->i_lenAlloc;
 	} else {
+		struct allocExtDesc *header =
+			(struct allocExtDesc *)epos->bh->b_data;
+
 		if (!epos->offset)
 			epos->offset = sizeof(struct allocExtDesc);
 		ptr = epos->bh->b_data + epos->offset;
-		alen = sizeof(struct allocExtDesc) +
-			le32_to_cpu(((struct allocExtDesc *)epos->bh->b_data)->
-							lengthAllocDescs);
+		alen = le32_to_cpu(header->lengthAllocDescs) +
+						sizeof(struct allocExtDesc);
+		if (alen < sizeof(struct allocExtDesc))
+			return -1;
 	}
 
 	switch (iinfo->i_alloc_type) {
