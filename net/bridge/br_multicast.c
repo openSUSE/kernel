f7f68838e7962112d693b1334bef6f7527903ac4
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2024-12-09 11:11:51 +0100

    net: bridge: mcast: wait for previous gc cycles when removing port (CVE-2024-44934 bsc#1229809).
    suse-commit: fc48798e6466d63b49fb0454a196dfe10cf83cdc

--- net/bridge/br_multicast.c
+++ net/bridge/br_multicast.c
@@ -1701,16 +1701,14 @@ void br_multicast_del_port(struct net_bridge_port *port)
 {
 	struct net_bridge *br = port->br;
 	struct net_bridge_port_group *pg;
-	HLIST_HEAD(deleted_head);
 	struct hlist_node *n;
 
 	/* Take care of the remaining groups, only perm ones should be left */
 	spin_lock_bh(&br->multicast_lock);
 	hlist_for_each_entry_safe(pg, n, &port->mglist, mglist)
 		br_multicast_find_del_pg(br, pg);
-	hlist_move_list(&br->mcast_gc_list, &deleted_head);
 	spin_unlock_bh(&br->multicast_lock);
-	br_multicast_gc(&deleted_head);
+	flush_work(&br->mcast_gc_work);
 	del_timer_sync(&port->ip4_mc_router_timer);
 #if IS_ENABLED(CONFIG_IPV6)
 	del_timer_sync(&port->ip6_mc_router_timer);
