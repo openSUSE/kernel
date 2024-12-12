c033c68ca2b751c5173495cb18ee601ac56fbe07
Oliver Neukum <oneukum@suse.com>
2024-12-12 18:27:01 +0100

    - media: ipu-bridge: Add a parse_sensor_fwnode callback to   ipu_bridge_init() (jsc#PED-10837). - Refresh   patches.suse/media-ipu-bridge-Move-ipu-bridge.h-to-include-media.patch.
    suse-commit: b4fe24710b343cdac620f479c130d6750f267e37

--- drivers/media/pci/intel/ipu3/ipu3-cio2.c
+++ drivers/media/pci/intel/ipu3/ipu3-cio2.c
@@ -1726,7 +1726,7 @@ static int cio2_pci_probe(struct pci_dev *pci_dev,
 			return -EINVAL;
 		}
 
-		r = ipu_bridge_init(dev);
+		r = ipu_bridge_init(dev, ipu_bridge_parse_ssdb);
 		if (r)
 			return r;
 	}
