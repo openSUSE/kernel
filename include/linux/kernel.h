678281b4dab2a64c9b0270842c74886401e14e93
Jiri Slaby <jslaby@suse.cz>
2024-12-19 18:12:37 +0100

    Refresh patches.suse/0002-security-Add-a-kernel-lockdown-flag-for-early-boot-s.patch.
    Fixups.
    
    suse-commit: 7185e51ae6b819381af79c513e451e5988182799

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -402,13 +402,13 @@ static inline void ftrace_dump(enum ftrace_dump_mode oops_dump_mode) { }
 	 /* OTHER_WRITABLE?  Generally considered a bad idea. */		\
 	 BUILD_BUG_ON_ZERO((perms) & 2) +					\
 	 (perms))
-#endif
 
 #ifdef CONFIG_LOCK_DOWN_KERNEL_EARLY
 int kernel_is_locked_down_early(int what);
 #else
-static int kernel_is_locked_down_early(int what)
+static inline int kernel_is_locked_down_early(int what)
 {
 	return 0;
 }
+#endif
 #endif
