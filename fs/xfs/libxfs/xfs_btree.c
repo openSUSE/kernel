b5ba7b08374d6d665399d55f70b8b1b3a33296c4
Anthony Iliopoulos <ailiop@suse.com>
2024-12-23 12:22:50 +0100

    xfs: return a 64-bit block count from xfs_btree_count_blocks (git-fixes).
    suse-commit: 2c2d3e0f12045899acb2bf90f06e92e27d0755fa

--- fs/xfs/libxfs/xfs_btree.c
+++ fs/xfs/libxfs/xfs_btree.c
@@ -5156,7 +5156,7 @@ xfs_btree_count_blocks_helper(
 	int			level,
 	void			*data)
 {
-	xfs_extlen_t		*blocks = data;
+	xfs_filblks_t		*blocks = data;
 	(*blocks)++;
 
 	return 0;
@@ -5166,7 +5166,7 @@ xfs_btree_count_blocks_helper(
 int
 xfs_btree_count_blocks(
 	struct xfs_btree_cur	*cur,
-	xfs_extlen_t		*blocks)
+	xfs_filblks_t		*blocks)
 {
 	*blocks = 0;
 	return xfs_btree_visit_blocks(cur, xfs_btree_count_blocks_helper,
