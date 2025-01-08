84e46f17f58f183bab99edabe8988f64ef4793b7
Takashi Iwai <tiwai@suse.de>
2025-01-08 15:33:45 +0100

    wifi: iwlwifi: mvm: add firmware debug points for EMLSR entry / exit (jsc#PED-10412).
    suse-commit: f6c5fac95222798ca3e1e270830cbce8db9a94ea

--- drivers/net/wireless/intel/iwlwifi/mvm/mld-mac80211.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/mld-mac80211.c
@@ -277,6 +277,9 @@ static int iwl_mvm_esr_mode_active(struct iwl_mvm *mvm,
 	 */
 	iwl_mvm_restart_mpdu_count(mvm, mvmvif);
 
+	iwl_dbg_tlv_time_point(&mvm->fwrt, IWL_FW_INI_TIME_ESR_LINK_UP,
+			       NULL);
+
 	return ret;
 }
 
@@ -463,6 +466,9 @@ static int iwl_mvm_esr_mode_inactive(struct iwl_mvm *mvm,
 
 	/* Start a new counting window */
 	iwl_mvm_restart_mpdu_count(mvm, mvmvif);
+
+	iwl_dbg_tlv_time_point(&mvm->fwrt, IWL_FW_INI_TIME_ESR_LINK_DOWN,
+			       NULL);
 
 	return ret;
 }
