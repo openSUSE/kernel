0da3cdb4963341939b5ef9906d98e849d98e4c29
Takashi Iwai <tiwai@suse.de>
2025-01-08 14:37:04 +0100

    wifi: mac80211: inform the low level if drv_stop() is a suspend (jsc#PED-10412).
    suse-commit: 17e571ee65714fc7b2922b2a61cb1d5a3585a5cd

--- net/mac80211/util.c
+++ net/mac80211/util.c
@@ -1564,7 +1564,7 @@ u32 ieee80211_sta_get_rates(struct ieee80211_sub_if_data *sdata,
 	return supp_rates;
 }
 
-void ieee80211_stop_device(struct ieee80211_local *local)
+void ieee80211_stop_device(struct ieee80211_local *local, bool suspend)
 {
 	local_bh_disable();
 	ieee80211_handle_queued_frames(local);
@@ -1577,7 +1577,7 @@ void ieee80211_stop_device(struct ieee80211_local *local)
 
 	flush_workqueue(local->workqueue);
 	wiphy_work_flush(local->hw.wiphy, NULL);
-	drv_stop(local);
+	drv_stop(local, suspend);
 }
 
 static void ieee80211_flush_completed_scan(struct ieee80211_local *local,
