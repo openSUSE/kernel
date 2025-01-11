0f2d8c5e9e90e42755130c3a9bbe98b6f4ade57b
Juergen Gross <jgross@suse.com>
2025-01-11 08:41:45 +0100

    - KVM: x86: Move nEPT exit_qualification field from kvm_vcpu_arch   to x86_exception (PED#348). - Refresh   patches.suse/KVM-VMX-Set-PFERR_GUEST_-FINAL-PAGE-_MASK-if-and-onl.patch.
    suse-commit: 0401405118873edd3037869cb299b025ecc09e03

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -5773,8 +5773,6 @@ static int handle_ept_violation(struct kvm_vcpu *vcpu)
 		error_code |= (exit_qualification & EPT_VIOLATION_GVA_TRANSLATED) ?
 			      PFERR_GUEST_FINAL_MASK : PFERR_GUEST_PAGE_MASK;
 
-	vcpu->arch.exit_qualification = exit_qualification;
-
 	/*
 	 * Check that the GPA doesn't exceed physical memory limits, as that is
 	 * a guest page fault.  We have to emulate the instruction here, because
