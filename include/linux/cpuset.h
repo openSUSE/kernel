8897253819c7d80396104f680c925983b62be6c7
Michal Koutný <mkoutny@suse.com>
2025-01-15 14:22:22 +0100

    cgroup/cpuset: Remove cpuset_do_slab_mem_spread() (jsc#PED-11934).
    suse-commit: 646f6447eb84b5ee16d554cf626097eeaf1b1bb4

--- include/linux/cpuset.h
+++ include/linux/cpuset.h
@@ -120,11 +120,6 @@ static inline int cpuset_do_page_mem_spread(void)
 	return task_spread_page(current);
 }
 
-static inline int cpuset_do_slab_mem_spread(void)
-{
-	return task_spread_slab(current);
-}
-
 extern bool current_cpuset_is_being_rebound(void);
 
 extern void rebuild_sched_domains(void);
@@ -254,11 +249,6 @@ static inline int cpuset_mem_spread_node(void)
 }
 
 static inline int cpuset_do_page_mem_spread(void)
-{
-	return 0;
-}
-
-static inline int cpuset_do_slab_mem_spread(void)
 {
 	return 0;
 }
