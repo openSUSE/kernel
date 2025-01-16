1c5c1655415c2e517790f5829d2574760371a165
Juergen Gross <jgross@suse.com>
2025-01-16 16:50:09 +0100

    - KVM: rename CONFIG_HAVE_KVM_GMEM_* to   CONFIG_HAVE_KVM_ARCH_GMEM_* (jsc#PED-348). - Update config files.
    suse-commit: 1f7ccab4a1c3bb64b05da941e1e33248c045bd40

--- virt/kvm/guest_memfd.c
+++ virt/kvm/guest_memfd.c
@@ -15,7 +15,7 @@ struct kvm_gmem {
 
 static int kvm_gmem_prepare_folio(struct inode *inode, pgoff_t index, struct folio *folio)
 {
-#ifdef CONFIG_HAVE_KVM_GMEM_PREPARE
+#ifdef CONFIG_HAVE_KVM_ARCH_GMEM_PREPARE
 	struct list_head *gmem_list = &inode->i_mapping->private_list;
 	struct kvm_gmem *gmem;
 
@@ -347,7 +347,7 @@ static int kvm_gmem_error_page(struct address_space *mapping, struct page *page)
 	return MF_DELAYED;
 }
 
-#ifdef CONFIG_HAVE_KVM_GMEM_INVALIDATE
+#ifdef CONFIG_HAVE_KVM_ARCH_GMEM_INVALIDATE
 static void kvm_gmem_free_folio(struct folio *folio)
 {
 	struct page *page = folio_page(folio, 0);
@@ -362,7 +362,7 @@ static const struct address_space_operations kvm_gmem_aops = {
 	.dirty_folio = noop_dirty_folio,
 	.migrate_folio	= kvm_gmem_migrate_folio,
 	.error_remove_page = kvm_gmem_error_page,
-#ifdef CONFIG_HAVE_KVM_GMEM_INVALIDATE
+#ifdef CONFIG_HAVE_KVM_ARCH_GMEM_INVALIDATE
 	.free_folio = kvm_gmem_free_folio,
 #endif
 };
