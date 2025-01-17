2c393084d95cc97279178294754c69badd940ebd
Gabriel Krisman Bertazi <krisman@suse.de>
2025-01-17 16:27:55 -0500

    io_uring: fix cancellation overwriting req->flags (bsc#1230569).
    suse-commit: be335d68f8c4074056e85d57aef19a6575b3a6fe

--- io_uring/io_uring.c
+++ io_uring/io_uring.c
@@ -2079,6 +2079,7 @@ static int io_init_req(struct io_ring_ctx *ctx, struct io_kiocb *req,
 	req->file = NULL;
 	req->rsrc_node = NULL;
 	req->task = current;
+	req->cancel_seq_set = false;
 
 	if (unlikely(opcode >= IORING_OP_LAST)) {
 		req->opcode = 0;
