12ee94b6e88b2998e71d76c69937b10bbc382b32
David Sterba <dsterba@suse.com>
2025-01-22 19:40:52 +0100

    tracing: Prevent bad count for tracing_cpumask_write (CVE-2024-56763 bsc#1235638)
    suse-commit: 224036df68d30e084a6962d71f2d972e41886b8d

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -4120,6 +4120,9 @@ tracing_cpumask_write(struct file *filp, const char __user *ubuf,
 	cpumask_var_t tracing_cpumask_new;
 	int err, cpu;
 
+	if (count == 0 || count > KMALLOC_MAX_SIZE)
+		return -EINVAL;
+
 	if (!alloc_cpumask_var(&tracing_cpumask_new, GFP_KERNEL))
 		return -ENOMEM;
 
