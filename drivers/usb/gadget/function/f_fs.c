cc3d0d7a517f35906c8f834f2e85be0714bde665
Oliver Neukum <oneukum@suse.com>
2025-02-06 15:06:21 +0100

    usb: gadget: add missing MODULE_DESCRIPTION() macros (git-fixes).
    suse-commit: 69e7418466cda8c650dc7578532e902b537e5d72

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -3803,5 +3803,6 @@ static char *ffs_prepare_buffer(const char __user *buf, size_t len)
 }
 
 DECLARE_USB_FUNCTION_INIT(ffs, ffs_alloc_inst, ffs_alloc);
+MODULE_DESCRIPTION("user mode file system API for USB composite function controllers");
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Michal Nazarewicz");
