b76f78d43eac9276cff01dc90657eafc7bbb1403
Enzo Matsumiya <ematsumiya@suse.de>
2025-02-14 10:51:48 -0300

    cifs: update desired access while requesting for directory lease (git-fixes).
    suse-commit: 357793318be06cb1c7ecadd6006e9ac9946c71b4

--- fs/smb/client/cached_dir.c
+++ fs/smb/client/cached_dir.c
@@ -285,7 +285,7 @@ reval_cfid:
 		.tcon = tcon,
 		.path = path,
 		.create_options = cifs_create_options(cifs_sb, CREATE_NOT_FILE),
-		.desired_access = FILE_READ_ATTRIBUTES,
+		.desired_access =  FILE_READ_DATA | FILE_READ_ATTRIBUTES,
 		.disposition = FILE_OPEN,
 		.fid = pfid,
 	};
