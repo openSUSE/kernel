2049a823331e020429d40960e9a54c68d8b3323e
Hannes Reinecke <hare@suse.de>
2025-02-16 02:56:50 +0100

    cxl: Remove leftover attribute documentation in 'struct cxl_dev_state' (jsc#PED-10836).
    suse-commit: 21385ba0e8c57283791f56303ca05cb217bf8470

--- drivers/cxl/cxlmem.h
+++ drivers/cxl/cxlmem.h
@@ -390,7 +390,6 @@ struct cxl_security_state {
  * @next_volatile_bytes: volatile capacity change pending device reset
  * @next_persistent_bytes: persistent capacity change pending device reset
  * @component_reg_phys: register base of component registers
- * @info: Cached DVSEC information about the device.
  * @serial: PCIe Device Serial Number
  * @event: event log driver state
  * @poison: poison driver state info
