fdda9eb527bbef5c3921b825666392f85ca40734
Michal Koutný <mkoutny@suse.com>
2025-02-21 11:17:50 +0100

    hid: cp2112: Fix duplicate workqueue initialization (CVE-2023-52853 bsc#1224988).
    suse-commit: 0767a8ea741f8db580c9111261e28cff580a4927

--- drivers/hid/hid-cp2112.c
+++ drivers/hid/hid-cp2112.c
@@ -1147,8 +1147,6 @@ static unsigned int cp2112_gpio_irq_startup(struct irq_data *d)
 	struct gpio_chip *gc = irq_data_get_irq_chip_data(d);
 	struct cp2112_device *dev = gpiochip_get_data(gc);
 
-	INIT_DELAYED_WORK(&dev->gpio_poll_worker, cp2112_gpio_poll_callback);
-
 	if (!dev->gpio_poll) {
 		dev->gpio_poll = true;
 		schedule_delayed_work(&dev->gpio_poll_worker, 0);
@@ -1331,6 +1329,8 @@ static int cp2112_probe(struct hid_device *hdev, const struct hid_device_id *id)
 	dev->gc.ngpio			= 8;
 	dev->gc.can_sleep		= 1;
 	dev->gc.parent			= &hdev->dev;
+
+	INIT_DELAYED_WORK(&dev->gpio_poll_worker, cp2112_gpio_poll_callback);
 
 	ret = gpiochip_add_data(&dev->gc, dev);
 	if (ret < 0) {
