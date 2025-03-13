e58162fb43ce08562d47ff85b59500bc4596347a
Michal Koutný <mkoutny@suse.com>
2025-03-13 14:51:13 +0100

    cgroup/cpuset-v1: Add deprecation messages to mem_exclusive and mem_hardwall (jsc#PED-12405).
    suse-commit: 899b63118e4ddc448f89cfcc51e21ee8a61ada9c

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -3599,9 +3599,11 @@ static int cpuset_write_u64(struct cgroup_subsys_state *css, struct cftype *cft,
 		retval = update_flag(CS_CPU_EXCLUSIVE, cs, val);
 		break;
 	case FILE_MEM_EXCLUSIVE:
+		pr_info_once("cpuset.%s is deprecated\n", cft->name);
 		retval = update_flag(CS_MEM_EXCLUSIVE, cs, val);
 		break;
 	case FILE_MEM_HARDWALL:
+		pr_info_once("cpuset.%s is deprecated\n", cft->name);
 		retval = update_flag(CS_MEM_HARDWALL, cs, val);
 		break;
 	case FILE_SCHED_LOAD_BALANCE:
