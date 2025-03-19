5c3cbab7f9e1c76dcc794995674f5d05da1745c6
Jiri Slaby <jslaby@suse.cz>
2025-03-19 08:56:00 +0100

    ovpn: add basic interface creation/destruction/management routines (bsc#1239783).
    suse-commit: 8bab18ceedef6567608e37c3e97f278d004edb90

--- drivers/net/ovpn/ovpnpriv.h
+++ drivers/net/ovpn/ovpnpriv.h
@@ -10,12 +10,19 @@
 #ifndef _NET_OVPN_OVPNSTRUCT_H_
 #define _NET_OVPN_OVPNSTRUCT_H_
 
+#include <uapi/linux/if_link.h>
+#include <uapi/linux/ovpn.h>
+
 /**
  * struct ovpn_priv - per ovpn interface state
  * @dev: the actual netdev representing the tunnel
+ * @registered: whether dev is still registered with netdev or not
+ * @mode: device operation mode (i.e. p2p, mp, ..)
  */
 struct ovpn_priv {
 	struct net_device *dev;
+	bool registered;
+	enum ovpn_mode mode;
 };
 
 #endif /* _NET_OVPN_OVPNSTRUCT_H_ */
