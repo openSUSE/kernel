253a07372873c2335e0950338fa37b22a462d4db
Takashi Iwai <tiwai@suse.de>
2025-03-21 08:29:13 +0100

    can: flexcan: only change CAN state when link up in system PM (git-fixes).
    suse-commit: 541138914d539ce244848963bd5878f6853ca0c0

--- drivers/net/can/flexcan/flexcan-core.c
+++ drivers/net/can/flexcan/flexcan-core.c
@@ -2253,8 +2253,9 @@ static int __maybe_unused flexcan_suspend(struct device *device)
 		}
 		netif_stop_queue(dev);
 		netif_device_detach(dev);
+
+		priv->can.state = CAN_STATE_SLEEPING;
 	}
-	priv->can.state = CAN_STATE_SLEEPING;
 
 	return 0;
 }
@@ -2265,7 +2266,6 @@ static int __maybe_unused flexcan_resume(struct device *device)
 	struct flexcan_priv *priv = netdev_priv(dev);
 	int err;
 
-	priv->can.state = CAN_STATE_ERROR_ACTIVE;
 	if (netif_running(dev)) {
 		netif_device_attach(dev);
 		netif_start_queue(dev);
@@ -2285,6 +2285,8 @@ static int __maybe_unused flexcan_resume(struct device *device)
 
 			flexcan_chip_interrupts_enable(dev);
 		}
+
+		priv->can.state = CAN_STATE_ERROR_ACTIVE;
 	}
 
 	return 0;
