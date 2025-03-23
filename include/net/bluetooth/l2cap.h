97e8554810685316d07067b9148326631c06b877
Takashi Iwai <tiwai@suse.de>
2025-03-23 10:00:17 +0100

    kABI workaround for l2cap_conn changes (git-fixes).
    suse-commit: 4c0b0d004650dd812c1cdbe57b71c8436df73105

--- include/net/bluetooth/l2cap.h
+++ include/net/bluetooth/l2cap.h
@@ -668,7 +668,11 @@ struct l2cap_conn {
 	struct l2cap_chan	*smp;
 
 	struct list_head	chan_l;
+#ifdef __GENKSYMS__
+	struct mutex		chan_lock;
+#else
 	struct mutex		lock;
+#endif
 	struct kref		ref;
 	struct list_head	users;
 };
