0aabb6004ddac0363bcff9eacf8715df88f81d6d
Hannes Reinecke <hare@suse.de>
2025-03-24 14:57:30 +0100

    block: add a bdev_limits helper (jsc#PED-9651).
    suse-commit: 60bdfe96c603bce7324ed4864442ea85de4050ee

--- block/blk-merge.c
+++ block/blk-merge.c
@@ -430,10 +430,9 @@ struct bio *bio_split_write_zeroes(struct bio *bio,
  */
 struct bio *bio_split_to_limits(struct bio *bio)
 {
-	const struct queue_limits *lim = &bdev_get_queue(bio->bi_bdev)->limits;
 	unsigned int nr_segs;
 
-	return __bio_split_to_limits(bio, lim, &nr_segs);
+	return __bio_split_to_limits(bio, bdev_limits(bio->bi_bdev), &nr_segs);
 }
 EXPORT_SYMBOL(bio_split_to_limits);
 
