5ef5fa092b91761c6191c088ceb9bb8660dd1c0d
Pedro Falcato <pfalcato@suse.de>
2025-03-25 09:25:54 +0000

    ipv4: use RCU protection in inet_select_addr() (bsc#1239994).
    suse-commit: 442e2c40046977242bc558162c2e7bd24ddd65b3

--- net/ipv4/devinet.c
+++ net/ipv4/devinet.c
@@ -1248,10 +1248,11 @@ __be32 inet_select_addr(const struct net_device *dev, __be32 dst, int scope)
 {
 	__be32 addr = 0;
 	struct in_device *in_dev;
-	struct net *net = dev_net(dev);
+	struct net *net;
 	int master_idx;
 
 	rcu_read_lock();
+	net = dev_net_rcu(dev);
 	in_dev = __in_dev_get_rcu(dev);
 	if (!in_dev)
 		goto no_in_dev;
