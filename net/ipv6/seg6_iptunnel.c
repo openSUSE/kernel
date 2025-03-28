aff6a9d9eee81824f6b6cc3c22ff60b584a6821b
Steffen Jaeckel <sjaeckel@suse.de>
2025-03-28 17:52:58 +0100

    net: ipv6: fix dst refleaks in rpl, seg6 and ioam6 lwtunnels (git-fixes).
    suse-commit: 2678976879447f76b19cc41a869c5174814be056

--- net/ipv6/seg6_iptunnel.c
+++ net/ipv6/seg6_iptunnel.c
@@ -490,8 +490,10 @@ static int seg6_input_core(struct net *net, struct sock *sk,
 	local_bh_enable();
 
 	err = seg6_do_srh(skb, dst);
-	if (unlikely(err))
+	if (unlikely(err)) {
+		dst_release(dst);
 		goto drop;
+	}
 
 	if (!dst) {
 		ip6_route_input(skb);
@@ -581,7 +583,6 @@ static int seg6_output_core(struct net *net, struct sock *sk,
 		dst = ip6_route_output(net, NULL, &fl6);
 		if (dst->error) {
 			err = dst->error;
-			dst_release(dst);
 			goto drop;
 		}
 
@@ -603,6 +604,7 @@ static int seg6_output_core(struct net *net, struct sock *sk,
 
 	return dst_output(net, sk, skb);
 drop:
+	dst_release(dst);
 	kfree_skb(skb);
 	return err;
 }
