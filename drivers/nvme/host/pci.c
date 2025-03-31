9834f574dfe18c05e4698a39037164f575a4588d
Daniel Wagner <dwagner@suse.de>
2025-03-31 16:10:25 +0200

    nvme-pci: use sgls for all user requests if possible (git-fixes).
    suse-commit: 1dab0e31b94729720a7f49f03cafe3e1089f155d

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -513,7 +513,8 @@ static inline bool nvme_pci_metadata_use_sgls(struct nvme_dev *dev,
 {
 	if (!nvme_ctrl_meta_sgl_supported(&dev->ctrl))
 		return false;
-	return req->nr_integrity_segments > 1;
+	return req->nr_integrity_segments > 1 ||
+		nvme_req(req)->flags & NVME_REQ_USERCMD;
 }
 
 static inline bool nvme_pci_use_sgls(struct nvme_dev *dev, struct request *req,
@@ -531,7 +532,7 @@ static inline bool nvme_pci_use_sgls(struct nvme_dev *dev, struct request *req,
 	if (nvme_pci_metadata_use_sgls(dev, req))
 		return true;
 	if (!sgl_threshold || avg_seg_size < sgl_threshold)
-		return false;
+		return nvme_req(req)->flags & NVME_REQ_USERCMD;
 	return true;
 }
 
