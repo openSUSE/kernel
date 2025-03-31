46992307ac7150c53b3b8a46d8b8f8357523de54
Daniel Wagner <dwagner@suse.de>
2025-03-31 13:56:29 +0200

    nvme-ioctl: fix leaked requests on mapping error (git-fixes).
    suse-commit: 7130a7060be07d92be0474c5d23a5f2e269282df

--- drivers/nvme/host/ioctl.c
+++ drivers/nvme/host/ioctl.c
@@ -124,15 +124,19 @@ static int nvme_map_user_request(struct request *req, u64 ubuffer,
 	struct bio *bio = NULL;
 	int ret;
 
-	if (has_metadata && !supports_metadata)
-		return -EINVAL;
+	if (has_metadata && !supports_metadata) {
+		ret = -EINVAL;
+		goto out;
+	}
 
 	if (ioucmd && (ioucmd->flags & IORING_URING_CMD_FIXED)) {
 		struct iov_iter iter;
 
 		/* fixedbufs is only for non-vectored io */
-		if (WARN_ON_ONCE(flags & NVME_IOCTL_VEC))
-			return -EINVAL;
+		if (WARN_ON_ONCE(flags & NVME_IOCTL_VEC)) {
+			ret = -EINVAL;
+			goto out;
+		}
 		ret = io_uring_cmd_import_fixed(ubuffer, bufflen,
 				rq_data_dir(req), &iter, ioucmd);
 		if (ret < 0)
