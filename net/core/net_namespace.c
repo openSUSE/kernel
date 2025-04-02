1e5766d5d34c6b281f74b52eb7176e4a053a98ca
Michal Kubecek <mkubecek@suse.cz>
2025-04-03 00:22:30 +0200

    net: Add net_passive_inc() and net_passive_dec() (CVE-2025-21884 bsc#1240171).
    suse-commit: 8f933a27fea0ca8e211f07e1f22d034902689790

--- net/core/net_namespace.c
+++ net/core/net_namespace.c
@@ -457,7 +457,7 @@ static void net_complete_free(void)
 
 }
 
-static void net_free(struct net *net)
+void net_passive_dec(struct net *net)
 {
 	if (refcount_dec_and_test(&net->passive)) {
 		kfree(rcu_access_pointer(net->gen));
@@ -475,7 +475,7 @@ void net_drop_ns(void *p)
 	struct net *net = (struct net *)p;
 
 	if (net)
-		net_free(net);
+		net_passive_dec(net);
 }
 
 struct net *copy_net_ns(unsigned long flags,
@@ -517,7 +517,7 @@ put_userns:
 		key_remove_domain(net->key_domain);
 #endif
 		put_user_ns(user_ns);
-		net_free(net);
+		net_passive_dec(net);
 dec_ucounts:
 		dec_net_namespaces(ucounts);
 		return ERR_PTR(rv);
@@ -653,7 +653,7 @@ static void cleanup_net(struct work_struct *work)
 		key_remove_domain(net->key_domain);
 #endif
 		put_user_ns(net->user_ns);
-		net_free(net);
+		net_passive_dec(net);
 	}
 }
 
