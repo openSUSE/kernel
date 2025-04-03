afcc3367af745586cfd818039a5ccbb49da9d4bb
Dario Faggioli <dfaggioli@suse.com>
2025-04-03 23:42:39 +0200

    kABI: Fix kABI after backport od CVE-2025-21839 (bsc#1239061 CVE-2025-21839).
    suse-commit: 38fa6d3989fa1125504bf08273a0d5a3303ff16a

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1332,7 +1332,6 @@ struct kvm_x86_ops {
 	void (*get_gdt)(struct kvm_vcpu *vcpu, struct desc_ptr *dt);
 	void (*set_gdt)(struct kvm_vcpu *vcpu, struct desc_ptr *dt);
 	void (*sync_dirty_debug_regs)(struct kvm_vcpu *vcpu);
-	void (*set_dr6)(struct kvm_vcpu *vcpu, unsigned long value);
 	void (*set_dr7)(struct kvm_vcpu *vcpu, unsigned long value);
 	void (*cache_reg)(struct kvm_vcpu *vcpu, enum kvm_reg reg);
 	unsigned long (*get_rflags)(struct kvm_vcpu *vcpu);
@@ -1485,6 +1484,7 @@ struct kvm_x86_ops {
 #ifndef __GENKSYMS__
 	void (*guest_memory_reclaimed)(struct kvm *kvm);
 	bool (*get_if_flag)(struct kvm_vcpu *vcpu);
+	void (*set_dr6)(struct kvm_vcpu *vcpu, unsigned long value);
 #endif
 };
 
