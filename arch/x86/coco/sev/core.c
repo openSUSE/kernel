f276729e845b0d0328f6f3dbf85759438f7e7d84
Joerg Roedel <jroedel@suse.de>
2025-05-16 16:51:47 +0200

    x86/sev: Register tpm-svsm platform device (bsc#1241191).
    suse-commit: a35885f82c782e6447e6c52ea582a569d8ab2e15

--- arch/x86/coco/sev/core.c
+++ arch/x86/coco/sev/core.c
@@ -2693,6 +2693,11 @@ static struct platform_device sev_guest_device = {
 	.id		= -1,
 };
 
+static struct platform_device tpm_svsm_device = {
+	.name		= "tpm-svsm",
+	.id		= -1,
+};
+
 static int __init snp_init_platform_device(void)
 {
 	if (!cc_platform_has(CC_ATTR_GUEST_SEV_SNP))
@@ -2701,7 +2706,11 @@ static int __init snp_init_platform_device(void)
 	if (platform_device_register(&sev_guest_device))
 		return -ENODEV;
 
-	pr_info("SNP guest platform device initialized.\n");
+	if (snp_svsm_vtpm_probe() &&
+	    platform_device_register(&tpm_svsm_device))
+		return -ENODEV;
+
+	pr_info("SNP guest platform devices initialized.\n");
 	return 0;
 }
 device_initcall(snp_init_platform_device);
