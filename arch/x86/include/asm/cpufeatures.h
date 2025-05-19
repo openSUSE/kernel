57ed46f44bbbe9ad04dbcff3c6a9647b3ce2d4e3
Nikolay Borisov <nik.borisov@suse.com>
2025-05-19 14:22:50 +0300

    x86/its: Enumerate Indirect Target Selection (ITS) bug (bsc#1242006 CVE-2024-28956).
    suse-commit: 28f721617ea7b39dc0d97cba5e7b7c889e7dce34

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -490,8 +490,9 @@
 #define X86_BUG_GDS			X86_BUG(31) /* CPU is affected by Gather Data Sampling */
 
 /* BUG word 2 */
-#define X86_BUG_DIV0                   X86_BUG(1*32 + 0) /* AMD DIV0 speculation bug */
-#define X86_BUG_RFDS		       X86_BUG(1*32 + 1) /* CPU is vulnerable to Register File Data Sampling */
-#define X86_BUG_BHI		X86_BUG(1*32 + 2) /* CPU is affected by Branch History Injection */
-#define X86_BUG_IBPB_NO_RET	   	X86_BUG(1*32 + 4) /* "ibpb_no_ret" IBPB omits return target predictions */
+#define X86_BUG_DIV0			X86_BUG(1*32 + 0) /* AMD DIV0 speculation bug */
+#define X86_BUG_RFDS			X86_BUG(1*32 + 1) /* CPU is vulnerable to Register File Data Sampling */
+#define X86_BUG_BHI			X86_BUG(1*32 + 2) /* CPU is affected by Branch History Injection */
+#define X86_BUG_IBPB_NO_RET		X86_BUG(1*32 + 4) /* "ibpb_no_ret" IBPB omits return target predictions */
+#define X86_BUG_ITS			X86_BUG(1*32 + 6) /* "its" CPU is affected by Indirect Target Selection */
 #endif /* _ASM_X86_CPUFEATURES_H */
