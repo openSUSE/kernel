9cbdbcd5d8865fb55ab70ebf788f2132bf476233
NeilBrown <neilb@suse.de>
2025-05-19 11:08:01 +1000

    nfsd: fix race between laundromat and free_stateid() (CVE-2024-50106 bsc#1232882).
    suse-commit: a790b423f446e6e317aa2f4763e6de92a0270048

--- fs/nfsd/nfs4state.c
+++ fs/nfsd/nfs4state.c
@@ -1008,8 +1008,8 @@ static void revoke_delegation(struct nfs4_delegation *dp)
 	if (clp->cl_minorversion == 0)
 		nfs4_put_stid(&dp->dl_stid);
 	else {
-		dp->dl_stid.sc_type = NFS4_REVOKED_DELEG_STID;
 		spin_lock(&clp->cl_lock);
+		dp->dl_stid.sc_type = NFS4_REVOKED_DELEG_STID;
 		list_add(&dp->dl_recall_lru, &clp->cl_revoked);
 		spin_unlock(&clp->cl_lock);
 	}
