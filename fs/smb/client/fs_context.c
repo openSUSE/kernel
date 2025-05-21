82b0b6212bafd86658fcff29d925bfdde6f435d2
Enzo Matsumiya <ematsumiya@suse.de>
2025-05-21 13:31:02 +0200

    smb: client: fix DFS mount against old servers with NTLMSSP (git-fixes).
    suse-commit: 9bdc840c343bf2fc2ca386a1a0a3e4127c2d1424

--- fs/smb/client/fs_context.c
+++ fs/smb/client/fs_context.c
@@ -381,6 +381,7 @@ smb3_fs_context_dup(struct smb3_fs_context *new_ctx, struct smb3_fs_context *ctx
 	new_ctx->source = NULL;
 	new_ctx->iocharset = NULL;
 	new_ctx->leaf_fullpath = NULL;
+	new_ctx->dns_dom = NULL;
 	/*
 	 * Make sure to stay in sync with smb3_cleanup_fs_context_contents()
 	 */
@@ -394,6 +395,7 @@ smb3_fs_context_dup(struct smb3_fs_context *new_ctx, struct smb3_fs_context *ctx
 	DUP_CTX_STR(nodename);
 	DUP_CTX_STR(iocharset);
 	DUP_CTX_STR(leaf_fullpath);
+	DUP_CTX_STR(dns_dom);
 
 	return 0;
 }
@@ -1751,6 +1753,8 @@ smb3_cleanup_fs_context_contents(struct smb3_fs_context *ctx)
 	ctx->prepath = NULL;
 	kfree(ctx->leaf_fullpath);
 	ctx->leaf_fullpath = NULL;
+	kfree(ctx->dns_dom);
+	ctx->dns_dom = NULL;
 }
 
 void
