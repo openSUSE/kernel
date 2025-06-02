94ddc3c1e655d8da5d37b411a8c97f1807f72c53
David Sterba <dsterba@suse.com>
2025-06-02 14:51:43 +0200

    net_sched: sch_sfq: move the limit validation (CVE-2025-37752 bsc#1242504)
    suse-commit: e09b6aad5d2ab48ce9abe092580d25ba09b8290f

--- net/sched/sch_sfq.c
+++ net/sched/sch_sfq.c
@@ -663,8 +663,7 @@ static int sfq_change(struct Qdisc *sch, struct nlattr *opt)
 		if (!p)
 			return -ENOMEM;
 	}
-	if (ctl->limit == 1)
-		return -EINVAL;
+
 	sch_tree_lock(sch);
 
 	limit = q->limit;
@@ -705,6 +704,11 @@ static int sfq_change(struct Qdisc *sch, struct nlattr *opt)
 	if (ctl->limit) {
 		limit = min_t(u32, ctl->limit, maxdepth * maxflows);
 		maxflows = min_t(u32, maxflows, limit);
+	}
+	if (limit == 1) {
+		sch_tree_unlock(sch);
+		kfree(p);
+		return -EINVAL;
 	}
 
 	/* commit configuration */
