c32da5b9e3400634fc55c9032a5cfacfb8313d6f
Juergen Gross <jgross@suse.com>
2025-06-03 13:41:38 +0200

    KVM: x86/mmu: Implement memslot deletion for TDX (jsc#PED-348).
    suse-commit: 3d2ab8bd6e927d5fc8c7f200ae9b5cd21f4de306

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -7073,6 +7073,7 @@ static void kvm_mmu_zap_memslot(struct kvm *kvm,
 		.start = slot->base_gfn,
 		.end = slot->base_gfn + slot->npages,
 		.may_block = true,
+		.attr_filter = KVM_FILTER_PRIVATE | KVM_FILTER_SHARED,
 	};
 	bool flush;
 
