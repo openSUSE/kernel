9cdf5d1b7cfa4f142abad1208ca3bd89623a5c99
Juergen Gross <jgross@suse.com>
2025-06-06 07:58:22 +0200

    x86/kexec: Disable kexec/kdump on platforms with TDX partial write erratum (jsc#PED-348).
    suse-commit: 0956988a9672605291a61d523d05683acab112bf

--- arch/x86/kernel/machine_kexec_64.c
+++ arch/x86/kernel/machine_kexec_64.c
@@ -300,6 +300,22 @@ int machine_kexec_prepare(struct kimage *image)
 	unsigned long start_pgtable;
 	int result;
 
+	/*
+	 * Some early TDX-capable platforms have an erratum.  A kernel
+	 * partial write (a write transaction of less than cacheline
+	 * lands at memory controller) to TDX private memory poisons that
+	 * memory, and a subsequent read triggers a machine check.
+	 *
+	 * On those platforms the old kernel must reset TDX private
+	 * memory before jumping to the new kernel otherwise the new
+	 * kernel may see unexpected machine check.  For simplicity
+	 * just fail kexec/kdump on those platforms.
+	 */
+	if (boot_cpu_has_bug(X86_BUG_TDX_PW_MCE)) {
+		pr_info_once("Not allowed on platform with tdx_pw_mce bug\n");
+		return -EOPNOTSUPP;
+	}
+
 	/* Calculate the offsets */
 	start_pgtable = page_to_pfn(image->control_code_page) << PAGE_SHIFT;
 
