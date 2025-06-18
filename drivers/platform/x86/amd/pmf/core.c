c7aaa858ec04dd5f7b4ad27504275a27419004c2
Takashi Iwai <tiwai@suse.de>
2025-06-18 07:46:53 +0200

    platform/x86/amd: pmf: Use device managed allocations (git-fixes).
    suse-commit: 75363b804ee96601e22fec8cbe007a5511b8e463

--- drivers/platform/x86/amd/pmf/core.c
+++ drivers/platform/x86/amd/pmf/core.c
@@ -282,7 +282,7 @@ int amd_pmf_set_dram_addr(struct amd_pmf_dev *dev, bool alloc_buffer)
 			dev_err(dev->dev, "Invalid CPU id: 0x%x", dev->cpu_id);
 		}
 
-		dev->buf = kzalloc(dev->mtable_size, GFP_KERNEL);
+		dev->buf = devm_kzalloc(dev->dev, dev->mtable_size, GFP_KERNEL);
 		if (!dev->buf)
 			return -ENOMEM;
 	}
@@ -495,7 +495,6 @@ static void amd_pmf_remove(struct platform_device *pdev)
 	amd_pmf_dbgfs_unregister(dev);
 	mutex_destroy(&dev->lock);
 	mutex_destroy(&dev->update_mutex);
-	kfree(dev->buf);
 }
 
 static const struct attribute_group *amd_pmf_driver_groups[] = {
