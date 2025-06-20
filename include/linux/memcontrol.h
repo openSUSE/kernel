8867b02460474ace1da1a09182506139d3142f35
Michal Koutný <mkoutny@suse.com>
2025-06-20 15:03:33 +0200

    mm, memcg: cg2 memory{.swap,}.peak write handlers (jsc#PED-12551).
    suse-commit: 97c4d3758ea28b2741b06596a40868beabc66894

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -222,6 +222,11 @@ struct mem_cgroup {
 	struct page_counter kmem;		/* v1 only */
 	struct page_counter tcpmem;		/* v1 only */
 
+	/* registered local peak watchers */
+	struct list_head memory_peaks;
+	struct list_head swap_peaks;
+	spinlock_t	 peaks_lock;
+
 	/* Range enforcement for interrupt charges */
 	struct work_struct high_work;
 
