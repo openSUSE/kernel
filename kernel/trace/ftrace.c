6f536b75f5fdd2a3ed94fce1a055869f02a7ebe4
Petr Pavlu <petr.pavlu@suse.com>
2025-06-24 17:04:56 +0200

    ftrace: Return the first found result in lookup_rec() (bsc#1226837).
    suse-commit: 548c54ef4fe6aacd6b174c2206451a1d4d4828aa

--- kernel/trace/ftrace.c
+++ kernel/trace/ftrace.c
@@ -1576,6 +1576,8 @@ static struct dyn_ftrace *lookup_rec(unsigned long start, unsigned long end)
 		rec = bsearch(&key, pg->records, pg->index,
 			      sizeof(struct dyn_ftrace),
 			      ftrace_cmp_recs);
+		if (rec)
+			break;
 	}
 	return rec;
 }
