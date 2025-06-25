ee609f0f36d042334d156102ab1d9581f9250fd9
Shung-Hsi Yu <shung-hsi.yu@suse.com>
2025-06-25 15:20:27 +0800

    bpf: free verifier states when they are no longer referenced (CVE-2025-38060 bsc#1245155).
    - Refresh patches.suse/kABI-padding-for-bpf.patch.
    
    suse-commit: 06e2482da5d550e6f6f72e0345581986afea8d7a

--- include/linux/bpf_verifier.h
+++ include/linux/bpf_verifier.h
@@ -438,11 +438,6 @@ struct bpf_verifier_state {
 	bool speculative;
 	bool active_rcu_lock;
 	u32 active_preempt_lock;
-	/* If this state was ever pointed-to by other state's loop_entry field
-	 * this flag would be set to true. Used to avoid freeing such states
-	 * while they are still in use.
-	 */
-	bool used_as_loop_entry;
 	bool in_sleepable;
 
 	/* first and last insn idx of this verifier state */
@@ -468,6 +463,11 @@ struct bpf_verifier_state {
 	u32 dfs_depth;
 	u32 callback_unroll_depth;
 	u32 may_goto_depth;
+	/* If this state was ever pointed-to by other state's loop_entry field
+	 * this flag would be set to true. Used to avoid freeing such states
+	 * while they are still in use.
+	 */
+	u32 used_as_loop_entry;
 	void *suse_kabi_padding;
 };
 
@@ -510,7 +510,9 @@ struct bpf_verifier_state {
 struct bpf_verifier_state_list {
 	struct bpf_verifier_state state;
 	struct list_head node;
-	int miss_cnt, hit_cnt;
+	u32 miss_cnt;
+	u32 hit_cnt:31;
+	u32 in_free_list:1;
 };
 
 struct bpf_loop_inline_state {
