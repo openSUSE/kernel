ee06a4f14829f4a5a784a287efd341a370c174be
Jan Kara <jack@suse.cz>
2025-07-03 11:02:03 +0200

    ext4: fix warning in ext4_iomap_begin as race between bmap and write (bsc#1245115 CVE-2022-50082).
    suse-commit: 06b2a8c6e6e17b61cf5aeea238e8448ba4d64fa7

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -3288,13 +3288,15 @@ static sector_t ext4_bmap(struct address_space *mapping, sector_t block)
 {
 	struct inode *inode = mapping->host;
 	journal_t *journal;
+	sector_t ret = 0;
 	int err;
 
+	inode_lock_shared(inode);
 	/*
 	 * We can get here for an inline file via the FIBMAP ioctl
 	 */
 	if (ext4_has_inline_data(inode))
-		return 0;
+		goto out;
 
 	if (mapping_tagged(mapping, PAGECACHE_TAG_DIRTY) &&
 			test_opt(inode->i_sb, DELALLOC)) {
@@ -3333,10 +3335,13 @@ static sector_t ext4_bmap(struct address_space *mapping, sector_t block)
 		jbd2_journal_unlock_updates(journal);
 
 		if (err)
-			return 0;
+			goto out;
 	}
 
-	return generic_block_bmap(mapping, block, ext4_get_block);
+	ret = generic_block_bmap(mapping, block, ext4_get_block);
+out:
+	inode_unlock_shared(inode);
+	return ret;
 }
 
 static int ext4_readpage(struct file *file, struct page *page)
