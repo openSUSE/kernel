35a02530b301772fcc17a1c6f9e60e73ffd7443c
Oliver Neukum <oneukum@suse.com>
2025-07-07 15:43:38 +0200

    struct cdns: move new member to the end (git-fixes).
    suse-commit: 4384b08b086154ba62259d0aced26d63049081d6

--- drivers/usb/cdns3/core.h
+++ drivers/usb/cdns3/core.h
@@ -119,9 +119,11 @@ struct cdns {
 	struct cdns3_platform_data	*pdata;
 	spinlock_t			lock;
 	struct xhci_plat_priv		*xhci_plat_data;
-	u32                             override_apb_timeout;
 
 	int (*gadget_init)(struct cdns *cdns);
+#ifndef __GENKSYMS__
+	u32				override_apb_timeout;
+#endif
 };
 
 int cdns_hw_role_switch(struct cdns *cdns);
