9e5e9627bb8ef52adbde8b1cbe40a96fdbad36e7
Oliver Neukum <oneukum@suse.com>
2025-07-08 11:49:38 +0200

    - usb: typec: ucsi: add callback for connector status updates   (git-fixes). - blacklist.conf: needed as infrastructure. kABI workaround following - Refresh patches.suse/paddings-add-paddings-to-TypeC-stuff.patch. - Refresh   patches.suse/usb-typec-ucsi-displayport-Fix-deadlock.patch.
    suse-commit: de5a5b0a25dcf3ce9b97c023e2ae4a7052cfb0e1

--- drivers/usb/typec/ucsi/ucsi.h
+++ drivers/usb/typec/ucsi/ucsi.h
@@ -60,6 +60,7 @@ struct dentry;
  * @sync_write: Blocking write operation
  * @async_write: Non-blocking write operation
  * @update_altmodes: Squashes duplicate DP altmodes
+ * @connector_status: Updates connector status, called holding connector lock
  *
  * Read and write routines for UCSI interface. @sync_write must wait for the
  * Command Completion Event from the PPM before returning, and @async_write must
@@ -74,6 +75,8 @@ struct ucsi_operations {
 			   const void *val, size_t val_len);
 	bool (*update_altmodes)(struct ucsi *ucsi, struct ucsi_altmode *orig,
 				struct ucsi_altmode *updated);
+	void (*connector_status)(struct ucsi_connector *con);
+
 	void *suse_kabi_padding;
 };
 
