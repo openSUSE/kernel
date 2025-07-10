d1022db94dc8922c4ff978fdb3b08d4bede85f9e
Oliver Neukum <oneukum@suse.com>
2025-07-10 10:52:28 +0200

    restore UCSI_CONNECTOR_RESET_HARD definition (git-fixes).
    suse-commit: 3a50af7aeea2a90795b4b2e7fb43dc3d548e2bb1

--- drivers/usb/typec/ucsi/ucsi.h
+++ drivers/usb/typec/ucsi/ucsi.h
@@ -128,6 +128,7 @@ void ucsi_connector_change(struct ucsi *ucsi, u8 num);
 #define UCSI_COMMAND(_cmd_)			((_cmd_) & 0xff)
 
 /* CONNECTOR_RESET command bits */
+#define UCSI_CONNECTOR_RESET_HARD		BIT(23)	/* Deprecated in v1.1 */
 #define UCSI_CONNECTOR_RESET_HARD_VER_1_0	BIT(23) /* Deprecated in v1.1 */
 #define UCSI_CONNECTOR_RESET_DATA_VER_2_0	BIT(23) /* Redefined in v2.0 */
 
