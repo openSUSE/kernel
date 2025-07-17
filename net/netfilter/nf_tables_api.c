b3b5ebead0602e9651432da9127c447910d2a0b7
Fernando Fernandez Mancera <fmancera@suse.de>
2025-07-17 16:04:01 +0200

    - netfilter: nft_set_pipapo: call nft_trans_gc_queue_sync()   in catchall GC (CVE-2023-52923 bsc#1236104).
    - Refresh
      patches.suse/netfilter-nf_tables-use-timestamp-to-check-for-set-element.patch.
    
    suse-commit: 01a51e75f486236856d0c7442ee6c41a9ec775aa

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -8727,8 +8727,9 @@ void nft_trans_gc_queue_sync_done(struct nft_trans_gc *trans)
 	call_rcu(&trans->rcu, nft_trans_gc_trans_free);
 }
 
-struct nft_trans_gc *nft_trans_gc_catchall(struct nft_trans_gc *gc,
-					   unsigned int gc_seq)
+static struct nft_trans_gc *nft_trans_gc_catchall(struct nft_trans_gc *gc,
+						  unsigned int gc_seq,
+						  bool sync)
 {
 	struct nft_set_elem_catchall *catchall;
 	u64 tstamp = nft_net_tstamp(gc->net);
@@ -8745,7 +8746,11 @@ struct nft_trans_gc *nft_trans_gc_catchall(struct nft_trans_gc *gc,
 
 		nft_set_elem_dead(ext);
 dead_elem:
-		gc = nft_trans_gc_queue_async(gc, gc_seq, GFP_ATOMIC);
+		if (sync)
+			gc = nft_trans_gc_queue_sync(gc, GFP_ATOMIC);
+		else
+			gc = nft_trans_gc_queue_async(gc, gc_seq, GFP_ATOMIC);
+
 		if (!gc)
 			return NULL;
 
@@ -8753,6 +8758,17 @@ dead_elem:
 	}
 
 	return gc;
+}
+
+struct nft_trans_gc *nft_trans_gc_catchall_async(struct nft_trans_gc *gc,
+						 unsigned int gc_seq)
+{
+	return nft_trans_gc_catchall(gc, gc_seq, false);
+}
+
+struct nft_trans_gc *nft_trans_gc_catchall_sync(struct nft_trans_gc *gc)
+{
+	return nft_trans_gc_catchall(gc, 0, true);
 }
 
 static void nf_tables_module_autoload_cleanup(struct net *net)
