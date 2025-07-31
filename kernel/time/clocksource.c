8968e30711abd3a9f3eae9699829f105cdf64339
Jiri Wiesner <jwiesner@suse.de>
2025-07-31 10:57:55 +0200

    - clocksource: Scale the watchdog read retries automatically   (bsc#1241345 bsc#1244457). - Refresh   patches.suse/clocksource-Fix-brown-bag-boolean-thinko-in-cs_watch.patch. - Refresh   patches.suse/clocksource-Make-watchdog-and-suspend-timing-multipl.patch.
    suse-commit: fdf040b557a18e50797abb8a600906bf5cbd2e81

--- kernel/time/clocksource.c
+++ kernel/time/clocksource.c
@@ -220,7 +220,7 @@ void clocksource_mark_unstable(struct clocksource *cs)
 	spin_unlock_irqrestore(&watchdog_lock, flags);
 }
 
-ulong max_cswd_read_retries = 2;
+ulong max_cswd_read_retries = ~0UL;
 module_param(max_cswd_read_retries, ulong, 0644);
 EXPORT_SYMBOL_GPL(max_cswd_read_retries);
 static int verify_n_cpus = 8;
@@ -234,11 +234,12 @@ enum wd_read_status {
 
 static enum wd_read_status cs_watchdog_read(struct clocksource *cs, u64 *csnow, u64 *wdnow)
 {
-	unsigned int nretries;
-	u64 wd_end, wd_end2;
+	unsigned int nretries, max_retries;
 	int64_t wd_delay, wd_seq_delay;
+	u64 wd_end, wd_end2;
 
-	for (nretries = 0; nretries <= max_cswd_read_retries; nretries++) {
+	max_retries = clocksource_get_max_watchdog_retry();
+	for (nretries = 0; nretries <= max_retries; nretries++) {
 		local_irq_disable();
 		*wdnow = watchdog->read(watchdog);
 		*csnow = cs->read(cs);
@@ -248,7 +249,7 @@ static enum wd_read_status cs_watchdog_read(struct clocksource *cs, u64 *csnow, 
 
 		wd_delay = cycles_to_nsec_safe(watchdog, *wdnow, wd_end);
 		if (wd_delay <= WATCHDOG_MAX_SKEW) {
-			if (nretries > 1 && nretries >= max_cswd_read_retries) {
+			if (nretries > 1 && nretries >= max_retries) {
 				pr_warn("timekeeping watchdog on CPU%d: %s retried %d times before success\n",
 					smp_processor_id(), watchdog->name, nretries);
 			}
