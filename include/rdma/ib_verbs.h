030c30af95e80fe78c3a44500f9b2ddf2c2393ee
Nicolas Morey <nmorey@suse.com>
2025-08-01 09:46:14 +0200

    RDMA/mlx5: Fix compilation warning when USER_ACCESS isn't set (git-fixes)
    suse-commit: dce79bd87c885f3815b94e2fed5d5ae22db3745c

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -4723,7 +4723,14 @@ void rdma_roce_rescan_device(struct ib_device *ibdev);
 
 struct ib_ucontext *ib_uverbs_get_ucontext_file(struct ib_uverbs_file *ufile);
 
+#if IS_ENABLED(CONFIG_INFINIBAND_USER_ACCESS)
 int uverbs_destroy_def_handler(struct uverbs_attr_bundle *attrs);
+#else
+static inline int uverbs_destroy_def_handler(struct uverbs_attr_bundle *attrs)
+{
+	return 0;
+}
+#endif
 
 struct net_device *rdma_alloc_netdev(struct ib_device *device, u32 port_num,
 				     enum rdma_netdev_t type, const char *name,
