30823bf368fd14c745d8c60845a06ae017a66256
Nikolay Borisov <nik.borisov@suse.com>
2025-08-06 16:01:48 +0300

    - x86/alternative: Merge include files (git-fixes).
    - Refresh
      patches.suse/x86-lib-atomic64_386_32-rename-things.patch.
    - Refresh
      patches.suse/x86-srso-add-a-speculative-ras-overflow-mitigation.patch.
    
    suse-commit: d6a4cdbca9198c10f7fee1964e4140c2d600567d

--- arch/x86/include/asm/smap.h
+++ arch/x86/include/asm/smap.h
@@ -16,14 +16,13 @@
 #include <linux/stringify.h>
 #include <asm/nops.h>
 #include <asm/cpufeatures.h>
+#include <asm/alternative.h>
 
 /* "Raw" instruction opcodes */
 #define __ASM_CLAC	.byte 0x0f,0x01,0xca
 #define __ASM_STAC	.byte 0x0f,0x01,0xcb
 
 #ifdef __ASSEMBLY__
-
-#include <asm/alternative-asm.h>
 
 #ifdef CONFIG_X86_SMAP
 
@@ -41,8 +40,6 @@
 #endif /* CONFIG_X86_SMAP */
 
 #else /* __ASSEMBLY__ */
-
-#include <asm/alternative.h>
 
 #ifdef CONFIG_X86_SMAP
 
