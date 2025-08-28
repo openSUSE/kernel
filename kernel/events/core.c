950cb1476e7edde6121cf4f0e8c3b8ca58f0e3f3
Vlastimil Babka <vbabka@suse.cz>
2025-08-28 10:56:53 +0200

    perf/core: Exit early on perf_mmap() fail (CVE-2025-38563 bsc#1248306 dependency).
    suse-commit: 4deadd812fb324b48324a63cfb35b3158f94cdb5

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -6659,6 +6659,9 @@ aux_unlock:
 		mutex_unlock(aux_mutex);
 	mutex_unlock(&event->mmap_mutex);
 
+	if (ret)
+		return ret;
+
 	/*
 	 * Since pinned accounting is per vm we cannot allow fork() to copy our
 	 * vma.
