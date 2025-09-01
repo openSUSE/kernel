d5874b3abdc579cea5248794150c73666e143254
Petr Mladek <pmladek@suse.com>
2025-09-01 14:20:37 +0200

    slab: Decouple slab_debug and no_hash_pointers (bsc#1249022).
    suse-commit: 3da3d781d7eb88dc528d3d0f69f37411dd220a36

--- include/linux/sprintf.h
+++ include/linux/sprintf.h
@@ -25,6 +25,6 @@ __scanf(2, 0) int vsscanf(const char *, const char *, va_list);
 
 /* These are for specific cases, do not use without real need */
 extern bool no_hash_pointers;
-int no_hash_pointers_enable(char *str);
+void hash_pointers_finalize(bool slub_debug);
 
 #endif	/* _LINUX_KERNEL_SPRINTF_H */
