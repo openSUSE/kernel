25d04f67a8f854a7bd1ecff93306ef780470855a
Fernando Fernandez Mancera <fmancera@suse.de>
2025-09-08 17:51:32 +0200

    netfilter: nf_tables: bogus ENOENT when destroying element which does not exist (git-fixes).
    suse-commit: 1720cdf40417ab6e2ca5f209722ce0890cf3f2f0

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -7303,10 +7303,11 @@ static int nf_tables_delsetelem(struct sk_buff *skb,
 
 		if (err < 0) {
 			NL_SET_BAD_ATTR(extack, attr);
-			break;
+			return err;
 		}
 	}
-	return err;
+
+	return 0;
 }
 
 void nft_set_gc_batch_release(struct rcu_head *rcu)
