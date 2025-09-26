f8321ec3f0a8f786986fcdb242851f5535e6632b
Michal Koutný <mkoutny@suse.com>
2025-09-26 17:26:42 +0200

    kabi: Restore layout of parallel_data (bsc1248343).
    suse-commit: 3819e361b8197fdc629bd1a499d6f054236a7388

--- include/linux/padata.h
+++ include/linux/padata.h
@@ -101,6 +101,8 @@ struct parallel_data {
 	unsigned int			processed;
 	int				cpu;
 	struct padata_cpumask		cpumask;
+	struct work_struct              reorder_work;
+	spinlock_t                      ____cacheline_aligned lock;
 };
 
 /**
