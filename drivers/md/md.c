3fb168008c32efae1219b83dc065426f25638600
Martin Wilck <mwilck@suse.com>
2025-09-30 15:48:02 +0200

    md: fix a crash in mempool_free (CVE-2022-50381, bsc#1250257).
    suse-commit: dfced035f0cc9ceb04a23256867bc6732f80d456

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -411,13 +411,14 @@ static void md_end_flush(struct bio *bio, int err)
 	mdk_rdev_t *rdev = bio->bi_private;
 	mddev_t *mddev = rdev->mddev;
 
+	bio_put(bio);
+
 	rdev_dec_pending(rdev, mddev);
 
 	if (atomic_dec_and_test(&mddev->flush_pending)) {
 		/* The pre-request flush has finished */
 		queue_work(md_wq, &mddev->flush_work);
 	}
-	bio_put(bio);
 }
 
 static void md_submit_flush_data(struct work_struct *ws);
@@ -879,10 +880,12 @@ static void super_written(struct bio *bio, int error)
 	} else
 		clear_bit(LastDev, &rdev->flags);
 
+	bio_put(bio);
+
+	rdev_dec_pending(rdev, mddev);
+
 	if (atomic_dec_and_test(&mddev->pending_writes))
 		wake_up(&mddev->sb_wait);
-	rdev_dec_pending(rdev, mddev);
-	bio_put(bio);
 }
 
 void md_super_write(mddev_t *mddev, mdk_rdev_t *rdev,
