b457d465d9a6824cbdd488777a2628faae393aa5
Nikolay Borisov <nik.borisov@suse.com>
2025-10-10 13:57:25 +0300

    intel_idle: Provide the default enter_dead() handler (jsc#PED-13815).
    suse-commit: 3fe4d1d732fa9ea086b9a40a1ba33af65627ad55

--- drivers/idle/intel_idle.c
+++ drivers/idle/intel_idle.c
@@ -56,6 +56,7 @@
 #include <asm/mwait.h>
 #include <asm/spec-ctrl.h>
 #include <asm/fpu/api.h>
+#include <asm/smp.h>
 
 #define INTEL_IDLE_VERSION "0.5.1"
 
@@ -223,6 +224,17 @@ static __cpuidle int intel_idle_s2idle(struct cpuidle_device *dev,
 		fpu_idle_fpregs();
 
 	mwait_idle_with_hints(eax, ecx);
+
+	return 0;
+}
+
+static int intel_idle_enter_dead(struct cpuidle_device *dev, int index)
+{
+	struct cpuidle_driver *drv = cpuidle_get_cpu_driver(dev);
+	struct cpuidle_state *state = &drv->states[index];
+	unsigned long eax = flg2MWAIT(state->flags);
+
+	mwait_play_dead(eax);
 
 	return 0;
 }
@@ -1799,6 +1811,7 @@ static void __init intel_idle_init_cstates_acpi(struct cpuidle_driver *drv)
 			state->flags |= CPUIDLE_FLAG_TIMER_STOP;
 
 		state->enter = intel_idle;
+		state->enter_dead = intel_idle_enter_dead;
 		state->enter_s2idle = intel_idle_s2idle;
 	}
 }
@@ -2147,6 +2160,9 @@ static void __init intel_idle_init_cstates_icpu(struct cpuidle_driver *drv)
 		if (!cpuidle_state_table[cstate].enter &&
 		    !cpuidle_state_table[cstate].enter_s2idle)
 			break;
+
+		if (!cpuidle_state_table[cstate].enter_dead)
+			cpuidle_state_table[cstate].enter_dead = intel_idle_enter_dead;
 
 		/* If marked as unusable, skip this state. */
 		if (cpuidle_state_table[cstate].flags & CPUIDLE_FLAG_UNUSABLE) {
