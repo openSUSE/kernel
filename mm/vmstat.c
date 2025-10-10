5ee2c0790a672516c2a5a54d91aab89cfc73e17f
Vlastimil Babka <vbabka@suse.cz>
2025-10-10 14:10:02 +0200

    mm: memory-tiering: fix PGPROMOTE_CANDIDATE counting (bsc#1245630).
    suse-commit: 0cbd9715c3243b6c035aaf7d65c50ee0d1b69218

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1276,6 +1276,7 @@ const char * const vmstat_text[] = {
 #ifdef CONFIG_NUMA_BALANCING
 	"pgpromote_success",
 	"pgpromote_candidate",
+	"pgpromote_candidate_nrl",
 #endif
 	"pgdemote_kswapd",
 	"pgdemote_direct",
