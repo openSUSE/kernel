af9795dee3b1d1dbe3a092e6da17e1a81895f0cb
Petr Pavlu <petr.pavlu@suse.com>
2025-10-15 08:35:51 +0200

    bpf: Check link_create.flags parameter for multi_kprobe (git-fixes).
    suse-commit: 0e758251e4101da8f4d5d01c7d372861d014a33f

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -2895,6 +2895,9 @@ int bpf_kprobe_multi_link_attach(const union bpf_attr *attr, struct bpf_prog *pr
 	if (sizeof(u64) != sizeof(void *))
 		return -EOPNOTSUPP;
 
+	if (attr->link_create.flags)
+		return -EINVAL;
+
 	if (prog->expected_attach_type != BPF_TRACE_KPROBE_MULTI)
 		return -EINVAL;
 
