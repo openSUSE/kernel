b58023b71e04aff5f69f3fd610173b343d9f3939
Steffen Jaeckel <sjaeckel@suse.de>
2025-10-27 15:40:50 +0100

    octeontx2-pf: Fix potential use after free in otx2_tc_add_flow() (CVE-2025-39978 bsc#1252069).
    suse-commit: 54a21ef204104f75b00e3e6a4e699a3dbbf1b6ca

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_tc.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_tc.c
@@ -789,7 +789,6 @@ static int otx2_tc_add_flow(struct otx2_nic *nic,
 	if (rc) {
 		NL_SET_ERR_MSG_MOD(extack, "Failed to install MCAM flow entry");
 		mutex_unlock(&nic->mbox.lock);
-		kfree_rcu(new_node, rcu);
 		goto free_leaf;
 	}
 	mutex_unlock(&nic->mbox.lock);
@@ -799,7 +798,6 @@ static int otx2_tc_add_flow(struct otx2_nic *nic,
 				    nic->tc_info.flow_ht_params);
 	if (rc) {
 		otx2_del_mcam_flow_entry(nic, req->entry);
-		kfree_rcu(new_node, rcu);
 		goto free_leaf;
 	}
 
@@ -828,6 +826,7 @@ free_leaf:
 
 		mutex_unlock(&nic->mbox.lock);
 	}
+	kfree_rcu(new_node, rcu);
 
 	return rc;
 }
