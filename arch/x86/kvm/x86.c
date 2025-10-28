cf3ac9fa88206b5bea3078565faec66c1e31fee2
Carlos López <clopez@suse.de>
2025-10-28 18:42:38 +0100

    KVM: x86: Don't load/put vCPU when unloading its MMU during teardown (git-fixes).
    suse-commit: 625c23b154e5c29442c63e1c4d4952ff25bcd104

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -12839,13 +12839,6 @@ out:
 	return ret;
 }
 
-static void kvm_unload_vcpu_mmu(struct kvm_vcpu *vcpu)
-{
-	vcpu_load(vcpu);
-	kvm_mmu_unload(vcpu);
-	vcpu_put(vcpu);
-}
-
 static void kvm_unload_vcpu_mmus(struct kvm *kvm)
 {
 	unsigned long i;
@@ -12853,7 +12846,7 @@ static void kvm_unload_vcpu_mmus(struct kvm *kvm)
 
 	kvm_for_each_vcpu(i, vcpu, kvm) {
 		kvm_clear_async_pf_completion_queue(vcpu);
-		kvm_unload_vcpu_mmu(vcpu);
+		kvm_mmu_unload(vcpu);
 	}
 }
 
