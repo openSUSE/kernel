d692710523b8c239f66ee9aae0ba5694c97e4c4d
Pedro Falcato <pfalcato@suse.de>
2025-10-31 11:04:18 +0000

    tmpfs: preserve SB_I_VERSION on remount (git-fixes).
    suse-commit: 16a0fb3f5f29eab4366b86a55b2e8ad34ad3faf2

--- mm/shmem.c
+++ mm/shmem.c
@@ -4735,7 +4735,7 @@ static int shmem_fill_super(struct super_block *sb, struct fs_context *fc)
 		sb->s_flags |= SB_NOUSER;
 	}
 	sb->s_export_op = &shmem_export_ops;
-	sb->s_flags |= SB_NOSEC | SB_I_VERSION;
+	sb->s_flags |= SB_NOSEC;
 #else
 	sb->s_flags |= SB_NOUSER;
 #endif
@@ -5019,6 +5019,9 @@ int shmem_init_fs_context(struct fs_context *fc)
 
 	fc->fs_private = ctx;
 	fc->ops = &shmem_fs_context_ops;
+#ifdef CONFIG_TMPFS
+	fc->sb_flags |= SB_I_VERSION;
+#endif
 	return 0;
 }
 
