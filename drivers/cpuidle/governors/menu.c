84d16882cd86b3fb5d04450af425c090d7e8ab48
Thomas Renninger <trenn@suse.de>
2025-11-02 20:32:22 +0100

    Delete patches.suse/cpuidle-menu-Avoid-discarding-useful-information.patch.
    suse-commit: 8ddc5007e8be69a76718829664f24208aa5420ab

--- drivers/cpuidle/governors/menu.c
+++ drivers/cpuidle/governors/menu.c
@@ -181,19 +181,8 @@ again:
 	 * This can deal with workloads that have long pauses interspersed
 	 * with sporadic activity with a bunch of short pauses.
 	 */
-	if (divisor * 4 <= INTERVALS * 3) {
-		/*
-		 * If there are sufficiently many data points still under
-		 * consideration after the outliers have been eliminated,
-		 * returning without a prediction would be a mistake because it
-		 * is likely that the next interval will not exceed the current
-		 * maximum, so return the latter in that case.
-		 */
-		if (divisor >= INTERVALS / 2)
-			return max;
-
+	if ((divisor * 4) <= INTERVALS * 3)
 		return UINT_MAX;
-	}
 
 	/* Update the thresholds for the next round. */
 	if (avg - min > max - avg)
