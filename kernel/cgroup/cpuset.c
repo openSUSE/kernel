31381b2cbeb291bd3cd0e24f28634a3dd3aa51eb
Michal Koutný <mkoutny@suse.com>
2025-11-05 17:01:52 +0100

    cpuset: fix failure to enable isolated partition when containing isolcpus (bsc#1241166).
    suse-commit: 9093c259e19cdca9f073cd4add335c2a56eeba96

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -1686,6 +1686,7 @@ static int update_parent_effective_cpumask(struct cpuset *cs, int cmd,
 		xcpus = tmp->new_cpus;
 		if (compute_effective_exclusive_cpumask(cs, xcpus, NULL))
 			WARN_ON_ONCE(!cpumask_empty(cs->exclusive_cpus));
+		new_prs = (cmd == partcmd_enable) ? PRS_ROOT : PRS_ISOLATED;
 
 		/*
 		 * Enabling partition root is not allowed if its
@@ -1707,7 +1708,6 @@ static int update_parent_effective_cpumask(struct cpuset *cs, int cmd,
 		deleting = cpumask_and(tmp->delmask, xcpus, parent->effective_xcpus);
 		if (deleting)
 			subparts_delta++;
-		new_prs = (cmd == partcmd_enable) ? PRS_ROOT : PRS_ISOLATED;
 	} else if (cmd == partcmd_disable) {
 		/*
 		 * May need to add cpus back to parent's effective_cpus
