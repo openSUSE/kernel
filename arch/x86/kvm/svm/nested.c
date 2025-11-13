d8c44d566187431bd572660a314eb9ae7767691a
Sean Christopherson <seanjc@google.com>
2025-11-13 14:56:14 -0800

    KVM: nSVM: Set exit_code_hi to -1 when synthesizing SVM_EXIT_ERR (failed VMRUN)
    commit f402ecd7a8b6446547076f4bd24bd5d4dcc94481 upstream.
    
    Set exit_code_hi to -1u as a temporary band-aid to fix a long-standing
    (effectively since KVM's inception) bug where KVM treats the exit code as
    a 32-bit value, when in reality it's a 64-bit value.  Per the APM, offset
    0x70 is a single 64-bit value:
    
      070h 63:0 EXITCODE
    
    And a sane reading of the error values defined in "Table C-1. SVM Intercept
    Codes" is that negative values use the full 64 bits:
    
      –1 VMEXIT_INVALID Invalid guest state in VMCB.
      –2 VMEXIT_BUSYBUSY bit was set in the VMSA
      –3 VMEXIT_IDLE_REQUIREDThe sibling thread is not in an idle state
      -4 VMEXIT_INVALID_PMC Invalid PMC state
    
    And that interpretation is confirmed by testing on Milan and Turin (by
    setting bits in CR0[63:32] to generate VMEXIT_INVALID on VMRUN).
    
    Furthermore, Xen has treated exitcode as a 64-bit value since HVM support
    was adding in 2006 (see Xen commit d1bd157fbc ("Big merge the HVM
    full-virtualisation abstractions.")).
    
    Cc: Jim Mattson <jmattson@google.com>
    Cc: Yosry Ahmed <yosry.ahmed@linux.dev>
    Cc: stable@vger.kernel.org
    Reviewed-by: Yosry Ahmed <yosry.ahmed@linux.dev>
    Link: https://patch.msgid.link/20251113225621.1688428-3-seanjc@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kvm/svm/nested.c
+++ arch/x86/kvm/svm/nested.c
@@ -667,7 +667,7 @@ int nested_svm_vmrun(struct kvm_vcpu *vcpu)
 	if (!nested_vmcb_valid_sregs(vcpu, &vmcb12->save) ||
 	    !nested_vmcb_check_controls(vcpu, &svm->nested.ctl)) {
 		vmcb12->control.exit_code    = SVM_EXIT_ERR;
-		vmcb12->control.exit_code_hi = 0;
+		vmcb12->control.exit_code_hi = -1u;
 		vmcb12->control.exit_info_1  = 0;
 		vmcb12->control.exit_info_2  = 0;
 		goto out;
@@ -698,7 +698,7 @@ out_exit_err:
 	svm->nested.nested_run_pending = 0;
 
 	svm->vmcb->control.exit_code    = SVM_EXIT_ERR;
-	svm->vmcb->control.exit_code_hi = 0;
+	svm->vmcb->control.exit_code_hi = -1u;
 	svm->vmcb->control.exit_info_1  = 0;
 	svm->vmcb->control.exit_info_2  = 0;
 
