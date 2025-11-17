f5c30ea01d073b4527cc9a322105cfd5ee780f64
Mel Gorman <mgorman@suse.de>
2025-11-17 13:35:50 +0000

    sched/fair: Proportional newidle balance -KABI (bsc#1248792).
    suse-commit: 2a30a31c57a56137bf945f11f5407d6531e63eb7

--- include/linux/sched/topology.h
+++ include/linux/sched/topology.h
@@ -100,15 +100,112 @@ struct sched_domain {
 	int flags;			/* See SD_* */
 	int level;
 
+#ifndef __GENKSYMS__
+	/*
+	 * The newidle_* fields break KABI but not in any way that should
+	 * matter.  For KABI purposes, the location of the fields is slightly
+	 * changed so any module that accesses the offsets would have been
+	 * examining schedstat counters. It is unlikely this will cause any
+	 * functional error. There is a potential impact on binary kprobe
+	 * modules getting unexpected values but it is unlikely as probes and
+	 * tracing programs are generally built against a specific kernel
+	 * source or offsets are probed at runtime.
+	 *
+	 * Locations within struct are to fit in existing holes as suggested
+	 * by Petr Pavlu.
+	 */
+	unsigned int newidle_call;
+#endif
+
 	/* Runtime fields. */
 	unsigned long last_balance;	/* init to jiffies. units in jiffies */
 	unsigned int balance_interval;	/* initialise to 1. units in ms. */
 	unsigned int nr_balance_failed; /* initialise to 0 */
 
 	/* idle_balance() stats */
-	unsigned int newidle_call;
+	u64 max_newidle_lb_cost;
+	unsigned long last_decay_max_lb_cost;
+
+#ifdef CONFIG_SCHEDSTATS
+	/* sched_balance_rq() stats */
+	unsigned int lb_count[CPU_MAX_IDLE_TYPES];
+	unsigned int lb_failed[CPU_MAX_IDLE_TYPES];
+	unsigned int lb_balanced[CPU_MAX_IDLE_TYPES];
+	unsigned int lb_imbalance_load[CPU_MAX_IDLE_TYPES];
+	unsigned int lb_imbalance_util[CPU_MAX_IDLE_TYPES];
+	unsigned int lb_imbalance_task[CPU_MAX_IDLE_TYPES];
+	unsigned int lb_imbalance_misfit[CPU_MAX_IDLE_TYPES];
+	unsigned int lb_gained[CPU_MAX_IDLE_TYPES];
+	unsigned int lb_hot_gained[CPU_MAX_IDLE_TYPES];
+	unsigned int lb_nobusyg[CPU_MAX_IDLE_TYPES];
+	unsigned int lb_nobusyq[CPU_MAX_IDLE_TYPES];
+
+	/* Active load balancing */
+	unsigned int alb_count;
+	unsigned int alb_failed;
+	unsigned int alb_pushed;
+
+	/* SD_BALANCE_EXEC stats */
+	unsigned int sbe_count;
+	unsigned int sbe_balanced;
+	unsigned int sbe_pushed;
+
+	/* SD_BALANCE_FORK stats */
+	unsigned int sbf_count;
+	unsigned int sbf_balanced;
+	unsigned int sbf_pushed;
+
+	/* try_to_wake_up() stats */
+	unsigned int ttwu_wake_remote;
+	unsigned int ttwu_move_affine;
+	unsigned int ttwu_move_balance;
+#endif
+#ifndef __GENKSYMS__
 	unsigned int newidle_success;
+#endif
+	char *name;
+	union {
+		void *private;		/* used during construction */
+		struct rcu_head rcu;	/* used during destruction */
+	};
+	struct sched_domain_shared *shared;
+
+	unsigned int span_weight;
+#ifndef __GENKSYMS__
 	unsigned int newidle_ratio;
+#endif
+	/*
+	 * Span of all CPUs in this domain.
+	 *
+	 * NOTE: this field is variable length. (Allocated dynamically
+	 * by attaching extra space to the end of the structure,
+	 * depending on how many CPUs the kernel has booted up with)
+	 */
+	unsigned long span[];
+};
+
+struct __orig_sched_domain {
+	/* These fields must be setup */
+	struct sched_domain __rcu *parent;	/* top domain must be null terminated */
+	struct sched_domain __rcu *child;	/* bottom domain must be null terminated */
+	struct sched_group *groups;	/* the balancing groups of the domain */
+	unsigned long min_interval;	/* Minimum balance interval ms */
+	unsigned long max_interval;	/* Maximum balance interval ms */
+	unsigned int busy_factor;	/* less balancing by factor if busy */
+	unsigned int imbalance_pct;	/* No balance until over watermark */
+	unsigned int cache_nice_tries;	/* Leave cache hot tasks for # tries */
+	unsigned int imb_numa_nr;	/* Nr running tasks that allows a NUMA imbalance */
+
+	int nohz_idle;			/* NOHZ IDLE status */
+	int flags;			/* See SD_* */
+	int level;
+
+	/* Runtime fields. */
+	unsigned long last_balance;	/* init to jiffies. units in jiffies */
+	unsigned int balance_interval;	/* initialise to 1. units in ms. */
+	unsigned int nr_balance_failed; /* initialise to 0 */
+
+	/* idle_balance() stats */
 	u64 max_newidle_lb_cost;
 	unsigned long last_decay_max_lb_cost;
 
@@ -163,6 +260,24 @@ struct sched_domain {
 	 */
 	unsigned long span[];
 };
+
+/* newidle_call */
+suse_kabi_static_assert(offsetof(struct sched_domain, level) ==
+                        offsetof(struct __orig_sched_domain, level));
+suse_kabi_static_assert(offsetof(struct sched_domain, last_balance) ==
+                        offsetof(struct __orig_sched_domain, last_balance));
+
+/* newidle_success */
+suse_kabi_static_assert(offsetof(struct sched_domain, ttwu_move_balance) ==
+                        offsetof(struct __orig_sched_domain, ttwu_move_balance));
+suse_kabi_static_assert(offsetof(struct sched_domain, name) ==
+                        offsetof(struct __orig_sched_domain, name));
+
+/* newidle_ratio */
+suse_kabi_static_assert(offsetof(struct sched_domain, span_weight) ==
+                        offsetof(struct __orig_sched_domain, span_weight));
+suse_kabi_static_assert(offsetof(struct sched_domain, span) ==
+                        offsetof(struct __orig_sched_domain, span));
 
 static inline struct cpumask *sched_domain_span(struct sched_domain *sd)
 {
