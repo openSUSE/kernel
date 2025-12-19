62943a68e0d685d189c3d43fbd8a0865b862785e
Juergen Gross <jgross@suse.com>
2025-12-19 11:36:29 +0100

    xen/events: Cleanup find_virq() return codes (CVE-2025-40160, bsc#1253400).
    suse-commit: 41c00f26e62806ec3a319c94c5f539491762bc98

--- drivers/xen/events/events_base.c
+++ drivers/xen/events/events_base.c
@@ -1220,10 +1220,12 @@ EXPORT_SYMBOL_GPL(bind_interdomain_evtchn_to_irq_lateeoi);
 static int find_virq(unsigned int virq, unsigned int cpu)
 {
 	struct evtchn_status status;
-	int port, rc = -ENOENT;
+	int port;
 
 	memset(&status, 0, sizeof(status));
 	for (port = 0; port < xen_evtchn_max_channels(); port++) {
+		int rc;
+
 		status.dom = DOMID_SELF;
 		status.port = port;
 		rc = HYPERVISOR_event_channel_op(EVTCHNOP_status, &status);
@@ -1232,11 +1234,10 @@ static int find_virq(unsigned int virq, unsigned int cpu)
 		if (status.status != EVTCHNSTAT_virq)
 			continue;
 		if (status.u.virq == virq && status.vcpu == xen_vcpu_nr(cpu)) {
-			rc = port;
-			break;
+			return port;
 		}
 	}
-	return rc;
+	return -ENOENT;
 }
 
 /**
