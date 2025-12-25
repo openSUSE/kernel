e9711bd0e64812c694a228cf58c9e6032decee54
SeongJae Park <sj@kernel.org>
2025-12-24 18:30:37 -0800

    mm/damon/sysfs-scheme: cleanup access_pattern subdirs on scheme dir setup failure
    commit 392b3d9d595f34877dd745b470c711e8ebcd225c upstream.
    
    When a DAMOS-scheme DAMON sysfs directory setup fails after setup of
    access_pattern/ directory, subdirectories of access_pattern/ directory are
    not cleaned up.  As a result, DAMON sysfs interface is nearly broken until
    the system reboots, and the memory for the unremoved directory is leaked.
    
    Cleanup the directories under such failures.
    
    Link: https://lkml.kernel.org/r/20251225023043.18579-5-sj@kernel.org
    Fixes: 9bbb820a5bd5 ("mm/damon/sysfs: support DAMOS quotas")
    Signed-off-by: SeongJae Park <sj@kernel.org>
    Cc: chongjiapeng <jiapeng.chong@linux.alibaba.com>
    Cc: <stable@vger.kernel.org> # 5.18.x
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: SeongJae Park <sj@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/damon/sysfs-schemes.c
+++ mm/damon/sysfs-schemes.c
@@ -1282,9 +1282,10 @@ static int damon_sysfs_scheme_add_dirs(struct damon_sysfs_scheme *scheme)
 	err = damon_sysfs_scheme_set_access_pattern(scheme);
 	if (err)
 		return err;
+
 	err = damon_sysfs_scheme_set_quotas(scheme);
 	if (err)
-		goto put_access_pattern_out;
+		goto rmdir_put_access_pattern_out;
 	err = damon_sysfs_scheme_set_watermarks(scheme);
 	if (err)
 		goto put_quotas_access_pattern_out;
@@ -1311,7 +1312,8 @@ put_watermarks_quotas_access_pattern_out:
 put_quotas_access_pattern_out:
 	kobject_put(&scheme->quotas->kobj);
 	scheme->quotas = NULL;
-put_access_pattern_out:
+rmdir_put_access_pattern_out:
+	damon_sysfs_access_pattern_rm_dirs(scheme->access_pattern);
 	kobject_put(&scheme->access_pattern->kobj);
 	scheme->access_pattern = NULL;
 	return err;
