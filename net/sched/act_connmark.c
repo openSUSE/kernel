9710e440323f149c9affd9eb506abef2b5f66c4b
Pedro Falcato <pfalcato@suse.de>
2026-01-05 10:39:43 +0000

    net: sched: act_connmark: initialize struct tc_ife to fix kernel leak (CVE-2025-40279 bsc#1254846).
    suse-commit: dacd4a66efac5b079aff5fed98c6fe1a5306b65c

--- net/sched/act_connmark.c
+++ net/sched/act_connmark.c
@@ -161,12 +161,14 @@ static inline int tcf_connmark_dump(struct sk_buff *skb, struct tc_action *a,
 {
 	unsigned char *b = skb_tail_pointer(skb);
 	struct tcf_connmark_info *ci = to_connmark(a);
-	struct tc_connmark opt = {
-		.index   = ci->tcf_index,
-		.refcnt  = refcount_read(&ci->tcf_refcnt) - ref,
-		.bindcnt = atomic_read(&ci->tcf_bindcnt) - bind,
-	};
+	struct tc_connmark opt;
 	struct tcf_t t;
+
+	memset(&opt, 0, sizeof(opt));
+
+	opt.index   = ci->tcf_index;
+	opt.refcnt  = refcount_read(&ci->tcf_refcnt) - ref;
+	opt.bindcnt = atomic_read(&ci->tcf_bindcnt) - bind;
 
 	spin_lock_bh(&ci->tcf_lock);
 	opt.action = ci->tcf_action;
