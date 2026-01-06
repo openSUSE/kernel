04468205ce7aa8e7f667e34233b0f4902453d780
Henrique Carvalho <henrique.carvalho@suse.com>
2026-01-06 16:20:33 -0300

    cifs: make sure that channel scaling is done only once (git-fixes).
    suse-commit: 3175d690bb3b37f7975fa091fcbada8526d3cdba

--- fs/smb/client/cifsglob.h
+++ fs/smb/client/cifsglob.h
@@ -1044,6 +1044,8 @@ struct cifs_chan {
 	__u8 signkey[SMB3_SIGN_KEY_SIZE];
 };
 
+#define CIFS_SES_FLAG_SCALE_CHANNELS (0x1)
+
 /*
  * Session structure.  One of these for each uid session with a particular host
  */
@@ -1078,6 +1080,7 @@ struct cifs_ses {
 	enum upcall_target_enum upcall_target; /* what upcall target was specified? */
 	bool sign;		/* is signing required? */
 	bool domainAuto:1;
+       unsigned int flags;
 	__u16 session_flags;
 	__u8 smb3signingkey[SMB3_SIGN_KEY_SIZE];
 	__u8 smb3encryptionkey[SMB3_ENC_DEC_KEY_SIZE];
