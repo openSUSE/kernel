08ba796adc041f048c011c347d1a9c1219130c7a
Michal Kubecek <mkubecek@suse.cz>
2026-01-08 02:20:24 +0100

    tcp: convert to dev_net_rcu() (CVE-2025-40139 bsc#1253409).
    suse-commit: e5ca7ffdfcc6c08ddc9603e617ee2389a22ae30c

--- include/net/inet_hashtables.h
+++ include/net/inet_hashtables.h
@@ -387,12 +387,13 @@ static inline struct sock *__inet_lookup_skb(struct inet_hashinfo *hashinfo,
 					     bool *refcounted)
 {
 	struct sock *sk = skb_steal_sock(skb, refcounted);
+	struct net *net = dev_net_rcu(skb_dst(skb)->dev);
 	const struct iphdr *iph = ip_hdr(skb);
 
 	if (sk)
 		return sk;
 
-	return __inet_lookup(dev_net(skb_dst(skb)->dev), hashinfo, skb,
+	return __inet_lookup(net, hashinfo, skb,
 			     doff, iph->saddr, sport,
 			     iph->daddr, dport, inet_iif(skb), sdif,
 			     refcounted);
