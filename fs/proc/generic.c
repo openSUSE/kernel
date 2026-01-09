bb79790a8348ca87ba32beb2961d4376dd98c1b1
Jan Kara <jack@suse.cz>
2026-01-09 17:06:10 +0100

    fs/proc: fix uaf in proc_readdir_de() (bsc#1255297 CVE-2025-40271).
    suse-commit: e43869a94d425f671a3f1858a4b9050e75e47f07

--- fs/proc/generic.c
+++ fs/proc/generic.c
@@ -547,6 +547,12 @@ void pde_put(struct proc_dir_entry *pde)
 		free_proc_entry(pde);
 }
 
+static void pde_erase(struct proc_dir_entry *pde, struct proc_dir_entry *parent)
+{
+	rb_erase_cached(&pde->subdir_node, &parent->subdir);
+	RB_CLEAR_NODE(&pde->subdir_node);
+}
+
 /*
  * Remove a /proc entry and free it if it's not currently in use.
  */
@@ -565,7 +571,7 @@ void remove_proc_entry(const char *name, struct proc_dir_entry *parent)
 
 	de = pde_subdir_find(parent, fn, len);
 	if (de)
-		rb_erase_cached(&de->subdir_node, &parent->subdir);
+		pde_erase(de, parent);
 	write_unlock(&proc_subdir_lock);
 	if (!de) {
 		WARN(1, "name '%s'\n", name);
@@ -602,13 +608,13 @@ int remove_proc_subtree(const char *name, struct proc_dir_entry *parent)
 		write_unlock(&proc_subdir_lock);
 		return -ENOENT;
 	}
-	rb_erase_cached(&root->subdir_node, &parent->subdir);
+	pde_erase(root, parent);
 
 	de = root;
 	while (1) {
 		next = pde_subdir_first(de);
 		if (next) {
-			rb_erase_cached(&next->subdir_node, &de->subdir);
+			pde_erase(next, de);
 			de = next;
 			continue;
 		}
