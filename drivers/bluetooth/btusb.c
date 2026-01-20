1bd38eb8513c9ee0b0205db567251d4459c168f7
Takashi Iwai <tiwai@suse.de>
2026-01-20 16:18:09 +0100

    Bluetooth: btusb: revert use of devm_kzalloc in btusb (CVE-2025-71082 bsc#1256611).
    suse-commit: b7a4df103f9057bc67640374677acc19aa1f422b

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -3025,7 +3025,7 @@ static int btusb_probe(struct usb_interface *intf,
 			return -ENODEV;
 	}
 
-	data = devm_kzalloc(&intf->dev, sizeof(*data), GFP_KERNEL);
+	data = kzalloc(sizeof(*data), GFP_KERNEL);
 	if (!data)
 		return -ENOMEM;
 
@@ -3048,8 +3048,10 @@ static int btusb_probe(struct usb_interface *intf,
 		}
 	}
 
-	if (!data->intr_ep || !data->bulk_tx_ep || !data->bulk_rx_ep)
+	if (!data->intr_ep || !data->bulk_tx_ep || !data->bulk_rx_ep) {
+		kfree(data);
 		return -ENODEV;
+	}
 
 	if (id->driver_info & BTUSB_AMP) {
 		data->cmdreq_type = USB_TYPE_CLASS | 0x01;
@@ -3084,8 +3086,10 @@ static int btusb_probe(struct usb_interface *intf,
 	}
 
 	hdev = hci_alloc_dev();
-	if (!hdev)
+	if (!hdev) {
+		kfree(data);
 		return -ENOMEM;
+	}
 
 	hdev->bus = HCI_USB;
 	hci_set_drvdata(hdev, data);
@@ -3297,6 +3301,7 @@ static int btusb_probe(struct usb_interface *intf,
 
 out_free_dev:
 	hci_free_dev(hdev);
+	kfree(data);
 	return err;
 }
 
@@ -3340,6 +3345,7 @@ static void btusb_disconnect(struct usb_interface *intf)
 	}
 
 	hci_free_dev(hdev);
+	kfree(data);
 }
 
 #ifdef CONFIG_PM
