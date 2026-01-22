0f41614cea09fb762784b5c7acfaac4b6351b97f
Jan Kara <jack@suse.cz>
2026-01-22 15:10:17 +0100

    nbd: defer config unlock in nbd_genl_connect (bsc#1255622 CVE-2025-68366).
    suse-commit: 3c027350e6b5a353bd071d8ee0f3214e5ebdb78d

--- drivers/block/nbd.c
+++ drivers/block/nbd.c
@@ -1910,12 +1910,12 @@ again:
 	}
 	ret = nbd_start_device(nbd);
 out:
-	mutex_unlock(&nbd->config_lock);
 	if (!ret) {
 		set_bit(NBD_HAS_CONFIG_REF, &config->runtime_flags);
 		refcount_inc(&nbd->config_refs);
 		nbd_connect_reply(info, nbd->index);
 	}
+	mutex_unlock(&nbd->config_lock);
 	nbd_config_put(nbd);
 	if (put_dev)
 		nbd_put(nbd);
