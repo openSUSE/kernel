63dc317dfcd3faffd082c2bf3080f9ad070273da
Zilin Guan <zilin@seu.edu.cn>
2026-01-22 03:26:44 +0000

    ice: Fix memory leak in ice_set_ringparam()
    [ Upstream commit fe868b499d16f55bbeea89992edb98043c9de416 ]
    
    In ice_set_ringparam, tx_rings and xdp_rings are allocated before
    rx_rings. If the allocation of rx_rings fails, the code jumps to
    the done label leaking both tx_rings and xdp_rings. Furthermore, if
    the setup of an individual Rx ring fails during the loop, the code jumps
    to the free_tx label which releases tx_rings but leaks xdp_rings.
    
    Fix this by introducing a free_xdp label and updating the error paths to
    ensure both xdp_rings and tx_rings are properly freed if rx_rings
    allocation or setup fails.
    
    Compile tested only. Issue found using a prototype static analysis tool
    and code review.
    
    Fixes: fcea6f3da546 ("ice: Add stats and ethtool support")
    Fixes: efc2214b6047 ("ice: Add support for XDP")
    Signed-off-by: Zilin Guan <zilin@seu.edu.cn>
    Reviewed-by: Paul Menzel <pmenzel@molgen.mpg.de>
    Reviewed-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
    Tested-by: Rinitha S <sx.rinitha@intel.com> (A Contingent worker at Intel)
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/intel/ice/ice_ethtool.c
+++ drivers/net/ethernet/intel/ice/ice_ethtool.c
@@ -3320,7 +3320,7 @@ process_rx:
 	rx_rings = kcalloc(vsi->num_rxq, sizeof(*rx_rings), GFP_KERNEL);
 	if (!rx_rings) {
 		err = -ENOMEM;
-		goto done;
+		goto free_xdp;
 	}
 
 	ice_for_each_rxq(vsi, i) {
@@ -3350,7 +3350,7 @@ rx_unwind:
 			}
 			kfree(rx_rings);
 			err = -ENOMEM;
-			goto free_tx;
+			goto free_xdp;
 		}
 	}
 
@@ -3400,6 +3400,13 @@ process_link:
 		ice_up(vsi);
 	}
 	goto done;
+
+free_xdp:
+	if (xdp_rings) {
+		ice_for_each_xdp_txq(vsi, i)
+			ice_free_tx_ring(&xdp_rings[i]);
+		kfree(xdp_rings);
+	}
 
 free_tx:
 	/* error cleanup if the Rx allocations failed after getting Tx */
