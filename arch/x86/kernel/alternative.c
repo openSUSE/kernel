704ba44960cf6c01b7d0d1bbf63d56febfb25d83
Nikolay Borisov <nik.borisov@suse.com>
2026-01-26 13:44:34 +0200

    x86/alternatives: Disable LASS when patching kernel code (jsc#PED-14226).
    suse-commit: 1d0aa378fb08e045207fcc2d449d587443069a1d

--- arch/x86/kernel/alternative.c
+++ arch/x86/kernel/alternative.c
@@ -2548,16 +2548,30 @@ static inline void unuse_temporary_mm(temp_mm_state_t prev_state)
 __ro_after_init struct mm_struct *poking_mm;
 __ro_after_init unsigned long poking_addr;
 
+/*
+ * Text poking creates and uses a mapping in the lower half of the
+ * address space. Relax LASS enforcement when accessing the poking
+ * address.
+ *
+ * objtool enforces a strict policy of "no function calls within AC=1
+ * regions". Adhere to the policy by using inline versions of
+ * memcpy()/memset() that will never result in a function call.
+ */
+
 static void text_poke_memcpy(void *dst, const void *src, size_t len)
 {
-	memcpy(dst, src, len);
+	lass_stac();
+	__inline_memcpy(dst, src, len);
+	lass_clac();
 }
 
 static void text_poke_memset(void *dst, const void *src, size_t len)
 {
 	int c = *(const int *)src;
 
-	memset(dst, c, len);
+	lass_stac();
+	__inline_memset(dst, c, len);
+	lass_clac();
 }
 
 typedef void text_poke_f(void *dst, const void *src, size_t len);
