4387b29bb6e7ca439279d827c63899d9b80b3bd6
Takashi Iwai <tiwai@suse.de>
2026-01-26 10:14:54 +0100

    ASoC: remove disable_route_checks (jsc#PED-14430 jsc#PED-14297 jsc#PED-14024).
    suse-commit: 45824830e741ae8e057145434bd6a382ec39034d

--- include/sound/soc.h
+++ include/sound/soc.h
@@ -1120,7 +1120,6 @@ struct snd_soc_card {
 	unsigned int instantiated:1;
 	unsigned int topology_shortname_created:1;
 	unsigned int fully_routed:1;
-	unsigned int disable_route_checks:1;
 	unsigned int probed:1;
 	unsigned int component_chaining:1;
 
