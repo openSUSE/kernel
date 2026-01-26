0db937d3ac81a0935759e93115292feab780434c
Takashi Iwai <tiwai@suse.de>
2026-01-26 18:19:32 +0100

    soundwire: bus: add bpt_stream pointer (jsc#PED-14430 jsc#PED-14297 jsc#PED-14024).
    suse-commit: efa1435629d16f1b5211e7b52a8e56240bc373eb

--- include/linux/soundwire/sdw.h
+++ include/linux/soundwire/sdw.h
@@ -981,6 +981,7 @@ struct sdw_stream_runtime {
  * @stream_refcount: number of streams currently using this bus
  * @btp_stream_refcount: number of BTP streams currently using this bus (should
  * be zero or one, multiple streams per link is not supported).
+ * @bpt_stream: pointer stored to handle BTP streams.
  * @ops: Master callback ops
  * @port_ops: Master port callback ops
  * @prop: Master properties
@@ -1019,6 +1020,7 @@ struct sdw_bus {
 	struct sdw_bus_params params;
 	int stream_refcount;
 	int bpt_stream_refcount;
+	struct sdw_stream_runtime *bpt_stream;
 	const struct sdw_master_ops *ops;
 	const struct sdw_master_port_ops *port_ops;
 	struct sdw_master_prop prop;
