233ba3dca91617c4ce2f2a4202e5595f155ca1d0
Takashi Iwai <tiwai@suse.de>
2026-01-26 10:05:54 +0100

    ASoc: pcm6240: Remove use of i2c_match_id() (jsc#PED-14430 jsc#PED-14297 jsc#PED-14024).
    suse-commit: 98a2e97737583eb596b75ffb03b624ebd27cf5a6

--- sound/soc/codecs/pcm6240.c
+++ sound/soc/codecs/pcm6240.c
@@ -2057,7 +2057,6 @@ static char *str_to_upper(char *str)
 
 static int pcmdevice_i2c_probe(struct i2c_client *i2c)
 {
-	const struct i2c_device_id *id = i2c_match_id(pcmdevice_i2c_id, i2c);
 	struct pcmdevice_priv *pcm_dev;
 	struct device_node *np;
 	unsigned int dev_addrs[PCMDEVICE_MAX_I2C_DEVICES];
@@ -2067,7 +2066,7 @@ static int pcmdevice_i2c_probe(struct i2c_client *i2c)
 	if (!pcm_dev)
 		return -ENOMEM;
 
-	pcm_dev->chip_id = (id != NULL) ? id->driver_data : 0;
+	pcm_dev->chip_id = (uintptr_t)i2c_get_match_data(i2c);
 
 	pcm_dev->dev = &i2c->dev;
 	pcm_dev->client = i2c;
