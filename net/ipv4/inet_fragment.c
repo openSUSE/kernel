f10c975dc6d01e31f7befab67bff14792d69c0f2
Michal Kubecek <mkubecek@suse.cz>
2026-01-29 12:26:49 +0100

    kabi: export inet_frag_rbtree_purge() function again (CVE-2025-68768 bsc#1256579).
    suse-commit: d066c8d6214fdcc01e6215a5068a2eb8f4755545

--- net/ipv4/inet_fragment.c
+++ net/ipv4/inet_fragment.c
@@ -266,7 +266,7 @@ static void inet_frag_destroy_rcu(struct rcu_head *head)
 	kmem_cache_free(f->frags_cachep, q);
 }
 
-static unsigned int
+unsigned int
 inet_frag_rbtree_purge(struct rb_root *root, enum skb_drop_reason reason)
 {
 	struct rb_node *p = rb_first(root);
@@ -287,6 +287,7 @@ inet_frag_rbtree_purge(struct rb_root *root, enum skb_drop_reason reason)
 	}
 	return sum;
 }
+EXPORT_SYMBOL(inet_frag_rbtree_purge);
 
 void inet_frag_queue_flush(struct inet_frag_queue *q,
 			   enum skb_drop_reason reason)
