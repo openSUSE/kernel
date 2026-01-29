0bfe9fc7baf875d236e7c5f4a066b38cbac9e057
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2026-01-29 16:46:33 +0100

    idpf: Fix RSS LUT NULL ptr issue after soft reset (CVE-2026-22993 bsc#1257180).
    suse-commit: bb6b8535ec43a47606615d0fb19f600b1d9a1463

--- drivers/net/ethernet/intel/idpf/idpf_lib.c
+++ drivers/net/ethernet/intel/idpf/idpf_lib.c
@@ -1366,8 +1366,6 @@ static int idpf_vport_open(struct idpf_vport *vport, bool alloc_res)
 {
 	struct idpf_netdev_priv *np = netdev_priv(vport->netdev);
 	struct idpf_adapter *adapter = vport->adapter;
-	struct idpf_vport_config *vport_config;
-	struct idpf_rss_data *rss_data;
 	int err;
 
 	if (np->state != __IDPF_VPORT_DOWN)
@@ -1450,19 +1448,6 @@ static int idpf_vport_open(struct idpf_vport *vport, bool alloc_res)
 	}
 
 	idpf_restore_features(vport);
-
-	vport_config = adapter->vport_config[vport->idx];
-	rss_data = &vport_config->user_config.rss_data;
-
-	if (!rss_data->rss_lut) {
-		err = idpf_init_rss_lut(vport);
-		if (err) {
-			dev_err(&adapter->pdev->dev,
-				"Failed to initialize RSS LUT for vport %u: %d\n",
-				vport->vport_id, err);
-			goto disable_vport;
-		}
-	}
 
 	err = idpf_config_rss(vport);
 	if (err) {
@@ -1951,7 +1936,6 @@ int idpf_initiate_soft_reset(struct idpf_vport *vport,
 		idpf_vport_stop(vport);
 	}
 
-	idpf_deinit_rss_lut(vport);
 	/* We're passing in vport here because we need its wait_queue
 	 * to send a message and it should be getting all the vport
 	 * config data out of the adapter but we need to be careful not
@@ -2016,6 +2000,10 @@ int idpf_initiate_soft_reset(struct idpf_vport *vport,
 	err = idpf_set_real_num_queues(vport);
 	if (err)
 		goto err_reset;
+
+	if (reset_cause == IDPF_SR_Q_CHANGE &&
+	    !netif_is_rxfh_configured(vport->netdev))
+		idpf_fill_dflt_rss_lut(vport);
 
 	if (current_state == __IDPF_VPORT_UP)
 		err = idpf_vport_open(vport, false);
