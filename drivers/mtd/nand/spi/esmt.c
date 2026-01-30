f238760149d6e1a1c0fc0d4d35e3f109ba6d9911
Takashi Iwai <tiwai@suse.de>
2026-01-30 09:12:48 +0100

    Revert "mtd: spinand: esmt: fix id code for F50D1G41LB" (stable-fixes).
    suse-commit: 46e120bd3e8fdeca69949ee45188cf134a10213a

--- drivers/mtd/nand/spi/esmt.c
+++ drivers/mtd/nand/spi/esmt.c
@@ -115,7 +115,7 @@ static const struct spinand_info esmt_c8_spinand_table[] = {
 		     SPINAND_ECCINFO(&f50l1g41lb_ooblayout, NULL)),
 	SPINAND_INFO("F50D1G41LB",
 		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_ADDR, 0x11, 0x7f,
-				0x7f),
+				0x7f, 0x7f),
 		     NAND_MEMORG(1, 2048, 64, 64, 1024, 20, 1, 1, 1),
 		     NAND_ECCREQ(1, 512),
 		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
