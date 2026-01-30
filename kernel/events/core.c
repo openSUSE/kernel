cbfb13bc626ef8e78fa4e5e7b53033b26f6c9918
Tony Jones <tonyj@suse.de>
2026-01-29 19:10:58 -0800

    perf: Move perf_mmap_calc_limits() into both rb and aux branches (perf-events-v6.19-update).
    suse-commit: fb8b9147c6df9862b22b788423d366b938bf1e93

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -7060,6 +7060,16 @@ static int perf_mmap(struct file *file, struct vm_area_struct *vma)
 			ring_buffer_attach(event, NULL);
 		}
 
+		if (!perf_mmap_calc_limits(vma, &user_extra, &extra)) {
+			ret = -EPERM;
+			goto unlock;
+		}
+
+		WARN_ON(!rb && event->rb);
+
+		if (vma->vm_flags & VM_WRITE)
+			flags |= RING_BUFFER_WRITABLE;
+
 	} else {
 		/*
 		 * AUX area mapping: if rb->aux_nr_pages != 0, it's already
@@ -7106,17 +7116,17 @@ static int perf_mmap(struct file *file, struct vm_area_struct *vma)
 			ret = 0;
 			goto unlock;
 		}
+
+		if (!perf_mmap_calc_limits(vma, &user_extra, &extra)) {
+			ret = -EPERM;
+			goto unlock;
+		}
+
+		WARN_ON(!rb && event->rb);
+
+		if (vma->vm_flags & VM_WRITE)
+			flags |= RING_BUFFER_WRITABLE;
 	}
-
-	if (!perf_mmap_calc_limits(vma, &user_extra, &extra)) {
-		ret = -EPERM;
-		goto unlock;
-	}
-
-	WARN_ON(!rb && event->rb);
-
-	if (vma->vm_flags & VM_WRITE)
-		flags |= RING_BUFFER_WRITABLE;
 
 	if (!rb) {
 		rb = rb_alloc(nr_pages,
