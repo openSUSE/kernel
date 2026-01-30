cd21cffae305b1780133c0a8bf045fd5be2ff531
Tony Jones <tonyj@suse.de>
2026-01-29 19:10:58 -0800

    perf/core: Exit early on perf_mmap() fail (CVE-2025-38563 bsc#1248306 dependency CVE-2025-38565 bsc#1248377).
    suse-commit: ecdec1b01f040088ea3c96949ea25db97422dc9e

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -7148,6 +7148,9 @@ aux_unlock:
 		mutex_unlock(aux_mutex);
 	mutex_unlock(&event->mmap_mutex);
 
+	if (ret)
+		return ret;
+
 	/*
 	 * Since pinned accounting is per vm we cannot allow fork() to copy our
 	 * vma.
@@ -7155,8 +7158,7 @@ aux_unlock:
 	vm_flags_set(vma, VM_DONTCOPY | VM_DONTEXPAND | VM_DONTDUMP);
 	vma->vm_ops = &perf_mmap_vmops;
 
-	if (!ret)
-		ret = map_range(rb, vma);
+	ret = map_range(rb, vma);
 
 	mapped = get_mapped(event, event_mapped);
 	if (mapped)
