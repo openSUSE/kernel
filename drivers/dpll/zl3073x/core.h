ac7e93f3bd61eeb12ceeb13d2874da9b89ec93d1
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2026-01-30 12:10:24 +0100

    - dpll: zl3073x: Implement phase offset monitor feature   (jsc#PED-13387). - Refresh   patches.suse/dpll-zl3073x-Add-support-to-get-fractional-frequency.patch.
    suse-commit: dc05141e3722db1f020bd21d1c981a33c6b1fb8b

--- drivers/dpll/zl3073x/core.h
+++ drivers/dpll/zl3073x/core.h
@@ -165,6 +165,7 @@ int zl3073x_write_hwreg_seq(struct zl3073x_dev *zldev,
  *****************/
 
 int zl3073x_ref_freq_factorize(u32 freq, u16 *base, u16 *mult);
+int zl3073x_ref_phase_offsets_update(struct zl3073x_dev *zldev, int channel);
 
 static inline bool
 zl3073x_is_n_pin(u8 id)
