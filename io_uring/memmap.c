b84ee9689f5c55ff40b27bc192ef6ca7b46c4785
Gabriel Krisman Bertazi <krisman@suse.de>
2026-02-10 14:41:49 -0500

    io_uring/register: add IORING_REGISTER_RESIZE_RINGS (bsc#1257958).
    suse-commit: be104c0fb93e589bc2ec5f63c77d243490388a1f

--- io_uring/memmap.c
+++ io_uring/memmap.c
@@ -258,6 +258,8 @@ __cold int io_uring_mmap(struct file *file, struct vm_area_struct *vma)
 	unsigned int npages;
 	void *ptr;
 
+	guard(mutex)(&ctx->resize_lock);
+
 	ptr = io_uring_validate_mmap_request(file, vma->vm_pgoff, sz);
 	if (IS_ERR(ptr))
 		return PTR_ERR(ptr);
@@ -281,6 +283,7 @@ unsigned long io_uring_get_unmapped_area(struct file *filp, unsigned long addr,
 					 unsigned long len, unsigned long pgoff,
 					 unsigned long flags)
 {
+	struct io_ring_ctx *ctx = filp->private_data;
 	void *ptr;
 
 	/*
@@ -290,6 +293,8 @@ unsigned long io_uring_get_unmapped_area(struct file *filp, unsigned long addr,
 	 */
 	if (addr)
 		return -EINVAL;
+
+	guard(mutex)(&ctx->resize_lock);
 
 	ptr = io_uring_validate_mmap_request(filp, pgoff, len);
 	if (IS_ERR(ptr))
@@ -336,7 +341,10 @@ unsigned long io_uring_get_unmapped_area(struct file *file, unsigned long addr,
 					 unsigned long len, unsigned long pgoff,
 					 unsigned long flags)
 {
+	struct io_ring_ctx *ctx = file->private_data;
 	void *ptr;
+
+	guard(mutex)(&ctx->resize_lock);
 
 	ptr = io_uring_validate_mmap_request(file, pgoff, len);
 	if (IS_ERR(ptr))
