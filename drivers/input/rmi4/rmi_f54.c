d7a21e717941516fe0012658b60bb3ee35924bab
Bart Van Assche <bvanassche@acm.org>
2026-02-23 15:05:15 -0800

    Input: synaptics-rmi4 - fix a locking bug in an error path
    commit 7adaaee5edd35a423ae199c41b86bd1ed60ed483 upstream.
    
    Lock f54->data_mutex when entering the function statement since jumping
    to the 'error' label when checking report_size fails causes that mutex
    to be unlocked.
    
    This bug has been detected by the Clang thread-safety checker.
    
    Fixes: 3a762dbd5347 ("[media] Input: synaptics-rmi4 - add support for F54 diagnostics")
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Link: https://patch.msgid.link/20260223215118.2154194-16-bvanassche@acm.org
    Cc: stable@vger.kernel.org
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/input/rmi4/rmi_f54.c
+++ drivers/input/rmi4/rmi_f54.c
@@ -534,6 +534,8 @@ static void rmi_f54_work(struct work_struct *work)
 	int error;
 	int i;
 
+	mutex_lock(&f54->data_mutex);
+
 	report_size = rmi_f54_get_report_size(f54);
 	if (report_size == 0) {
 		dev_err(&fn->dev, "Bad report size, report type=%d\n",
@@ -541,8 +543,6 @@ static void rmi_f54_work(struct work_struct *work)
 		error = -EINVAL;
 		goto error;     /* retry won't help */
 	}
-
-	mutex_lock(&f54->data_mutex);
 
 	/*
 	 * Need to check if command has completed.
