ddbccf0612dfef54e572349481b4fcdf83b3e27f
Robert Frohl <rfrohl@suse.com>
2026-02-27 12:47:05 +0100

    accel/amdxdna: Hold mm structure across iommu_sva_unbind_device() (bsc#1258210).
    suse-commit: f8e0d2896197cf322b7652afb376fc3f8f010489

--- drivers/accel/amdxdna/amdxdna_pci_drv.c
+++ drivers/accel/amdxdna/amdxdna_pci_drv.c
@@ -88,6 +88,8 @@ static int amdxdna_drm_open(struct drm_device *ddev, struct drm_file *filp)
 		ret = -ENODEV;
 		goto unbind_sva;
 	}
+	client->mm = current->mm;
+	mmgrab(client->mm);
 	init_srcu_struct(&client->hwctx_srcu);
 	xa_init_flags(&client->hwctx_xa, XA_FLAGS_ALLOC);
 	mutex_init(&client->mm_lock);
@@ -127,6 +129,7 @@ static void amdxdna_drm_close(struct drm_device *ddev, struct drm_file *filp)
 		drm_gem_object_put(to_gobj(client->dev_heap));
 
 	iommu_sva_unbind_device(client->sva);
+	mmdrop(client->mm);
 
 	XDNA_DBG(xdna, "pid %d closed", client->pid);
 	kfree(client);
