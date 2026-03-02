73ba52dc63fbdf5818745b79e6870f6dbc5418d0
Thomas Renninger <trenn@suse.de>
2026-03-02 15:19:37 +0100

    Refresh patches.suse/tools-power-turbostat-Add-Wildcat-Lake-and-Nova-Lake-support.patch.
    suse-commit: 0c4ebe3283697e4dcc8f470e9a5958cce8e614a9

--- tools/power/x86/turbostat/turbostat.c
+++ tools/power/x86/turbostat/turbostat.c
@@ -10129,7 +10129,7 @@ int get_and_dump_counters(void)
 
 void print_version()
 {
-	fprintf(outf, "turbostat version 2025.09.09 - Len Brown <lenb@kernel.org>\n");
+	fprintf(outf, "turbostat version 2025.10.18 - Len Brown <lenb@kernel.org>\n");
 }
 
 #define COMMAND_LINE_SIZE 2048
