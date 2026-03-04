8d276374440e6181f13504344875b140bbc122e4
Hannes Reinecke <hare@suse.de>
2026-03-04 09:05:21 +0100

    - net: introduce CONFIG_NET_CRC32C (jsc#PED-14161). - Update config files.
    suse-commit: 63e87af3efca92f6980a061d57465ab47b07fae5

--- net/core/dev.c
+++ net/core/dev.c
@@ -3421,6 +3421,7 @@ out:
 }
 EXPORT_SYMBOL(skb_checksum_help);
 
+#ifdef CONFIG_NET_CRC32C
 int skb_crc32c_csum_help(struct sk_buff *skb)
 {
 	__le32 crc32c_csum;
@@ -3460,6 +3461,7 @@ out:
 	return ret;
 }
 EXPORT_SYMBOL(skb_crc32c_csum_help);
+#endif /* CONFIG_NET_CRC32C */
 
 __be16 skb_network_protocol(struct sk_buff *skb, int *depth)
 {
