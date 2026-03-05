28ea836cc44cb8b89c1c174707ead0c1133c60e9
Kamal Dasu <kamal.dasu@broadcom.com>
2026-03-05 14:49:06 -0500

    mtd: rawnand: serialize lock/unlock against other NAND operations
    [ Upstream commit bab2bc6e850a697a23b9e5f0e21bb8c187615e95 ]
    
    nand_lock() and nand_unlock() call into chip->ops.lock_area/unlock_area
    without holding the NAND device lock. On controllers that implement
    SET_FEATURES via multiple low-level PIO commands, these can race with
    concurrent UBI/UBIFS background erase/write operations that hold the
    device lock, resulting in cmd_pending conflicts on the NAND controller.
    
    Add nand_get_device()/nand_release_device() around the lock/unlock
    operations to serialize them against all other NAND controller access.
    
    Fixes: 92270086b7e5 ("mtd: rawnand: Add support for manufacturer specific lock/unlock operation")
    Signed-off-by: Kamal Dasu <kamal.dasu@broadcom.com>
    Reviewed-by: William Zhang <william.zhang@broadcom.com>
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/mtd/nand/raw/nand_base.c
+++ drivers/mtd/nand/raw/nand_base.c
@@ -4383,11 +4383,16 @@ static void nand_shutdown(struct mtd_info *mtd)
 static int nand_lock(struct mtd_info *mtd, loff_t ofs, uint64_t len)
 {
 	struct nand_chip *chip = mtd_to_nand(mtd);
+	int ret;
 
 	if (!chip->ops.lock_area)
 		return -ENOTSUPP;
 
-	return chip->ops.lock_area(chip, ofs, len);
+	nand_get_device(chip);
+	ret = chip->ops.lock_area(chip, ofs, len);
+	nand_release_device(chip);
+
+	return ret;
 }
 
 /**
@@ -4399,11 +4404,16 @@ static int nand_lock(struct mtd_info *mtd, loff_t ofs, uint64_t len)
 static int nand_unlock(struct mtd_info *mtd, loff_t ofs, uint64_t len)
 {
 	struct nand_chip *chip = mtd_to_nand(mtd);
+	int ret;
 
 	if (!chip->ops.unlock_area)
 		return -ENOTSUPP;
 
-	return chip->ops.unlock_area(chip, ofs, len);
+	nand_get_device(chip);
+	ret = chip->ops.unlock_area(chip, ofs, len);
+	nand_release_device(chip);
+
+	return ret;
 }
 
 /* Set default functions */
