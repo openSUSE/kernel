88a596cf50a5646b40e2401d834d356e01b790af
Takashi Iwai <tiwai@suse.de>
2026-03-05 17:01:44 +0100

    wifi: iwlwifi: mvm: clarify fw_id_to_link_sta protection (jsc#PED-14425).
    suse-commit: 22e96ce47b145110ffc08f3e657638675d76dbc5

--- drivers/net/wireless/intel/iwlwifi/mvm/fw.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/fw.c
@@ -1401,6 +1401,7 @@ int iwl_mvm_up(struct iwl_mvm *mvm)
 	int ret, i;
 	struct ieee80211_supported_band *sband = NULL;
 
+	lockdep_assert_wiphy(mvm->hw->wiphy);
 	lockdep_assert_held(&mvm->mutex);
 
 	ret = iwl_trans_start_hw(mvm->trans);
@@ -1620,6 +1621,7 @@ int iwl_mvm_load_d3_fw(struct iwl_mvm *mvm)
 {
 	int ret, i;
 
+	lockdep_assert_wiphy(mvm->hw->wiphy);
 	lockdep_assert_held(&mvm->mutex);
 
 	ret = iwl_trans_start_hw(mvm->trans);
