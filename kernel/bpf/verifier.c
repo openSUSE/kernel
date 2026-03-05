cd1537f5a4f63ac02fb987082674578dc94efe56
Hoyeon Lee <hoyeon.lee@suse.com>
2026-03-05 07:25:01 +0000

    bpf: move REG_LIVE_DONE check to clean_live_states() (jsc#PED-14653).
    suse-commit: 8d61458ef752ada1794baaa211d46fe3d849157f

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -18315,10 +18315,6 @@ static void clean_verifier_state(struct bpf_verifier_env *env,
 {
 	int i;
 
-	if (st->frame[0]->regs[0].live & REG_LIVE_DONE)
-		/* all regs in this state in all frames were already marked */
-		return;
-
 	for (i = 0; i <= st->curframe; i++)
 		clean_func_state(env, st->frame[i]);
 }
@@ -18372,6 +18368,9 @@ static void clean_live_states(struct bpf_verifier_env *env, int insn,
 			continue;
 		if (sl->state.insn_idx != insn ||
 		    !same_callsites(&sl->state, cur))
+			continue;
+		if (sl->state.frame[0]->regs[0].live & REG_LIVE_DONE)
+			/* all regs in this state in all frames were already marked */
 			continue;
 		clean_verifier_state(env, &sl->state);
 	}
