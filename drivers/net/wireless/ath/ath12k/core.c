4e10027f2ca938eb836b22ba98ca8f3bdac5f4ed
Takashi Iwai <tiwai@suse.de>
2026-03-06 10:21:42 +0100

    wifi: ath12k: Disable MLO in Factory Test Mode (jsc#PED-14425).
    suse-commit: 7cb4116d4ba63cad2aaad39cfb2b178e12ddd9be

--- drivers/net/wireless/ath/ath12k/core.c
+++ drivers/net/wireless/ath/ath12k/core.c
@@ -1622,6 +1622,9 @@ static struct ath12k_hw_group *ath12k_core_hw_group_assign(struct ath12k_base *a
 
 	lockdep_assert_held(&ath12k_hw_group_mutex);
 
+	if (ath12k_ftm_mode)
+		goto invalid_group;
+
 	/* The grouping of multiple devices will be done based on device tree file.
 	 * The platforms that do not have any valid group information would have
 	 * each device to be part of its own invalid group.
@@ -1808,6 +1811,9 @@ void ath12k_core_hw_group_set_mlo_capable(struct ath12k_hw_group *ag)
 {
 	struct ath12k_base *ab;
 	int i;
+
+	if (ath12k_ftm_mode)
+		return;
 
 	lockdep_assert_held(&ag->mutex);
 
