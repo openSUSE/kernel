acda33c7121c09a2cceb0f45bd798cc17c6e40b0
Hoyeon Lee <hoyeon.lee@suse.com>
2026-03-06 16:20:05 +0000

    - bpf: Allow struct_ops to get map id by kdata (jsc#PED-14653). - Refresh   patches.suse/bpf-Export-necessary-symbols-for-modules-with-struct_ops.patch.
    suse-commit: d86abaf6cb979ebd164e73e26991aa2c5c707449

--- kernel/bpf/bpf_struct_ops.c
+++ kernel/bpf/bpf_struct_ops.c
@@ -1176,6 +1176,18 @@ void bpf_struct_ops_put(const void *kdata)
 }
 EXPORT_SYMBOL_GPL(bpf_struct_ops_put);
 
+u32 bpf_struct_ops_id(const void *kdata)
+{
+	struct bpf_struct_ops_value *kvalue;
+	struct bpf_struct_ops_map *st_map;
+
+	kvalue = container_of(kdata, struct bpf_struct_ops_value, data);
+	st_map = container_of(kvalue, struct bpf_struct_ops_map, kvalue);
+
+	return st_map->map.id;
+}
+EXPORT_SYMBOL_GPL(bpf_struct_ops_id);
+
 static bool bpf_struct_ops_valid_to_reg(struct bpf_map *map)
 {
 	struct bpf_struct_ops_map *st_map = (struct bpf_struct_ops_map *)map;
