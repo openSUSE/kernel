0e442b79bf01bd08bfbdf5c4ebf4d1051661e258
Takashi Iwai <tiwai@suse.de>
2026-03-06 13:26:04 +0100

    - wifi: ath12k: enable ath12k AHB support (jsc#PED-14425). - Refresh   patches.suse/wifi-ath12k-Add-MODULE_FIRMWARE-entries.patch.
    suse-commit: 56daf5efc4eb2296893a97591bf7c6d5ca897884

--- drivers/net/wireless/ath/ath12k/pci.c
+++ drivers/net/wireless/ath/ath12k/pci.c
@@ -1835,7 +1835,7 @@ static struct pci_driver ath12k_pci_driver = {
 	.driver.pm = &ath12k_pci_pm_ops,
 };
 
-static int ath12k_pci_init(void)
+int ath12k_pci_init(void)
 {
 	int ret;
 
@@ -1848,17 +1848,11 @@ static int ath12k_pci_init(void)
 
 	return 0;
 }
-module_init(ath12k_pci_init);
 
-static void ath12k_pci_exit(void)
+void ath12k_pci_exit(void)
 {
 	pci_unregister_driver(&ath12k_pci_driver);
 }
-
-module_exit(ath12k_pci_exit);
-
-MODULE_DESCRIPTION("Driver support for Qualcomm Technologies PCIe 802.11be WLAN devices");
-MODULE_LICENSE("Dual BSD/GPL");
 
 /* firmware files */
 MODULE_FIRMWARE(ATH12K_FW_DIR "/QCN9274/hw2.0/*");
