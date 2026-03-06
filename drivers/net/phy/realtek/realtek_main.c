ac94529014872572b448df3daad776406b59c7ee
Ivan T. Ivanov <iivanov@suse.de>
2026-03-06 17:24:23 +0200

    net: phy: realtek: add RTL8127-internal PHY (jsc#PED-15988)
    suse-commit: 25909dcf0b6ef66224438793ef67d419be80cb65

--- drivers/net/phy/realtek/realtek_main.c
+++ drivers/net/phy/realtek/realtek_main.c
@@ -108,6 +108,7 @@
 #define RTL_8221B_VB_CG				0x001cc849
 #define RTL_8221B_VN_CG				0x001cc84a
 #define RTL_8251B				0x001cc862
+#define RTL_8261C				0x001cc890
 
 #define RTL8211F_LED_COUNT			3
 
@@ -1155,6 +1156,7 @@ static int rtl_internal_nbaset_match_phy_device(struct phy_device *phydev)
 	case RTL_GENERIC_PHYID:
 	case RTL_8221B:
 	case RTL_8251B:
+	case RTL_8261C:
 	case 0x001cc841:
 		break;
 	default:
