2aefd4ff077beb46bd91b91351d0c5349a6be6e5
Hoyeon Lee <hoyeon.lee@suse.com>
2026-03-07 15:54:46 +0000

    bpf: Allow uprobe program to change context registers (jsc#PED-14653).
    suse-commit: 099c463ceb3f705b1aeb9e75ce7d76f4e79162fb

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -1339,8 +1339,6 @@ static bool kprobe_prog_is_valid_access(int off, int size, enum bpf_access_type 
 {
 	if (off < 0 || off >= sizeof(struct pt_regs))
 		return false;
-	if (type != BPF_READ)
-		return false;
 	if (off % size != 0)
 		return false;
 	/*
@@ -1349,6 +1347,9 @@ static bool kprobe_prog_is_valid_access(int off, int size, enum bpf_access_type 
 	 */
 	if (off + size > sizeof(struct pt_regs))
 		return false;
+
+	if (type == BPF_WRITE)
+		prog->aux->kprobe_write_ctx = true;
 
 	return true;
 }
@@ -2734,6 +2735,10 @@ int bpf_kprobe_multi_link_attach(const union bpf_attr *attr, struct bpf_prog *pr
 		return -EINVAL;
 
 	if (!is_kprobe_multi(prog))
+		return -EINVAL;
+
+	/* Writing to context is not allowed for kprobes. */
+	if (prog->aux->kprobe_write_ctx)
 		return -EINVAL;
 
 	flags = attr->link_create.kprobe_multi.flags;
