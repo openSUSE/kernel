c72ad1c65298ecf937c15d5b42805dbeeefec24a
Patrik Jakobsson <pjakobsson@suse.de>
2026-03-14 23:27:29 +0100

    drm/amdgpu: Convert update_partition_sched_list into a common helper (jsc#PED-13979 jsc#PED-14039 jsc#PED-14046 jsc#PED-14211 jsc#PED-14333 jsc#PED-14487 jsc#PED-14488 jsc#PED-14497 jsc#PED-14499 jsc#PED-15868)
    suse-commit: c832f885d754e00224f520365e650ab4e992ed83

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -3014,7 +3014,8 @@ static int amdgpu_device_init_schedulers(struct amdgpu_device *adev)
 		}
 	}
 
-	amdgpu_xcp_update_partition_sched_list(adev);
+	if (adev->xcp_mgr)
+		amdgpu_xcp_update_partition_sched_list(adev);
 
 	return 0;
 }
