cfb557480b6f1062147726e820cc8f273abc2078
Patrik Jakobsson <pjakobsson@suse.de>
2026-03-15 00:07:45 +0100

    drm/amd/amdgpu: Initialize swnode for ISP MFD device (jsc#PED-13979 jsc#PED-14039 jsc#PED-14046 jsc#PED-14211 jsc#PED-14333 jsc#PED-14487 jsc#PED-14488 jsc#PED-14497 jsc#PED-14499 jsc#PED-15868)
    suse-commit: 56cb6f75efbe258a58da85e9050572a00926226f

--- drivers/gpu/drm/amd/amdgpu/amdgpu_acpi.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_acpi.c
@@ -1547,7 +1547,7 @@ static int isp_match_acpi_device_ids(struct device *dev, const void *data)
 	return acpi_match_device(data, dev) ? 1 : 0;
 }
 
-int amdgpu_acpi_get_isp4_dev_hid(u8 (*hid)[ACPI_ID_LEN])
+int amdgpu_acpi_get_isp4_dev(struct acpi_device **dev)
 {
 	struct device *pdev __free(put_device) = NULL;
 	struct acpi_device *acpi_pdev;
@@ -1561,7 +1561,7 @@ int amdgpu_acpi_get_isp4_dev_hid(u8 (*hid)[ACPI_ID_LEN])
 	if (!acpi_pdev)
 		return -ENODEV;
 
-	strscpy(*hid, acpi_device_hid(acpi_pdev));
+	*dev = acpi_pdev;
 
 	return 0;
 }
