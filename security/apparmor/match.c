034487e637bfc9383279127ca4435815e284cf6a
David Disseldorp <ddiss@suse.de>
2026-03-14 23:48:22 +1100

    apparmor: fix missing bounds check on DEFAULT table in verify_dfa() (bsc#1258849).
    suse-commit: 26bd855d8b5326db98ca60ddea56522906554219

--- security/apparmor/match.c
+++ security/apparmor/match.c
@@ -204,9 +204,10 @@ static int verify_dfa(struct aa_dfa *dfa)
 	if (state_count == 0)
 		goto out;
 	for (i = 0; i < state_count; i++) {
-		if (!(BASE_TABLE(dfa)[i] & MATCH_FLAG_DIFF_ENCODE) &&
-		    (DEFAULT_TABLE(dfa)[i] >= state_count))
+		if (DEFAULT_TABLE(dfa)[i] >= state_count) {
+			pr_err("AppArmor DFA default state out of bounds");
 			goto out;
+		}
 		if (base_idx(BASE_TABLE(dfa)[i]) + 255 >= trans_count) {
 			pr_err("AppArmor DFA next/check upper bounds error\n");
 			goto out;
