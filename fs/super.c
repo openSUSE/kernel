f2b133daa1935d31f91d6ce3ac817e40efdea93b
Anthony Iliopoulos <ailiop@suse.com>
2026-03-23 10:36:55 +0100

    fs: skip superblock shrink on frozen xfs filesystems (bsc#1259770).
    suse-commit: f01e7af1c7ff09b24f3278299dc5ae07d2f25df5

--- fs/super.c
+++ fs/super.c
@@ -75,6 +75,12 @@ static unsigned long super_cache_scan(struct shrinker *shrink,
 	if (!trylock_super(sb))
 		return SHRINK_STOP;
 
+	if (sb->s_writers.frozen != SB_UNFROZEN &&
+			sb->s_magic == 0x58465342 /* XFS_SB_MAGIC */) {
+		up_read(&sb->s_umount);
+		return SHRINK_STOP;
+	}
+
 	if (sb->s_op->nr_cached_objects)
 		fs_objects = sb->s_op->nr_cached_objects(sb, sc);
 
