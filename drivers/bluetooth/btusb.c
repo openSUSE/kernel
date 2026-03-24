1ad2f0261e288eadc703edc97f739299247ceff6
Chun-Yi Lee <jlee@suse.com>
2026-03-24 14:32:45 +0800

    Bluetooth: btusb: Add RTL8851BE device 0x0bda:0xb850 (jsc#PED-14261).
    Refresh
    patches.suse/Bluetooth-btusb-Add-USB-ID-3625-010b-for-TP-LINK-Arc.patch.
    
    suse-commit: 3dad81b31b1e78b445086fea998b72960cbb1bc3

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -515,6 +515,7 @@ static const struct usb_device_id quirks_table[] = {
 						     BTUSB_WIDEBAND_SPEECH },
 
 	/* Realtek 8851BE Bluetooth devices */
+	{ USB_DEVICE(0x0bda, 0xb850), .driver_info = BTUSB_REALTEK },
 	{ USB_DEVICE(0x13d3, 0x3600), .driver_info = BTUSB_REALTEK },
 
 	/* Realtek 8851BU Bluetooth devices */
