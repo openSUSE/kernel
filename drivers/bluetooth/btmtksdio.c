7611f498870ffac236a58dd8734ef81f20bf4ec6
Chun-Yi Lee <jlee@suse.com>
2026-03-24 14:32:45 +0800

    Bluetooth: btmtksdio: Fix wakeup source leaks on device unbind (jsc#PED-14261).
    suse-commit: 59d505b1b293f1579e5244c1c68534baed56826e

--- drivers/bluetooth/btmtksdio.c
+++ drivers/bluetooth/btmtksdio.c
@@ -1426,7 +1426,7 @@ static int btmtksdio_probe(struct sdio_func *func,
 	 */
 	pm_runtime_put_noidle(bdev->dev);
 
-	err = device_init_wakeup(bdev->dev, true);
+	err = devm_device_init_wakeup(bdev->dev);
 	if (err)
 		bt_dev_err(hdev, "failed to initialize device wakeup");
 
