00bd7976a401d27d7cb21f7fc99f3dcbbd839243
Stanimir Varbanov <svarbanov@suse.de>
2026-03-26 11:43:57 +0200

    KVM: arm64: nvhe: Disable branch generation in nVHE guests (jsc#PED-15733 jsc#PED-15734)
    suse-commit: 65e8c4e5a50fe6b7f23135b861c084558a7354cc

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -618,6 +618,7 @@ struct kvm_host_data {
 #define KVM_HOST_DATA_FLAG_HAS_TRBE			1
 #define KVM_HOST_DATA_FLAG_TRBE_ENABLED			4
 #define KVM_HOST_DATA_FLAG_EL1_TRACING_CONFIGURED	5
+#define KVM_HOST_DATA_FLAG_HAS_BRBE			8
 	unsigned long flags;
 
 	struct kvm_cpu_context host_ctxt;
@@ -651,6 +652,7 @@ struct kvm_host_data {
 		u64 trfcr_el1;
 		/* Values of trap registers for the host before guest entry. */
 		u64 mdcr_el2;
+		u64 brbcr_el1;
 	} host_debug_state;
 
 	/* Guest trace filter value */
