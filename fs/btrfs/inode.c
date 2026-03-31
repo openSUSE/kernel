225953b64f34d36bb4211215bdf03808a92c4a07
Filipe Manana <fdmanana@suse.com>
2026-03-31 16:45:17 +0100

    btrfs: fix reservation leak in some error paths when inserting inline extent (CVE-2025-71268 bsc#1259865).
    suse-commit: 9f5a3543aa1cebc6247afb21332597e7abedf409

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -312,7 +312,7 @@ static noinline int cow_file_range_inline(struct btrfs_root *root,
 					  struct page **compressed_pages)
 {
 	struct btrfs_fs_info *fs_info = root->fs_info;
-	struct btrfs_trans_handle *trans;
+	struct btrfs_trans_handle *trans = NULL;
 	u64 isize = i_size_read(inode);
 	u64 actual_end = min(end + 1, isize);
 	u64 inline_len = actual_end - start;
@@ -337,13 +337,16 @@ static noinline int cow_file_range_inline(struct btrfs_root *root,
 	}
 
 	path = btrfs_alloc_path();
-	if (!path)
-		return -ENOMEM;
+	if (!path) {
+		ret = -ENOMEM;
+		goto out;
+	}
 
 	trans = btrfs_join_transaction(root);
 	if (IS_ERR(trans)) {
-		btrfs_free_path(path);
-		return PTR_ERR(trans);
+		ret = PTR_ERR(trans);
+		trans = NULL;
+		goto out;
 	}
 	trans->block_rsv = &BTRFS_I(inode)->block_rsv;
 
@@ -391,7 +394,8 @@ out:
 	if (ret <= 0)
 		btrfs_qgroup_free_data(inode, NULL, 0, PAGE_SIZE);
 	btrfs_free_path(path);
-	btrfs_end_transaction(trans);
+	if (trans)
+		btrfs_end_transaction(trans);
 	return ret;
 }
 
