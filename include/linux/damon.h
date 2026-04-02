b9925e42ca8ee45498a66bf5c77b856c93e8dc1b
Robert Frohl <rfrohl@suse.com>
2026-04-02 14:08:32 +0200

    mm/damon/core: avoid use of half-online-committed context (bsc#1258210).
    suse-commit: b04353caa5d7d529eebe8b29058840dbc61f077a

--- include/linux/damon.h
+++ include/linux/damon.h
@@ -786,6 +786,12 @@ struct damon_ctx {
 	struct damos_walk_control *walk_control;
 	struct mutex walk_control_lock;
 
+	/*
+	 * indicate if this may be corrupted.  Currentonly this is set only for
+	 * damon_commit_ctx() failure.
+	 */
+	bool maybe_corrupted;
+
 /* public: */
 	struct task_struct *kdamond;
 	struct mutex kdamond_lock;
