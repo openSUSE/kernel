f49cd8bfb03cddbec9894dbd3afc9a6bde361cd3
Robert Frohl <rfrohl@suse.com>
2026-04-02 14:08:32 +0200

    dmaengine: idxd: Fix possible invalid memory access after FLR (bsc#1258210).
    suse-commit: 6b2b2e28aa25c5df46c0edf5ed7a05d362efa0b0

--- drivers/dma/idxd/init.c
+++ drivers/dma/idxd/init.c
@@ -1137,6 +1137,7 @@ static void idxd_reset_done(struct pci_dev *pdev)
 	}
 out:
 	kfree(idxd->idxd_saved);
+	idxd->idxd_saved = NULL;
 }
 
 static const struct pci_error_handlers idxd_error_handler = {
