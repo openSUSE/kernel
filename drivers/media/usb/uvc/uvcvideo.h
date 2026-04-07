9d48381326c7b21021dc58ff32e4c1c5773fe13e
Oliver Neukum <oneukum@suse.com>
2026-04-07 20:20:35 +0200

    media: uvcvideo: Drop the uvc_driver structure (jsc#PED-14445).
    suse-commit: 5068fd05037884c03b46cf6a5ee85fec60410886

--- drivers/media/usb/uvc/uvcvideo.h
+++ drivers/media/usb/uvc/uvcvideo.h
@@ -634,10 +634,6 @@ struct uvc_fh {
 	unsigned int pending_async_ctrls;
 };
 
-struct uvc_driver {
-	struct usb_driver driver;
-};
-
 /* ------------------------------------------------------------------------
  * Debugging, printing and logging
  */
@@ -687,9 +683,6 @@ do {									\
 /* --------------------------------------------------------------------------
  * Internal functions.
  */
-
-/* Core driver */
-extern struct uvc_driver uvc_driver;
 
 struct uvc_entity *uvc_entity_by_id(struct uvc_device *dev, int id);
 
