1b9c792b0e5a6df7112691e98e04729fe0d4ef0d
Patrik Jakobsson <pjakobsson@suse.de>
2026-04-07 10:58:24 +0200

    - Update patches.suse/drm-amd-display-Move-setup_stream_attribute.patch - Refresh patches.suse/drm-amd-display-avoid-dig-reg-access-timeout-on-usb4.patch
    suse-commit: 525c798a7a241f501ee474ac7e0b73009f549b4c

--- drivers/gpu/drm/amd/display/dc/hwss/dcn20/dcn20_hwseq.c
+++ drivers/gpu/drm/amd/display/dc/hwss/dcn20/dcn20_hwseq.c
@@ -3068,6 +3068,8 @@ void dcn20_enable_stream(struct pipe_ctx *pipe_ctx)
 		}
 	}
 
+	link_hwss->setup_stream_attribute(pipe_ctx);
+
 	if (dc->res_pool->dccg->funcs->set_pixel_rate_div)
 		dc->res_pool->dccg->funcs->set_pixel_rate_div(
 			dc->res_pool->dccg,
