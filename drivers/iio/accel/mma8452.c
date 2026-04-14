36b7f78366b102ee06f3edd875be82013adb8ee5
Hannes Reinecke <hare@suse.com>
2026-04-14 14:27:31 +0200

    iio: accel: mma8452: Switch to sparse friendly iio_device_claim/release_direct() (jsc#PED-15409).
    suse-commit: eb3888a1ce24037269b42303bd49d03a2d2c9e9c

--- drivers/iio/accel/mma8452.c
+++ drivers/iio/accel/mma8452.c
@@ -496,14 +496,13 @@ static int mma8452_read_raw(struct iio_dev *indio_dev,
 
 	switch (mask) {
 	case IIO_CHAN_INFO_RAW:
-		ret = iio_device_claim_direct_mode(indio_dev);
-		if (ret)
-			return ret;
+		if (!iio_device_claim_direct(indio_dev))
+			return -EBUSY;
 
 		mutex_lock(&data->lock);
 		ret = mma8452_read(data, buffer);
 		mutex_unlock(&data->lock);
-		iio_device_release_direct_mode(indio_dev);
+		iio_device_release_direct(indio_dev);
 		if (ret < 0)
 			return ret;
 
@@ -780,12 +779,11 @@ static int mma8452_write_raw(struct iio_dev *indio_dev,
 {
 	int ret;
 
-	ret = iio_device_claim_direct_mode(indio_dev);
-	if (ret)
-		return ret;
+	if (!iio_device_claim_direct(indio_dev))
+		return -EBUSY;
 
 	ret = __mma8452_write_raw(indio_dev, chan, val, val2, mask);
-	iio_device_release_direct_mode(indio_dev);
+	iio_device_release_direct(indio_dev);
 	return ret;
 }
 
