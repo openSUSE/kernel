c67253c8d8f2ad3eb952f502152b01399b91b499
Takashi Iwai <tiwai@suse.de>
2026-04-16 10:17:22 +0200

    drm/xe/uapi: update used tracking kernel-doc (git-fixes).
    suse-commit: b68aa14999cb5e72ea0d4f049a8342c02b1f4400

--- include/uapi/drm/xe_drm.h
+++ include/uapi/drm/xe_drm.h
@@ -323,10 +323,6 @@ struct drm_xe_mem_region {
 	__u64 total_size;
 	/**
 	 * @used: Estimate of the memory used in bytes for this region.
-	 *
-	 * Requires CAP_PERFMON or CAP_SYS_ADMIN to get reliable
-	 * accounting.  Without this the value here will always equal
-	 * zero.
 	 */
 	__u64 used;
 	/**
@@ -351,9 +347,7 @@ struct drm_xe_mem_region {
 	 * @cpu_visible_used: Estimate of CPU visible memory used, in
 	 * bytes.
 	 *
-	 * Requires CAP_PERFMON or CAP_SYS_ADMIN to get reliable
-	 * accounting. Without this the value here will always equal
-	 * zero.  Note this is only currently tracked for
+	 * Note this is only currently tracked for
 	 * DRM_XE_MEM_REGION_CLASS_VRAM regions (for other types the value
 	 * here will always be zero).
 	 */
