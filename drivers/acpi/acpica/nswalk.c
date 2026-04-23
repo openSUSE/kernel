bce6d661131bef7121fbebb9ef1c99c68544f705
Chun-Yi Lee <jlee@suse.com>
2026-04-23 19:19:01 +0800

    ACPICA: Avoid walking the Namespace if start_node is NULL (bsc#1256763 CVE-2025-71118).
    suse-commit: 353f157983c4d0e9c5b7133010530d325bc7e144

--- drivers/acpi/acpica/nswalk.c
+++ drivers/acpi/acpica/nswalk.c
@@ -171,6 +171,12 @@ acpi_ns_walk_namespace(acpi_object_type type,
 		start_node = acpi_gbl_root_node;
 	}
 
+	/* Avoid walking the namespace if the StartNode is NULL */
+
+	if (!start_node) {
+		return_ACPI_STATUS(AE_NO_NAMESPACE);
+	}
+
 	/* Null child means "get first node" */
 
 	parent_node = start_node;
