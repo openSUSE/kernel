70d8797c15d640982365e96e34e93a3aa38e82da
SeongJae Park <sj@kernel.org>
2026-04-28 21:12:23 -0700

    mm/damon: introduce damon_set_region_system_rams_default()
    Patch series "mm/damon/reclaim,lru_sort: monitor all system rams by
    default".
    
    DAMON_RECLAIM and DAMON_LRU_SORT set the biggest 'System RAM' resource of
    the system as the default monitoring target address range.  The main
    intention behind the design is to minimize the overhead coming from
    monitoring of non-System RAM areas.
    
    This could result in an odd setup when there are multiple discrete System
    RAMs of considerable sizes.  For example, there are System RAMs each
    having 500 GiB size.  In this case, only the first 500 GiB will be set as
    the monitoring region by default.  This is particularly common on NUMA
    systems.  Hence the modules allow users to set the monitoring target
    address range using the module parameters if the default setup doesn't
    work for them.  In other words, the current design trades ease of setup
    for lower overhead.
    
    However, because DAMON utilizes the sampling based access check and the
    adaptive regions adjustment mechanisms, the overhead from the monitoring
    of non-System RAM areas should be negligible in most setups.  Meanwhile,
    the setup complexity is causing real headaches for users who need to run
    those modules on various types of systems.  That is, the current tradeoff
    is not a good deal.
    
    Set the physical address range that can cover all System RAM areas of the
    system as the default monitoring regions for DAMON_RECLAIM and
    DAMON_LRU_SORT.
    
    Technically speaking, this is changing documented behavior.  However, it
    makes no sense to believe there is a real use case that really depends on
    the old weird default behavior.  If the old default behavior was working
    for them in the reasonable way, this change will only add a negligible
    amount of monitoring overhead.  If it didn't work, the users may already
    be using manual monitoring regions setup, and they will not be affected by
    this change.
    
    Patches Sequence
    ================
    
    Patch 1 introduces a new core function that will be used for the new
    default monitoring target region setup.  Patch 2 and 3 update
    DAMON_RECLAIM and DAMON_LRU_SORT to use the new function instead of the
    old one, respectively.  Patch 4 removes the old core function that was
    replaced by the new one, as there is no more user of it.  Patch 5 updates
    DAMON_STAT to use the new one instead of its in-house nearly-duplicate
    self implementation of the functionality.  Finally patches 6 and 7 update
    the DAMON_RECLAIM and DAMON_LRU_SORT user documentation for the new
    behaviors, respectively.
    
    
    This patch (of 7):
    
    damon_set_region_biggest_system_ram_default() sets the monitoring target
    region as the caller requested.  If the caller didn't specify the region,
    it finds the biggest System RAM of the system and sets it as the target
    region.  When there are more than one considerable size of System RAM
    resources in the system, the default target setup makes no sense. 
    Introduce a variant, namely damon_set_region_system_rams_default().  It
    sets a physical address range that covers all System RAM resources as the
    default target region.
    
    Link: https://lore.kernel.org/20260429041232.90257-1-sj@kernel.org
    Link: https://lore.kernel.org/20260429041232.90257-2-sj@kernel.org
    Signed-off-by: SeongJae Park <sj@kernel.org>
    Cc: David Hildenbrand <david@kernel.org>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Liam R. Howlett <liam@infradead.org>
    Cc: Lorenzo Stoakes <ljs@kernel.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/damon/core.c
+++ mm/damon/core.c
@@ -3328,14 +3328,20 @@ done:
 	return 0;
 }
 
-static int walk_system_ram(struct resource *res, void *arg)
-{
-	struct resource *a = arg;
+struct damon_system_ram_range_walk_arg {
+	bool walked;
+	struct resource res;
+};
 
-	if (resource_size(a) < resource_size(res)) {
-		a->start = res->start;
-		a->end = res->end;
+static int damon_system_ram_walk_fn(struct resource *res, void *arg)
+{
+	struct damon_system_ram_range_walk_arg *a = arg;
+
+	if (!a->walked) {
+		a->walked = true;
+		a->res.start = res->start;
 	}
+	a->res.end = res->end;
 	return 0;
 }
 
@@ -3350,6 +3356,67 @@ static unsigned long damon_res_to_core_addr(resource_size_t ra,
 	if (sizeof(ra) == 8 && sizeof(addr_unit) == 4)
 		return div_u64(ra, addr_unit);
 	return ra / addr_unit;
+}
+
+static bool damon_find_system_rams_range(unsigned long *start,
+		unsigned long *end, unsigned long addr_unit)
+{
+	struct damon_system_ram_range_walk_arg arg = {};
+
+	walk_system_ram_res(0, -1, &arg, damon_system_ram_walk_fn);
+	if (!arg.walked)
+		return false;
+	*start = damon_res_to_core_addr(arg.res.start, addr_unit);
+	*end = damon_res_to_core_addr(arg.res.end + 1, addr_unit);
+	if (*end <= *start)
+		return false;
+	return true;
+}
+
+/**
+ * damon_set_region_system_rams_default() - Set the region of the given
+ * monitoring target as requested, or to cover all 'System RAM' resources.
+ * @t:		The monitoring target to set the region.
+ * @start:	The pointer to the start address of the region.
+ * @end:	The pointer to the end address of the region.
+ * @addr_unit:	The address unit for the damon_ctx of @t.
+ * @min_region_sz:	Minimum region size.
+ *
+ * This function sets the region of @t as requested by @start and @end.  If the
+ * values of @start and @end are zero, however, this function finds 'System
+ * RAM' resources and sets the region to cover all the resource.  In the latter
+ * case, this function saves the start and the end addresseses of the first and
+ * the last resources in @start and @end, respectively.
+ *
+ * Return: 0 on success, negative error code otherwise.
+ */
+int damon_set_region_system_rams_default(struct damon_target *t,
+			unsigned long *start, unsigned long *end,
+			unsigned long addr_unit, unsigned long min_region_sz)
+{
+	struct damon_addr_range addr_range;
+
+	if (*start > *end)
+		return -EINVAL;
+
+	if (!*start && !*end &&
+		!damon_find_system_rams_range(start, end, addr_unit))
+		return -EINVAL;
+
+	addr_range.start = *start;
+	addr_range.end = *end;
+	return damon_set_regions(t, &addr_range, 1, min_region_sz);
+}
+
+static int walk_system_ram(struct resource *res, void *arg)
+{
+	struct resource *a = arg;
+
+	if (resource_size(a) < resource_size(res)) {
+		a->start = res->start;
+		a->end = res->end;
+	}
+	return 0;
 }
 
 /*
