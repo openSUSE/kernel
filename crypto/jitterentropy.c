650f09718adc327c7dc50f6347580079fdb361f6
Thorsten Blum <thorsten.blum@linux.dev>
2026-05-04 10:28:50 +0200

    crypto: jitterentropy - drop redundant delta check in jent_entropy_init
    Since start_time = end_time - delta, start_time can only equal end_time
    when delta is 0, making the explicit end_time == start_time check
    redundant. Remove it.
    
    Signed-off-by: Thorsten Blum <thorsten.blum@linux.dev>
    Reviewed-by: Stephan Mueller <smueller@chronox.de>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/jitterentropy.c
+++ crypto/jitterentropy.c
@@ -775,7 +775,7 @@ int jent_entropy_init(unsigned int osr, unsigned int flags,
 		 * delta even when called shortly after each other -- this
 		 * implies that we also have a high resolution timer
 		 */
-		if (!delta || (end_time == start_time)) {
+		if (!delta) {
 			ret = JENT_ECOARSETIME;
 			goto out;
 		}
