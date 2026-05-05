81d7e2979e371e5f359a544b53f38601f5150470
Krzysztof Kozlowski <krzysztof.kozlowski@oss.qualcomm.com>
2026-05-05 12:29:49 +0200

    crypto: drivers - Move MODULE_DEVICE_TABLE next to the table itself
    By convention MODULE_DEVICE_TABLE() immediately follows the ID table it
    exports, because this is easier to read and verify.  It also makes more
    sense since #ifdef for ACPI or OF could hide both of them.
    
    Most of the privers already have this correctly placed, so adjust
    the missing ones.  No functional impact.
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@oss.qualcomm.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/marvell/octeontx/otx_cptvf_main.c
+++ drivers/crypto/marvell/octeontx/otx_cptvf_main.c
@@ -960,6 +960,7 @@ static const struct pci_device_id otx_cptvf_id_table[] = {
 	{ PCI_VDEVICE(CAVIUM, OTX_CPT_PCI_VF_DEVICE_ID) },
 	{ }  /* end of table */
 };
+MODULE_DEVICE_TABLE(pci, otx_cptvf_id_table);
 
 static struct pci_driver otx_cptvf_pci_driver = {
 	.name = DRV_NAME,
@@ -974,4 +975,3 @@ MODULE_AUTHOR("Marvell International Ltd.");
 MODULE_DESCRIPTION("Marvell OcteonTX CPT Virtual Function Driver");
 MODULE_LICENSE("GPL v2");
 MODULE_VERSION(DRV_VERSION);
-MODULE_DEVICE_TABLE(pci, otx_cptvf_id_table);
