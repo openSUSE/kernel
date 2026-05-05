43401898fde586bdeb73ac2c11d4f949fcba9f77
Krzysztof Kozlowski <krzysztof.kozlowski@oss.qualcomm.com>
2026-05-05 12:27:52 +0200

    power: supply: cros_charge-control: Move MODULE_DEVICE_TABLE next to the table itself
    By convention MODULE_DEVICE_TABLE() immediately follows the ID table it
    exports, because this is easier to read and verify.  It also makes more
    sense since #ifdef for ACPI or OF could hide both of them.
    
    Most of the privers already have this correctly placed, so adjust
    the missing ones.  No functional impact.
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@oss.qualcomm.com>
    Acked-by: Thomas Weißschuh <linux@weissschuh.net>
    Reviewed-by: Tzung-Bi Shih <tzungbi@kernel.org>
    Link: https://patch.msgid.link/20260505102752.182089-2-krzysztof.kozlowski@oss.qualcomm.com
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>

--- drivers/power/supply/cros_charge-control.c
+++ drivers/power/supply/cros_charge-control.c
@@ -323,6 +323,7 @@ static const struct platform_device_id cros_chctl_id[] = {
 	{ .name = "cros-charge-control" },
 	{ }
 };
+MODULE_DEVICE_TABLE(platform, cros_chctl_id);
 
 static struct platform_driver cros_chctl_driver = {
 	.driver.name	= "cros-charge-control",
@@ -331,7 +332,6 @@ static struct platform_driver cros_chctl_driver = {
 };
 module_platform_driver(cros_chctl_driver);
 
-MODULE_DEVICE_TABLE(platform, cros_chctl_id);
 MODULE_DESCRIPTION("ChromeOS EC charge control");
 MODULE_AUTHOR("Thomas Weißschuh <linux@weissschuh.net>");
 MODULE_LICENSE("GPL");
