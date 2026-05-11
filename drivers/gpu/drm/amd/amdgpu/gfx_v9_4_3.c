ROCm/amdgpu

drm/amdgpu: fix ptl state isssue after GPU reset or suspend
Fix this by skipping the sysfs disable mapping when the GPU is
currently undergoing a reset or suspend flow.
Additionally, add debug logging in psp_ptl_invoke() to better
trace PTL state and format queries/updates cmd.

Signed-off-by: Perry Yuan <perry.yuan@amd.com>
Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>

drivers/gpu/drm/amd/amdgpu/gfx_v9_4_3.c
@@ -2430,7 +2430,7 @@ static int gfx_v9_4_3_perf_monitor_ptl_init(struct amdgpu_device *adev, bool sta
 	adev->psp.ptl_hw_supported_state = AMDGPU_PTL_HW_SUPPORTED;
 
 	atomic_set(&adev->psp.ptl_disable_ref, 0);
-	if (!state) {
+	if (!state && !amdgpu_in_reset(adev) && !adev->in_suspend) {
 		dev_dbg(adev->dev,
 			"PTL disabled (amdgpu.ptl=%d)\
 			To enable, set amdgpu.ptl=1 via module param or kernel cmdline\n",

openSUSE/kernel

Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

drivers/gpu/drm/amd/amdgpu/gfx_v9_4_3.c
@@ -2401,7 +2401,7 @@ static int gfx_v9_4_3_perf_monitor_ptl_init(struct amdgpu_device *adev, bool ena
 	ptl->hw_supported = true;
 
 	atomic_set(&ptl->disable_ref, 0);
-	if (!enable) {
+	if (!enable && !amdgpu_in_reset(adev) && !adev->in_suspend) {
 		dev_dbg(adev->dev,
 			"PTL disabled (amdgpu.ptl=%d)\
 			To enable, set amdgpu.ptl=1 via module param or kernel cmdline\n",