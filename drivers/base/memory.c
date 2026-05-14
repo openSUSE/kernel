a2b8d7827f48ee54a686cb80e4a1d0ff954ec42a
Georgi Djakov <georgi.djakov@oss.qualcomm.com>
2026-05-14 02:26:57 -0700

    drivers/base/memory: set mem->altmap after successful device registration
    If __add_memory_block() fails at xa_store() (under memory pressure for
    example), device_unregister() is called, which eventually triggers
    memory_block_release() with mem->altmap still set, causing a
    WARN_ON(mem->altmap).  This was triggered by modifying virtio-mem driver.
    
    Fix this by delaying the assignment of mem->altmap until after
    __add_memory_block() has succeeded.
    
    Link: https://lore.kernel.org/20260514092657.3057141-1-georgi.djakov@oss.qualcomm.com
    Fixes: 1a8c64e11043 ("mm/memory_hotplug: embed vmem_altmap details in memory block")
    Signed-off-by: Georgi Djakov <georgi.djakov@oss.qualcomm.com>
    Acked-by: Oscar Salvador (SUSE) <osalvador@kernel.org>
    Cc: Vishal Verma <vishal.l.verma@intel.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Richard Cheng <icheng@nvidia.com>
    Cc: David Hildenbrand <david@kernel.org>
    Cc: Georgi Djakov <djakov@kernel.org>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: "Rafael J. Wysocki" <rafael@kernel.org>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- drivers/base/memory.c
+++ drivers/base/memory.c
@@ -797,7 +797,6 @@ static int add_memory_block(unsigned long block_id, int nid, unsigned long state
 	mem->start_section_nr = block_id * sections_per_block;
 	mem->state = state;
 	mem->nid = nid;
-	mem->altmap = altmap;
 	INIT_LIST_HEAD(&mem->group_next);
 
 #ifndef CONFIG_NUMA
@@ -814,6 +813,8 @@ static int add_memory_block(unsigned long block_id, int nid, unsigned long state
 	ret = __add_memory_block(mem);
 	if (ret)
 		return ret;
+
+	mem->altmap = altmap;
 
 	if (group) {
 		mem->group = group;
