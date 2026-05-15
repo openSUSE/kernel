5d7f0a1c4b8f35989469c0437e4f3ca3f8405361
Michal Hocko <mhocko@suse.com>
2026-05-15 15:57:15 +0200

    ptrace: slightly saner 'get_dumpable()' logic (bsc#1265308).
    suse-commit: ce6f4163e235f6efcd5a7534e0f29d7b8af22c46

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -619,6 +619,9 @@ struct task_struct {
 
 	/* Unserialized, strictly 'current' */
 
+	/* Save user-dumpable when mm goes away */
+	unsigned			user_dumpable:1;
+
 	/* Bit to tell LSMs we're in execve(): */
 	unsigned			in_execve:1;
 	unsigned			in_iowait:1;
