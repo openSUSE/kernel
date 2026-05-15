e2e664812f7558e5a999271db7d17a391862d701
Chang S. Bae <chang.seok.bae@intel.com>
2026-05-15 13:27:34 -0400

    KVM: SEV: Macrofy GPR swapping in __svm_sev_es_vcpu_run()
    Convert the SEV-ES entry code to use macros for saving guest GPRs,
    following VMX/SVM paths. Drop now-unused register offsets and
    __VCPU_REGS_* defines.
    
    No functional change intended.
    
    Signed-off-by: Chang S. Bae <chang.seok.bae@intel.com>
    Link: https://patch.msgid.link/20260512011502.53072-4-chang.seok.bae@intel.com/
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/include/asm/kvm_vcpu_regs.h
+++ arch/x86/include/asm/kvm_vcpu_regs.h
@@ -11,17 +11,6 @@
 #define __VCPU_REGS_RSI  6
 #define __VCPU_REGS_RDI  7
 
-#ifdef CONFIG_X86_64
-#define __VCPU_REGS_R8   8
-#define __VCPU_REGS_R9   9
-#define __VCPU_REGS_R10 10
-#define __VCPU_REGS_R11 11
-#define __VCPU_REGS_R12 12
-#define __VCPU_REGS_R13 13
-#define __VCPU_REGS_R14 14
-#define __VCPU_REGS_R15 15
-#endif
-
 #ifdef __ASSEMBLER__
 
 #define REG_NUM_INVALID		100
