d157c79643d98f6d42bc19395dba19b5fc8581b6
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2026-05-17 10:59:47 +0300

    wifi: iwlwifi: bump maximum core version for BZ/SC/DR to 105
    Start supporting Core 105 FW on these devices.
    
    Link: https://patch.msgid.link/20260517100550.8674feeafcad.I3d3ae3a7acb976a947cd7e65a8d7fb8199d2e1ab@changeid
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>

--- drivers/net/wireless/intel/iwlwifi/cfg/bz.c
+++ drivers/net/wireless/intel/iwlwifi/cfg/bz.c
@@ -10,7 +10,7 @@
 #include "fw/api/txq.h"
 
 /* Highest firmware core release supported */
-#define IWL_BZ_UCODE_CORE_MAX	104
+#define IWL_BZ_UCODE_CORE_MAX	105
 
 /* Lowest firmware core release supported */
 #define IWL_BZ_UCODE_CORE_MIN	101
