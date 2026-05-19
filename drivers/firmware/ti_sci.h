703b341fdd875c2d9c30a835021689e5a430a39a
Thomas Richard (TI) <thomas.richard@bootlin.com>
2026-05-19 17:06:57 +0200

    firmware: ti_sci: Add support for restoring IRQs during resume
    Some DM-Firmware are not able to restore the IRQ context after
    a suspend-resume. The IRQ_CONTEXT_LOST firmware capability has
    been introduced to identify this characteristic. In this case the
    responsibility is delegated to the ti_sci driver, which maintains
    an internal list of all requested IRQs. This list is updated on
    each set()/free() operation, and all IRQs are restored during the
    resume_noirq() phase.
    
    Reviewed-by: Dhruva Gole <d-gole@ti.com>
    Reviewed-by: Kendall Willis <k-willis@ti.com>
    Signed-off-by: Thomas Richard (TI) <thomas.richard@bootlin.com>
    Link: https://patch.msgid.link/20260519-ti-sci-jacinto-s2r-restore-irq-v9-2-c550a8ae0f31@bootlin.com
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- drivers/firmware/ti_sci.h
+++ drivers/firmware/ti_sci.h
@@ -152,6 +152,8 @@ struct ti_sci_msg_req_reboot {
  *		MSG_FLAG_CAPS_IO_ISOLATION: IO Isolation support
  *		MSG_FLAG_CAPS_LPM_BOARDCFG_MANAGED: LPM config done statically
  *			for the DM via boardcfg
+ *		MSG_FLAG_CAPS_LPM_IRQ_CONTEXT_LOST: DM is not able to restore IRQ
+ *			context
  *
  * Response to a generic message with message type TI_SCI_MSG_QUERY_FW_CAPS
  * providing currently available SOC/firmware capabilities. SoC that don't
@@ -165,6 +167,7 @@ struct ti_sci_msg_resp_query_fw_caps {
 #define MSG_FLAG_CAPS_LPM_ABORT		TI_SCI_MSG_FLAG(9)
 #define MSG_FLAG_CAPS_IO_ISOLATION	TI_SCI_MSG_FLAG(7)
 #define MSG_FLAG_CAPS_LPM_BOARDCFG_MANAGED	TI_SCI_MSG_FLAG(12)
+#define MSG_FLAG_CAPS_LPM_IRQ_CONTEXT_LOST	TI_SCI_MSG_FLAG(14)
 #define MSG_MASK_CAPS_LPM		GENMASK_ULL(4, 1)
 	u64 fw_caps;
 } __packed;
