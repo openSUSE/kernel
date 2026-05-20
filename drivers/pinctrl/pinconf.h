88f643ec105ea0d2872b2c34b23503ad6076518c
Arnd Bergmann <arnd@arndb.de>
2026-05-20 09:51:14 +0200

    pinctrl: avoid duplicate function definitions
    The pinctrl_generic_to_map() and pinctrl_generic_pins_function_dt_node_to_map()
    functions are built whenever CONFIG_GENERIC_PINCTRL is enabled, including
    configurations without CONFIG_OF.
    
    When CONFIG_OF is disabled, the dummy definitions are also present in the header,
    which causes the build to fail:
    
    drivers/pinctrl/pinctrl-generic.c:20:5: error: conflicting types for 'pinctrl_generic_to_map'; have 'int(struct pinctrl_dev *, struct device_node *, struct device_node *, struct pinctrl_map **, unsigned int *, unsigned int *, const char **, unsigned int,  const char **, unsigned int *, unsigned int)'
       20 | int pinctrl_generic_to_map(struct pinctrl_dev *pctldev, struct device_node *parent,
          |     ^~~~~~~~~~~~~~~~~~~~~~
    In file included from drivers/pinctrl/pinctrl-generic.c:16:
    drivers/pinctrl/pinconf.h:193:1: note: previous definition of 'pinctrl_generic_to_map' with type 'int(struct pinctrl_dev *, struct device_node *, struct device_node *, struct pinctrl_map **, unsigned int *, unsigned int *, const char **, unsigned int,  const char **, unsigned int *, void *)'
      193 | pinctrl_generic_to_map(struct pinctrl_dev *pctldev, struct device_node *parent,
          | ^~~~~~~~~~~~~~~~~~~~~~
    drivers/pinctrl/pinctrl-generic.c:130:5: error: redefinition of 'pinctrl_generic_pins_function_dt_node_to_map'
      130 | int pinctrl_generic_pins_function_dt_node_to_map(struct pinctrl_dev *pctldev,
          |     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    drivers/pinctrl/pinconf.h:184:1: note: previous definition of 'pinctrl_generic_pins_function_dt_node_to_map' with type 'int(struct pinctrl_dev *, struct device_node *, struct pinctrl_map **, unsigned int *)'
      184 | pinctrl_generic_pins_function_dt_node_to_map(struct pinctrl_dev *pctldev,
          | ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Removing either set of definitions is sufficient to avoid the problem.
    
    Remove the copy from the header for simplicity.
    
    Fixes: aaaf31be0426 ("pinctrl: extract pinctrl_generic_to_map() from pinctrl_generic_pins_function_dt_node_to_map()")
    Fixes: 43722575e5cd ("pinctrl: add generic functions + pins mapper")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Conor Dooley <conor.dooley@microchip.com>
    Signed-off-by: Linus Walleij <linusw@kernel.org>

--- drivers/pinctrl/pinconf.h
+++ drivers/pinctrl/pinconf.h
@@ -167,7 +167,7 @@ pinconf_generic_parse_dt_pinmux(struct device_node *np, struct device *dev,
 }
 #endif
 
-#if defined(CONFIG_GENERIC_PINCTRL) && defined (CONFIG_OF)
+#if defined(CONFIG_GENERIC_PINCTRL)
 int pinctrl_generic_pins_function_dt_node_to_map(struct pinctrl_dev *pctldev,
 						 struct device_node *np,
 						 struct pinctrl_map **maps,
