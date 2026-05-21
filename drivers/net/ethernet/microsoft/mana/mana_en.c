61a49331f215a796ed05081dd1a2934ffbbdb40a
Olaf Hering <ohering@suse.de>
2026-05-21 09:09:15 +0000

    net: mana: check xdp_rxq registration before unreg in mana_destroy_rxq() (git-fixes).
    suse-commit: f84329b37751e595943aa2c939d8f8e01ae85af2

--- drivers/net/ethernet/microsoft/mana/mana_en.c
+++ drivers/net/ethernet/microsoft/mana/mana_en.c
@@ -2333,7 +2333,9 @@ static void mana_destroy_rxq(struct mana_port_context *apc,
 
 		netif_napi_del(napi);
 	}
-	xdp_rxq_info_unreg(&rxq->xdp_rxq);
+
+	if (xdp_rxq_info_is_reg(&rxq->xdp_rxq))
+		xdp_rxq_info_unreg(&rxq->xdp_rxq);
 
 	mana_destroy_wq_obj(apc, GDMA_RQ, rxq->rxobj);
 
