77e76eba2666ff87f5997577691cb5765f00240b
Pedro Falcato <pfalcato@suse.de>
2026-05-25 13:40:24 +0100

    net: skbuff: preserve shared-frag marker during coalescing (CVE-2026-46300 bsc#1265209).
    suse-commit: 29c46fe903203342ccf86009d9757840166bf90c

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -4599,6 +4599,8 @@ bool skb_try_coalesce(struct sk_buff *to, struct sk_buff *from,
 	       skb_shinfo(from)->frags,
 	       skb_shinfo(from)->nr_frags * sizeof(skb_frag_t));
 	skb_shinfo(to)->nr_frags += skb_shinfo(from)->nr_frags;
+	if (skb_shinfo(from)->nr_frags)
+		skb_shinfo(to)->tx_flags |= skb_shinfo(to)->tx_flags & SKBTX_SHARED_FRAG;
 
 	if (!skb_cloned(from))
 		skb_shinfo(from)->nr_frags = 0;
