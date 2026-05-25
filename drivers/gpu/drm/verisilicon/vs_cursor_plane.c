e07761b9364f66736de06e159dd88173d175df92
Icenowy Zheng <zhengxingda@iscas.ac.cn>
2026-05-25 23:36:18 +0800

    drm: verisilicon: fix build failure of cursor plane code
    The cursor plane patch was stalled for a too long time that the
    struct drm_atomic_state parameter of atomic modeset hooks has been
    changed to struct drm_atomic_commit.
    
    Fix this by replacing the parameter's type. All helpers that retrieve
    information from this struct are also changed so simply replacing the
    type works.
    
    Fixes: 8c4ae2189125 ("drm: verisilicon: add support for cursor planes")
    Signed-off-by: Icenowy Zheng <zhengxingda@iscas.ac.cn>
    Reviewed-by: Thomas Zimmermann <tzimmermann@suse.de>
    Link: https://patch.msgid.link/20260525153618.1336239-1-zhengxingda@iscas.ac.cn

--- drivers/gpu/drm/verisilicon/vs_cursor_plane.c
+++ drivers/gpu/drm/verisilicon/vs_cursor_plane.c
@@ -40,7 +40,7 @@ static bool vs_cursor_plane_check_coord(int32_t coord)
 }
 
 static int vs_cursor_plane_atomic_check(struct drm_plane *plane,
-					struct drm_atomic_state *state)
+					struct drm_atomic_commit *state)
 {
 	struct drm_plane_state *new_plane_state = drm_atomic_get_new_plane_state(state,
 										 plane);
@@ -98,7 +98,7 @@ static void vs_cursor_plane_commit(struct vs_dc *dc, unsigned int output)
 }
 
 static void vs_cursor_plane_atomic_enable(struct drm_plane *plane,
-					   struct drm_atomic_state *atomic_state)
+					   struct drm_atomic_commit *atomic_state)
 {
 	struct drm_plane_state *state = drm_atomic_get_new_plane_state(atomic_state,
 								       plane);
@@ -115,7 +115,7 @@ static void vs_cursor_plane_atomic_enable(struct drm_plane *plane,
 }
 
 static void vs_cursor_plane_atomic_disable(struct drm_plane *plane,
-					    struct drm_atomic_state *atomic_state)
+					    struct drm_atomic_commit *atomic_state)
 {
 	struct drm_plane_state *state = drm_atomic_get_old_plane_state(atomic_state,
 								       plane);
@@ -132,7 +132,7 @@ static void vs_cursor_plane_atomic_disable(struct drm_plane *plane,
 }
 
 static void vs_cursor_plane_atomic_update(struct drm_plane *plane,
-					   struct drm_atomic_state *atomic_state)
+					   struct drm_atomic_commit *atomic_state)
 {
 	struct drm_plane_state *state = drm_atomic_get_new_plane_state(atomic_state,
 								       plane);
