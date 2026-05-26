fae5e96bb646e7a4f588973cd94f6b1947377d58
Thomas Lin <thomas_lin@lecomputing.com>
2026-05-26 16:28:43 +0800

    i2c: designware: Add ACPI ID LECA0003 for LECARC SoCs
    Add ACPI ID "LECA0003" for LECARC SoCs that integrate
    the DesignWare I2C controller.
    Also add corresponding ACPI description in acpi_apd.c.
    
    Signed-off-by: Thomas Lin <thomas_lin@lecomputing.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Acked-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Signed-off-by: Andi Shyti <andi.shyti@kernel.org>
    Link: https://lore.kernel.org/r/20260526-lecarc-i2c-acpi-id-v1-1-f0942bd491d2@lecomputing.com

--- drivers/i2c/busses/i2c-designware-platdrv.c
+++ drivers/i2c/busses/i2c-designware-platdrv.c
@@ -279,6 +279,7 @@ static const struct acpi_device_id dw_i2c_acpi_match[] = {
 	{ "INT3432", 0 },
 	{ "INT3433", 0 },
 	{ "INTC10EF", 0 },
+	{ "LECA0003", 0 },
 	{}
 };
 MODULE_DEVICE_TABLE(acpi, dw_i2c_acpi_match);
