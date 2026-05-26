495bc70aa32258c630c4f521ceadf30753f2a00b
Heiko Carstens <hca@linux.ibm.com>
2026-05-26 15:09:52 +0200

    s390/fpu: Move GR_NUM / VX_NUM macros to separate header file
    Move GR_NUM / VX_NUM macros to separate insn-common-asm.h header file
    so they can be reused for non-fpu insn constructs.
    
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>
    Reviewed-by: Steffen Eiden <seiden@linux.ibm.com>
    Signed-off-by: Alexander Gordeev <agordeev@linux.ibm.com>

--- arch/s390/include/asm/fpu-insn-asm.h
+++ arch/s390/include/asm/fpu-insn-asm.h
@@ -16,47 +16,9 @@
 #error only <asm/fpu-insn.h> can be included directly
 #endif
 
+#include <asm/insn-common-asm.h>
+
 #ifdef __ASSEMBLER__
-
-/* Macros to generate vector instruction byte code */
-
-/* GR_NUM - Retrieve general-purpose register number
- *
- * @opd:	Operand to store register number
- * @r64:	String designation register in the format "%rN"
- */
-.macro	GR_NUM	opd gr
-	\opd = 255
-	.irp rs,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
-	.ifc \gr,%r\rs
-		\opd = \rs
-	.endif
-	.endr
-	.if \opd == 255
-		\opd = \gr
-	.endif
-.endm
-
-/* VX_NUM - Retrieve vector register number
- *
- * @opd:	Operand to store register number
- * @vxr:	String designation register in the format "%vN"
- *
- * The vector register number is used for as input number to the
- * instruction and, as well as, to compute the RXB field of the
- * instruction.
- */
-.macro	VX_NUM	opd vxr
-	\opd = 255
-	.irp vs,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31
-	.ifc \vxr,%v\vs
-		\opd = \vs
-	.endif
-	.endr
-	.if \opd == 255
-		\opd = \vxr
-	.endif
-.endm
 
 /* RXB - Compute most significant bit used vector registers
  *
