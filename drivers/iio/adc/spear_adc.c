2fab7499006ddc87c8937b149b769b8eb4a65217
Rodrigo Gobbi <rodrigo.gobbi.7@gmail.com>
2026-05-26 22:36:39 -0300

    iio: adc: spear_adc: sort includes alphabetically
    Sort includes alphabetically, no functional change
    
    Signed-off-by: Rodrigo Gobbi <rodrigo.gobbi.7@gmail.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/adc/spear_adc.c
+++ drivers/iio/adc/spear_adc.c
@@ -5,19 +5,19 @@
  * Copyright 2012 Stefan Roese <sr@denx.de>
  */
 
+#include <linux/bitfield.h>
+#include <linux/clk.h>
+#include <linux/completion.h>
+#include <linux/device.h>
+#include <linux/err.h>
+#include <linux/interrupt.h>
+#include <linux/io.h>
+#include <linux/kernel.h>
 #include <linux/mod_devicetable.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/property.h>
-#include <linux/interrupt.h>
-#include <linux/device.h>
-#include <linux/kernel.h>
 #include <linux/slab.h>
-#include <linux/io.h>
-#include <linux/bitfield.h>
-#include <linux/clk.h>
-#include <linux/err.h>
-#include <linux/completion.h>
 
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
