6ce2d03bb87cf6fbd31573d0dc92f8482f03ed39
Pawel Chmielewski <pawel.chmielewski@intel.com>
2026-05-27 20:08:02 +0200

    ACPICA: Update the copyright year to 2026
    Update copyright notices in all ACPICA files.
    
    Link: https://github.com/acpica/acpica/commit/9def02549a9c
    Signed-off-by: Pawel Chmielewski <pawel.chmielewski@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Link: https://patch.msgid.link/4379132.1IzOArtZ34@rafael.j.wysocki

--- include/acpi/acrestyp.h
+++ include/acpi/acrestyp.h
@@ -3,7 +3,7 @@
  *
  * Name: acrestyp.h - Defines, types, and structures for resource descriptors
  *
- * Copyright (C) 2000 - 2025, Intel Corp.
+ * Copyright (C) 2000 - 2026, Intel Corp.
  *
  *****************************************************************************/
 
