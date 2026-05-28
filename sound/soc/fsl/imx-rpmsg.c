ec74d4f934a63f8146f704eb0ed7b3af2b819a8d
Chancel Liu <chancel.liu@nxp.com>
2026-05-28 11:07:24 +0900

    ASoC: imx-rpmsg: Support headphone jack detection
    Add headphone jack detection support for i.MX RPMSG audio cards.
    When the "hp-det-gpios" property is present in the device tree node,
    use simple_util_init_jack() from the ASoC simple card utilities to
    register a headphone jack with GPIO-based insertion detection.
    
    Signed-off-by: Chancel Liu <chancel.liu@nxp.com>
    Link: https://patch.msgid.link/20260528020725.2265321-3-chancel.liu@nxp.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/fsl/imx-rpmsg.c
+++ sound/soc/fsl/imx-rpmsg.c
@@ -12,6 +12,7 @@
 #include <sound/control.h>
 #include <sound/pcm_params.h>
 #include <sound/soc-dapm.h>
+#include <sound/simple_card_utils.h>
 #include "imx-pcm-rpmsg.h"
 
 struct imx_rpmsg {
@@ -19,6 +20,7 @@ struct imx_rpmsg {
 	struct snd_soc_card card;
 	unsigned long sysclk;
 	bool lpa;
+	struct simple_util_jack hp_jack;
 };
 
 static struct dev_pm_ops lpa_pm;
@@ -85,6 +87,15 @@ static int imx_rpmsg_late_probe(struct snd_soc_card *card)
 	struct snd_soc_dai *codec_dai = snd_soc_rtd_to_codec(rtd, 0);
 	struct device *dev = card->dev;
 	int ret;
+
+	if (of_property_present(card->dev->of_node, "hp-det-gpios")) {
+		ret = simple_util_init_jack(card, &data->hp_jack,
+					    1, NULL, "Headphone Jack");
+		if (ret) {
+			dev_err(dev, "failed to init hp jack\n");
+			return ret;
+		}
+	}
 
 	if (data->lpa) {
 		struct device_node *codec_np;
