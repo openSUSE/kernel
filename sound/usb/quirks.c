3207ed0f704987d412ac6a7fb32a9ed65b995882
Rong Zhang <i@rong.moe>
2026-05-31 23:45:21 +0800

    ALSA: usb-audio: Add quirk flag for Sennheiser MOMENTUM 3
    The Sennheiser MOMENTUM 3 is a wireless around-ear headphones featuring
    ANC, which can be connected via Bluetooth or USB-C.
    
    When connecting via USB-C, its UAC mixer works fine and precisely
    corresponds to the reported dB range. However, the mixer's volume
    GET_CUR method is somehow stubbed and returns a constant value (15dB).
    Since commit 86aa1ea1f15c ("ALSA: usb-audio: Do not expose sticky
    mixers"), the sticky check considers the mixer to be sticky and
    unnecessarily disables the mixer.
    
    Add a quirk table entry matching VID/PID=0x1377/0x6004 and applying
    the MIXER_GET_CUR_BROKEN quirk flag, so that the mixer is usable again.
    
    Quirky device sample:
    
      usb 7-1.4.4.1.1.1: new full-speed USB device number 30 using xhci_hcd
      usb 7-1.4.4.1.1.1: New USB device found, idVendor=1377, idProduct=6004, bcdDevice=38.85
      usb 7-1.4.4.1.1.1: New USB device strings: Mfr=1, Product=2, SerialNumber=3
      usb 7-1.4.4.1.1.1: Product: MOMENTUM 3
      usb 7-1.4.4.1.1.1: Manufacturer: Sennheiser electronic GmbH & Co. KG
      usb 7-1.4.4.1.1.1: SerialNumber: <REDACTED>
      usb 7-1.4.4.1.1.1: Found last interface = 0
      usb 7-1.4.4.1.1.1: 1:1: add audio endpoint 0x3
      usb 7-1.4.4.1.1.1: Creating new data endpoint #3
      usb 7-1.4.4.1.1.1: 1:1 Set sample rate 48000, clock 0
      usb 7-1.4.4.1.1.1: 6:0: sticky mixer values (0/11520/768 => 3840), disabling
      usb 7-1.4.4.1.1.1: [6] FU [PCM Playback Volume] skipped due to invalid volume
      input: Sennheiser electronic GmbH & Co. KG MOMENTUM 3 as /devices/pci0000:00/0000:00:08.3/0000:67:00.4/usb7/7-1/7-1.4/7-1.4.4/7-1.4.4.1/7-1.4.4.1.1/7-1.4.4.1.1.1/7-1.4.4.1.1.1:1.2/0003:1377:6004.002B/input/input208
      input: Sennheiser electronic GmbH & Co. KG MOMENTUM 3 Consumer Control as /devices/pci0000:00/0000:00:08.3/0000:67:00.4/usb7/7-1/7-1.4/7-1.4.4/7-1.4.4.1/7-1.4.4.1.1/7-1.4.4.1.1.1/7-1.4.4.1.1.1:1.2/0003:1377:6004.002B/input/input209
      hid-generic 0003:1377:6004.002B: input,hiddev99,hidraw12: USB HID v1.11 Device [Sennheiser electronic GmbH & Co. KG MOMENTUM 3] on usb-0000:67:00.4-1.4.4.1.1.1/input2
    
    Signed-off-by: Rong Zhang <i@rong.moe>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Link: https://patch.msgid.link/20260531-uac-quirk-get-cur-vol-v4-2-ede643dca151@rong.moe

--- sound/usb/quirks.c
+++ sound/usb/quirks.c
@@ -2357,6 +2357,8 @@ static const struct usb_audio_quirk_flags_table quirk_flags_table[] = {
 		   QUIRK_FLAG_FORCE_IFACE_RESET | QUIRK_FLAG_IFACE_DELAY),
 	DEVICE_FLG(0x1224, 0x2a25, /* Jieli Technology USB PHY 2.0 */
 		   QUIRK_FLAG_GET_SAMPLE_RATE | QUIRK_FLAG_MIC_RES_16),
+	DEVICE_FLG(0x1377, 0x6004, /* Sennheiser MOMENTUM 3 */
+		   QUIRK_FLAG_MIXER_GET_CUR_BROKEN),
 	DEVICE_FLG(0x1395, 0x740a, /* Sennheiser DECT */
 		   QUIRK_FLAG_GET_SAMPLE_RATE),
 	DEVICE_FLG(0x1397, 0x0507, /* Behringer UMC202HD */
