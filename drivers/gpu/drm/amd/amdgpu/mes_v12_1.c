bfc6042540b7795d2f96a6ddc71442f74438dc73
Sunil Khatri <sunil.khatri@amd.com>
2026-06-01 20:15:34 +0530

    drm/amdgpu: validate the mes firmware version for gfx12.1
    MES firmware should report the same version whether read from
    the register or from the firmware ucode binary. This is not
    always the case, so add a log when they mismatch.
    
    Signed-off-by: Sunil Khatri <sunil.khatri@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/mes_v12_1.c
+++ drivers/gpu/drm/amd/amdgpu/mes_v12_1.c
@@ -1917,6 +1917,7 @@ static int mes_v12_1_xcc_hw_init(struct amdgpu_ip_block *ip_block, int xcc_id)
 		goto failure;
 	}
 
+	amdgpu_mes_validate_fw_version(adev);
 out:
 	/*
 	 * Disable KIQ ring usage from the driver once MES is enabled.
