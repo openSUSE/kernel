4356c4d85050f473a2260af2d005bab4c0b809a5
Will Deacon <will@kernel.org>
2026-06-03 17:50:24 +0800

    arm64: io: Rename ioremap_prot() to __ioremap_prot()
    commit f6bf47ab32e0863df50f5501d207dcdddb7fc507 upstream.
    
    Rename our ioremap_prot() implementation to __ioremap_prot() and convert
    all arch-internal callers over to the new function.
    
    ioremap_prot() remains as a #define to __ioremap_prot() for
    generic_access_phys() and will be subsequently extended to handle user
    permissions in 'prot'.
    
    Cc: Zeng Heng <zengheng4@huawei.com>
    Cc: Jinjiang Tu <tujinjiang@huawei.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Xiangyu Chen <xiangyu.chen@windriver.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/arm64/include/asm/io.h
+++ arch/arm64/include/asm/io.h
@@ -139,14 +139,16 @@ extern void __memset_io(volatile void __iomem *, int, size_t);
  * I/O memory mapping functions.
  */
 
+void __iomem *__ioremap_prot(phys_addr_t phys, size_t size, pgprot_t prot);
+
 #define ioremap_prot ioremap_prot
 
 #define _PAGE_IOREMAP PROT_DEVICE_nGnRE
 
 #define ioremap_wc(addr, size)	\
-	ioremap_prot((addr), (size), PROT_NORMAL_NC)
+	__ioremap_prot((addr), (size), __pgprot(PROT_NORMAL_NC))
 #define ioremap_np(addr, size)	\
-	ioremap_prot((addr), (size), PROT_DEVICE_nGnRnE)
+	__ioremap_prot((addr), (size), __pgprot(PROT_DEVICE_nGnRnE))
 
 /*
  * io{read,write}{16,32,64}be() macros
@@ -167,7 +169,7 @@ static inline void __iomem *ioremap_cache(phys_addr_t addr, size_t size)
 	if (pfn_is_map_memory(__phys_to_pfn(addr)))
 		return (void __iomem *)__phys_to_virt(addr);
 
-	return ioremap_prot(addr, size, PROT_NORMAL);
+	return __ioremap_prot(addr, size, __pgprot(PROT_NORMAL));
 }
 
 /*
