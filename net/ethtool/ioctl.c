f32fe1d79a18592b96342a0016cd62cf9601a054
Jakub Kicinski <kuba@kernel.org>
2026-06-02 18:28:33 -0700

    net: ethtool: add netif_get_link_ksettings() for correct ops-locked use
    __ethtool_get_link_ksettings() is exported and called from sysfs
    and many drivers. It invokes ethtool_ops->get_link_ksettings
    so by our own docs it should be holding netdev lock for ops locked
    devices. Looks like commit 2bcf4772e45a ("net: ethtool:
    try to protect all callback with netdev instance lock")
    missed adding the ops lock here.
    
    There's a number of callers we need to fix up so let's add the
    netif_get_link_ksettings() helper first, without any actual
    locking changes (this commit is a nop).
    
    Not treating this as a fix because I don't think any driver cares
    at this point, but if we want to remove the rtnl_lock protection
    this will become critical.
    
    Reviewed-by: Maxime Chevallier <maxime.chevallier@bootlin.com>
    Acked-by: Stanislav Fomichev <sdf@fomichev.me>
    Link: https://patch.msgid.link/20260603012840.2254293-5-kuba@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/ethtool/ioctl.c
+++ net/ethtool/ioctl.c
@@ -436,10 +436,10 @@ struct ethtool_link_usettings {
 };
 
 /* Internal kernel helper to query a device ethtool_link_settings. */
-int __ethtool_get_link_ksettings(struct net_device *dev,
-				 struct ethtool_link_ksettings *link_ksettings)
+int netif_get_link_ksettings(struct net_device *dev,
+			     struct ethtool_link_ksettings *link_ksettings)
 {
-	ASSERT_RTNL();
+	/* once callers fixed - assert ops locked */
 
 	if (!dev->ethtool_ops->get_link_ksettings)
 		return -EOPNOTSUPP;
@@ -449,6 +449,17 @@ int __ethtool_get_link_ksettings(struct net_device *dev,
 
 	memset(link_ksettings, 0, sizeof(*link_ksettings));
 	return dev->ethtool_ops->get_link_ksettings(dev, link_ksettings);
+}
+EXPORT_SYMBOL(netif_get_link_ksettings);
+
+/* Convenience helper for callers that hold only rtnl_lock(). */
+int __ethtool_get_link_ksettings(struct net_device *dev,
+				 struct ethtool_link_ksettings *link_ksettings)
+{
+	ASSERT_RTNL();
+
+	/* once callers fixed - take the ops lock around this call */
+	return netif_get_link_ksettings(dev, link_ksettings);
 }
 EXPORT_SYMBOL(__ethtool_get_link_ksettings);
 
