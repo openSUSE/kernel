5ef1b0194b382fafe5023b5b014e4db3b948ee15
David Laight <david.laight.linux@gmail.com>
2026-06-06 21:27:44 +0100

    drivers/block/rbd: Use strscpy() to copy strings into arrays
    Replacing strcpy() with strscpy() ensures than overflow of the target
    buffer cannot happen.
    
    Signed-off-by: David Laight <david.laight.linux@gmail.com>
    Reviewed-by: Alex Elder <elder@riscstar.com>
    Link: https://patch.msgid.link/20260606202744.5113-5-david.laight.linux@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/block/rbd.c
+++ drivers/block/rbd.c
@@ -3672,7 +3672,7 @@ static void __rbd_lock(struct rbd_device *rbd_dev, const char *cookie)
 	struct rbd_client_id cid = rbd_get_cid(rbd_dev);
 
 	rbd_dev->lock_state = RBD_LOCK_STATE_LOCKED;
-	strcpy(rbd_dev->lock_cookie, cookie);
+	strscpy(rbd_dev->lock_cookie, cookie);
 	rbd_set_owner_cid(rbd_dev, &cid);
 	queue_work(rbd_dev->task_wq, &rbd_dev->acquired_lock_work);
 }
