91660aae2f864850f5e5494f6548ce3e534e6d4c
Juergen Gross <jgross@suse.com>
2026-06-08 07:17:38 +0200

    x86/msr: Switch rdmsr_safe_on_cpu() users to rdmsrq_safe_on_cpu()
    In order to prepare retiring rdmsr_safe_on_cpu() switch
    rdmsr_safe_on_cpu() users to rdmsrq_safe_on_cpu().
    
    Tested-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Guenter Roeck <linux@roeck-us.net>
    Cc: Rafael J. Wysocki <rafael@kernel.org>
    Cc: Daniel Lezcano <daniel.lezcano@kernel.org>
    Link: https://patch.msgid.link/20260608051741.3207435-9-jgross@suse.com

--- arch/x86/kernel/msr.c
+++ arch/x86/kernel/msr.c
@@ -53,7 +53,7 @@ static ssize_t msr_read(struct file *file, char __user *buf,
 			size_t count, loff_t *ppos)
 {
 	u32 __user *tmp = (u32 __user *) buf;
-	u32 data[2];
+	u64 data;
 	u32 reg = *ppos;
 	int cpu = iminor(file_inode(file));
 	int err = 0;
@@ -63,7 +63,7 @@ static ssize_t msr_read(struct file *file, char __user *buf,
 		return -EINVAL;	/* Invalid chunk size */
 
 	for (; count; count -= 8) {
-		err = rdmsr_safe_on_cpu(cpu, reg, &data[0], &data[1]);
+		err = rdmsrq_safe_on_cpu(cpu, reg, &data);
 		if (err)
 			break;
 		if (copy_to_user(tmp, &data, 8)) {
