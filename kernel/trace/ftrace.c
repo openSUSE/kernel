95d4694bc1de7d3378173fd649c8037dab6c3949
Petr Pavlu <petr.pavlu@suse.com>
2026-06-09 10:20:54 +0200

    scripts/sorttable: Zero out weak functions in mcount_loc table (bsc#1267600).
    suse-commit: 6c956b52241e12f40639c0be47073b6bbe52df60

--- kernel/trace/ftrace.c
+++ kernel/trace/ftrace.c
@@ -7025,6 +7025,7 @@ static int ftrace_process_locs(struct module *mod,
 	unsigned long count;
 	unsigned long *p;
 	unsigned long addr;
+	unsigned long kaslr;
 	unsigned long flags = 0; /* Shut up gcc */
 	int ret = -ENOMEM;
 
@@ -7073,6 +7074,9 @@ static int ftrace_process_locs(struct module *mod,
 		ftrace_pages->next = start_pg;
 	}
 
+	/* For zeroed locations that were shifted for core kernel */
+	kaslr = !mod ? kaslr_offset() : 0;
+
 	p = start;
 	pg = start_pg;
 	while (p < end) {
@@ -7084,7 +7088,7 @@ static int ftrace_process_locs(struct module *mod,
 		 * object files to satisfy alignments.
 		 * Skip any NULL pointers.
 		 */
-		if (!addr) {
+		if (!addr || addr == kaslr) {
 			skipped++;
 			continue;
 		}
