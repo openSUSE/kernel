a8a98efe5e2ce4db47e987c94e4e23a5340d3f7a
Tony Jones <tonyj@suse.de>
2026-06-10 14:52:28 -0700

    perf sched stats: Add record and rawdump support (jsc#PED-16028).
    suse-commit: deaf4ff227d4e0547ea3bec068c7d226c84ed429

--- tools/perf/builtin-inject.c
+++ tools/perf/builtin-inject.c
@@ -2527,6 +2527,8 @@ int cmd_inject(int argc, const char **argv)
 	inject.tool.compressed		= perf_event__repipe_op4_synth;
 	inject.tool.auxtrace		= perf_event__repipe_auxtrace;
 	inject.tool.bpf_metadata	= perf_event__repipe_op2_synth;
+	inject.tool.schedstat_cpu	= perf_event__repipe_op2_synth;
+	inject.tool.schedstat_domain	= perf_event__repipe_op2_synth;
 	inject.tool.dont_split_sample_group = true;
 	inject.session = __perf_session__new(&data, &inject.tool,
 					     /*trace_event_repipe=*/inject.output.is_pipe,
