9961c88b1cfb444ee03e9c1641cf170e9684c532
Uwe Kleine-König (The Capable Hub) <u.kleine-koenig@baylibre.com>
2026-06-16 16:29:52 +0200

    ipmi: Drop unused assignment of platform_device_id driver data
    The driver explicitly sets the .driver_data member of struct
    platform_device_id to zero without relying on that value. Drop these
    unused assignments.
    
    Signed-off-by: Uwe Kleine-König (The Capable Hub) <u.kleine-koenig@baylibre.com>
    Message-ID: <9afdb7b0894f51fba78c64612428f7bb117901d1.1781620139.git.u.kleine-koenig@baylibre.com>
    Signed-off-by: Corey Minyard <corey@minyard.net>

--- drivers/char/ipmi/ipmi_si_platform.c
+++ drivers/char/ipmi/ipmi_si_platform.c
@@ -436,9 +436,9 @@ void ipmi_remove_platform_device_by_name(char *name)
 }
 
 static const struct platform_device_id si_plat_ids[] = {
-	{ "dmi-ipmi-si", 0 },
-	{ "hardcode-ipmi-si", 0 },
-	{ "hotmod-ipmi-si", 0 },
+	{ .name = "dmi-ipmi-si" },
+	{ .name = "hardcode-ipmi-si" },
+	{ .name = "hotmod-ipmi-si" },
 	{ }
 };
 
