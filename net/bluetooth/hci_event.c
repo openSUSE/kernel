e22d90fdda2f55823f98e247b82dd22360a5f195
Oliver Neukum <oneukum@suse.com>
2026-06-16 10:35:14 +0200

    Bluetooth: Ignore HCI_ERROR_CANCELLED_BY_HOST on adv set terminated event (CVE-2026-31771 bsc#1264145).
    suse-commit: 87c1997e377511ea0ccf81e52bcdd88e90bd1731

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -5433,6 +5433,18 @@ static void hci_le_ext_adv_term_evt(struct hci_dev *hdev, struct sk_buff *skb)
 
 	BT_DBG("%s status 0x%2.2x", hdev->name, ev->status);
 
+	/* The Bluetooth Core 5.3 specification clearly states that this event
+	 * shall not be sent when the Host disables the advertising set. So in
+	 * case of HCI_ERROR_CANCELLED_BY_HOST, just ignore the event.
+	 *
+	 * When the Host disables an advertising set, all cleanup is done via
+	 * its command callback and not needed to be duplicated here.
+	 */
+	if (ev->status == HCI_ERROR_CANCELLED_BY_HOST) {
+		bt_dev_warn_ratelimited(hdev, "Unexpected advertising set terminated event");
+		return;
+	}
+
 	if (ev->status) {
 		struct adv_info *adv;
 
