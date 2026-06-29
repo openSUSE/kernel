da3071cccbd0f14f73bd81efb0ef0c3b4045a21d
Olaf Hering <ohering@suse.de>
2026-06-29 16:32:54 +0000

    net: mana: initialize gdma queue id to INVALID_QUEUE_ID (git-fixes).
    suse-commit: a6c8305f090fc1c8bbd52f7a3b0b6494eddbb055

--- drivers/net/ethernet/microsoft/mana/gdma_main.c
+++ drivers/net/ethernet/microsoft/mana/gdma_main.c
@@ -1214,6 +1214,8 @@ int mana_gd_create_mana_eq(struct gdma_dev *gd,
 	if (!queue)
 		return -ENOMEM;
 
+	queue->id = INVALID_QUEUE_ID;
+
 	gmi = &queue->mem_info;
 	err = mana_gd_alloc_memory(gc, spec->queue_size, gmi);
 	if (err) {
