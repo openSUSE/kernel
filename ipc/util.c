17c5e84e9579033938451c4e764edf357e8928de
Michal Koutný <mkoutny@suse.com>
2026-06-30 15:37:07 +0200

    ipc: limit next_id allocation to the valid ID range (CVE-2026-52923 bsc#1269033).
    suse-commit: d3385ac38505e65ef42759a23d73f699f04e79d1

--- ipc/util.c
+++ ipc/util.c
@@ -243,7 +243,7 @@ int ipc_addid(struct ipc_ids *ids, struct kern_ipc_perm *new, int size)
 	new->gid = new->cgid = egid;
 
 	id = idr_alloc(&ids->ipcs_idr, new,
-		       (next_id < 0) ? 0 : ipcid_to_idx(next_id), 0,
+		       (next_id < 0) ? 0 : ipcid_to_idx(next_id), IPCMNI,
 		       GFP_NOWAIT);
 	idr_preload_end();
 	if (id < 0) {
