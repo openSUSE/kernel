d090fe3c22028bd303618bc612ea7aa0cea70ddc
Nicolas Morey <nmorey@suse.com>
2026-07-01 15:25:42 +0200

    RDMA/umem: Fix truncation for block sizes >= 4G (CVE-2026-53133 bsc#1269821)
    suse-commit: c6d138c90ef091656eb65ed04832b535963b78b2

--- drivers/infiniband/core/verbs.c
+++ drivers/infiniband/core/verbs.c
@@ -3028,8 +3028,8 @@ EXPORT_SYMBOL(__rdma_block_iter_start);
 
 bool __rdma_block_iter_next(struct ib_block_iter *biter)
 {
-	unsigned int block_offset;
-	unsigned int delta;
+	dma_addr_t block_offset;
+	dma_addr_t delta;
 
 	if (!biter->__sg_nents || !biter->__sg)
 		return false;
