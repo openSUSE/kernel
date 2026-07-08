d64ad0a3d06cc868fccbfd5c02e73ba542d76c5c
Oscar Salvador <osalvador@suse.de>
2026-07-08 11:16:56 +0200

    arm64: mte: set VM_MTE_ALLOWED for hugetlbfs at correct place (git-fixes).
     Refresh:
    	patches.suse/mm-perform-all-memfd-seal-checks-in-a-single-place.patch
    
    suse-commit: 7e3533de91d6cc9b1a4deb7b4012951e53bb2484

--- fs/hugetlbfs/inode.c
+++ fs/hugetlbfs/inode.c
@@ -109,7 +109,7 @@ static int hugetlbfs_file_mmap(struct file *file, struct vm_area_struct *vma)
 	 * way when do_mmap unwinds (may be important on powerpc
 	 * and ia64).
 	 */
-	vm_flags_set(vma, VM_HUGETLB | VM_DONTEXPAND | VM_MTE_ALLOWED);
+	vm_flags_set(vma, VM_HUGETLB | VM_DONTEXPAND);
 	vma->vm_ops = &hugetlb_vm_ops;
 
 	/*
