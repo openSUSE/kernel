175f4b9f61ec4774fbd65a88b2c672b8ea294c40
Jiri Bohac <jbohac@suse.cz>
2026-07-08 20:34:52 +0200

    - powerpc/kdump: Add support for crashkernel CMA reservation   (jsc#PED-14518). - Refresh   patches.suse/powerpc-lock-down-kernel-in-secure-boot-mode.patch.
    suse-commit: 87f4df7daef6207c33e249a46f88206ebc7e31ee

--- arch/powerpc/kexec/core.c
+++ arch/powerpc/kexec/core.c
@@ -81,6 +81,8 @@ void machine_kexec(struct kimage *image)
 
 #ifdef CONFIG_CRASH_RESERVE
 
+static unsigned long long crashk_cma_size;
+
 static unsigned long long __init get_crash_base(unsigned long long crash_base)
 {
 
@@ -132,7 +134,7 @@ void __init arch_reserve_crashkernel(void)
 
 	/* use common parsing */
 	ret = parse_crashkernel(boot_command_line, total_mem_sz, &crash_size,
-				&crash_base, NULL, NULL, NULL);
+				&crash_base, NULL, &crashk_cma_size, NULL);
 
 	if (ret)
 		return;
@@ -150,6 +152,12 @@ void __init arch_reserve_crashkernel(void)
 	}
 
 	reserve_crashkernel_generic(crash_size, crash_base, 0, false);
+}
+
+void __init kdump_cma_reserve(void)
+{
+	if (crashk_cma_size)
+		reserve_crashkernel_cma(crashk_cma_size);
 }
 
 int __init overlaps_crashkernel(unsigned long start, unsigned long size)
