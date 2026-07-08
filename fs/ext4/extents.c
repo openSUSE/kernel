b15af4cb53ecd3305c168bb7ab3a4868053fb0fe
Goldwyn Rodrigues <rgoldwyn@suse.com>
2026-07-08 09:13:37 -0400

    ext4: drop extent cache when splitting extent fails (CVE-2026-45899 bsc#1266883).
    suse-commit: c09a2c114fc0eed426deb5dc6263710d90832d67

--- fs/ext4/extents.c
+++ fs/ext4/extents.c
@@ -3322,7 +3322,7 @@ static int ext4_split_extent_at(handle_t *handle,
 
 	err = ext4_ext_insert_extent(handle, inode, ppath, &newex, flags);
 	if (err != -ENOSPC && err != -EDQUOT)
-		goto out;
+		goto out_err;
 
 	/*
 	 * Update path is required because previous ext4_ext_insert_extent()
@@ -3337,7 +3337,8 @@ static int ext4_split_extent_at(handle_t *handle,
 	if (IS_ERR(path)) {
 		EXT4_ERROR_INODE(inode, "Failed split extent on %u, err %ld",
 				 split, PTR_ERR(path));
-		return PTR_ERR(path);
+		err = PTR_ERR(path);
+		goto out_err;
 	}
 	depth = ext_depth(inode);
 	ex = path[depth].p_ext;
@@ -3389,6 +3390,9 @@ fix_extent_len:
 	ex->ee_len = orig_ex.ee_len;
 	ext4_ext_dirty(handle, inode, path + path->p_depth);
 	return err;
+out_err:
+	/* Remove all remaining potentially stale extents. */
+	ext4_es_remove_extent(inode, ee_block, ee_len);
 out:
 	ext4_ext_show_leaf(inode, *ppath);
 	return err;
