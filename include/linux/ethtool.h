71addce737786d40bed712ae9366ce0f74aa5673
Olaf Hering <ohering@suse.de>
2026-07-10 10:04:52 +0000

    ethtool: Implement ethtool_puts() (git-fixes).
    suse-commit: 61c19c1ae4e387faf6e7161785055da3321f945c

--- include/linux/ethtool.h
+++ include/linux/ethtool.h
@@ -812,4 +812,17 @@ int ethtool_get_phc_vclocks(struct net_device *dev, int **vclock_index);
  * next string.
  */
 extern __printf(2, 3) void ethtool_sprintf(u8 **data, const char *fmt, ...);
+ 
+/**
+ * ethtool_puts - Write string to ethtool string data
+ * @data: Pointer to a pointer to the start of string to update
+ * @str: String to write
+ *
+ * Write string to *data without a trailing newline. Update *data
+ * to point at start of next string.
+ *
+ * Prefer this function to ethtool_sprintf() when given only
+ * two arguments or if @fmt is just "%s".
+ */
+extern void ethtool_puts(u8 **data, const char *str);
 #endif /* _LINUX_ETHTOOL_H */
