4623e53282cdfcfdd297bf8e412f6566182ebd08
Jan Kara <jack@suse.cz>
2026-07-12 16:24:24 +0200

    eventpoll: drop dead bool return from ep_remove_epi() (bsc#1267618 CVE-2026-46242).
    suse-commit: 240e99f24f2395ad2aad4c9637fa40bb2464d7e5

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -765,7 +765,7 @@ static void ep_remove_file(struct eventpoll *ep, struct epitem *epi,
 	free_ephead(to_free);
 }
 
-static bool ep_remove_epi(struct eventpoll *ep, struct epitem *epi)
+static void ep_remove_epi(struct eventpoll *ep, struct epitem *epi)
 {
 	lockdep_assert_held(&ep->mtx);
 
@@ -787,7 +787,6 @@ static bool ep_remove_epi(struct eventpoll *ep, struct epitem *epi)
 	kfree_rcu(epi, rcu);
 
 	percpu_counter_dec(&ep->user->epoll_watches);
-	return true;
 }
 
 /*
@@ -815,9 +814,8 @@ static void ep_remove(struct eventpoll *ep, struct epitem *epi)
 		return;
 
 	ep_remove_file(ep, epi, file);
-
-	if (ep_remove_epi(ep, epi))
-		WARN_ON_ONCE(ep_refcount_dec_and_test(ep));
+	ep_remove_epi(ep, epi);
+	WARN_ON_ONCE(ep_refcount_dec_and_test(ep));
 }
 
 static void ep_clear_and_put(struct eventpoll *ep)
@@ -986,7 +984,6 @@ void eventpoll_release_file(struct file *file)
 {
 	struct eventpoll *ep;
 	struct epitem *epi;
-	bool dispose;
 
 	/*
 	 * Use the 'dying' flag to prevent a concurrent ep_clear_and_put() from
@@ -1010,11 +1007,11 @@ again:
 		ep_unregister_pollwait(ep, epi);
 
 		ep_remove_file(ep, epi, file);
-		dispose = ep_remove_epi(ep, epi);
+		ep_remove_epi(ep, epi);
 
 		mutex_unlock(&ep->mtx);
 
-		if (dispose && ep_refcount_dec_and_test(ep))
+		if (ep_refcount_dec_and_test(ep))
 			ep_free(ep);
 		goto again;
 	}
