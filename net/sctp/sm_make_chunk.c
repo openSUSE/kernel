4a018b19eb946217bc77538ac7f6c1ee4c7a3db7
Nicolas Morey <nmorey@suse.com>
2026-07-15 10:13:22 +0200

    sctp: validate embedded INIT chunk and address list lengths in cookie (CVE-2026-53224 bsc#1269997)
    suse-commit: 309c07dce145aa5e3730136ee628fb5642c9dd30

--- net/sctp/sm_make_chunk.c
+++ net/sctp/sm_make_chunk.c
@@ -1710,7 +1710,7 @@ struct sctp_association *sctp_unpack_cookie(
 	int headersize, bodysize, fixed_size;
 	__u8 *digest = ep->digest;
 	struct sctp_chunkhdr *ch;
-	unsigned int len;
+	unsigned int len, chlen;
 	sctp_scope_t scope;
 	struct sk_buff *skb = chunk->skb;
 	ktime_t kt;
@@ -1741,7 +1741,12 @@ struct sctp_association *sctp_unpack_cookie(
 	bear_cookie = &cookie->c;
 
 	ch = (struct sctp_chunkhdr *)(bear_cookie + 1);
-	if (ntohs(ch->length) > len - fixed_size)
+	chlen = ntohs(ch->length);
+	if (chlen < sizeof(struct sctp_init_chunk))
+		goto malformed;
+	if (chlen > len - fixed_size)
+		goto malformed;
+	if (bear_cookie->raw_addr_list_len > len - fixed_size - chlen)
 		goto malformed;
 
 	if (!sctp_sk(ep->base.sk)->hmac)
