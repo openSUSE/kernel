5d31fa9e508616a64603e860bc0fc94eb87e04bf
Gabriel Krisman Bertazi <krisman@suse.de>
2026-07-15 12:51:22 -0400

    io_uring/kbuf: propagate BUF_MORE through early buffer commit path (bsc#1261250).
    suse-commit: f70d296d4cf6735ae531e2613c7f4b7083fb6eec

--- io_uring/kbuf.h
+++ io_uring/kbuf.h
@@ -173,7 +173,9 @@ static inline bool __io_put_kbuf_ring(struct io_kiocb *req, int len, int nr)
 		ret = io_kbuf_commit(req, bl, len, nr);
 		req->buf_index = bl->bgid;
 	}
-	req->flags &= ~REQ_F_BUFFER_RING;
+	if (ret && (req->flags & REQ_F_BUF_MORE))
+		return false;
+	req->flags &= ~(REQ_F_BUFFER_RING | REQ_F_BUF_MORE);
 	return ret;
 }
 
