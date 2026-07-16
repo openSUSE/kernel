b1f74cb3a000a86d12b599de6bb1767965814f29
David Sterba <dsterba@suse.com>
2026-07-16 03:27:46 +0200

    drm/amdgpu/userq: Fix reference leak in amdgpu_userq_wait_ioctl (CVE-2026-43399 bsc#1265098)
    suse-commit: 93eaf5cef9b0743e60b511f1dc8163ffc8713b82

--- drivers/gpu/drm/amd/amdgpu/amdgpu_userq_fence.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_userq_fence.c
@@ -835,6 +835,7 @@ int amdgpu_userq_wait_ioctl(struct drm_device *dev, void *data,
 				dma_fence_unwrap_for_each(f, &iter, fence) {
 					if (WARN_ON_ONCE(num_fences >= wait_info->num_fences)) {
 						r = -EINVAL;
+						dma_fence_put(fence);
 						goto free_fences;
 					}
 
@@ -859,6 +860,7 @@ int amdgpu_userq_wait_ioctl(struct drm_device *dev, void *data,
 
 			if (WARN_ON_ONCE(num_fences >= wait_info->num_fences)) {
 				r = -EINVAL;
+				dma_fence_put(fence);
 				goto free_fences;
 			}
 
