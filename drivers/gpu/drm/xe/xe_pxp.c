f9f648999846a41d8b4ae2fa3ccd53abc514f4af
Takashi Iwai <tiwai@suse.de>
2026-07-24 13:42:54 +0200

    drm/xe/pxp: Remove incorrect handling of impossible state during suspend (git-fixes).
    suse-commit: c0268d9bf9bb83d35574a6da38f41018b5c60f6c

--- drivers/gpu/drm/xe/xe_pxp.c
+++ drivers/gpu/drm/xe/xe_pxp.c
@@ -892,11 +892,6 @@ wait_for_activation:
 		pxp->key_instance++;
 		needs_queue_inval = true;
 		break;
-	default:
-		drm_err(&pxp->xe->drm, "unexpected state during PXP suspend: %u",
-			pxp->status);
-		ret = -EIO;
-		goto out;
 	}
 
 	/*
@@ -921,7 +916,6 @@ wait_for_activation:
 
 	pxp->last_suspend_key_instance = pxp->key_instance;
 
-out:
 	return ret;
 }
 
