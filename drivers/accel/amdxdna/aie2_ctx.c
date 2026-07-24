9b119e20ff148a908b982e57abbd0a00984175c1
Takashi Iwai <tiwai@suse.de>
2026-07-24 12:43:41 +0200

    accel/amdxdna: Fix mm_struct reference leak in aie2_populate_range() (git-fixes).
    suse-commit: 9bed91af3673e9b09d34c1b9e6ba3c16329db695

--- drivers/accel/amdxdna/aie2_ctx.c
+++ drivers/accel/amdxdna/aie2_ctx.c
@@ -819,6 +819,7 @@ again:
 
 		if (ret == -EBUSY) {
 			amdxdna_umap_put(mapp);
+			mmput(mm);
 			goto again;
 		}
 
@@ -829,11 +830,13 @@ again:
 	if (mmu_interval_read_retry(&mapp->notifier, mapp->range.notifier_seq)) {
 		up_write(&xdna->notifier_lock);
 		amdxdna_umap_put(mapp);
+		mmput(mm);
 		goto again;
 	}
 	mapp->invalid = false;
 	up_write(&xdna->notifier_lock);
 	amdxdna_umap_put(mapp);
+	mmput(mm);
 	goto again;
 
 put_mm:
