866c62e37763ffe3ee818930dd0f79c2dc3407ef
Goldwyn Rodrigues <rgoldwyn@suse.com>
2026-07-28 10:15:26 -0400

    md raid: fix hang when stopping arrays with metadata through dm-raid (CVE-2026-43309 bsc#1264827).
    suse-commit: 15504ea03ffddd8f8d390dba13787bdb89162c54

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -6149,11 +6149,13 @@ static void __md_stop_writes(struct mddev *mddev)
 
 	del_timer_sync(&mddev->safemode_timer);
 
-	if (mddev->pers && mddev->pers->quiesce) {
-		mddev->pers->quiesce(mddev, 1);
-		mddev->pers->quiesce(mddev, 0);
+	if (!mddev->ro || mddev->gendisk) {
+		if (mddev->pers && mddev->pers->quiesce) {
+			mddev->pers->quiesce(mddev, 1);
+			mddev->pers->quiesce(mddev, 0);
+		}
+		md_bitmap_flush(mddev);
 	}
-	md_bitmap_flush(mddev);
 
 	if (mddev->ro == 0 &&
 	    ((!mddev->in_sync && !mddev_is_clustered(mddev)) ||
