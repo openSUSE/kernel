b286d5e8c142a1932542616f42be07c9be03b67b
cve-kpm <cve-kpm@suse.de>
2026-07-31 08:13:53 +0000

    fs/fcntl: fix SOFTIRQ-unsafe lock order in fasync signaling (CVE-2026-52946 bsc#1269113).
    suse-commit: 9a9970ce411a84c014af876f3b208062af39ad1f

--- fs/fcntl.c
+++ fs/fcntl.c
@@ -738,11 +738,11 @@ void send_sigio(struct fown_struct *fown, int fd, int band)
 	if (!pid)
 		goto out_unlock_fown;
 	
-	read_lock(&tasklist_lock);
+	rcu_read_lock();
 	do_each_pid_task(pid, type, p) {
 		send_sigio_to_task(p, fown, fd, band, group);
 	} while_each_pid_task(pid, type, p);
-	read_unlock(&tasklist_lock);
+	rcu_read_unlock();
  out_unlock_fown:
 	read_unlock_irqrestore(&fown->lock, flags);
 }
@@ -777,11 +777,11 @@ int send_sigurg(struct fown_struct *fown)
 
 	ret = 1;
 	
-	read_lock(&tasklist_lock);
+	rcu_read_lock();
 	do_each_pid_task(pid, type, p) {
 		send_sigurg_to_task(p, fown, group);
 	} while_each_pid_task(pid, type, p);
-	read_unlock(&tasklist_lock);
+	rcu_read_unlock();
  out_unlock_fown:
 	read_unlock_irqrestore(&fown->lock, flags);
 	return ret;
