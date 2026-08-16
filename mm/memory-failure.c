3a52be0b5544de7e77e5550dba644c88b1008d4a
Oscar Salvador <osalvador@suse.de>
2026-08-16 19:15:13 +0200

    mm/memory-failure: fix hugetlb_lock AA deadlock in get_huge_page_for_hwpoison (CVE-2026-53207 bsc#1269590).
    suse-commit: d0f34e6bdf13eb7bd6a39fc161f87db4f4c51a8b

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -2011,16 +2011,18 @@ void folio_clear_hugetlb_hwpoison(struct folio *folio)
  *   -EBUSY        - the hugepage is busy (try to retry)
  *   -EHWPOISON    - the hugepage is already hwpoisoned
  */
-int __get_huge_page_for_hwpoison(unsigned long pfn, int flags,
+int get_huge_page_for_hwpoison(unsigned long pfn, int flags,
 				 bool *migratable_cleared)
 {
 	struct page *page = pfn_to_page(pfn);
-	struct folio *folio = page_folio(page);
+	struct folio *folio;
 	int ret = 2;	/* fallback to normal page handling */
 	bool count_increased = false;
 
+	spin_lock_irq(&hugetlb_lock);
+	folio = page_folio(page);
 	if (!folio_test_hugetlb(folio))
-		goto out;
+		goto out_unlock;
 
 	if (flags & MF_COUNT_INCREASED) {
 		ret = 1;
@@ -2034,12 +2036,12 @@ int __get_huge_page_for_hwpoison(unsigned long pfn, int flags,
 	} else {
 		ret = -EBUSY;
 		if (!(flags & MF_NO_RETRY))
-			goto out;
+			goto out_unlock;
 	}
 
 	if (folio_set_hugetlb_hwpoison(folio, page)) {
 		ret = -EHWPOISON;
-		goto out;
+		goto out_unlock;
 	}
 
 	/*
@@ -2051,8 +2053,10 @@ int __get_huge_page_for_hwpoison(unsigned long pfn, int flags,
 		*migratable_cleared = true;
 	}
 
+	spin_unlock_irq(&hugetlb_lock);
 	return ret;
-out:
+out_unlock:
+	spin_unlock_irq(&hugetlb_lock);
 	if (count_increased)
 		folio_put(folio);
 	return ret;
