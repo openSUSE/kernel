c4cdb18e54b357141dcec6b5d6b2fea358e62292
Thomas Zimmermann <tzimmermann@suse.com>
2026-08-20 10:47:17 +0200

    drm/amd/display: Fix out-of-bounds read in (bsc#1270090 CVE-2026-53330)
    suse-commit: 2f570473825cb46029bc3c4df6623b8c1f400c51

--- drivers/gpu/drm/amd/display/dc/dc_dp_types.h
+++ drivers/gpu/drm/amd/display/dc/dc_dp_types.h
@@ -1100,7 +1100,7 @@ struct dc_lttpr_caps {
 	uint8_t max_ext_timeout;
 	union dp_main_link_channel_coding_lttpr_cap main_link_channel_coding;
 	union dp_128b_132b_supported_lttpr_link_rates supported_128b_132b_rates;
-	uint8_t aux_rd_interval[MAX_REPEATER_CNT - 1];
+	uint8_t aux_rd_interval[MAX_REPEATER_CNT];
 };
 
 struct dc_dongle_dfp_cap_ext {
