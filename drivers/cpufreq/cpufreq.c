71e04b52139263253228d8eb417f72f95c9fbf2b
cve-kpm <cve-kpm@suse.de>
2026-08-24 12:45:17 +0000

    cpufreq: Fix hotplug-suspend race during reboot (CVE-2026-64373 bsc#1273776).
    suse-commit: aeebdab1fed5910adb418dad85d1749cc6f0c732

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1935,6 +1935,7 @@ void cpufreq_suspend(void)
 	if (!cpufreq_driver)
 		return;
 
+	cpus_read_lock();
 	if (!has_target() && !cpufreq_driver->suspend)
 		goto suspend;
 
@@ -1954,6 +1955,7 @@ void cpufreq_suspend(void)
 
 suspend:
 	cpufreq_suspended = true;
+	cpus_read_unlock();
 }
 
 /**
