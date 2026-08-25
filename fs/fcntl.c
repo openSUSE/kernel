e4286f992849a7db43f6dfb57751220c2dbadcfa
Michal Koutný <mkoutny@suse.com>
2026-08-25 17:17:00 +0200

    fcntl: Fix potential deadlock in send_sig{io, urg}() (bsc#1269113).
    suse-commit: 995665edf167af283f0e5bfd22718f73a76ee356

--- fs/fcntl.c
+++ fs/fcntl.c
@@ -722,10 +722,11 @@ void send_sigio(struct fown_struct *fown, int fd, int band)
 {
 	struct task_struct *p;
 	enum pid_type type;
+	unsigned long flags;
 	struct pid *pid;
 	int group = 1;
 	
-	read_lock(&fown->lock);
+	read_lock_irqsave(&fown->lock, flags);
 
 	type = fown->pid_type;
 	if (type == PIDTYPE_MAX) {
@@ -743,7 +744,7 @@ void send_sigio(struct fown_struct *fown, int fd, int band)
 	} while_each_pid_task(pid, type, p);
 	read_unlock(&tasklist_lock);
  out_unlock_fown:
-	read_unlock(&fown->lock);
+	read_unlock_irqrestore(&fown->lock, flags);
 }
 
 static void send_sigurg_to_task(struct task_struct *p,
@@ -759,9 +760,10 @@ int send_sigurg(struct fown_struct *fown)
 	enum pid_type type;
 	struct pid *pid;
 	int group = 1;
+	unsigned long flags;
 	int ret = 0;
 	
-	read_lock(&fown->lock);
+	read_lock_irqsave(&fown->lock, flags);
 
 	type = fown->pid_type;
 	if (type == PIDTYPE_MAX) {
@@ -781,7 +783,7 @@ int send_sigurg(struct fown_struct *fown)
 	} while_each_pid_task(pid, type, p);
 	read_unlock(&tasklist_lock);
  out_unlock_fown:
-	read_unlock(&fown->lock);
+	read_unlock_irqrestore(&fown->lock, flags);
 	return ret;
 }
 
