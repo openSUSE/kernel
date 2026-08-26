3c977e5329db0a6de8a41746ac1aa0845f1ece93
Nicolas Morey <nmorey@suse.com>
2026-08-26 13:27:38 +0200

    RDMA/mlx5: Fix undefined shift of user RQ WQE size (CVE-2026-74297 bsc#1276446)
    suse-commit: 4d3a9bd7ec83e33cbb856c0edd5d1fd2d3d984eb

--- drivers/infiniband/hw/mlx5/qp.c
+++ drivers/infiniband/hw/mlx5/qp.c
@@ -246,12 +246,14 @@ static int set_rq_size(struct mlx5_ib_dev *dev, struct ib_qp_cap *cap,
 	} else {
 		if (ucmd) {
 			qp->rq.wqe_cnt = ucmd->rq_wqe_count;
-			if (ucmd->rq_wqe_shift > BITS_PER_BYTE * sizeof(ucmd->rq_wqe_shift))
-				return -EINVAL;
 			qp->rq.wqe_shift = ucmd->rq_wqe_shift;
-			if ((1 << qp->rq.wqe_shift) / sizeof(struct mlx5_wqe_data_seg) < qp->wq_sig)
+			if (check_shl_overflow(1, qp->rq.wqe_shift, &wqe_size))
 				return -EINVAL;
-			qp->rq.max_gs = (1 << qp->rq.wqe_shift) / sizeof(struct mlx5_wqe_data_seg) - qp->wq_sig;
+			if (wqe_size / sizeof(struct mlx5_wqe_data_seg) < qp->wq_sig)
+				return -EINVAL;
+			qp->rq.max_gs =
+				wqe_size / sizeof(struct mlx5_wqe_data_seg) -
+				qp->wq_sig;
 			qp->rq.max_post = qp->rq.wqe_cnt;
 		} else {
 			wqe_size = qp->wq_sig ? sizeof(struct mlx5_wqe_signature_seg) : 0;
