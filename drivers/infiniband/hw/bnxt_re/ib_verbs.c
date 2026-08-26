76e85a9a18ec2ace54b232c015a299cee8ecb20e
Nicolas Morey <nmorey@suse.com>
2026-08-26 13:38:36 +0200

    RDMA/bnxt_re: Initialize dpi variable to zero (CVE-2026-72501 bsc#1276546)
    suse-commit: 661469762e653e3dbb46badc48312e73628eab36

--- drivers/infiniband/hw/bnxt_re/ib_verbs.c
+++ drivers/infiniband/hw/bnxt_re/ib_verbs.c
@@ -4491,8 +4491,8 @@ static int UVERBS_HANDLER(BNXT_RE_METHOD_ALLOC_PAGE)(struct uverbs_attr_bundle *
 	struct bnxt_re_ucontext *uctx;
 	struct bnxt_re_dev *rdev;
 	u64 mmap_offset;
+	u32 dpi = 0;
 	u32 length;
-	u32 dpi;
 	u64 addr;
 	int err;
 
