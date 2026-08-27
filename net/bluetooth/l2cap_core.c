02b85c09acb3a9c497596f07d0a011abaeba2957
cve-kpm <cve-kpm@suse.de>
2026-08-27 18:19:13 +0000

    Bluetooth: L2CAP: fix UAF in l2cap_le_connect_rsp (CVE-2026-74540 bsc#1275681).
    suse-commit: d4e3700bc8f1c8d354867cbf294740ed79164f41

--- net/bluetooth/l2cap_core.c
+++ net/bluetooth/l2cap_core.c
@@ -5341,6 +5341,10 @@ static int l2cap_le_connect_rsp(struct l2cap_conn *conn,
 	if (!chan)
 		return -EBADSLT;
 
+	chan = l2cap_chan_hold_unless_zero(chan);
+	if (!chan)
+		return -EBADSLT;
+
 	err = 0;
 
 	l2cap_chan_lock(chan);
@@ -5386,6 +5390,7 @@ static int l2cap_le_connect_rsp(struct l2cap_conn *conn,
 	}
 
 	l2cap_chan_unlock(chan);
+	l2cap_chan_put(chan);
 
 	return err;
 }
