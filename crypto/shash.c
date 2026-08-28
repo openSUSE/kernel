2bbdd440f778b6d272139e9940210b765fcea4f7
Nicolai Stange <nstange@suse.de>
2026-08-28 11:34:21 +0200

    crypto: shash - Fix buffer overrun in import function (jsc#PED-15972).
    suse-commit: c169c2b4b8a41618c8181e2e04f9b584b46949b3

--- crypto/shash.c
+++ crypto/shash.c
@@ -263,12 +263,13 @@ int crypto_shash_import_core(struct shash_desc *desc, const void *in)
 	if (crypto_shash_get_flags(tfm) & CRYPTO_TFM_NEED_KEY)
 		return -ENOKEY;
 
-	plen = crypto_shash_blocksize(tfm) + 1;
-	descsize = crypto_shash_descsize(tfm);
 	ss = crypto_shash_statesize(tfm);
-	buf[descsize - 1] = 0;
-	if (crypto_shash_block_only(tfm))
+	if (crypto_shash_block_only(tfm)) {
+		plen = crypto_shash_blocksize(tfm) + 1;
 		ss -= plen;
+		descsize = crypto_shash_descsize(tfm);
+		buf[descsize - 1] = 0;
+	}
 	import = crypto_shash_alg(tfm)->import;
 	if (!import) {
 		memcpy(buf, in, ss);
