94b05064c2ae7c895186ee579e367fc48ddd9c1e
Michal Suchanek <msuchanek@suse.de>
2026-08-31 11:59:04 +0200

    iommu: Get DT/ACPI parsing into the proper probe path (bsc#1272627 jsc#NVIDIA-77).
     - Refresh patches.suse/ACPI-scan-Add-support-for-RISC-V-in-acpi_iommu_confi.patch
    
    suse-commit: c27e79ebe4472b2567f403cbd7c1d6798007206a

--- drivers/acpi/scan.c
+++ drivers/acpi/scan.c
@@ -1645,13 +1645,6 @@ static int acpi_iommu_configure_id(struct device *dev, const u32 *id_in)
 
 	mutex_unlock(&iommu_probe_device_lock);
 
-	/*
-	 * If we have reason to believe the IOMMU driver missed the initial
-	 * iommu_probe_device() call for dev, replay it to get things in order.
-	 */
-	if (!err && dev->bus)
-		err = iommu_probe_device(dev);
-
 	return err;
 }
 
