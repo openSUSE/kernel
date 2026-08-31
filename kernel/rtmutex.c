a76797215c531fd3ea9c42d6f8b559319a0d4df9
Jiri Wiesner <jwiesner@suse.de>
2026-08-31 11:48:49 +0200

    locking/rtmutex: Skip remove_waiter() when waiter is not enqueued (CVE-2026-53163 bsc#1269306).
    suse-commit: 4b72607b73dc13f598625db7fd35b3c34542eddb

--- kernel/rtmutex.c
+++ kernel/rtmutex.c
@@ -574,6 +574,9 @@ static void remove_waiter(struct rt_mutex *lock,
 	struct task_struct *waiter_task = waiter->task;
 	int chain_walk = 0;
 
+	if (!waiter_task) /* never enqueued */
+		return;
+
 	raw_spin_lock_irqsave(&waiter_task->pi_lock, flags);
 	plist_del(&waiter->list_entry, &lock->wait_list);
 	waiter_task->pi_blocked_on = NULL;
@@ -1049,7 +1052,7 @@ int rt_mutex_start_proxy_lock(struct rt_mutex *lock,
 		ret = 0;
 	}
 
-	if (unlikely(ret))
+	if (unlikely(ret < 0))
 		remove_waiter(lock, waiter);
 
 	raw_spin_unlock(&lock->wait_lock);
