2133e139eec36ec4462407d13855a4e694e201a9
Michal Kubecek <mkubecek@suse.cz>
2026-09-02 21:29:07 +0200

    kabi: move skb_dev out of struct nf_queue_entry (CVE-2026-52912 bsc#1269000).
    suse-commit: 003299afb9b2498a7eb176abd0c81847792b04a0

--- net/netfilter/nfnetlink_queue.c
+++ net/netfilter/nfnetlink_queue.c
@@ -910,7 +910,8 @@ dev_cmp(struct nf_queue_entry *entry, unsigned long ifindex)
 	if (physinif == ifindex || physoutif == ifindex)
 		return 1;
 #endif
-	if (entry->skb_dev && entry->skb_dev->ifindex == ifindex)
+	if (nf_queue_entry_skbdev(entry) &&
+	    nf_queue_entry_skbdev(entry)->ifindex == ifindex)
 		return 1;
 	if (entry->state.in)
 		if (entry->state.in->ifindex == ifindex)
