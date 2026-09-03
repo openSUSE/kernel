e86ed843348dd364cffc8308ff6771c769dd6a74
Chun-Yi Lee <jlee@suse.com>
2026-09-03 15:20:26 +0800

    security: don't use RCU accessors for cred->session_keyring (bsc#1272182 CVE-2026-63823).
    Refresh
    patches.suse/0001-keys-safe-concurrent-user-session-uid-_keyring-acces.patch.
    
    suse-commit: d91672467bdc01c2c578a05cfcd83b963b6c62fe

--- security/keys/request_key.c
+++ security/keys/request_key.c
@@ -145,12 +145,10 @@ static int call_sbin_request_key(struct key_construction *cons,
 		prkey = cred->process_keyring->serial;
 	sprintf(keyring_str[1], "%d", prkey);
 
-	rcu_read_lock();
-	session = rcu_dereference(cred->session_keyring);
+	session = cred->session_keyring;
 	if (!session)
 		session = cred->user->session_keyring;
 	sskey = session->serial;
-	rcu_read_unlock();
 
 	sprintf(keyring_str[2], "%d", sskey);
 
@@ -298,10 +296,7 @@ static int construct_get_dest_keyring(struct key **_dest_keyring)
 				break;
 
 		case KEY_REQKEY_DEFL_SESSION_KEYRING:
-			rcu_read_lock();
-			dest_keyring = key_get(
-				rcu_dereference(cred->session_keyring));
-			rcu_read_unlock();
+			dest_keyring = key_get(cred->session_keyring);
 
 			if (dest_keyring)
 				break;
