2cab89e9ccd73546eeb7b086bac6a6d2b2527802
Jiri Slaby <jslaby@suse.cz>
2026-09-07 17:57:41 +0200

    entry: Fix seccomp bypass after ptrace with TSYNC (bsc#1012628).
    suse-commit: 0b9103b7deb4b8fb166d0a0985d4a96675e4baf0

--- include/linux/entry-common.h
+++ include/linux/entry-common.h
@@ -96,6 +96,9 @@ static __always_inline long syscall_trace_enter(struct pt_regs *regs, unsigned l
 		ret = arch_ptrace_report_syscall_entry(regs);
 		if (ret || (work & SYSCALL_WORK_SYSCALL_EMU))
 			return -1L;
+
+		/* ptrace might have changed work flags */
+		work = READ_ONCE(current_thread_info()->syscall_work);
 	}
 
 	/* Do seccomp after ptrace, to catch any tracer changes. */
