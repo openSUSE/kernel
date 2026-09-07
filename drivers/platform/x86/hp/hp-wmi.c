89c7b34f896dd70370112c6b1938cae6b1c0fb69
Jiri Slaby <jslaby@suse.cz>
2026-09-07 17:57:41 +0200

    platform/x86: hp-wmi: Add support for OMEN MAX 16-ak0xxx (8DD6) (bsc#1012628).
    suse-commit: 01a4e858b9abd3cc5cedff6876e2f00b72a15e2d

--- drivers/platform/x86/hp/hp-wmi.c
+++ drivers/platform/x86/hp/hp-wmi.c
@@ -266,6 +266,10 @@ static const struct dmi_system_id victus_s_thermal_profile_boards[] __initconst 
 		.driver_data = (void *)&omen_v1_no_ec_thermal_params,
 	},
 	{
+		.matches = { DMI_MATCH(DMI_BOARD_NAME, "8DD6") },
+		.driver_data = (void *)&omen_v1_no_ec_thermal_params,
+	},
+	{
 		.matches = { DMI_MATCH(DMI_BOARD_NAME, "8E35") },
 		.driver_data = (void *)&omen_v1_legacy_thermal_params,
 	},
