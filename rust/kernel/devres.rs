fbe1cee16f4a978fbd71af31baf257122896df5e
Jiri Slaby <jslaby@suse.cz>
2026-09-07 17:57:41 +0200

    rust: devres: ensure revocation is complete before device finishes unbinding (bsc#1012628).
    suse-commit: 5e788fbb7f49caf000315329e65af936545d1942

--- rust/kernel/devres.rs
+++ rust/kernel/devres.rs
@@ -265,6 +265,11 @@ impl<T: Send + 'static> Devres<T> {
 
         if inner.data.revoke() {
             inner.revocation.complete_all();
+        } else {
+            // Devres::drop() is concurrently revoking; wait for it to finish `drop_in_place()`
+            // before returning to `devres_release_all()`, ensuring `T` is fully torn down before
+            // the device finishes unbinding.
+            inner.revocation.wait_for_completion();
         }
     }
 
@@ -365,6 +370,8 @@ impl<T: Send + 'static> Drop for Devres<T> {
         // SAFETY: When `drop` runs, it is guaranteed that nobody is accessing the revocable data
         // anymore, hence it is safe not to wait for the grace period to finish.
         if unsafe { self.data().revoke_nosync() } {
+            self.inner.revocation.complete_all();
+
             // We revoked `self.data` before devres did, hence try to remove it.
             if self.remove_node() {
                 // SAFETY: In `Self::new` we have taken an additional reference count of `self.data`
