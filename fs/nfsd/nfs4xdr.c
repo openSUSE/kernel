584bd380c8b7ae5a3cedaca58b8c2b71b071ffce
Jiri Slaby <jslaby@suse.cz>
2026-09-07 17:57:41 +0200

    nfsd: release OPEN-decoded posix ACLs via op_release (bsc#1012628).
    suse-commit: 2b396ec7d1eb40ba1f95684d8ed1df447cb62624

--- fs/nfsd/nfs4xdr.c
+++ fs/nfsd/nfs4xdr.c
@@ -6420,9 +6420,6 @@ status:
 	write_bytes_to_xdr_buf(xdr->buf, op_status_offset,
 			       &op->status, XDR_UNIT);
 release:
-	if (opdesc && opdesc->op_release)
-		opdesc->op_release(&op->u);
-
 	/*
 	 * Account for pages consumed while encoding this operation.
 	 * The xdr_stream primitives don't manage rq_next_page.
