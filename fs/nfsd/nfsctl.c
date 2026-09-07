783b0d9226690815c26d8532bb13f176e8d78eb2
Jiri Slaby <jslaby@suse.cz>
2026-09-07 17:57:41 +0200

    nfsd: move nfsd_debugfs_init() after nfsd4_init_slabs() in init_nfsd() (bsc#1012628).
    suse-commit: ae3332f3681f2185ac08ea865da2d0d31d184f94

--- fs/nfsd/nfsctl.c
+++ fs/nfsd/nfsctl.c
@@ -2592,11 +2592,12 @@ static int __init init_nfsd(void)
 {
 	int retval;
 
-	nfsd_debugfs_init();
-
 	retval = nfsd4_init_slabs();
 	if (retval)
 		return retval;
+
+	nfsd_debugfs_init();
+
 	retval = nfsd4_init_pnfs();
 	if (retval)
 		goto out_free_slabs;
@@ -2641,8 +2642,8 @@ out_free_lockd:
 out_free_pnfs:
 	nfsd4_exit_pnfs();
 out_free_slabs:
-	nfsd4_free_slabs();
 	nfsd_debugfs_exit();
+	nfsd4_free_slabs();
 	return retval;
 }
 
@@ -2657,9 +2658,9 @@ static void __exit exit_nfsd(void)
 	unregister_pernet_subsys(&nfsd_net_ops);
 	nfsd_drc_slab_free();
 	nfsd_lockd_shutdown();
-	nfsd4_free_slabs();
 	nfsd4_exit_pnfs();
 	nfsd_debugfs_exit();
+	nfsd4_free_slabs();
 }
 
 MODULE_AUTHOR("Olaf Kirch <okir@monad.swb.de>");
