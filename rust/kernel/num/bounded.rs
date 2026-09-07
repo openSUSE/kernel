05268c0f66ecebbb837c81cd7705fdd3f7a4098c
Jiri Slaby <jslaby@suse.cz>
2026-09-07 17:57:41 +0200

    rust: num: reject Bounded::shr overshifts at build time (bsc#1012628).
    suse-commit: 19042421620985f10bc60bee5919694b8cad8b9a

--- rust/kernel/num/bounded.rs
+++ rust/kernel/num/bounded.rs
@@ -495,6 +495,7 @@ where
     /// assert_eq!(v_shifted.get(), 0xff);
     /// ```
     pub fn shr<const SHIFT: u32, const RES: u32>(self) -> Bounded<T, RES> {
+        const_assert!(SHIFT < T::BITS);
         const { assert!(RES + SHIFT >= N) }
 
         // SAFETY: We shift the value right by `SHIFT`, reducing the number of bits needed to
