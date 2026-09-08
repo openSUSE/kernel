d2c541054da807ca6af91cd23ac29010d0975dec
Michal Kubecek <mkubecek@suse.cz>
2026-09-08 17:48:52 +0200

    Update patches.kabi/kabi-move-skb_dev-out-of-struct-nf_queue_entry.patch (bsc#1279813)
    - handle also the allocation and freeing in nf_queue_entry_dup()
    
    suse-commit: 374c5f7934bbbd5958284e3a00e06ae110bb706c

--- net/netfilter/nfnetlink_queue.c
+++ net/netfilter/nfnetlink_queue.c
@@ -978,15 +978,20 @@ err_out:
 static struct nf_queue_entry *
 nf_queue_entry_dup(struct nf_queue_entry *e)
 {
-	struct nf_queue_entry *entry = kmemdup(e, e->size, GFP_ATOMIC);
+	struct nf_queue_entry_kabi_wrapper *e_wrapper =
+		container_of(e, struct nf_queue_entry_kabi_wrapper, entry);
+	struct nf_queue_entry_kabi_wrapper *entry_wrapper =
+		kmemdup(e_wrapper, sizeof(e_wrapper->skb_dev) + e->size, GFP_ATOMIC);
+	struct nf_queue_entry *entry;
 
-	if (!entry)
+	if (!entry_wrapper)
 		return NULL;
+	entry = &entry_wrapper->entry;
 
 	if (nf_queue_entry_get_refs(entry))
 		return entry;
 
-	kfree(entry);
+	kfree(entry_wrapper);
 	return NULL;
 }
 
