b106408ed9b3bf62c67e6c64675774f1d954698e
Carlos López <clopez@suse.de>
2026-09-09 13:09:53 +0200

    KVM: x86: Prioritize DR7.GD #DB over #GP due to illegal DR6/7 value (git-fixes).
    suse-commit: bb924b78754ea4b2bbc52a6264f8b5dd8c44adea

--- arch/x86/kvm/emulate.c
+++ arch/x86/kvm/emulate.c
@@ -3907,11 +3907,16 @@ static int check_dr_write(struct x86_emulate_ctxt *ctxt)
 {
 	u64 new_val = ctxt->src.val64;
 	int dr = ctxt->modrm_reg;
+	int rc;
+
+	rc = check_dr_read(ctxt);
+	if (rc != X86EMUL_CONTINUE)
+		return rc;
 
 	if ((dr == 6 || dr == 7) && (new_val & 0xffffffff00000000ULL))
 		return emulate_gp(ctxt, 0);
 
-	return check_dr_read(ctxt);
+	return X86EMUL_CONTINUE;
 }
 
 static int check_svme(struct x86_emulate_ctxt *ctxt)
