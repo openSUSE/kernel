6e8ad2876c9c47e2528cf192a81168ed4321a218
Martin Wilck <mwilck@suse.com>
2026-09-09 10:51:05 +0200

    md/raid10: fix writes_pending and barrier reference leaks on discard failures (CVE-2026-72438 bsc#1277115).
    suse-commit: 5869c9743fff780ec0517c29362058ff82212966

--- drivers/md/raid10.c
+++ drivers/md/raid10.c
@@ -1642,6 +1642,7 @@ static int raid10_handle_discard(struct mddev *mddev, struct bio *bio)
 
 	if (!wait_barrier(conf, bio->bi_opf & REQ_NOWAIT)) {
 		bio_wouldblock_error(bio);
+		md_write_end(mddev);
 		return 0;
 	}
 
@@ -1684,6 +1685,8 @@ static int raid10_handle_discard(struct mddev *mddev, struct bio *bio)
 		if (IS_ERR(split)) {
 			bio->bi_status = errno_to_blk_status(PTR_ERR(split));
 			bio_endio(bio);
+			md_write_end(mddev);
+			allow_barrier(conf);
 			return 0;
 		}
 
@@ -1701,6 +1704,8 @@ static int raid10_handle_discard(struct mddev *mddev, struct bio *bio)
 		if (IS_ERR(split)) {
 			bio->bi_status = errno_to_blk_status(PTR_ERR(split));
 			bio_endio(bio);
+			md_write_end(mddev);
+			allow_barrier(conf);
 			return 0;
 		}
 
