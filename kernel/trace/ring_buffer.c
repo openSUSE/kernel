e9c584654fc4cc71511944e74034f1f26df31ec2
Robert Frohl <rfrohl@suse.com>
2026-09-12 22:11:26 +0200

    ring-buffer: Fix subbuf resize race with ring_buffer_alloc_read_page() (bsc#1258210).
    suse-commit: ca46f790b725ce2e4e434e59c975c8315da05b11

--- kernel/trace/ring_buffer.c
+++ kernel/trace/ring_buffer.c
@@ -6536,7 +6536,7 @@ ring_buffer_alloc_read_page(struct trace_buffer *buffer, int cpu)
 	if (bpage->data) {
 		rb_init_page(bpage->data);
 	} else {
-		bpage->data = alloc_cpu_data(cpu, cpu_buffer->buffer->subbuf_order);
+		bpage->data = alloc_cpu_data(cpu, bpage->order);
 		if (!bpage->data) {
 			kfree(bpage);
 			return ERR_PTR(-ENOMEM);
