c5c50137d117b48149f6d62b11f62d9799a32c29
Vlastimil Babka <vbabka@suse.cz>
2026-09-14 14:31:24 +0200

    slab.h: disable completely broken overflow handling in flex allocations (bsc#1280139).
    suse-commit: d7182e77c6fcc4c4744374bd601443e17d293a2f

--- include/linux/slab.h
+++ include/linux/slab.h
@@ -920,11 +920,7 @@ static __always_inline __alloc_size(1) void *kmalloc_noprof(size_t size, gfp_t f
 ({									\
 	const size_t __count = (COUNT);					\
 	const size_t __obj_size = struct_size_t(TYPE, FAM, __count);	\
-	TYPE *__obj_ptr;						\
-	if (WARN_ON_ONCE(overflows_flex_counter_type(TYPE, FAM,	__count))) \
-		__obj_ptr = NULL;					\
-	else								\
-		__obj_ptr = KMALLOC(__obj_size, GFP);			\
+	TYPE *__obj_ptr = KMALLOC(__obj_size, GFP);			\
 	if (__obj_ptr)							\
 		__set_flex_counter(__obj_ptr->FAM, __count);		\
 	__obj_ptr;							\
