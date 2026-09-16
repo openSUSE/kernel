81c7b4431f4643c9bc50ee647851c80b6ed73127
Fernando Fernandez Mancera <fmancera@suse.de>
2026-09-16 09:54:00 +0200

    xfrm: Check for underflow in xfrm_state_mtu (CVE-2026-64009 bsc#1273879).
    suse-commit: 634951fe5b69f0d49227a1315f50f7feb898f966

--- net/ipv4/esp4.c
+++ net/ipv4/esp4.c
@@ -786,6 +786,7 @@ static u32 esp4_get_mtu(struct xfrm_state *x, int mtu)
 	struct crypto_aead *aead = x->data;
 	u32 blksize = ALIGN(crypto_aead_blocksize(aead), 4);
 	unsigned int net_adj;
+	u32 overhead, payload_mtu;
 
 	switch (x->props.mode) {
 	case XFRM_MODE_TRANSPORT:
@@ -799,8 +800,16 @@ static u32 esp4_get_mtu(struct xfrm_state *x, int mtu)
 		BUG();
 	}
 
-	return ((mtu - x->props.header_len - crypto_aead_authsize(aead) -
-		 net_adj) & ~(blksize - 1)) + net_adj - 2;
+	overhead = x->props.header_len + crypto_aead_authsize(aead) + net_adj;
+	if (mtu <= overhead)
+		return 1;
+
+	payload_mtu = mtu - overhead;
+	payload_mtu &= ~(blksize - 1);
+	if (payload_mtu <= 2)
+		return 1;
+
+	return payload_mtu + net_adj - 2;
 }
 
 static int esp4_err(struct sk_buff *skb, u32 info)
--- net/xfrm/xfrm_state.c
+++ net/xfrm/xfrm_state.c
@@ -2224,8 +2224,12 @@ int xfrm_state_mtu(struct xfrm_state *x, int mtu)
 	const struct xfrm_type *type = READ_ONCE(x->type);
 
 	if (x->km.state == XFRM_STATE_VALID &&
-	    type && type->get_mtu)
+	    type && type->get_mtu) {
 		return type->get_mtu(x, mtu);
+	}
+
+	if (mtu <= x->props.header_len)
+		return 1;
 
 	return mtu - x->props.header_len;
 }
