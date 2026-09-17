c468d06b6fba6f5c0a0ac35e50ea21e97bb8c064
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2026-09-17 10:57:37 +0200

    ipv4: reject undersized MTUs in ip_do_fragment() (CVE-2026-80793 bsc#1279799).
    suse-commit: 8bd637d8806c702f5ae9b4a5a6c111b182773e32

--- net/ipv4/ip_output.c
+++ net/ipv4/ip_output.c
@@ -613,6 +613,10 @@ int ip_do_fragment(struct net *net, struct sock *sk, struct sk_buff *skb,
 	 */
 
 	hlen = iph->ihl * 4;
+	if (mtu < hlen + 8) {
+		err = -EMSGSIZE;
+		goto fail;
+	}
 	mtu = mtu - hlen;	/* Size of data space */
 	IPCB(skb)->flags |= IPSKB_FRAG_COMPLETE;
 
