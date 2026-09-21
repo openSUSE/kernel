9f8ca6f94d9aa2c660d625807e6ae7ce594c5eb6
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2026-09-21 20:12:28 +0200

    flow_dissector: fix false-positive __read_overflow2_field() warning (CVE-2026-72444 bsc#1278100).
    suse-commit: 7f5cb39e95caac366b993ad7bf4608d82f6fe07e

--- net/core/flow_dissector.c
+++ net/core/flow_dissector.c
@@ -600,7 +600,7 @@ bool __skb_flow_dissect(const struct sk_buff *skb,
 		key_eth_addrs = skb_flow_dissector_target(flow_dissector,
 							  FLOW_DISSECTOR_KEY_ETH_ADDRS,
 							  target_container);
-		memcpy(key_eth_addrs, &eth->h_dest, sizeof(*key_eth_addrs));
+		memcpy(key_eth_addrs, eth, sizeof(*key_eth_addrs));
 	}
 
 proto_again:
