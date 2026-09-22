9ccb5a7671bed2274416791ca8b2d0fe2e962ed9
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2026-09-22 11:14:33 +0200

    net/sched: cls_flow: Dont expose folded kernel pointers (CVE-2026-74290 bsc#1276462).
    suse-commit: a590f579b4d6849d07af55b97b3184f5cff869bd

--- net/sched/cls_flow.c
+++ net/sched/cls_flow.c
@@ -25,6 +25,7 @@
 #include <net/inet_sock.h>
 
 #include <net/pkt_cls.h>
+#include <linux/siphash.h>
 #include <net/ip.h>
 #include <net/route.h>
 #include <net/flow_dissector.h>
@@ -60,11 +61,16 @@ struct flow_filter {
 	struct rcu_work		rwork;
 };
 
+#define siphash_aligned_key_t siphash_key_t __aligned(16)
+static siphash_aligned_key_t flow_keys_secret __read_mostly;
+
 static inline u32 addr_fold(void *addr)
 {
-	unsigned long a = (unsigned long)addr;
-
-	return (a & 0xFFFFFFFF) ^ (BITS_PER_LONG > 32 ? a >> 32 : 0);
+#ifdef CONFIG_64BIT
+	return (u32)siphash_1u64((u64)addr, &flow_keys_secret);
+#else
+	return (u32)siphash_1u32((u32)addr, &flow_keys_secret);
+#endif
 }
 
 static u32 flow_get_src(const struct sk_buff *skb, const struct flow_keys *flow)
@@ -597,6 +603,7 @@ static int flow_init(struct tcf_proto *tp)
 		return -ENOBUFS;
 	INIT_LIST_HEAD(&head->filters);
 	rcu_assign_pointer(tp->root, head);
+	net_get_random_once(&flow_keys_secret, sizeof(flow_keys_secret));
 	return 0;
 }
 
