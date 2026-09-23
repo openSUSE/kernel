e7719451afeca0b4f5026ef095a47825c6aeef1c
Nicolas Morey <nmorey@suse.com>
2026-09-23 11:18:57 +0200

    RDMA/erdma: Hold QP references for AE and CM processing (CVE-2026-90308 bsc#1281786)
    suse-commit: c0ab2c1793cb729a8979560cf52339f09a037667

--- drivers/infiniband/hw/erdma/erdma_verbs.c
+++ drivers/infiniband/hw/erdma/erdma_verbs.c
@@ -1266,6 +1266,7 @@ int erdma_destroy_qp(struct ib_qp *ibqp, struct ib_udata *udata)
 	struct erdma_ucontext *ctx = rdma_udata_to_drv_context(
 		udata, struct erdma_ucontext, ibucontext);
 	struct erdma_qp_attrs qp_attrs;
+	unsigned long flags;
 	int err;
 	struct erdma_cmdq_destroy_qp_req req;
 
@@ -1283,6 +1284,10 @@ int erdma_destroy_qp(struct ib_qp *ibqp, struct ib_udata *udata)
 	err = erdma_post_cmd_wait(&dev->cmdq, &req, sizeof(req), NULL, NULL);
 	if (err)
 		return err;
+
+	xa_lock_irqsave(&dev->qp_xa, flags);
+	__xa_erase(&dev->qp_xa, QP_ID(qp));
+	xa_unlock_irqrestore(&dev->qp_xa, flags);
 
 	erdma_qp_put(qp);
 	wait_for_completion(&qp->safe_free);
@@ -1306,7 +1311,6 @@ int erdma_destroy_qp(struct ib_qp *ibqp, struct ib_udata *udata)
 
 	if (qp->cep)
 		erdma_cep_put(qp->cep);
-	xa_erase(&dev->qp_xa, QP_ID(qp));
 
 	return 0;
 }
