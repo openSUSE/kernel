ce2707416bc79ed84bc1e6b81bbafb55db59d9f3
Oscar Salvador <osalvador@suse.de>
2026-09-23 10:46:22 +0200

    xfrm6: clear dst.dev on error to avoid double netdev_put in xfrm6_fill_dst() (CVE-2026-64580 bsc#1274037).
    suse-commit: ca091a4ec0fea4fc237869a50bfc550ad5f8eed6

--- net/ipv6/xfrm6_policy.c
+++ net/ipv6/xfrm6_policy.c
@@ -105,6 +105,7 @@ static int xfrm6_fill_dst(struct xfrm_dst *xdst, struct net_device *dev,
 	xdst->u.rt6.rt6i_idev = in6_dev_get(dev);
 	if (!xdst->u.rt6.rt6i_idev) {
 		dev_put(dev);
+		xdst->u.dst.dev = NULL;
 		return -ENODEV;
 	}
 
