2531a4ba4898de493e253be9da8298c859c69a21
Juergen Gross <jgross@suse.com>
2026-09-24 20:01:16 +0200

    Delete patches.kabi/kabi-firmware-arm-ffa-rollback-to-version-1.1.patch.
    suse-commit: ad3c290cffe70a4b9670df3bf428cf4072b2154c

--- include/linux/arm_ffa.h
+++ include/linux/arm_ffa.h
@@ -339,6 +339,7 @@ struct ffa_mem_region_attributes {
 	 * an `struct ffa_mem_region_addr_range`.
 	 */
 	u32 composite_off;
+	u8 impdef_val[16];
 	u64 reserved;
 };
 
@@ -418,6 +419,7 @@ struct ffa_mem_region {
 #define CONSTITUENTS_OFFSET(x)	\
 	(offsetof(struct ffa_composite_mem_region, constituents[x]))
 
+#define FFA_EMAD_HAS_IMPDEF_FIELD(version)	((version) >= FFA_VERSION_1_2)
 #define FFA_MEM_REGION_HAS_EP_MEM_OFFSET(version) ((version) > FFA_VERSION_1_0)
 
 /* The layout changed from FFA_VERSION_1_0 and the region includes an
@@ -432,7 +434,10 @@ static inline u32 ffa_emad_size_get(u32 ffa_version)
 	u32 sz;
 	struct ffa_mem_region_attributes *ep_mem_access;
 
-	sz = sizeof(*ep_mem_access);
+	if (FFA_EMAD_HAS_IMPDEF_FIELD(ffa_version))
+		sz = sizeof(*ep_mem_access);
+	else
+		sz = sizeof(*ep_mem_access) - sizeof(ep_mem_access->impdef_val);
 
 	return sz;
 }
