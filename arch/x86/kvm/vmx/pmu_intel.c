e3c85067f9ef0f842dfbf8e0d82f98c9f92f05a3
Carlos López <clopez@suse.de>
2026-09-24 15:35:57 +0200

    KVM: VMX: Handle bad values on proxied writes to LBR MSRs (git-fixes).
    suse-commit: ab7af9a1f4511dceca76ab18b2c2c1b70f2a02cb

--- arch/x86/kvm/vmx/pmu_intel.c
+++ arch/x86/kvm/vmx/pmu_intel.c
@@ -319,13 +319,15 @@ static bool intel_pmu_handle_lbr_msrs_access(struct kvm_vcpu *vcpu,
 	 */
 	local_irq_disable();
 	if (lbr_desc->event->state == PERF_EVENT_STATE_ACTIVE) {
+		int err = 0;
+
 		if (read)
 			rdmsrl(index, msr_info->data);
 		else
-			wrmsrl(index, msr_info->data);
+			err = wrmsrl_safe(index, msr_info->data);
 		__set_bit(INTEL_PMC_IDX_FIXED_VLBR, vcpu_to_pmu(vcpu)->pmc_in_use);
 		local_irq_enable();
-		return true;
+		return !err;
 	}
 	clear_bit(INTEL_PMC_IDX_FIXED_VLBR, vcpu_to_pmu(vcpu)->pmc_in_use);
 	local_irq_enable();
