5e0755ae3a6c0cd747fbe4d857838a61a92e5add
Steffen Jaeckel <sjaeckel@suse.de>
2026-09-25 09:21:24 +0200

    kABI fix for: net/sched: reject overly deep qdisc hierarchies (CVE-2026-74663 bsc#1278311).
    suse-commit: 2fec0108bc83d295ca3932c128c5f4524dee3c92

--- include/net/sch_generic.h
+++ include/net/sch_generic.h
@@ -101,7 +101,72 @@ struct Qdisc {
 	struct hlist_node       hash;
 	u32			handle;
 	u32			parent;
+
+	struct netdev_queue	*dev_queue;
+
+	struct net_rate_estimator __rcu *rate_est;
+	struct gnet_stats_basic_sync __percpu *cpu_bstats;
+	struct gnet_stats_queue	__percpu *cpu_qstats;
+	int			pad;
+	refcount_t		refcnt;
+#ifndef __GENKSYMS__
 	int			depth;
+#endif
+
+	/*
+	 * For performance sake on SMP, we put highly modified fields at the end
+	 */
+	struct sk_buff_head	gso_skb ____cacheline_aligned_in_smp;
+	struct qdisc_skb_head	q;
+	struct gnet_stats_basic_sync bstats;
+	struct gnet_stats_queue	qstats;
+	int                     owner;
+	unsigned long		state;
+	unsigned long		state2; /* must be written under qdisc spinlock */
+	struct Qdisc            *next_sched;
+	struct sk_buff_head	skb_bad_txq;
+
+	spinlock_t		busylock ____cacheline_aligned_in_smp;
+	spinlock_t		seqlock;
+
+	struct rcu_head		rcu;
+	netdevice_tracker	dev_tracker;
+	struct lock_class_key	root_lock_key;
+	/* private data */
+	long privdata[] ____cacheline_aligned;
+};
+
+struct __orig_Qdisc {
+	int 			(*enqueue)(struct sk_buff *skb,
+					   struct Qdisc *sch,
+					   struct sk_buff **to_free);
+	struct sk_buff *	(*dequeue)(struct Qdisc *sch);
+	unsigned int		flags;
+#define TCQ_F_BUILTIN		1
+#define TCQ_F_INGRESS		2
+#define TCQ_F_CAN_BYPASS	4
+#define TCQ_F_MQROOT		8
+#define TCQ_F_ONETXQUEUE	0x10 /* dequeue_skb() can assume all skbs are for
+				      * q->dev_queue : It can test
+				      * netif_xmit_frozen_or_stopped() before
+				      * dequeueing next packet.
+				      * Its true for MQ/MQPRIO slaves, or non
+				      * multiqueue device.
+				      */
+#define TCQ_F_WARN_NONWC	(1 << 16)
+#define TCQ_F_CPUSTATS		0x20 /* run using percpu statistics */
+#define TCQ_F_NOPARENT		0x40 /* root of its hierarchy :
+				      * qdisc_tree_decrease_qlen() should stop.
+				      */
+#define TCQ_F_INVISIBLE		0x80 /* invisible by default in dump */
+#define TCQ_F_NOLOCK		0x100 /* qdisc does not require locking */
+#define TCQ_F_OFFLOADED		0x200 /* qdisc is offloaded to HW */
+	u32			limit;
+	const struct Qdisc_ops	*ops;
+	struct qdisc_size_table	__rcu *stab;
+	struct hlist_node       hash;
+	u32			handle;
+	u32			parent;
 
 	struct netdev_queue	*dev_queue;
 
@@ -133,6 +198,10 @@ struct Qdisc {
 	/* private data */
 	long privdata[] ____cacheline_aligned;
 };
+
+suse_kabi_static_assert(sizeof(struct Qdisc) == sizeof(struct __orig_Qdisc));
+suse_kabi_static_assert(offsetof(struct Qdisc, gso_skb) ==
+                        offsetof(struct __orig_Qdisc, gso_skb));
 
 static inline void qdisc_refcount_inc(struct Qdisc *qdisc)
 {
