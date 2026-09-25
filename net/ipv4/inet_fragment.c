b74077934b57a1c574a93a72bd2a7e2b113f7805
Steffen Jaeckel <sjaeckel@suse.de>
2026-09-25 09:21:27 +0200

    inet: frags: publish queues before arming timer (CVE-2026-74662 bsc#1277277).
    suse-commit: 0486456560cddcc37a6bac45e8b7336b82f74de3

--- net/ipv4/inet_fragment.c
+++ net/ipv4/inet_fragment.c
@@ -393,16 +393,18 @@ static struct inet_frag_queue *inet_frag_create(struct fqdir *fqdir,
 		*prev = ERR_PTR(-ENOMEM);
 		return NULL;
 	}
-	mod_timer(&q->timer, jiffies + fqdir->timeout);
 
+	spin_lock_bh(&q->lock);
 	*prev = rhashtable_lookup_get_insert_key(&fqdir->rhashtable, &q->key,
 						 &q->node, f->rhash_params);
 	if (*prev) {
 		q->flags |= INET_FRAG_COMPLETE;
-		inet_frag_kill(q);
-		inet_frag_destroy(q);
+		spin_unlock_bh(&q->lock);
+		inet_frag_putn(q, 2);
 		return NULL;
 	}
+	mod_timer(&q->timer, jiffies + fqdir->timeout);
+	spin_unlock_bh(&q->lock);
 	return q;
 }
 
